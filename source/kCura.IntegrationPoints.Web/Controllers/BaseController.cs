0eacc31112fa930f0ba1e781c2a358c5341fe807
David Barnes <dbarnes@kcura.com>
2014-11-06 13:25:46 -0600

    Added session to base controller

---
+++ source/kCura.IntegrationPoints.Web/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kCura.IntegrationPoints.Web.Controllers
+{
+	public abstract class BaseController : Controller
+	{
+		public const string SESSION_KEY = "__WEB_SESSION_KEY__";
+
+		public ISessionService SessionService
+		{
+			get
+			{
+				var session = Session[SESSION_KEY] as ISessionService;
+				if (session == null)
+				{
+					session = new SessionService();
+					Session[SESSION_KEY] = session;
+				}
+				return session;
+			}
+		}
+		
+	}
+}
---
+++ source/kCura.IntegrationPoints.Web/SessionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace kCura.IntegrationPoints.Web
+{
+	public class SessionService : ISessionService
+	{
+	}
+}
