3da1ab5342ef62dd5b6e6b9e604cedf1fda8fed7
David Barnes <dbarnes@kcura.com>
2014-11-06 14:04:18 -0600

    Added error func to base controller

---
+++ source/kCura.IntegrationPoints.Core/Services/CreateError.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using kCura.IntegrationPoints.Core.Models;
+
+namespace kCura.IntegrationPoints.Core.Services
+{
+	public class CreateError
+	{
+		private readonly Data.Queries.CreateErrorRdo _createErrorRdo;
+		public CreateError(Data.Queries.CreateErrorRdo createError)
+		{
+			_createErrorRdo = createError;
+		}
+
+		public virtual void Log(ErrorModel error)
+		{
+			try
+			{
+				var appVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+				var message = string.Format("(Integration Points v({0}) {1}", appVersion, error.Message);
+				_createErrorRdo.Execute(error.WorkspaceID, message, error.Exception);
+			}
+			catch
+			{
+				//Eat me
+			}
+		}
+	}
+}
--- source/kCura.IntegrationPoints.Web/Controllers/BaseController.cs
+++ source/kCura.IntegrationPoints.Web/Controllers/BaseController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,5 +10,20 @@ namespace kCura.IntegrationPoints.Web.Controllers
 	public abstract class BaseController : Controller
 	{
 		public ISessionService SessionService { get; set; }
+		public Core.Services.CreateError CreateError { get; set; }
+
+		protected void LogException(Exception e, string controller = null, string action = null)
+		{
+			var message = String.Format("{0}/{1}", controller, action);
+			var errorModel = new Core.Models.ErrorModel(SessionService.WorkspaceID, message, e);
+			CreateError.Log(errorModel);
+		}
+
+		protected ActionResult RedirectToBase(string url)
+		{
+			//:( we have to do this since we are in an iframe :(
+			return Content("<html><script>window.top.location.href = '" + url + "'; </script></html>");
+		}
+
 	}
 }
