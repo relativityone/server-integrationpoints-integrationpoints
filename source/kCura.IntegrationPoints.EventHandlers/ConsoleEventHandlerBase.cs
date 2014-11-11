10e0bb3632cc82f195320c3fa8ec829b8a70c07b
David Barnes <dbarnes@kcura.com>
2014-11-11 16:47:25 -0600

    Added console and preload event handlers

---
+++ source/kCura.IntegrationPoints.EventHandlers/ConsoleEventHandlerBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.EventHandler;
+
+namespace kCura.IntegrationPoints.EventHandlers
+{
+	public abstract class ConsoleEventHandlerBase : kCura.EventHandler.ConsoleEventHandler
+	{
+	}
+}
---
+++ source/kCura.IntegrationPoints.EventHandlers/IntegrationPoints/ConsoleEventHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.EventHandler;
+using Console = kCura.EventHandler.Console;
+
+namespace kCura.IntegrationPoints.EventHandlers.IntegrationPoints
+{
+	public class ConsoleEventHandler : ConsoleEventHandlerBase
+	{
+		public override Console GetConsole(PageEvent pageEvent)
+		{
+			var console = new Console();
+			console.Title = "IMPORT";
+			console.ButtonList = new List<ConsoleButton>();
+			console.ButtonList.Add(new ConsoleButton
+			{
+				DisplayText = "Import Data Now",
+				RaisesPostBack = false,
+				OnClickEvent = "alert('hello world')",
+				Enabled = true
+			});
+
+			return console;
+		}
+
+		public override void OnButtonClick(ConsoleButton consoleButton)
+		{
+
+		}
+
+		public override FieldCollection RequiredFields
+		{
+			get { return new FieldCollection(); }
+		}
+	}
+}
