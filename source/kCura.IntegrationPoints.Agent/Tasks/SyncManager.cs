ba349a2ab845dfd58257a009110dc33185f2178b
David Barnes <dbarnes@kcura.com>
2014-11-18 11:01:30 -0600

    Hopefully fixed build

--- source/kCura.IntegrationPoints.Agent/Tasks/SyncManager.cs
+++ source/kCura.IntegrationPoints.Agent/Tasks/SyncManager.cs
@@ -4,15 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using kCura.Agent.ScheduleQueueAgent;
+using kCura.IntegrationPoints.Core.Services.Provider;
 
 namespace kCura.IntegrationPoints.Agent.Tasks
 {
 	public class SyncManager : ITask
 	{
+		public SyncManager(IDataProviderFactory providerFactory)
+		{
+			
+		}
+
 		public void Execute(Job job)
 		{
-			throw new NotImplementedException();
-
 		}
 	}
 }
