676813fd691b5a063d59f69a22777a247521d018
Arthur Kelenzon <akelenzon@kcura.com>
2014-11-18 11:43:37 -0600

    DEFECT: fixing build

--- source/kCura.IntegrationPoints.Agent/Agent.cs
+++ source/kCura.IntegrationPoints.Agent/Agent.cs
@@ -1,13 +1,11 @@
 ﻿using System.Runtime.InteropServices;
-using kCura.Agent.CustomAttributes;
 using kCura.Agent.ScheduleQueueAgent;
-using kCura.Agent.ScheduleQueueAgent.CustomAttributes;
 
 namespace kCura.IntegrationPoints.Agent
 {
 	[kCura.Agent.CustomAttributes.Name("Relativity Integration Points Agent")]
 	[Guid("08C0CE2D-8191-4E8F-B037-899CEAEE493D")]
-	[QueueTable(Name = "IntegrationPointsQueue")]
+	[kCura.Agent.ScheduleQueueAgent.CustomAttributes.QueueTable(Name = "IntegrationPointsQueue")]
 	public class Agent : ScheduleQueueAgentBase
 	{
 		public override string Name
