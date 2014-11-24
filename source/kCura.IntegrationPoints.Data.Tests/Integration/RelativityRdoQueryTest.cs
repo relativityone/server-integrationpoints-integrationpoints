9f2a33f6a9a3045be9afb3ffa83e6349e479d06e
Muhammad Khan <mkhan@kcura.com>
2014-11-24 14:56:17 -0600

    Added rdo query and filter

---
+++ source/kCura.IntegrationPoints.Data.Tests/Integration/RelativityRdoQueryTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using kCura.Relativity.Client;
+using NUnit.Framework;
+
+namespace kCura.IntegrationPoints.Data.Tests.Integration
+{
+
+
+	[TestFixture]
+	public class RelativityRdoQueryTest
+	{
+		[Test]
+		[Explicit]
+		public void RelativityRdoQueryReturnsAllRdo()
+		{	//ARRANGE
+			var client = new RSAPIClient(new Uri("http://localhost/Relativity.Services"), new IntegratedAuthCredentials())
+			{
+				APIOptions = { WorkspaceID = 1383218 }
+			};
+			var rdoQuery = new RelativityRdoQuery(client);
+
+			var data = rdoQuery.GetAllRdo();
+		}
+
+	}
+}
---
+++ source/kCura.IntegrationPoints.Web/Controllers/API/RdoFilter.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace kCura.IntegrationPoints.Web.Controllers.API
+{
+	public class RdoFilter : ApiController
+	{
+		private Core.Models.RdoFilter _rdoFilter;
+
+		public RdoFilter(Core.Models.RdoFilter rdoFilter)
+		{
+			_rdoFilter = rdoFilter;
+		}
+
+
+		// GET api/<controller>
+		public HttpResponseMessage Get()
+		{
+			var list = _rdoFilter.FilterRdo().Select(x => new { Name = x.Name, Value = x.ArtifactID }).ToList();
+			return Request.CreateResponse(HttpStatusCode.OK, list);
+
+		}
+
+	}
+}
