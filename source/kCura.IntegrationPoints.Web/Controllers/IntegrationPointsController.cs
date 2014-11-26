2d7e54e95226e6a04e135e59ef02de60b4b2807b
Muhammad Khan <mkhan@kcura.com>
2014-11-26 11:49:11 -0600

    [TBF 610,611,612,613] Added the details page and updated the styling for it

--- source/kCura.IntegrationPoints.Web/Controllers/IntegrationPointsController.cs
+++ source/kCura.IntegrationPoints.Web/Controllers/IntegrationPointsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using kCura.IntegrationPoints.Core.Models;
 using kCura.IntegrationPoints.Web.Attributes;
 using kCura.Relativity.Client;
 
@@ -23,19 +24,23 @@ namespace kCura.IntegrationPoints.Web.Controllers
 			return PartialView("_IntegrationDetailsPartial");
 		}
 
-		public ActionResult StepDetails2()
+		public ActionResult Details(int? objectID)
 		{
-			return PartialView("_IntegrationDetailsPartial2");
-		}
-
-		public ActionResult Edit(int? id)
-		{
-			return View();
-		}
-
-		public ActionResult Index()
-		{
-			return View(); 
+			var integrationViewModel = new IntegrationViewModel()
+			{
+				Name = "Main Active Directory Sync",Overwrite = "Append", SourceProvider = "LDAP",Destination = "Custodian", EnableScheduler = true, Frequency = "Daily",StartDate = new DateTime(2014,7,13),
+				EndDate = null,
+				ScheduleTime = new DateTime(2014, 12, 12, 5, 0, 0),
+				ConnectionPath = "kcura.corp",
+				FilterString = "&(objectCategory=person)(objectClass=user)",
+				Authentication = "fastBind",
+				Username = "kcura\rellockdown",
+				Password = "********",
+				NestedItems = "*******",
+				NextRun = new DateTime(2014, 2, 7, 5, 4, 0),
+				LastRun = new DateTime(2014, 2, 7, 5, 4, 0)
+			};
+			return View(integrationViewModel);
 		}
 
 	}
