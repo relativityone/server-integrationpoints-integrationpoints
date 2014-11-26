d692430458afb137b27f438e064012f24eda17ea
David Barnes <dbarnes@kcura.com>
2014-11-26 12:29:13 -0600

    Scheduler now persists state

--- source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-details.js
+++ source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-details.js
@@ -61,7 +61,140 @@
 	};
 
 	var Scheduler = function () {
+		var stateManager = {};
+
+		var SendOnWeekly = function (state) {
+			var defaults = {
+				selectedDays: []
+			};
+			var currentState = $.extend({}, defaults, state);
+			this.days = ko.observableArray([
+				"Monday",
+				"Tuesday",
+				"Wednesday",
+				"Thursday",
+				"Friday",
+				"Saturday",
+				"Sunday"
+			]);
+			this.selectedDays = ko.observableArray(currentState.selectedDays);
+			this.templateID = 'weeklySendOn';
+		};
+
+		var SendOnMonthly = function (state) {
+			var defaults = {
+				monthChoice:  'days'
+			};
+
+			var self = this;
+
+			var currentState = $.extend({}, defaults, state);
+			this.templateID = 'monthlySendOn';
+			var days = [];
+			for (var i = 1; i <= 31; i++) {
+				days.push(new Choice(i.toString(), i));
+			}
+
+			this.days = ko.observableArray(days);
+			this.selectedDay = ko.observable(currentState.selectedDay);
+
+			this.overflowMessage = ko.computed(function () {
+				return 'Months with fewer than ' + self.selectedDay() + ' days will send on the last day of the month.';
+			});
+
+			this.dayTypes = ko.observableArray([
+				new Choice("first", 1),
+				new Choice("second", 2),
+				new Choice("third", 3),
+				new Choice("fourth", 4),
+				new Choice("last", 5)
+			]);
+			
+			this.selectedType = ko.observable(currentState.selectedType);
+
+			this.selectedType.subscribe(function () {
+				var selected = self.selectedDayOfTheMonth();
+				var value = this.target();
+				if (value === 1 || value == 5) {
+					self.selectedDayOfTheMonth(selected);
+				} else {
+					self.selectedDayOfTheMonth(2);
+				}
+			});
+
+			this.daysOfMonth = ko.observableArray([
+				new Choice("day", 1),
+				new Choice("Monday", 2),
+				new Choice("Tuesday", 3),
+				new Choice("Wednesday", 4),
+				new Choice("Thursday", 5),
+				new Choice("Friday", 6),
+				new Choice("Saturday", 7),
+				new Choice("Sunday", 8),
+				new Choice("last", 9),
+			]);
+
+			this.daysOfMonthComputed = ko.computed(function () {
+				var type = self.selectedType();
+				var hideSpecial = type !== 1 && type !== 5;
+				return ko.utils.arrayFilter(self.daysOfMonth(), function (entry) {
+					if (hideSpecial) {
+						return entry.value >1 && entry.value < 9;
+					}
+					return true;
+				});
+			});
+
+			this.selectedDayOfTheMonth = ko.observable(currentState.selectedDayOfTheMonth);
+			
+			this.monthChoice = ko.observable(currentState.monthChoice);
+
+		};
+		
+		var self = this;
+		this.frequency = ko.observableArray([
+			new Choice("Daily", 1),
+			new Choice("Weekly", 2),
+			new Choice("Monthly", 3)
+		]);
+		
+		this.selectedFrequency = ko.observable();
+		this.reoccur = ko.observable('asdf');
+
+		this.reoccurEvery = ko.computed(function () {
+			var state = self.selectedFrequency();
+			var states = {
+				"1": "",
+				"2": "week(s)",
+				"3": 'month(s)'
+			}
+			return states[state] || '';
+		});
+		
+		this.selectedFrequency.subscribe(function (previousValue) {
+			stateManager[previousValue] = ko.toJS(self.sendOn());
+		}, this, "beforeChange");
+
+		this.selectedFrequency.subscribe(function () {
+			var value = this.target();
+			var oldState = stateManager[value];
+			if (value === 1) {
+				self.sendOn({});
+			} else if (value === 2) {
+				self.sendOn(new SendOnWeekly(oldState));
+			} else if (value === 3) {
+				self.sendOn(new SendOnMonthly(oldState));
+			}
+		});
+
+
+
+		this.startDate = ko.observable();
+		this.endDate = ko.observable();
+		this.scheduledTime = ko.observable();
+		this.enabled = ko.observable('true');
 		this.templateID = 'schedulingConfig';
+		this.sendOn = ko.observable({});
 	};
 
 	var Model = function () {
@@ -120,6 +253,13 @@
 		D('.dragon-panel')[func]();
 	}
 	
+
+	IP.messaging.subscribe('details-loaded', function () {
+		//this comes before initing the drowndown filter so we will populate the state
+		//before going forward
+		
+	});
+
 	IP.messaging.subscribe('details-loaded', function () {
 		D('.dragon-panel').scheduleControl();
 
@@ -157,7 +297,4 @@
 		});
 
 	});
-
-
-
 })();
