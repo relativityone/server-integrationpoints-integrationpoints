4843995502c286696f2faeac403a4f54312638d3
Arthur Kelenzon <akelenzon@kcura.com>
2014-11-30 21:56:42 -0600

    DEFECT: fixing build nunit test - work in progress

--- source/kCura.ScheduleQueueAgent.Tests/Integration/Service/JobLogServiceTests.cs
+++ source/kCura.ScheduleQueueAgent.Tests/Integration/Service/JobLogServiceTests.cs
@@ -12,7 +12,7 @@ namespace kCura.ScheduleQueueAgent.Tests.Integration.Service
 		[Explicit]
 		public void jobService_CreateUnscheduledJob()
 		{
-			
+
 			int caseID1 = 1015641;
 			int caseID2 = 1018513;
 			int relatedObjectArtifactID = 1111111;
@@ -23,21 +23,21 @@ namespace kCura.ScheduleQueueAgent.Tests.Integration.Service
 			IDBContext c1Context = new TestDBContextHelper().GetEDDSDBContext();
 			IDBContext c2Context = new TestDBContextHelper().GetEDDSDBContext();
 			//AgentInformation ai = jobService.GetAgentInformation(new Guid("08C0CE2D-8191-4E8F-B037-899CEAEE493D")); //Integration Points agent
-			IAgentService agentService = new AgentService(dbContext, agentGuid); //RLH agent
-			var jobService1 = new JobService(agentService, dbContext);
-			var jobService2 = new JobService(agentService, dbContext);
+			IAgentService agentService = new AgentService(eddsContext, agentGuid); //RLH agent
+			var jobService1 = new JobService(agentService, c1Context);
+			var jobService2 = new JobService(agentService, c2Context);
 
-			Job jobOld = jobService.GetJob(caseID1, relatedObjectArtifactID, taskType);
-			if (jobOld != null) jobService.DeleteJob(jobOld.JobId);
-			jobOld = jobService.GetJob(caseID2, relatedObjectArtifactID, taskType);
-			if (jobOld != null) jobService.DeleteJob(jobOld.JobId);
+			Job jobOld = jobService1.GetJob(caseID1, relatedObjectArtifactID, taskType);
+			if (jobOld != null) jobService1.DeleteJob(jobOld.JobId);
+			jobOld = jobService1.GetJob(caseID2, relatedObjectArtifactID, taskType);
+			if (jobOld != null) jobService1.DeleteJob(jobOld.JobId);
 
-			Job job = jobService.CreateJob(workspaceID, relatedObjectArtifactID, taskType, DateTime.UtcNow, "My Test Job Detail", 1212121);
-			Job job1 = jobService.GetJob(job.JobId);
-			Job job2 = jobService.GetJob(workspaceID, job.RelatedObjectArtifactID, taskType);
-			Job job3 = jobService.GetNextQueueJob(new int[] { 1015040 });
-			jobService.UnlockJobs(agentService.AgentInformation.AgentID);
-			jobService.DeleteJob(job3.JobId);
+			Job job = jobService1.CreateJob(caseID1, relatedObjectArtifactID, taskType, DateTime.UtcNow, "My Test Job Detail", 1212121);
+			Job job1 = jobService1.GetJob(job.JobId);
+			Job job2 = jobService1.GetJob(caseID1, job.RelatedObjectArtifactID, taskType);
+			Job job3 = jobService1.GetNextQueueJob(new int[] { 1015040 });
+			jobService1.UnlockJobs(agentService.AgentInformation.AgentID);
+			jobService1.DeleteJob(job3.JobId);
 		}
-}
+	}
 }
