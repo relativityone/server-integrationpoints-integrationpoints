fdef1c34e242b770da491c8157e04c632ff4dc55
David Barnes <dbarnes@kcura.com>
2014-12-01 10:26:35 -0600

    Added date picker

--- source/kCura.IntegrationPoints.Web/Scripts/integration-point/binding.js
+++ source/kCura.IntegrationPoints.Web/Scripts/integration-point/binding.js
@@ -11,3 +11,24 @@
 		$element.parent().find('.filter-container span.select2-arrow').removeClass("select2-arrow").addClass("icon legal-hold icon-chevron-down");
 	}
 };
+
+ko.bindingHandlers.datePicker = {
+	init: function (element) {
+		$(element).datepicker({
+			beforeShow: function (el, inst) {
+				if ($(el).attr('readonly')) {
+					return false;
+				}
+				inst.dpDiv.css({ marginTop: -el.offsetHeight + 'px', marginLeft: el.offsetWidth + 5 + 'px' });
+				return true;
+			},
+			onSelect: function () {
+				//get the shim to work properly
+				$(this).blur();
+			}
+		});
+	},
+	update: function (element) {
+		
+	}
+};
