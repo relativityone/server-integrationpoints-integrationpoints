b2d3b39f8d8adf3f27d9f38655d150dbb3aa6dea
Muhammad Khan <mkhan@kcura.com>
2014-12-10 10:02:33 -0600

    Added the left add, remove and shift up and down options for the map fields page

--- source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-mapFields.js
+++ source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-mapFields.js
@@ -1,64 +1,55 @@
 ﻿var IP = IP || {};
 (function (root, ko) {
-
-
-
-	var Field = function (name, identifier) {
-		this.name = name;
-		this.identifier = identifier;
-
-	}
-	
 	var viewModel = function () {
 		var self = this;
 		this.workspaceFields = ko.observableArray([]);
-		this.selectedWorkspaceFields = ko.observableArray([]);
 		this.mappedWorkspace = ko.observableArray([]);
-		this.selectedMappedWorkspace = ko.observableArray([]);
 		this.sourceMapped = ko.observableArray([]);
 		this.sourceField = ko.observableArray([]);
-		this.selectedSourceMapped = ko.observableArray([]);
+		this.selectedWorkspaceField = ko.observableArray([]);
+		this.selectedMappedWorkspace = ko.observableArray([]);
 		this.selectedSourceField = ko.observableArray([]);
-
+		this.selectedMappedSource = ko.observableArray([]);
 
 		root.data.ajax({ type: 'get', url: root.utils.generateWebAPIURL('WorkspaceField') }).then(function (result) {
 			var types = $.map(result, function (entry) {
-				return new Field(entry.displayName, entry.fieldIdentifier);
+				return { name: entry.displayName, identifier: entry.fieldIdentifier };
 			});
 			self.workspaceFields(types);
 		});
 		
+		root.data.ajax({ type: 'get', url: root.utils.generateWebAPIURL('WorkspaceField') }).then(function (result) {
+			var types = $.map(result, function (entry) {
+				return {name: entry.displayName, identifier: entry.fieldIdentifier};
+			});
+			self.sourceField(types);
+		});
 
 		/********** WorkspaceFields control  **********/
 
 		this.addSelectFields = function () {
 			var requested = this.mappedWorkspace;
-			$.each(self.selectedWorkspaceFields(), function (n, item) {
-			
-				requested.push(item);
+			$.each(self.selectedWorkspaceField(), function () {
+				requested.push(this);
 			});
-			this.workspaceFields.removeAll(self.selectedWorkspaceFields());
-			this.selectedWorkspaceFields.splice(0, this.selectedWorkspaceFields().length);
+			this.workspaceFields.removeAll(self.selectedWorkspaceField());
+			this.selectedWorkspaceField.splice(0, this.selectedWorkspaceField().length);
 		}
-
-		this.addAllWorkspaceFields = function (addAll) {
-			var requested = this.mappedWorkspace;
-			$.each(self.workspaceFields(), function (n, item) {
-			
-				requested.push(item);
-			});
-			this.workspaceFields.removeAll();
-			this.selectedWorkspaceFields.splice(0, this.selectedWorkspaceFields().length);
-		}
-
 		this.addToWorkspaceField = function() {
 			var requested = this.workspaceFields;
 			$.each(self.selectedMappedWorkspace(), function (n, item) {
-
 				requested.push(item);
 			});
 			this.mappedWorkspace.removeAll(self.selectedMappedWorkspace());
 			this.selectedMappedWorkspace.splice(0, this.selectedMappedWorkspace().length);
+		}
+		this.addAllWorkspaceFields = function (addAll) {
+			var requested = this.mappedWorkspace;
+			$.each(self.workspaceFields(), function (n, item) {
+				requested.push(item);
+			});
+			this.workspaceFields.removeAll();
+			this.selectedWorkspaceField.splice(0, this.selectedWorkspaceField().length);
 		}
 
 		this.addAlltoWorkspaceField = function() {
@@ -72,48 +63,68 @@
 
 		/********** Source Attribute control  **********/
 		this.addToMappedSource = function () {
-			var requested = this.mappedWorkspace;
+			var requested = this.sourceMapped;
 			$.each(self.selectedSourceField(), function (n, item) {
 				requested.push(item);
 			});
-			this.workspaceFields.removeAll(self.selectedWorkspaceFields());
-			this.selectedWorkspaceFields.splice(0, this.selectedWorkspaceFields().length);
+			this.sourceField.removeAll(self.selectedSourceField());
+			this.selectedSourceField.splice(0, this.selectedSourceField().length);
 		}
-
-		this.addAllSourceFields = function (addAll) {
-			var requested = this.mappedWorkspace;
-			$.each(self.workspaceFields(), function (n, item) {
-
-				requested.push(item);
-			});
-			this.workspaceFields.removeAll();
-			this.selectedWorkspaceFields.splice(0, this.selectedWorkspaceFields().length);
-		}
-
 		this.addToSourceField = function () {
-			var requested = this.workspaceFields;
-			$.each(self.selectedMappedWorkspace(), function (n, item) {
-
+			var requested = this.sourceField;
+			$.each(self.selectedMappedSource(), function (n, item) {
 				requested.push(item);
 			});
-			this.mappedWorkspace.removeAll(self.selectedMappedWorkspace());
-			this.selectedMappedWorkspace.splice(0, this.selectedMappedWorkspace().length);
+			this.sourceMapped.removeAll(self.selectedMappedSource());
+			this.selectedMappedSource.splice(0, this.selectedMappedSource().length);
 		}
 
-		this.addAlltoSourceField = function () {
-			var requested = this.workspaceFields;
-			$.each(self.mappedWorkspace(), function (n, item) {
+		this.addSourceToMapped = function () {
+			var requested = this.sourceMapped;
+			$.each(self.sourceField(), function (n, item) {
 				requested.push(item);
 			});
-			this.mappedWorkspace.removeAll();
-			this.selectedMappedWorkspace.splice(0, this.selectedMappedWorkspace().length);
+			this.sourceField.removeAll();
+			this.selectedSourceField.splice(0, this.selectedSourceField.length);
 		}
+		
+		this.addAlltoSourceField = function (addAll) {
+			var requested = this.sourceField;
+			$.each(self.sourceMapped(), function (n, item) {
+				requested.push(item);
+			});
+			this.sourceMapped.removeAll();
+			alert(this.selectedMappedSource().length);
+			this.selectedMappedSource.splice(0, this.selectedMappedSource().length);
+			
+		}
+
+		this.moveUp = function () {
+			
+			var i = this.mappedWorkspace.indexOf(this.selectedMappedWorkspace()[0]);
+			if (i >= 1) {
+				var array = this.mappedWorkspace();
+				this.mappedWorkspace.splice(i - 1, 2, array[i], array[i - 1]);
+				this.selectedMappedWorkspace.splice(0, this.selectedMappedWorkspace().length);
+			}	
+		}
+		
+		this.moveDown = function () {
+			var i = this.mappedWorkspace.indexOf(this.selectedMappedWorkspace()[0]);
+			if (i >= this.mappedWorkspace.length) {
+				var array = this.mappedWorkspace();
+				this.mappedWorkspace.splice(i, 2, array[i + 1], array[i]);
+				this.selectedMappedWorkspace.splice(0, this.selectedMappedWorkspace().length);
+			}
+			
+		}
+		
 
 	};// end of the viewmodel
 
 
 
-	var step = function (settings) {
+	var Step = function (settings) {
 		var self = this;
 		self.settings = settings;
 		this.template = ko.observable();
@@ -137,7 +148,7 @@
 
 
 	
-	var step = new step({
+	var step = new Step({
 		url: IP.utils.generateWebURL('IntegrationPoints', 'StepDetails3'),
 		templateID: 'step3'
 	});
