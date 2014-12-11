1cda2694d4603e0c2bb404c52557bbbb4ac4d138
Muhammad Khan <mkhan@kcura.com>
2014-12-11 16:08:05 -0600

    added the up and down feature

---
+++ source/kCura.IntegrationPoints.Web/Controllers/API/FieldMapController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using kCura.IntegrationPoints.Core.Services;
+
+namespace kCura.IntegrationPoints.Web.Controllers.API
+{
+	public class FieldMapController : ApiController
+    {
+        //
+        // GET: /FieldMap/
+		private readonly IntegrationPointReader _integrationPointReader;
+		public FieldMapController(IntegrationPointReader integrationPointReader)
+		{
+			_integrationPointReader = integrationPointReader;
+		}
+
+
+		[Route("{workspaceID}/api/FieldMap/{id}")]
+
+	public HttpResponseMessage Get(string id)
+		{
+			int _id = 0; 
+			Int32.TryParse(id,out _id);
+			var fieldsmap = _integrationPointReader.GetFieldMap(_id);
+			return Request.CreateResponse(HttpStatusCode.OK, fieldsmap, Configuration.Formatters.JsonFormatter);
+
+		}
+
+    }
+}
