37d4b02170f4819c3e213c48e2e61e3c1ca37f8e
Arthur Kelenzon <akelenzon@kcura.com>
2014-12-15 08:41:01 -0600

    FUNCTIONALITY: adding import api functionality

--- source/kCura.IntegrationPoints.Syncronizers.RDO/RDOSynchronizer.cs
+++ source/kCura.IntegrationPoints.Syncronizers.RDO/RDOSynchronizer.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using kCura.IntegrationPoints.Core.Models;
+using kCura.IntegrationPoints.Synchronizers.RDO.ImportAPI;
+using kCura.Relativity.Client;
 using Newtonsoft.Json;
 
 namespace kCura.IntegrationPoints.Synchronizers.RDO
@@ -8,8 +12,11 @@ namespace kCura.IntegrationPoints.Synchronizers.RDO
 	public class RdoSynchronizer : kCura.IntegrationPoints.Core.Services.Syncronizer.IDataSyncronizer
 	{
 		private RelativityFieldQuery _fieldQuery;
-		public RdoSynchronizer(RelativityFieldQuery fieldQuery)
+		private readonly IRSAPIClient _rsapiClient;
+
+		public RdoSynchronizer(IRSAPIClient rsapiClient, RelativityFieldQuery fieldQuery)
 		{
+			_rsapiClient = rsapiClient;
 			_fieldQuery = fieldQuery;
 		}
 
@@ -47,9 +54,95 @@ namespace kCura.IntegrationPoints.Synchronizers.RDO
 			return allFieldsForRdo;
 		}
 
+
+		private IImportService _importService;
+		private bool _isJobComplete = false;
+		private Exception _jobError;
+		private List<KeyValuePair<string, string>> _rowErrors;
 		public void SyncData(IEnumerable<IDictionary<FieldEntry, object>> data, IEnumerable<FieldMap> fieldMap, string options)
 		{
-			throw new NotImplementedException();
+
+			ImportSettings settings = JsonConvert.DeserializeObject<ImportSettings>(options);
+			Dictionary<string, int> importFieldMap = fieldMap.ToDictionary(x => x.SourceField.FieldIdentifier, x => int.Parse(x.DestinationField.FieldIdentifier));
+
+			if (fieldMap.Any(x => x.FieldMapType == FieldMapTypeEnum.Parent))
+			{
+				settings.ParentObjectIdSourceFieldName =
+					fieldMap.Where(x => x.FieldMapType == FieldMapTypeEnum.Parent).Select(x => x.SourceField.FieldIdentifier).First();
+			}
+			if (fieldMap.Any(x => x.FieldMapType == FieldMapTypeEnum.Identifier))
+			{
+				settings.IdentityFieldId =
+					fieldMap.Where(x => x.FieldMapType == FieldMapTypeEnum.Identifier).Select(x => int.Parse(x.DestinationField.FieldIdentifier)).First();
+			}
+
+			_importService = new ImportService(settings, importFieldMap, new BatchManager());
+			_importService.OnBatchComplete += new BatchCompleted(Finish);
+			_importService.OnDocumentError += new RowError(ItemError);
+			_importService.OnJobError += new JobError(JobError);
+
+
+			_isJobComplete = false;
+			_jobError = null;
+			_rowErrors = new List<KeyValuePair<string, string>>();
+
+			_importService.Initialize();
+
+			foreach (var row in data)
+			{
+				Dictionary<string, object> importRow = row.ToDictionary(x => x.Key.FieldIdentifier, x => x.Value);
+				_importService.AddRow(importRow);
+			}
+			_importService.PushBatchIfFull(true);
+
+			bool isJobDone = false;
+			do
+			{
+				lock (_importService)
+				{
+					isJobDone = bool.Parse(_isJobComplete.ToString());
+				}
+				Thread.Sleep(1000);
+			} while (!isJobDone);
+
+			ProcessExceptions();
+		}
+
+		private void ProcessExceptions()
+		{
+			if (_jobError != null)
+			{
+				throw _jobError;
+			}
+			else
+			{
+				foreach (var _rowError in _rowErrors)
+				{
+					//TODO:					
+				}
+			}
+		}
+
+		private void Finish(DateTime startTime, DateTime endTime, int totalRows, int errorRowCount)
+		{
+			lock (_importService)
+			{
+				_isJobComplete = true;
+			}
+		}
+
+		private void JobError(Exception ex)
+		{
+			lock (_importService)
+			{
+				_isJobComplete = true;
+				_jobError = ex;
+			}
+		}
+
+		private void ItemError(string documentIdentifier, string errorMessage)
+		{
+			_rowErrors.Add(new KeyValuePair<string, string>(documentIdentifier, errorMessage));
 		}
 	}
 }
