c5204478952b318a5ec13a409c37c17f534d7b54
Arthur Kelenzon <akelenzon@kcura.com>
2014-12-16 14:22:41 -0600

    FUNCTIONALITY: agent - work in progress; updating kcura DLLs to 9.0

--- source/kCura.IntegrationPoints.Agent/Tasks/SyncWorker.cs
+++ source/kCura.IntegrationPoints.Agent/Tasks/SyncWorker.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using kCura.IntegrationPoints.Core;
+using kCura.IntegrationPoints.Data;
+using kCura.IntegrationPoints.Synchronizers.RDO;
 using kCura.ScheduleQueueAgent;
 
 namespace kCura.IntegrationPoints.Agent.Tasks
@@ -15,8 +17,16 @@ namespace kCura.IntegrationPoints.Agent.Tasks
 		public void Execute(Job job)
 		{
 			int integrationPointID = job.RelatedObjectArtifactID;
-			_serviceContext.RsapiService.IntegrationPointLibrary.Read(integrationPointID);
+			IntegrationPoint rdoIntegrationPoint = _serviceContext.RsapiService.IntegrationPointLibrary.Read(integrationPointID);
 
+			if (rdoIntegrationPoint==null) throw new Exception("Failed to retrieved corresponding Integration Point.");
+
+
+
+			//RdoSynchronizer rdoSync = new RdoSynchronizer();
+			
+			//rdoIntegrationPoint.DestinationConfiguration
+			
 			//var client = new RSAPIClient(new Uri("net.pipe://localhost/Relativity.Services"), new IntegratedAuthCredentials())
 			//{
 			//	APIOptions = { WorkspaceID = CaseArtifactId }
