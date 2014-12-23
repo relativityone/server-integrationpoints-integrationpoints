65f1a51e715e8713b9e968b9b4aa088537c6efbc
Chris Griffin <cgriffin@kcura.com>
2014-12-23 15:56:39 -0600

    Backed out changeset: e055fb561c89

--- source/kCura.IntegrationPoints.LDAPProvider/LDAPProvider.cs
+++ source/kCura.IntegrationPoints.LDAPProvider/LDAPProvider.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.DirectoryServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using kCura.Apps.Common.Utils.Serializers;
-using kCura.IntegrationPoints.Contracts.Models;
 using kCura.IntegrationPoints.Contracts.Provider;
-using NSubstitute;
 
 namespace kCura.IntegrationPoints.LDAPProvider
 {
@@ -21,25 +17,16 @@ namespace kCura.IntegrationPoints.LDAPProvider
 		public System.Data.IDataReader GetBatchableIds(Contracts.Models.FieldEntry identifier, string options)
 		{
 			throw new NotImplementedException();
-			
-			LDAPSettings settings = new JSONSerializer().Deserialize<LDAPSettings>(options);
-			
-			LDAPService ldapService = new LDAPService(settings);
-			ldapService.InitializeConnection();
-			IEnumerable<SearchResult> items = ldapService.FetchItems(settings.GetPropertiesItemSearchLimit);
-			return new LDAPDataReader(items, new List<string>() { identifier.FieldIdentifier });
+//			DirectorySearcher src = new DirectorySearcher("…");
+//src.PropertiesToLoad = new string[] {ntSecurityDescriptor,…};
+//src.ExtendedDN = ExtendedDN.HexString;
+//SearchResultCollection res = src.FindAll();
+
 		}
 
 		public IEnumerable<Contracts.Models.FieldEntry> GetFields(string options)
 		{
-			LDAPSettings settings = new JSONSerializer().Deserialize<LDAPSettings>(options);
-
-			LDAPService ldapService = new LDAPService(settings);
-			ldapService.InitializeConnection();
-			IEnumerable<SearchResult> items = ldapService.FetchItems(settings.GetPropertiesItemSearchLimit);
-			List<string> fields = ldapService.GetAllProperties(items);
-
-			return fields.Select(f => new FieldEntry() { DisplayName = f, FieldIdentifier = f }).AsEnumerable();
+			throw new NotImplementedException();
 		}
 	}
 }
