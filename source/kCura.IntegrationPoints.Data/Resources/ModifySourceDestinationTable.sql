d86e166b365120ba31d1cea67751cc840ec69f40
Arthur Kelenzon <akelenzon@kcura.com>
2015-01-14 14:53:41 -0600

    FUNCTIONALITY: implementing source provider installer - work in progress

---
+++ source/kCura.IntegrationPoints.Data/Resources/ModifySourceDestinationTable.sql
@@ -0,0 +1,6 @@
+﻿IF NOT EXISTS(SELECT * FROM sys.columns 
+        WHERE Name = N'LibFileStream' and Object_ID = Object_ID(N'EDDSDBO.SourceProvider'))
+BEGIN
+		ALTER TABLE EDDSDBO.SourceProvider 
+		ADD	LibFileStream varbinary(MAX) NULL
+END
---
+++ source/kCura.IntegrationPoints.SourceProviderInstaller/IntegrationPointSourceProviderInstaller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kCura.IntegrationPoints.SourceProviderInstaller
+{
+    public class IntegrationPointSourceProviderInstaller
+    {
+    }
+}
