503ceb1bd17fbc2ca08c1fffa5edda76f3c9808a
David Barnes <dbarnes@kcura.com>
2015-02-10 10:04:03 -0600

    Added comments to the first-provider.js file
    --HG--
    branch : release-1.0

--- example/MyFirstProvider/Web/Scripts/first-provider.js
+++ example/MyFirstProvider/Web/Scripts/first-provider.js
@@ -1,23 +1,27 @@
-﻿var IP = IP || {};
-
-$(function () {
+﻿$(function () {
+	//create a new communication object to talk to the host page.
 	var message = IP.frameMessaging();
 
 	var _getModel = function () {
 		return $('#fileLocation').val()
 	};
 
-
+	//raised when the user has clicked the submit button
 	message.subscribe('submit', function () {
+		//do any save logic to persist the state here
 		var localModel = _getModel();
 		this.publish("saveState", localModel);
+		//tell the host page that it is ok to continue
 		this.publish('saveComplete', localModel);
 	});
 
+	//raised when a user clicks the back button
 	message.subscribe('back', function () {
+		//do any save logic to persist the state here
 		this.publish('saveState', _getModel());
 	});
-	
+
+	//raised when the host page has loaded the current settings page.
 	message.subscribe('load', function (model) {
 		$('#fileLocation').val(model);
 	});
