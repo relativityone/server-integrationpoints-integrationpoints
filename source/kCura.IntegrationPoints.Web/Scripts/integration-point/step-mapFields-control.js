058c77ce9be3fe5be21225091efe5ce021b5652f
David Barnes <dbarnes@kcura.com>
2015-02-11 16:53:17 -0600

    REFACTOR of hover for constants
    --HG--
    branch : release-1.0

--- source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-mapFields-control.js
+++ source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-mapFields-control.js
@@ -58,32 +58,38 @@ IP.workspaceFieldsControls = (function () {
 IP.affects = (function() {
 
 	var hover = function () {
+		var SOURCE_FIELD = '#selected-source-fields';
+		var WORKSPACE_FIELD = '#selected-workspace-fields'
+		var HOVER_CLASS = 'hover';
+
+
+		var _forceIERedraw = function () {
+			$('#forceRedraw').text(1); //force IE to redraw
+		};
 		var _init = function (source, destination) {
 			$(source).on('mousemove', function (event) {
 				var $this = $(this);
 				var $opt = $this.find('option:hover');
 				var idx = $opt.index();
-				$opt.siblings().removeClass('hover');
+				$opt.siblings().removeClass(HOVER_CLASS);
 				if (idx < 0) {
 					idx = $this.find('option').length - 1;
 				}
-				$this.find('option').eq(idx).addClass('hover');
-				$(destination).find('option').removeClass('hover').eq(idx).addClass('hover');
-				$('#forceRedraw').text(1); //force IE to redraw
+				$this.find('option').eq(idx).addClass(HOVER_CLASS);
+				$(destination).find('option').removeClass(HOVER_CLASS).eq(idx).addClass(HOVER_CLASS);
+				_forceIERedraw();
 			});
-		}
+		};
 
 		var removeHoverClass = function (source, destination) {
 			$(source).on('mouseleave', function (event) {
-				$(this).find('option').removeClass('hover');
-				$(destination).find('option').removeClass('hover');
-				$('#forceRedraw').text(1); //force IE to redraw
+				$(this).find('option').removeClass(HOVER_CLASS);
+				$(destination).find('option').removeClass(HOVER_CLASS);
+				_forceIERedraw();
 			});
-		}
+		};
 
-		var SOURCE_FIELD = '#selected-source-fields';
-		var WORKSPACE_FIELD ='#selected-workspace-fields' 
-
+		
 		removeHoverClass(WORKSPACE_FIELD, SOURCE_FIELD);
 		removeHoverClass(SOURCE_FIELD, WORKSPACE_FIELD);
 		_init(WORKSPACE_FIELD, SOURCE_FIELD);
