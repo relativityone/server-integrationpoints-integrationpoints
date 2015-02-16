d1c326275036acf5563e514ca9763574f95fd96a
Arthur Kelenzon <akelenzon@kcura.com>
2015-02-16 12:45:07 -0600

    FUNCTIONALITY: cleaning up EDDResources DB
    --HG--
    branch : release-1.0

--- source/kCura.IntegrationPoints.Data/Resources/CreateCustodianManagerResourceTable.sql
+++ source/kCura.IntegrationPoints.Data/Resources/CreateCustodianManagerResourceTable.sql
@@ -1,6 +1,33 @@
 ﻿SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 
+--Do cleanup first - delete old tables (over 24 hours old)
+DECLARE @table varchar(255) 
+DECLARE @dropCommand varchar(300) 
+
+DECLARE tableCursor CURSOR FOR 
+		SELECT QUOTENAME('EDDSResource')+'.'+QUOTENAME(s.name)+'.'+QUOTENAME(t.name) 
+		FROM [EDDSResource].[sys].[tables] AS t 
+		INNER JOIN [EDDSResource].[sys].[schemas] AS s 
+		ON t.[schema_id] = s.[schema_id] 
+		WHERE DATEDIFF(HOUR,t.create_date,GETUTCDATE())>24 
+		AND t.name LIKE 'RIP_CustodianManager_%'
+
+OPEN tableCursor 
+FETCH next FROM tableCursor INTO @table 
+
+WHILE @@fetch_status=0 
+BEGIN 
+		SET @dropcommand = N'DROP TABLE ' + @table 
+		--PRINT(@dropcommand) 
+		EXECUTE(@dropcommand) 
+		FETCH next FROM tableCursor INTO @table 
+END 
+
+CLOSE tableCursor 
+DEALLOCATE tableCursor
+
+
 IF NOT EXISTS (SELECT * FROM EDDSResource.sys.objects WHERE object_id = OBJECT_ID(N'[EDDSResource].[eddsdbo].[{0}]') AND type in (N'U'))
 BEGIN
 	CREATE TABLE [EDDSResource].[eddsdbo].[{0}](
@@ -15,14 +42,3 @@ BEGIN
 		)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 	) ON [PRIMARY]
 END
-
---TODO: Delete old tables
-
---delete records older than 1 day
---DELETE FROM [EDDSResource].[eddsdbo].[{0}]
---WHERE	
---			DATEDIFF(HOUR,[CreatedOn],GETUTCDATE()) > 24
---AND 
---			NOT [LockedByJobID] IS NULL
-
- 
