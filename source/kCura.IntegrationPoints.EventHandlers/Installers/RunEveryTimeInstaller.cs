e1d7a5c63dff606aa6ed44c23ca5856cd6d38f9d
David Barnes <dbarnes@kcura.com>
2015-02-18 17:27:29 -0600

    Forgot the EH

---
+++ source/kCura.IntegrationPoints.EventHandlers/Installers/RunEveryTimeInstaller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.EventHandler;
+using System.Runtime.InteropServices;
+using kCura.IntegrationPoints.Data;
+using kCura.IntegrationPoints.Data.Migrations;
+
+namespace kCura.IntegrationPoints.EventHandlers.Installers
+{
+	[kCura.EventHandler.CustomAttributes.Description("Runs Post install Migration scripts for Integration Points.")]
+	[kCura.EventHandler.CustomAttributes.RunOnce(false)]
+	[Guid("fb52b882-22d3-481f-b000-b976c13baf49")]
+	public class RunEveryTimeInstaller : kCura.EventHandler.PostInstallEventHandler
+	{
+		public override Response Execute()
+		{
+			new MigrationRunner(new EddsContext(Helper.GetDBContext(-1))).Run();
+			return new Response { Success = true };
+		}
+	}
+}
