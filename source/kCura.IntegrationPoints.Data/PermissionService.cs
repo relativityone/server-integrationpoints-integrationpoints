6b09bb3a5e585e75c7b2e5a3bb24add1d156567f
Muhammad Khan <mkhan@kcura.com>
2015-02-20 12:39:28 -0600

    STILL BROKEN MISSING MERGE

---
+++ source/kCura.IntegrationPoints.Data/PermissionService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using kCura.Relativity.Client.DTOs;
+
+namespace kCura.IntegrationPoints.Data
+{
+	public class PermissionService : IPermissionService
+	{
+		private readonly IWorkspaceDBContext _context;
+		public PermissionService(IWorkspaceDBContext context)
+		{
+			_context = context;
+		}
+
+
+		public bool userCanImport(int userId)
+		{
+			var sql = Resources.Resource.CheckImportPermission;
+			var param = new SqlParameter("@userID", userId);
+			var result = _context.ExecuteSqlStatementAsDataTable(sql, new List<SqlParameter> { param });
+			if (result != null && result.Rows != null)
+			{
+				var user = result.Rows.Cast<DataRow>().Select(x => x.Field<int>("UserArtifactID")).FirstOrDefault();
+				if (user > 0)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+	}
+}
