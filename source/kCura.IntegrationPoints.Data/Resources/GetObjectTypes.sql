6a9c9f29d51c3a511d47252dbdc81d14c49320cb
Muhammad Khan <mkhan@kcura.com>
2015-02-20 12:28:59 -0600

    Backed out changeset: e9905fe02ea0
    --HG--
    branch : release-1.0

--- source/kCura.IntegrationPoints.Data/Resources/GetObjectTypes.sql
+++ source/kCura.IntegrationPoints.Data/Resources/GetObjectTypes.sql
@@ -1,14 +1,14 @@
 ﻿SELECT
 		ot.DescriptorArtifactTypeID
 		,ot.Name
-  FROM [EDDSDBO].ObjectType ot
+  FROM [EddsDBO].ObjectType ot
   WHERE DescriptorArtifactTypeID in 
   (select atg.ArtifactTypeID
-  From [EDDSDBO].[GroupUser] gu
+  FRom [EDDSDBO].[GroupUser] gu
   join [EDDSDBO].[AccessControlListPermission]  acl on gu.GroupArtifactID = acl.GroupID
   join [EDDSDBO].[Permission] p on p.PermissionID = acl.PermissionID
   join [EDDSDBO].[ArtifactTypeGrouping] atg on atg.ArtifactGroupingID = p.ArtifactGrouping
   where UserArtifactID = @userID AND p.[Type] = 6 
 )
-AND (DescriptorArtifactTypeID > 1000000 OR DescriptorArtifactTypeID = 10) 
+AND IsSystem = 0
 order by ot.Name
