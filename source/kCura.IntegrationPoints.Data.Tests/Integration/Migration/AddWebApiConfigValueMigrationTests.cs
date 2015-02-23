0bf519e6ed3b70251abe6a74b7d296bdffc3a4cb
David Barnes <dbarnes@kcura.com>
2015-02-23 16:24:52 -0600

    Updated examples

---
+++ source/kCura.IntegrationPoints.Data.Tests/Integration/Migration/AddWebApiConfigValueMigrationTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Relativity.API;
+
+namespace kCura.IntegrationPoints.Data.Tests.Integration.Migration
+{
+	[TestFixture]
+	[IntegrationTest]
+	public class AddWebApiConfigValueMigrationTests
+	{
+		private IEddsDBContext _context;
+		[SetUp]
+		public void Setup()
+		{
+			_context = new EddsContext(new MockDBContext(string.Empty));
+		}
+
+		[Test]
+		public void Execute_KeyAlreadyExists_DoesNotUpdate()
+		{
+			
+		}
+
+		[Test]
+		public void Execute_KeyDoesNotExistButProcessingDoes_UsesProcessingKey()
+		{
+
+		}
+
+		[Test]
+		public void Execute_KeyDoesNotExistAndProcessingDoesnot_UsesDBMTKey()
+		{
+
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			//restore previous value
+		}
+
+	}
+}
---
+++ source/kCura.IntegrationPoints.Data.Tests/MockDBContext.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Relativity.API;
+
+namespace kCura.IntegrationPoints.Data.Tests
+{
+	public class MockDBContext : IDBContext
+	{
+		private readonly string _connectionString;
+		public MockDBContext(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		public SqlConnection GetConnection()
+		{
+			return new SqlConnection(_connectionString);
+		}
+
+		public DbParameter CreateDbParameter()
+		{
+			throw new NotImplementedException();
+		}
+
+		public SqlConnection GetConnection(bool openConnectionIfClosed)
+		{
+			var cmd = GetConnection();
+			if (cmd.State != ConnectionState.Open)
+			{
+				cmd.Open();
+			}
+			return cmd;
+		}
+
+		public SqlTransaction GetTransaction()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void BeginTransaction()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CommitTransaction()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RollbackTransaction()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RollbackTransaction(Exception originatingException)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ReleaseConnection()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Cancel()
+		{
+			throw new NotImplementedException();
+		}
+
+		public DataTable ExecuteSqlStatementAsDataTable(string sqlStatement)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DataTable ExecuteSqlStatementAsDataTable(string sqlStatement, int timeoutValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DataTable ExecuteSqlStatementAsDataTable(string sqlStatement, IEnumerable<SqlParameter> parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DataTable ExecuteSqlStatementAsDataTable(string sqlStatement, int timeoutValue, IEnumerable<SqlParameter> parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T ExecuteSqlStatementAsScalar<T>(string sqlStatement)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T ExecuteSqlStatementAsScalar<T>(string sqlStatement, IEnumerable<SqlParameter> parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T ExecuteSqlStatementAsScalar<T>(string sqlStatement, int timeoutValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T ExecuteSqlStatementAsScalar<T>(string sqlStatement, IEnumerable<SqlParameter> parameters, int timeoutValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T ExecuteSqlStatementAsScalar<T>(string sqlStatement, params SqlParameter[] parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object ExecuteSqlStatementAsScalar(string sqlStatement, params SqlParameter[] parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object ExecuteSqlStatementAsScalar(string sqlStatement, IEnumerable<SqlParameter> parameters, int timeoutValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object ExecuteSqlStatementAsScalarWithInnerTransaction(string sqlStatement, IEnumerable<SqlParameter> parameters, int timeoutValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int ExecuteNonQuerySQLStatement(string sqlStatement)
+		{
+			using (var con = this.GetConnection(true))
+			{
+				using (var cmd = con.CreateCommand())
+				{
+					cmd.CommandText = sqlStatement;
+					return cmd.ExecuteNonQuery();
+				}
+			}
+		}
+
+		public int ExecuteNonQuerySQLStatement(string sqlStatement, int timeoutValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int ExecuteNonQuerySQLStatement(string sqlStatement, IEnumerable<SqlParameter> parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int ExecuteNonQuerySQLStatement(string sqlStatement, IEnumerable<SqlParameter> parameters, int timeoutValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DbDataReader ExecuteSqlStatementAsDbDataReader(string sqlStatement)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DbDataReader ExecuteSqlStatementAsDbDataReader(string sqlStatement, int timeoutValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DbDataReader ExecuteSqlStatementAsDbDataReader(string sqlStatement, IEnumerable<DbParameter> parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DbDataReader ExecuteSqlStatementAsDbDataReader(string sqlStatement, IEnumerable<DbParameter> parameters, int timeoutValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DataTable ExecuteSQLStatementGetSecondDataTable(string sqlStatement, int timeout = -1)
+		{
+			throw new NotImplementedException();
+		}
+
+		public SqlDataReader ExecuteSQLStatementAsReader(string sqlStatement, int timeout = -1)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DbDataReader ExecuteProcedureAsReader(string procedureName, IEnumerable<SqlParameter> parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int ExecuteProcedureNonQuery(string procedureName, IEnumerable<SqlParameter> parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		public SqlDataReader ExecuteParameterizedSQLStatementAsReader(string sqlStatement, IEnumerable<SqlParameter> parameters,
+			int timeoutValue = -1, bool sequentialAccess = false)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DataSet ExecuteSqlStatementAsDataSet(string sqlStatement)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DataSet ExecuteSqlStatementAsDataSet(string sqlStatement, IEnumerable<SqlParameter> parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DataSet ExecuteSqlStatementAsDataSet(string sqlStatement, int timeoutValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DataSet ExecuteSqlStatementAsDataSet(string sqlStatement, IEnumerable<SqlParameter> parameters, int timeoutValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string Database { get; private set; }
+		public string ServerName { get; private set; }
+		public bool IsMasterDatabase { get; private set; }
+	}
+}
