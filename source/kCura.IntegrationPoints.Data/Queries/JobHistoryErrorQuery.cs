80d0625e816bb8cfa59eb573255945a60584aa68
David Barnes <dbarnes@kcura.com>
2015-02-26 14:52:30 -0600

    Updated error query

--- source/kCura.IntegrationPoints.Data/Queries/JobHistoryErrorQuery.cs
+++ source/kCura.IntegrationPoints.Data/Queries/JobHistoryErrorQuery.cs
@@ -26,13 +26,15 @@ namespace kCura.IntegrationPoints.Data.Queries
 
 			var historyCondition = new ObjectCondition(Guid.Parse(JobHistoryErrorFieldGuids.JobHistory), ObjectConditionEnum.EqualTo, jobHistoryId);
 
-			var choiceJobErrorCondition = new SingleChoiceCondition(Guid.Parse(JobHistoryErrorFieldGuids.ErrorType), SingleChoiceConditionEnum.AnyOfThese, new List<Guid> { Guid.Parse("FA8BB625-05E6-4BF7-8573-012146BAF19B") });
+			var choiceJobErrorCondition = new SingleChoiceCondition(Guid.Parse(JobHistoryErrorFieldGuids.ErrorType), SingleChoiceConditionEnum.AnyOfThese,
+				new List<Guid> { Guid.Parse("FA8BB625-05E6-4BF7-8573-012146BAF19B") });
 			query.Condition = new CompositeCondition(choiceJobErrorCondition, CompositeConditionEnum.And, historyCondition);
 			JobHistoryError historyError = _service.JobHistoryErrorLibrary.Query(query, 1).FirstOrDefault();
 
 			if (historyError == null)
 			{
-				var choiceJobItemCondition = new SingleChoiceCondition(Guid.Parse(JobHistoryErrorFieldGuids.ErrorType), SingleChoiceConditionEnum.AnyOfThese, new List<Guid> { Guid.Parse("9DDC4914-FEF3-401F-89B7-2967CD76714B") });
+				var choiceJobItemCondition = new SingleChoiceCondition(Guid.Parse(JobHistoryErrorFieldGuids.ErrorType), SingleChoiceConditionEnum.AnyOfThese,
+					new List<Guid> { Guid.Parse("9DDC4914-FEF3-401F-89B7-2967CD76714B") });
 				query.Condition = new CompositeCondition(choiceJobItemCondition, CompositeConditionEnum.And, historyCondition);
 				historyError = _service.JobHistoryErrorLibrary.Query(query, 1).FirstOrDefault();
 			}
