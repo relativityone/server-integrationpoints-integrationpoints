4b462e2a64e5109279c3c65b4ad1db6d49b6576e
Muhammad Khan <mkhan@kcura.com>
2015-03-02 10:06:45 -0600

    completed the on upgrade change records where log error is set to blank to null and working on uninstaller source provider removes other relationships

---
+++ source/kCura.IntegrationPoints.Data/IntegrationPointQuery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.Relativity.Client;
+using kCura.Relativity.Client.DTOs;
+
+namespace kCura.IntegrationPoints.Data
+{
+	public class IntegrationPointQuery
+	{
+			private readonly IRSAPIService _context;
+		public IntegrationPointQuery(IRSAPIService context)
+		{
+			_context = context;
+		}
+
+		public List<IntegrationPoint> GetIntegrationPoints(List<int> allSourceProviderid)
+		{
+			var qry = new Query<Relativity.Client.DTOs.RDO>();
+			qry.Fields = new List<FieldValue>()
+				{
+					new FieldValue(Guid.Parse(Data.IntegrationPointFieldGuids.Name))
+				};
+			qry.Condition = new WholeNumberCondition(IntegrationPointFields.SourceProvider, NumericConditionEnum.In, allSourceProviderid);
+			var result = _context.IntegrationPointLibrary.Query(qry);
+
+			return result.ToList();
+		} 
+	}
+}
---
+++ source/kCura.IntegrationPoints.Data/Migrations/UpdateJobErrorsBlankToNo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kCura.IntegrationPoints.Data.Migrations
+{
+	public class UpdateJobErrorsBlankToNo
+	{	private readonly IEddsDBContext _context;
+	public UpdateJobErrorsBlankToNo(IEddsDBContext context)
+		{
+			_context = context;
+		}
+
+		public void Execute()
+		{
+			var sql = Resources.Resource.SetBlankLogErrorsToNo;
+			_context.ExecuteNonQuerySQLStatement(sql);
+		}
+	}
+}
