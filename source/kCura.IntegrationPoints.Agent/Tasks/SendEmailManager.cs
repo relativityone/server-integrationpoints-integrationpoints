06e8e49dbf0a8414ee80c7b9597ffe546f4b36ab
David Barnes <dbarnes@kcura.com>
2015-03-04 17:32:34 -0600

    Added sendemail skeleton

---
+++ source/kCura.IntegrationPoints.Agent/Tasks/SendEmailManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.IntegrationPoints.Core.Models;
+using kCura.ScheduleQueue.Core;
+using kCura.ScheduleQueue.Core.BatchProcess;
+
+namespace kCura.IntegrationPoints.Agent.Tasks
+{
+	public class SendEmailManager : BatchManagerBase<Core.Models.EmailMessage>
+	{
+		public override IEnumerable<EmailMessage> GetUnbatchedIDs(Job job)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void CreateBatchJob(Job job, List<EmailMessage> batchIDs)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- source/kCura.IntegrationPoints.Agent/Tasks/SendEmailWorker.cs
+++ source/kCura.IntegrationPoints.Agent/Tasks/SendEmailWorker.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using kCura.ScheduleQueue.Core;
 
 namespace kCura.IntegrationPoints.Agent.Tasks
 {
-	class SendEmailWorker
+	public class SendEmailWorker : ITask
 	{
+		public void Execute(Job job)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
