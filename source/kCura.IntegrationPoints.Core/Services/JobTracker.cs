61183be5346a522342407d4667a85deceea46bfd
David Barnes <dbarnes@kcura.com>
2015-03-05 13:59:29 -0600

    Added config
    --HG--
    branch : feature-branch-1

---
+++ source/kCura.IntegrationPoints.Core/Services/JobTracker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.IntegrationPoints.Data;
+using kCura.ScheduleQueue.Core;
+
+namespace kCura.IntegrationPoints.Core.Services
+{
+	public class JobTracker
+	{
+		private readonly IWorkspaceDBContext _context;
+		public JobTracker(IWorkspaceDBContext context)
+		{
+			_context = context;
+		}
+
+		public string GenerateTableTempTableName(Job job, string batchID)
+		{
+			return string.Format("JobTracker_{0}_{1}_{2}", job.WorkspaceID, job.RootJobId);
+		}
+
+		public void CreateTrackingEntry(Job job, string batchId)
+		{
+			var table = GenerateTableTempTableName(job, batchId);
+		}
+
+		public bool CheckEntries(Job job)
+		{
+			return false;
+		}
+
+	}
+}
