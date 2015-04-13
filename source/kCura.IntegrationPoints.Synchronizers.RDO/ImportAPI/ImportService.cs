61645744fad455faba5814872812afe25693015a
Arthur Kelenzon <akelenzon@kcura.com>
2015-04-13 17:33:34 -0500

    FUNCTIONALITY: creating configurable properties for RIP: DisableNativeValidation DisableNativeLocationValidation

--- source/kCura.IntegrationPoints.Synchronizers.RDO/ImportAPI/ImportService.cs
+++ source/kCura.IntegrationPoints.Synchronizers.RDO/ImportAPI/ImportService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Security.Authentication;
