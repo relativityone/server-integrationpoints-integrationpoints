fef4b4d7160343ddfbec08398fb75be109d170a9
Arunas Slepikas <aslepikas@kcura.com>
2015-05-05 15:09:23 -0500

    adding nuget step

--- DevelopmentScripts/nuget.ps1
+++ DevelopmentScripts/nuget.ps1
@@ -1,9 +1,43 @@
 . .\common.ps1
 
 
-task default -depends nuget
+task default -depends update_nuspec, nuget_pack
 
 
-task nuget {
- 
+task update_nuspec {
+    foreach($o in Get-ChildItem $nuspec_directory){
+       
+       if($o.Extension -ne '.nuspec') {continue}
+
+       Write-Host "Updating" $o.FullName "to version" $version "..."
+       
+       $x = Select-Xml -Path $o.FullName -XPath '/package/metadata/version'
+       $x.Node.InnerText = $version   
+       $x.Node.OwnerDocument.Save($x.Path)   
+       
+       $x = Select-Xml -Path $o.FullName -XPath '/package/metadata/copyright'
+       $x.Node.InnerText = "Copyright (c) " + [System.DateTime]::Now.Year + ", " + $Company  
+       $x.Node.OwnerDocument.Save($x.Path)  
+       
+       $x = Select-Xml -Path $o.FullName -XPath '/package/metadata/authors'
+       $x.Node.InnerText = $Company  
+       $x.Node.OwnerDocument.Save($x.Path)   
+       
+       $x = Select-Xml -Path $o.FullName -XPath '/package/metadata/owners'
+       $x.Node.InnerText = $Company  
+       $x.Node.OwnerDocument.Save($x.Path)       
+    }   
+}
+
+task nuget_pack {
+     foreach($o in Get-ChildItem $nuspec_directory){
+        
+        if($o.Extension -ne '.nuspec') {continue}
+
+        Write-Host "Packing" $o.FullName "..."
+
+        exec {
+            & $nuget_exe @('pack', $o.FullName)
+        }
+     }    
 }
