81827f791947a577de3a30b6497ae3e14d1d97dc
Arunas Slepikas <aslepikas@kcura.com>
2015-05-06 08:49:44 -0500

    updating psake.ps1 to allow execution and debugging via powershell ISE

--- DevelopmentScripts/psake.ps1
+++ DevelopmentScripts/psake.ps1
@@ -1,2 +1,16 @@
 Import-Module ..\Vendor\psake\tools\psake.psm1
-Invoke-psake .\build.ps1 -properties @{"version"="1.3.3.7";"server_type"="local";}
+
+$BUILDCONFIG = 'Debug'
+$BUILDTYPE = 'DEV'
+$VERSION = '1.0.0.0'
+
+#
+# Uncomment one or more lines below to execute. Run in Windows Powershell ISE to debug
+#
+
+#Invoke-psake .\version.ps1 -properties @{'version'=$VERSION;}
+#Invoke-psake .\build.ps1 -properties @{'version'=$VERSION;'server_type'='local';'build_config'=$BUILDCONFIG;'build_type'=$BUILDTYPE;}
+#Invoke-psake .\application.ps1 -properties @{'version'=$VERSION;'server_type'='local';'build_config'=$BUILDCONFIG;'build_type'=$BUILDTYPE;}
+#Invoke-psake .\test.ps1
+#Invoke-psake .\nuget.ps1 -properties @{'version'=$VERSION;'server_type'='local';'build_config'=$BUILDCONFIG;'build_type'=$BUILDTYPE;}
+#Invoke-psake .\package.ps1 -properties @{'version'=$VERSION;'server_type'='local';'build_config'=$BUILDCONFIG;'build_type'=$BUILDTYPE;}
