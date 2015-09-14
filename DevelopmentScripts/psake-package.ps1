6beb35d7c57f453f2f0919c96a9ea3155e576b48
Arunas Slepikas <aslepikas@kcura.com>
2015-09-14 10:11:59 -0500

    Adding custom pages to auto-package. Only make folders if needed

--- DevelopmentScripts/psake-package.ps1
+++ DevelopmentScripts/psake-package.ps1
@@ -8,13 +8,13 @@ task package_initalize {
     $script:package_directory = [System.IO.Path]::Combine($package_root_directory, $Product, $branch, $version)
     $script:package_bin_directory = [System.IO.Path]::Combine($package_directory, 'bin')
     $script:package_rap_directory = [System.IO.Path]::Combine($package_directory, 'RAP')
+    $script:package_cp_directory = [System.IO.Path]::Combine($package_directory, 'CustomPages')
     $script:package_nuget_directory = [System.IO.Path]::Combine($package_directory, 'NuGet')
-    $script:package_pdb_directory = [System.IO.Path]::Combine($package_directory, 'PDBs - INTERNAL USE ONLY')
+    $script:package_pdb_directory = [System.IO.Path]::Combine($package_directory, 'PDBs')
 
     [System.IO.Directory]::CreateDirectory($package_directory)
-    [System.IO.Directory]::CreateDirectory($package_bin_directory)
-    [System.IO.Directory]::CreateDirectory($package_rap_directory)
     [System.IO.Directory]::CreateDirectory($package_nuget_directory)
+    [System.IO.Directory]::CreateDirectory($package_bin_directory) 
     [System.IO.Directory]::CreateDirectory($package_pdb_directory)
 }
 
@@ -23,7 +23,13 @@ task package -depends package_initalize <# package_sample #> {
     Copy-Item -Path ([System.IO.Path]::Combine($nuspec_directory, '*')) -Destination $package_nuget_directory -Include '*.nupkg'
     
     if ([System.IO.Directory]::Exists($application_directory)) {
+        [System.IO.Directory]::CreateDirectory($package_rap_directory)
         Copy-Item -Path ([System.IO.Path]::Combine($application_directory, '*')) -Destination $package_rap_directory -Include '*.rap'
+    }
+
+    if ([System.IO.Directory]::Exists([System.IO.Path]::Combine($source_directory, 'CustomPages'))) {
+        [System.IO.Directory]::CreateDirectory($package_cp_directory)
+        Copy-Item -Path ([System.IO.Path]::Combine($source_directory, 'CustomPages', '*')) -Destination $package_cp_directory -Include '**' -Recurse
     }
     
 }
