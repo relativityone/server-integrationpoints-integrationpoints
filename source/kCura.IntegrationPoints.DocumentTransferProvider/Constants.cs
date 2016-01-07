dc8e365d0a6a2d3333222aa08c253281f4de0e84
Sorawit Amornborvornwong <samornborvornwong@kcura.com>
2016-01-07 17:50:16 -0600

    add DocumentTransferProvider project to into the main solution
    --HG--
    branch : feature-UrsaMinor

---
+++ source/kCura.IntegrationPoints.DocumentTransferProvider/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace kCura.IntegrationPoints.DocumentTransferProvider
+{
+	using System;
+
+	public class Constants
+	{
+		public const String ProviderGuid = "423B4D43-EAE9-4E14-B767-17D629DE4BB2";
+	}
+}
---
+++ source/kCura.IntegrationPoints.DocumentTransferProvider/DocumentTransferProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace kCura.IntegrationPoints.DocumentTransferProvider
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Data;
+	using Contracts.Provider;
+	using Contracts.Models;
+
+	[Contracts.DataSourceProvider(Constants.ProviderGuid)]
+	public class DocumentTransferProvider : IDataSourceProvider
+	{
+		public IEnumerable<FieldEntry> GetFields(string options)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public IDataReader GetBatchableIds(FieldEntry identifier, string options)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public IDataReader GetData(IEnumerable<FieldEntry> fields, IEnumerable<string> entryIds, string options)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
