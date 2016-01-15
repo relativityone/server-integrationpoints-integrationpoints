a5fe766a364b952e52d8175e5e676db533c19b90
Dan Nelson <dnelson@kcura.com>
2016-01-15 17:01:04 -0600

    Updating build scripts.
    --HG--
    branch : feature-UrsaMinor

---
+++ source/DevelopmentScripts/build.bat
@@ -0,0 +1,36 @@
+@echo off
+
+SET BUILDCONFIG=Debug
+SET BUILDACTION=build
+SET BUILDPROJECT=Build.build
+SET SIGNOUTPUT=false
+
+if "%1" == "/?" goto help
+if "%1" == "-?" goto help
+
+if not "%1" == "" (
+	SET "BUILDCONFIG=%1"
+)
+if not "%2" == "" (
+	SET "BUILDACTION=%2"
+)
+if not "%3" == "" (
+	SET "buildType=%3"
+)
+
+SET LDAPSyncRoot=%LDAPSync%
+pushd %LDAPSync%\developmentscripts
+nant build_all -buildfile:"%BUILDPROJECT%" "-D:root=%LDAPSyncRoot%" "-D:buildconfig=%BUILDCONFIG%" "-D:action=%BUILDACTION%" "-D:DevEnv=DEVENV" "-D:signOutput=SIGNOUTPUT"
+popd
+goto end
+
+
+:help
+echo      Build the specified branch
+echo.
+echo      usage: %0 [config] [action]
+echo.
+echo.           config    The configuration for the build. Possibilities are 'debug' and 'release'. (default: %BUILDCONFIG%)
+echo.
+echo.		action	  Preforms an action on the build. Possibilities are 'build' and clean. (default: %BUILDACTION%)
+:end
