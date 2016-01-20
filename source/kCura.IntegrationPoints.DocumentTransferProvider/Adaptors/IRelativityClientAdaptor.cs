41685cef16a50f36e94ba3a1a54db838ba7f1bb6
David Biedrzycki <dbiedrzycki@kcura.com>
2016-01-20 11:08:42 -0600

    Added comments to IRelativityClientAdaptor
    --HG--
    branch : feature-UrsaMinor

--- source/kCura.IntegrationPoints.DocumentTransferProvider/Adaptors/IRelativityClientAdaptor.cs
+++ source/kCura.IntegrationPoints.DocumentTransferProvider/Adaptors/IRelativityClientAdaptor.cs
@@ -2,8 +2,16 @@
 
 namespace kCura.IntegrationPoints.DocumentTransferProvider.Adaptors
 {
+	/// <summary>
+	/// Wraps the RSAPI functionality in order to increase testability and remove direct dependency on the RSAPI client itself
+	/// </summary>
 	public interface IRelativityClientAdaptor
 	{
+		/// <summary>
+		/// Executes a Document Query
+		/// </summary>
+		/// <param name="query">The Query to execute</param>
+		/// <returns>A ResultSet of Documents</returns>
 		ResultSet<Document> ExecuteDocumentQuery(Query<Document> query);
 	}
 }
