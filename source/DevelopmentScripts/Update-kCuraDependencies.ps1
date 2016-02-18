a07f68d34f1199914af68d4d49547a6806f2db16
David Biedrzycki <dbiedrzycki@kcura.com>
2016-02-18 12:39:41 -0600

    Added Update-kCuraDependencies.ps1
    --HG--
    branch : feature-UrsaMinor

---
+++ source/DevelopmentScripts/Update-kCuraDependencies.ps1
@@ -0,0 +1,16 @@
+﻿$destinationDir = "C:\SourceCode\IntegrationPoints\source\Dependencies\kCura"
+$mainlineLibDir = "C:\SourceCode\Mainline\lib"
+
+$dependencies = Get-ChildItem -Path $destinationDir
+
+foreach($dependency in $dependencies)
+{
+    $fileExists = Test-Path "$mainlineLibDir\$dependency"
+    if ($fileExists) 
+    {
+        Write-Host "Updating $dependency.Name ..."
+        Copy-Item -Path "$mainlineLibDir\$dependency" -Destination $destinationDir
+    }
+}
+
+Write-Host "Update complete!"
