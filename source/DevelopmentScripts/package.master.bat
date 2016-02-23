ccc4999dcfd069f152c46e649bf3f2d953aa75a7
Dan Nelson <dnelson@kcura.com>
2016-02-23 16:13:51 -0600

    Moving RAP inside of folder in build location. (grafted from 45ddb1be362ded14d00024f41719d533aa589888)

--- source/DevelopmentScripts/package.master.bat
+++ source/DevelopmentScripts/package.master.bat
@@ -6,7 +6,7 @@ echo PACKAGEMASTER: Calling package scripts
 mkdir "%revisionDirectory%"
 mkdir "%revisionDirectory%\web"
 
-echo f | xcopy "%basePath%\bin\Application\*" "%revisionDirectory%\" /EY /EXCLUDE:packageCreationExcludeList.txt
+echo f | xcopy "%basePath%\bin\Application\*" "%revisionDirectory%\Application\" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\bin\Release\*" "%revisionDirectory%\" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\publish\bin\web\Release\*" "%revisionDirectory%\web\" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\nugspec\*.nupkg" "%revisionDirectory%\%buildType% %version% NuGet Packages - INTERNAL USE ONLY" /E /I
