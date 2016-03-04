2967d76bed109be8656cc743fb968a06868a71d7
Andrzej KorczyÅ„ski <akorczynski@kcura.com>
2016-03-04 14:20:29 +0100

    UAL318 Removed /nolibraryupdate parameter from buildall.bat, it is needed for custom pages update
    --HG--
    branch : feature-export

--- source/DevelopmentScripts/deploy.bat
+++ source/DevelopmentScripts/deploy.bat
@@ -20,7 +20,7 @@ if not "%1" == "" (
 SET BUILDCONFIGPATH=%BUILDPATH%
 
 pushd "%BUILDCONFIGPATH%"
-call "%resourceUploader%" /masterurl:http://localhost/Relativity.Services/ /casedbserver:localhost /casedbusername:eddsdbo /casedbuserpassword:edds /caseusername:relativity.admin@kcura.com /caseuserpassword:Test1234! /caseid:%CASEID% /applicationguid:DCF6E9D1-22B6-4DA3-98F6-41381E93C30C /assembly:%BUILDCONFIGPATH%\kCura.IntegrationPoints.Contracts.dll /assembly:%BUILDCONFIGPATH%\kCura.IntegrationPoints.dll /assembly:%BUILDCONFIGPATH%\kCura.LDAPProvider.dll /assembly:%BUILDCONFIGPATH%\kCura.DocumentTransferProvider.dll /custompage:C2C93191-7B15-4A9B-AA56-6A8C2DAC5494=C:\SourceCode\IntegrationPoints\source\kCura.IntegrationPoints.Web\ /nolibraryupdate
+"%resourceUploader%" /masterurl:http://localhost/Relativity.Services/ /casedbserver:localhost /casedbusername:eddsdbo /casedbuserpassword:edds /caseusername:relativity.admin@kcura.com /caseuserpassword:Test1234! /caseid:%CASEID% /applicationguid:DCF6E9D1-22B6-4DA3-98F6-41381E93C30C /assembly:%BUILDCONFIGPATH%\kCura.IntegrationPoints.Contracts.dll /assembly:%BUILDCONFIGPATH%\kCura.IntegrationPoints.dll /assembly:%BUILDCONFIGPATH%\kCura.LDAPProvider.dll /assembly:%BUILDCONFIGPATH%\kCura.DocumentTransferProvider.dll /custompage:C2C93191-7B15-4A9B-AA56-6A8C2DAC5494=C:\SourceCode\IntegrationPoints\source\kCura.IntegrationPoints.Web\
 popd
 goto end
 
