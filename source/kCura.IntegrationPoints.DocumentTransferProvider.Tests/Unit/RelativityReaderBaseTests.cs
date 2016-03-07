4921f51f83801f917f0a3f09e03831068bf287f8
David Biedrzycki <dbiedrzycki@kcura.com>
2016-03-07 14:14:24 -0600

    RelativityReaderBaseTests updated to pass with new logic
    --HG--
    branch : feature-UrsaMinor

--- source/kCura.IntegrationPoints.DocumentTransferProvider.Tests/Unit/RelativityReaderBaseTests.cs
+++ source/kCura.IntegrationPoints.DocumentTransferProvider.Tests/Unit/RelativityReaderBaseTests.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using kCura.IntegrationPoints.Contracts.RDO;
+using kCura.IntegrationPoints.DocumentTransferProvider.DataReaders;
+using kCura.IntegrationPoints.DocumentTransferProvider.Managers;
+using kCura.IntegrationPoints.DocumentTransferProvider.Models;
 using kCura.Relativity.Client.DTOs;
 using NSubstitute;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace kCura.IntegrationPoints.DocumentTransferProvider.Tests.Unit
 {
@@ -11,186 +17,142 @@ namespace kCura.IntegrationPoints.DocumentTransferProvider.Tests.Unit
 	public class RelativityReaderBaseTests
 	{
 		private const string _TOKEN_FOR_MORE = "There are some more things to get";
-		private IRDORepository _rdoRepository;
 
 		[SetUp]
 		public void Setup()
 		{
-			_rdoRepository = Substitute.For<IRDORepository>();
 		}
 
-		protected QueryResultSet<Document> ExecuteQueryToGetInitialResult()
+		protected ArtifactDTO[] ExecuteQueryToGetInitialResult()
 		{
-			return new QueryResultSet<Document>()
+			return new ArtifactDTO []
 			{
-				Success = true,
-				Results = new List<Result<Document>>()
-				{
-					new Result<Document>()
-					{
-						Artifact = new Document(1)
-					},
-					new Result<Document>()
-					{
-						Artifact = new Document(2)
-					}
-				},
-				Message = "lol",
-				QueryToken = _TOKEN_FOR_MORE,
-				TotalCount = 3
+				new ArtifactDTO(),
+				new ArtifactDTO(),
 			};
 		}
 
 		[Test]
 		public void ReadDocumentCounterGetUpdated()
 		{
-//			MockRelativityReaderBase instance = new MockRelativityReaderBase(_rdoRepository, ExecuteQueryToGetInitialResult());
-//			Assert.AreEqual(0, instance.Counter);
-//
-//			instance.Read();
-//			instance.Read();
-//
-//			Assert.AreEqual(2, instance.Counter);
+			MockRelativityReaderBase instance = new MockRelativityReaderBase(ExecuteQueryToGetInitialResult, () => true);
+			Assert.AreEqual(0, instance.Counter);
+
+			instance.Read();
+			instance.Read();
+
+			Assert.AreEqual(2, instance.Counter);
 		}
 
 		[Test]
 		public void ReadDocumentsUntilTheEnd_FetchMoreData()
 		{
-//			MockRelativityReaderBase instance = new MockRelativityReaderBase(_rdoRepository, ExecuteQueryToGetInitialResult());
+			MockRelativityReaderBase instance = new MockRelativityReaderBase(ExecuteQueryToGetInitialResult, () => false);
 
-			var newResult = new QueryResultSet<Document>()
-			{
-				Success = true,
-				Results = new List<Result<Document>>()
-				{
-					new Result<Document>()
-					{
-						Artifact = new Document(3)
-					}
-				},
-				Message = "lol",
-				QueryToken = String.Empty,
-				TotalCount = 3
-			};
-
-//			_rdoRepository.ExecuteSubSetOfDocumentQuery(_TOKEN_FOR_MORE, 2, Shared.Constants.QUERY_BATCH_SIZE).Returns(newResult);
-//
-//			Assert.AreEqual(true, instance.Read());
-//			Assert.AreEqual(true, instance.Read());
-//			Assert.AreEqual(true, instance.Read());
-//			Assert.AreEqual(3, instance.Counter);
-//			Assert.AreEqual(false, instance.IsClosed);
+			Assert.AreEqual(true, instance.Read());
+			Assert.AreEqual(true, instance.Read());
+			Assert.AreEqual(true, instance.Read());
+			Assert.AreEqual(3, instance.Counter);
+			Assert.AreEqual(false, instance.IsClosed);
 		}
 
 		[Test]
 		public void ReadDocumentsUntilTheEnd_NoMoreData_TheReaderClose()
 		{
-//			MockRelativityReaderBase instance = new MockRelativityReaderBase(_rdoRepository, ExecuteQueryToGetInitialResult());
+			MockRelativityReaderBase instance = new MockRelativityReaderBase(ExecuteQueryToGetInitialResult, () => true);
 
-//			Assert.AreEqual(true, instance.Read());
-//			Assert.AreEqual(true, instance.Read());
-//			Assert.AreEqual(false, instance.Read());
-//			Assert.AreEqual(true, instance.IsClosed);
+			Assert.AreEqual(true, instance.Read());
+			Assert.AreEqual(true, instance.Read());
+			Assert.AreEqual(false, instance.Read());
+			Assert.AreEqual(true, instance.IsClosed);
 		}
 
 		[Test]
 		public void ReadDocumentsUntilTheEnd_FetchMoreDataAndReadToEnd()
 		{
-//			MockRelativityReaderBase instance = new MockRelativityReaderBase(_rdoRepository, ExecuteQueryToGetInitialResult());
-//
-//			var newResult = new QueryResultSet<Document>()
-//			{
-//				Success = true,
-//				Results = new List<Result<Document>>()
-//				{
-//					new Result<Document>()
-//					{
-//						Artifact = new Document(3)
-//					}
-//				},
-//				Message = "lol",
-//				QueryToken = String.Empty,
-//				TotalCount = 3
-//			};
-//
-//			_rdoRepository.ExecuteSubSetOfDocumentQuery(_TOKEN_FOR_MORE, 2, Shared.Constants.QUERY_BATCH_SIZE).Returns(newResult);
-//
-//			Assert.AreEqual(true, instance.Read());
-//			Assert.AreEqual(true, instance.Read());
-//			Assert.AreEqual(true, instance.Read());
-//			Assert.AreEqual(false, instance.Read());
-//			Assert.AreEqual(true, instance.IsClosed);
+			int fetchCount = 0;
+			MockRelativityReaderBase instance = new MockRelativityReaderBase(ExecuteQueryToGetInitialResult, () =>
+			{
+				fetchCount++;
+				return fetchCount > 1;
+			});
+
+			Assert.AreEqual(true, instance.Read());
+			Assert.AreEqual(true, instance.Read());
+			Assert.AreEqual(true, instance.Read());
+			Assert.AreEqual(true, instance.Read());
+			Assert.AreEqual(false, instance.Read());
+			Assert.AreEqual(true, instance.IsClosed);
 		}
 
 		[Test]
 		public void ReadDocumentsUntilTheEnd_FetchMoreDataFail()
 		{
-//			MockRelativityReaderBase instance = new MockRelativityReaderBase(_rdoRepository, ExecuteQueryToGetInitialResult());
-//
-//			var newResult = new QueryResultSet<Document>()
-//			{
-//				Success = false,
-//				Results = null,
-//				Message = "lol",
-//				QueryToken = String.Empty,
-//				TotalCount = 3
-//			};
-//
-//			_rdoRepository.ExecuteSubSetOfDocumentQuery(_TOKEN_FOR_MORE, 2, Shared.Constants.QUERY_BATCH_SIZE).Returns(newResult);
-//
-//			Assert.AreEqual(true, instance.Read());
-//			Assert.AreEqual(true, instance.Read());
-//			Assert.AreEqual(false, instance.Read());
-//			Assert.AreEqual(true, instance.IsClosed);
-//			Assert.AreEqual(2, instance.Counter);
+			int fetchCount = 0;
+			MockRelativityReaderBase instance = new MockRelativityReaderBase(
+				() => fetchCount == 0 ? this.ExecuteQueryToGetInitialResult() : null, 
+				() => true);
+
+			Assert.AreEqual(true, instance.Read());
+			Assert.AreEqual(true, instance.Read());
+			Assert.AreEqual(false, instance.Read());
+			Assert.AreEqual(true, instance.IsClosed);
+			Assert.AreEqual(2, instance.Counter);
 		}
 
-//		private class MockRelativityReaderBase : RelativityReaderBase
-//		{
-//			private readonly QueryResultSet<Document> _initialResult;
-//
-////			public MockRelativityReaderBase(IRelativityClient relativityClient, QueryResultSet<Document> initialResult)
-////				: base(relativityClient, new[] { new DataColumn("Testing") })
-////			{
-////				_initialResult = initialResult;
-////			}
-//
-//			public int Counter { get { return ReadEntriesCount; } }
-//
-//			public override string GetName(int i)
-//			{
-//				throw new NotImplementedException();
-//			}
-//
-//			public override int GetOrdinal(string name)
-//			{
-//				throw new NotImplementedException();
-//			}
-//
-//			public override DataTable GetSchemaTable()
-//			{
-//				throw new NotImplementedException();
-//			}
-//
-//			public override string GetDataTypeName(int i)
-//			{
-//				throw new NotImplementedException();
-//			}
-//
-//			public override Type GetFieldType(int i)
-//			{
-//				throw new NotImplementedException();
-//			}
-//
-//			public override object GetValue(int i)
-//			{
-//				throw new NotImplementedException();
-//			}
-//
-//			protected override QueryResultSet<Document> ExecuteQueryToGetInitialResult()
-//			{
-//				return _initialResult;
-//			}
-//		}
+		private class MockRelativityReaderBase : RelativityReaderBase
+		{
+			private readonly Func<ArtifactDTO[]> _fetchFunction;
+			private readonly Func<bool> _allArtifactsFetchedFunc;
+
+			public MockRelativityReaderBase(Func<ArtifactDTO[]> fetchFunction, Func<bool> allArtifactsFetchedFunc)
+				: base(new[] { new DataColumn("Testing")})
+			{
+				_fetchFunction = fetchFunction;
+				_allArtifactsFetchedFunc = allArtifactsFetchedFunc;
+			}
+
+			public int Counter { get { return ReadEntriesCount; } }
+
+			public override string GetName(int i)
+			{
+				throw new NotImplementedException();
+			}
+
+			public override int GetOrdinal(string name)
+			{
+				throw new NotImplementedException();
+			}
+
+			public override DataTable GetSchemaTable()
+			{
+				throw new NotImplementedException();
+			}
+
+			public override string GetDataTypeName(int i)
+			{
+				throw new NotImplementedException();
+			}
+
+			public override Type GetFieldType(int i)
+			{
+				throw new NotImplementedException();
+			}
+
+			public override object GetValue(int i)
+			{
+				throw new NotImplementedException();
+			}
+
+			protected override ArtifactDTO[] FetchArtifactDTOs()
+			{
+				return _fetchFunction.Invoke();
+			}
+
+			protected override bool AllArtifactsFetched()
+			{
+				return _allArtifactsFetchedFunc.Invoke();
+			}
+		}
 	}
 }
