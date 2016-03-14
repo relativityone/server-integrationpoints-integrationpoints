da6195634f1009c9cfc0e2453e77f778859c3223
Sorawit Amornborvornwong <samornborvornwong@kcura.com>
2016-03-14 16:44:07 -0500

    create const variables for multi value and nested value delimiter
    --HG--
    branch : feature-UrsaMinor

--- source/kCura.IntegrationPoints.Contracts/Constants.cs
+++ source/kCura.IntegrationPoints.Contracts/Constants.cs
@@ -3,5 +3,7 @@
 	public class Constants
 	{
 		public const string SPECIAL_NATIVE_FILE_LOCATION_FIELD = "fb83020a-1853-4a14-8e92-accb6dbd2ef1";
+		public const char MULTI_VALUE_DEIMITER = ';';
+		public const char NESTED_VALUE_DELIMITER = ';';
 	}
 }
