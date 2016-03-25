c0027bf438614ce1c15d9c03989de25cba8616e6
Dan Nelson <dnelson@kcura.com>
2016-03-25 12:06:50 -0500

    Add kepler service to retrieve job history information for eca and investigations fluid widget.
    --HG--
    branch : feature-UrsaMinor

---
+++ source/kCura.IntegrationPoints.Services.Interfaces.Private/Models/JobHistoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace kCura.IntegrationPoints.Services.Interfaces.Private.Models
+{
+	public class JobHistoryModel
+	{
+		public int Documents { get; set; }
+
+		public DateTime Date { get; set; }
+
+		public string WorkspaceName { get; set; }
+	}
+}
