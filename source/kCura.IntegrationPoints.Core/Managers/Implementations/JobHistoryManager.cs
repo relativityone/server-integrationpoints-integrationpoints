767490008439705a28b24225ef6175330d89a98c
Michael Gonnella <mgonnella@kcura.com>
2016-04-07 14:40:01 -0500

    [Z006]: Adding new files to revision
    --HG--
    branch : feature-UrsaMinor

---
+++ source/kCura.IntegrationPoints.Core/Managers/Implementations/JobHistoryManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using kCura.IntegrationPoints.Data;
+using kCura.IntegrationPoints.Data.Factories;
+using kCura.IntegrationPoints.Data.Repositories;
+using kCura.IntegrationPoints.Data.Repositories.Implementations;
+using kCura.Relativity.Client;
+using kCura.ScheduleQueue.Core;
+using Relativity.API;
+
+namespace kCura.IntegrationPoints.Core.Managers.Implementations
+{
+	public class JobHistoryManager : IJobHistoryManager, IBatchStatus
+	{
+		private readonly ITempDocTableHelper _tempDocHelper;
+		private readonly IRSAPIClient _client;
+		private readonly IJobHistoryRepository _jobHistoryRepository;
+		private readonly int _jobHistoryInstanceId;
+		private readonly int _sourceWorkspaceArtifactId;
+		private readonly string _uniqueJobId;
+
+		public JobHistoryManager(IHelper helper, int jobHistoryInstanceId, int sourceWorkspaceArtifactId, string uniqueJobId)
+		{
+			_sourceWorkspaceArtifactId = sourceWorkspaceArtifactId;
+			_client = new RsapiClientFactory(helper).CreateClientForWorkspace(_sourceWorkspaceArtifactId, ExecutionIdentity.System);
+			_jobHistoryInstanceId = jobHistoryInstanceId;
+			_uniqueJobId = uniqueJobId;
+			_tempDocHelper = new TempDocumentFactory().GetDocTableHelper(helper, _uniqueJobId, _sourceWorkspaceArtifactId);
+			_jobHistoryRepository = new JobHistoryRepository();
+		}
+
+		/// <summary>
+		/// For internal unit testing
+		/// </summary>
+		internal JobHistoryManager(ITempDocTableHelper tempDocHelper, IJobHistoryRepository jobHistoryRepository, 
+			int jobHistoryInstanceId, int sourceWorkspaceArtifactId, string uniqueJobId)
+		{
+			_tempDocHelper = tempDocHelper;
+			_jobHistoryRepository = jobHistoryRepository;
+			_jobHistoryInstanceId = jobHistoryInstanceId;
+			_sourceWorkspaceArtifactId = sourceWorkspaceArtifactId;
+			_uniqueJobId = uniqueJobId;	
+		}
+
+		public void JobStarted(Job job) { }
+
+		public void JobComplete(Job job)
+		{
+			List<int> documentIds = _tempDocHelper.GetDocumentIdsFromTable(ScratchTables.JobHistory); 
+			int documentCount = documentIds.Count;
+			if (documentCount == 0)
+			{
+				_tempDocHelper.DeleteTable(ScratchTables.JobHistory); 
+				return;
+			}
+			_jobHistoryRepository.TagDocsWithJobHistory(documentCount, _jobHistoryInstanceId, _sourceWorkspaceArtifactId, _uniqueJobId);
+		}
+	}
+}
---
+++ source/kCura.IntegrationPoints.Data/Repositories/Implementations/JobHistoryRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Security.Claims;
+using Relativity.Core;
+using Relativity.Core.Authentication;
+using Relativity.Core.Process;
+using Relativity.Data;
+using Field = Relativity.Core.DTO.Field;
+
+namespace kCura.IntegrationPoints.Data.Repositories.Implementations
+{
+	public class JobHistoryRepository : IJobHistoryRepository
+	{
+		private static int BATCH_SIZE = 1000;
+
+		public void TagDocsWithJobHistory(int numberOfDocs, int jobHistoryInstanceArtifactId, int sourceWorkspaceId, string tableSuffix)
+		{
+			BaseServiceContext baseService = ClaimsPrincipal.Current.GetServiceContextUnversionShortTerm(sourceWorkspaceId);
+
+			Guid[] guids = { new Guid(DocumentMultiObjectFields.JOB_HISTORY_FIELD)};
+			DataRowCollection fieldRows;
+			try
+			{
+				fieldRows = FieldQuery.RetrieveAllByGuids(baseService.ChicagoContext.DBContext, guids).Table.Rows;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Unable to query for multi-object field on Document associated with JobHistory object.", ex);
+			}
+
+			if (fieldRows.Count == 0)
+			{
+				throw new Exception("Multi-object field on Document associated with JobHistory object does not exist.");
+			}
+
+			Field multiObjectField = new Field(baseService, fieldRows[0]);
+			multiObjectField.Value = GetMultiObjectListUpdate(jobHistoryInstanceArtifactId);
+			var document = new global::Relativity.Query.ArtifactType(10, "Document");
+
+			MassProcessHelper.MassProcessInitArgs initArgs = new MassProcessHelper.MassProcessInitArgs(Constants.TEMPORARY_DOC_TABLE_JOB_HIST + "_" + tableSuffix, numberOfDocs, false);
+			SqlMassProcessBatch batch = new SqlMassProcessBatch(baseService, initArgs, BATCH_SIZE);
+
+			Field[] fields =
+			{
+				multiObjectField
+			};
+
+			Edit massEdit = new Edit(baseService, batch, fields, BATCH_SIZE, String.Empty, true, true, false, document);
+			try
+			{
+				massEdit.Execute(true);
+			}
+			catch (Exception e)
+			{
+				throw new Exception("Tagging Documents with JobHistory object failed - Mass Edit failure", e);
+			}
+		}
+
+		internal MultiObjectListUpdate GetMultiObjectListUpdate(int destinationWorkspaceInstanceId)
+		{
+			var objectstoUpdate = new MultiObjectListUpdate();
+			var instances = new List<int>()
+			{
+				destinationWorkspaceInstanceId
+			};
+
+			objectstoUpdate.tristate = true;
+			objectstoUpdate.Selected = instances;
+
+			return objectstoUpdate;
+		}
+	}
+}
