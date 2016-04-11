1c830b23c9d3b0bc565b6dd2abd318a713cccd97
Sorawit Amornborvornwong <samornborvornwong@kcura.com>
2016-04-11 13:35:52 -0500

    Add more unit test coverage in JobHistoryManagerTest
    --HG--
    branch : feature-UrsaMinor

--- source/kCura.IntegrationPoints.Core.Tests/Unit/BatchStatusCommands/JobHistoryManagerTest.cs
+++ source/kCura.IntegrationPoints.Core.Tests/Unit/BatchStatusCommands/JobHistoryManagerTest.cs
@@ -14,7 +14,7 @@ namespace kCura.IntegrationPoints.Core.Tests.Unit
 		private ITempDocTableHelper _tempDocHelper;
 		private ITempDocumentTableFactory _docTableFactory;
 		private IRepositoryFactory _repositoryFactory;
-		private IBatchStatus _instance;
+		private IConsumeScratchTableBatchStatus _instance;
 		private IJobHistoryRepository _jobHistoryRepository;
 		private readonly int _jobHistoryRdoId = 12345;
 		private readonly string _uniqueJobId = "12-25-96";
@@ -58,6 +58,32 @@ namespace kCura.IntegrationPoints.Core.Tests.Unit
 			//Assert
 			_tempDocHelper.Received().DeleteTable(Arg.Is(Data.Constants.TEMPORARY_DOC_TABLE_JOB_HIST));
 			_jobHistoryRepository.Received().TagDocsWithJobHistory(Arg.Any<int>(), _jobHistoryRdoId, _sourceWorkspaceId, _uniqueJobId);
+		}
+
+		[Test]
+		public void GetStratchTableRepo_AlwaysGivesTheSameObject()
+		{
+			//Act
+			IScratchTableRepository repository = _instance.ScratchTableRepository;
+			IScratchTableRepository repository2 = _instance.ScratchTableRepository;
+
+			//Assert
+			Assert.AreSame(repository, repository2);
+		}
+
+		[Test]
+		public void GetStratchTableRepo_UsingTheCorrectPrefix()
+		{
+			//Arrange
+			string expected = Data.Constants.TEMPORARY_DOC_TABLE_JOB_HIST;
+			_tempDocHelper.GetTempTableName(Data.Constants.TEMPORARY_DOC_TABLE_JOB_HIST).Returns(Data.Constants.TEMPORARY_DOC_TABLE_JOB_HIST);
+
+			//Act
+			IScratchTableRepository repository = _instance.ScratchTableRepository;
+			string name = repository.GetTempTableName();
+
+			//Assert
+			Assert.AreSame(expected, name);
 		}
 	}
 }
