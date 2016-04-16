0eee0a7e9a57e4708b88a8a56a76551ed5364f03
Billy Bafia <bbafia@kcura.com>
2016-04-16 16:52:43 -0500

    [Z012] Updated cleanup logic for scheduled jobs in the queue.  SQL query was incorrectly nullifying the locked agent because of the LockedByAgentID NOT IN portion had incorrect ids.
    --HG--
    branch : feature-UrsaMinor

--- source/kCura.ScheduleQueue.Core/Data/Queries/CleanupJobQueueTable.cs
+++ source/kCura.ScheduleQueue.Core/Data/Queries/CleanupJobQueueTable.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using kCura.Relativity.Client;
 using kCura.ScheduleQueue.Core.Properties;
 
 namespace kCura.ScheduleQueue.Core.Data.Queries
 {
 	public class CleanupJobQueueTable
 	{
+		private const string _RELATIVITY_INTEGRATION_POINTS_AGENT_GUID = "08C0CE2D-8191-4E8F-B037-899CEAEE493D";
+
 		private readonly IQueueDBContext _context;
 
 		public CleanupJobQueueTable(IQueueDBContext context)
@@ -14,8 +19,15 @@ namespace kCura.ScheduleQueue.Core.Data.Queries
 
 		public void Execute()
 		{
+			var agentGuidParameter = new SqlParameter("@agentGuid", SqlDbType.NVarChar)
+			{
+				Value = _RELATIVITY_INTEGRATION_POINTS_AGENT_GUID
+			};
+			var sqlParameters = new[] {agentGuidParameter};
+
 			string sql = String.Format(Resources.CleanupJobQueueTable, _context.TableName);
-			_context.EddsDBContext.ExecuteNonQuerySQLStatement(sql);
+
+			_context.EddsDBContext.ExecuteNonQuerySQLStatement(sql, sqlParameters);
 		}
 	}
 }
--- source/kCura.ScheduleQueue.Core/Data/Resources/CleanupJobQueueTable.sql
+++ source/kCura.ScheduleQueue.Core/Data/Resources/CleanupJobQueueTable.sql
@@ -1,12 +1,19 @@
 --If an agent was deleted while a job was running and not completed, we remove the agent lock
 --from the job so that another agent can pick it up.
 
-UPDATE [eddsdbo].[{0}] WITH(UPDLOCK, ROWLOCK, READPAST)
+DECLARE @agentArtifactIds TABLE(ArtifactId int)
+
+INSERT INTO @agentArtifactIds
+SELECT A.[ArtifactID]
+FROM [eddsdbo].[Agent] as A WITH(NOLOCK)
+INNER JOIN [eddsdbo].[AgentType] as AT WITH(NOLOCK)
+ON A.[AgentTypeArtifactID] = AT.[ArtifactID]
+WHERE AT.[Guid] = @agentGuid
+
+UPDATE [eddsdbo].[{0}] WITH(UPDLOCK, READPAST, ROWLOCK)
 SET [LockedByAgentID] = NULL
-WHERE JobID IN (
-	SELECT SAQ.[JobID]
-	FROM [eddsdbo].[{0}] as SAQ WITH(UPDLOCK, ROWLOCK, READPAST)
-	LEFT JOIN [eddsdbo].[Agent] as A WITH(NOLOCK)
-	ON SAQ.[AgentTypeID] = A.[AgentTypeArtifactID]
-	WHERE SAQ.[LockedByAgentID] NOT NULL AND SAQ.[LockedByAgentID] NOT IN (A.ArtifactID)
+FROM [eddsdbo].[{0}] as SAQ WITH(UPDLOCK, READPAST, ROWLOCK)
+WHERE SAQ.[LockedByAgentID] IS NOT NULL AND SAQ.[LockedByAgentID] NOT IN (
+	SELECT *
+	FROM @agentArtifactIds
 )
