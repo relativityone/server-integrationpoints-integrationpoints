539eb52e17196a26dc3fdb531d3f3d64947b7ac9
Sorawit Amornborvornwong <samornborvornwong@kcura.com>
2016-05-04 12:43:03 -0500

    make copy script to copy only dlls and pdb pt2
    --HG--
    branch : feature-UrsaMinor

--- source/DevelopmentScripts/package.master.bat
+++ source/DevelopmentScripts/package.master.bat
@@ -10,7 +10,7 @@ mkdir "%revisionDirectory%\dlls"
 mkdir "%revisionDirectory%\pdbs"
 
 echo f | xcopy "%basePath%\bin\Application\*" "%revisionDirectory%\Application\" /EY /EXCLUDE:packageCreationExcludeList.txt
-echo f | xcopy "%basePath%\bin\Release\*.dll" "%revisionDirectory%\dlls" /EY /EXCLUDE:packageCreationExcludeList.txt
+echo f | xcopy "%basePath%\bin\*.dll" "%revisionDirectory%\dlls" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\bin\*.pdb" "%revisionDirectory%\pdbs" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\publish\bin\web\Release\*" "%revisionDirectory%\web\" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\nugspec\*.nupkg" "%revisionDirectory%\%buildType% %version% NuGet Packages - INTERNAL USE ONLY" /E /I
