e07d775c4a95ac0336f70a5f682b1dc48ce78ac4
Sorawit Amornborvornwong <samornborvornwong@kcura.com>
2016-05-04 11:43:12 -0500

    update copying dlls and pdb into seperate directories
    --HG--
    branch : feature-UrsaMinor

--- source/DevelopmentScripts/package.master.bat
+++ source/DevelopmentScripts/package.master.bat
@@ -1,13 +1,15 @@
 @echo off
 
 echo PACKAGEMASTER: BEGIN
- 
+
 echo PACKAGEMASTER: Calling package scripts
+
 mkdir "%revisionDirectory%"
 mkdir "%revisionDirectory%\web"
 
 echo f | xcopy "%basePath%\bin\Application\*" "%revisionDirectory%\Application\" /EY /EXCLUDE:packageCreationExcludeList.txt
-echo f | xcopy "%basePath%\bin\Release\*" "%revisionDirectory%\" /EY /EXCLUDE:packageCreationExcludeList.txt
+echo f | xcopy "%basePath%\bin\Release\*.dll;*.xml*" "%revisionDirectory%\dlls" /EY /EXCLUDE:packageCreationExcludeList.txt
+echo f | xcopy "%basePath%\bin\*.pdb" "%revisionDirectory%\pdbs" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\publish\bin\web\Release\*" "%revisionDirectory%\web\" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\nugspec\*.nupkg" "%revisionDirectory%\%buildType% %version% NuGet Packages - INTERNAL USE ONLY" /E /I
 
