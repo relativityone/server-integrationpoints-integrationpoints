8553b396d2a92cdb81002919cf59443a6934d4d4
Sorawit Amornborvornwong <samornborvornwong@kcura.com>
2016-05-04 11:55:21 -0500

    add mkdir dlls and pdb to make sure that the folder exist before copying the files
    --HG--
    branch : feature-UrsaMinor

--- source/DevelopmentScripts/package.master.bat
+++ source/DevelopmentScripts/package.master.bat
@@ -6,6 +6,8 @@ echo PACKAGEMASTER: Calling package scripts
 
 mkdir "%revisionDirectory%"
 mkdir "%revisionDirectory%\web"
+mkdir "%revisionDirectory%\dlls"
+mkdir "%revisionDirectory%\pdbs"
 
 echo f | xcopy "%basePath%\bin\Application\*" "%revisionDirectory%\Application\" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\bin\Release\*.dll;*.xml*" "%revisionDirectory%\dlls" /EY /EXCLUDE:packageCreationExcludeList.txt
