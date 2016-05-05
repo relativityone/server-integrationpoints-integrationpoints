775749f65a79992dd292587d6752c4bbe8eaa650
Michael Gonnella <mgonnella@kcura.com>
2016-05-04 21:54:36 -0500

    [Z030]:Add logic/unit tests that query the ScheduleAgentQueue table, they are not utilized yet.
    --HG--
    branch : feature-UrsaMinor

---
+++ source/kCura.IntegrationPoints.Data/Repositories/Implementations/QueueRepository.cs
@@ -0,0 +1,46 @@
+﻿using Relativity.API;
+
+namespace kCura.IntegrationPoints.Data.Repositories.Implementations
+{
+	public class QueueRepository : IQueueRepository
+	{
+		private readonly IDBContext _dbContext;
+
+		public QueueRepository(IDBContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public bool HasJobsExecutingOrInQueue(int workspaceId, int integrationPointId)
+		{
+			string sql = $@"SELECT count(*) FROM [ScheduleAgentQueue_08C0CE2D-8191-4E8F-B037-899CEAEE493D]
+										WHERE [WorkspaceID] = {workspaceId} AND
+										[RelatedObjectArtifactID] = {integrationPointId} AND [ScheduleRuleType] is null";
+
+			int numberOfJobs = _dbContext.ExecuteSqlStatementAsScalar<int>(sql);
+
+			if (numberOfJobs > 0)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool HasJobsExecuting(int workspaceId, int integrationPointId)
+		{
+			string sql = $@"SELECT count(*) FROM [ScheduleAgentQueue_08C0CE2D-8191-4E8F-B037-899CEAEE493D]
+										WHERE [WorkspaceID] = {workspaceId} AND
+										[RelatedObjectArtifactID] = {integrationPointId} AND [LockedByAgentID] is not null";
+
+			int numberOfJobs = _dbContext.ExecuteSqlStatementAsScalar<int>(sql);
+
+			if (numberOfJobs > 0)
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
