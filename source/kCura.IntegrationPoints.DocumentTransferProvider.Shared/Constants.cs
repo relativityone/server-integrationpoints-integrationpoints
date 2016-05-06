00b50c5e743bb45921fe433eee3bfa2614fc6113
Dan Nelson <dnelson@kcura.com>
2016-05-06 14:43:54 -0500

    Just make this public for now so the build passes.
    --HG--
    branch : feature-UrsaMinor

--- source/kCura.IntegrationPoints.DocumentTransferProvider.Shared/Constants.cs
+++ source/kCura.IntegrationPoints.DocumentTransferProvider.Shared/Constants.cs
@@ -9,7 +9,7 @@
 		internal const string RELATIVITY_CUSTOMPAGE_GUID = "dcf6e9d1-22b6-4da3-98f6-41381e93c30c";
 		internal const string RELATIVITY_PROVIDER_CONFIGURATION = "RelativityProviderConfiguration";
 		public const string RELATIVITY_PROVIDER_GUID = "423b4d43-eae9-4e14-b767-17d629de4bb2";
-		internal const string RELATIVITY_PROVIDER_NAME = "Relativity";
+		public const string RELATIVITY_PROVIDER_NAME = "Relativity";
 		internal const string RELATIVITY_PROVIDER_VIEW = "RelativityProvider";
 		internal const string SAVED_SEARCH_ID = "Saved Search ID";
 		internal const string TARGET_WORKSPACE_ID = "Target Workspace ID";
