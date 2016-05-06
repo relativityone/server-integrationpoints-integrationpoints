ded1c7f69c9971dfeb8d3157bf6df2f46c4ec338
Dan Nelson <dnelson@kcura.com>
2016-05-06 14:48:54 -0500

    I hate our internals visible to usage. Make all the things public.
    --HG--
    branch : feature-UrsaMinor

--- source/kCura.IntegrationPoints.DocumentTransferProvider.Shared/Constants.cs
+++ source/kCura.IntegrationPoints.DocumentTransferProvider.Shared/Constants.cs
@@ -2,28 +2,28 @@
 {
 	public class Constants
 	{
-		internal const string ARTIFACT_ID_FIELD_NAME = "ArtifactId";
-		internal const string INVALID_PARAMETERS = "Invalid parameters";
-		internal const string OBJECT_IDENTIFIER_APPENDAGE_TEXT = " [Object Identifier]";
-		internal const int QUERY_BATCH_SIZE = 10000;
-		internal const string RELATIVITY_CUSTOMPAGE_GUID = "dcf6e9d1-22b6-4da3-98f6-41381e93c30c";
-		internal const string RELATIVITY_PROVIDER_CONFIGURATION = "RelativityProviderConfiguration";
+		public const string ARTIFACT_ID_FIELD_NAME = "ArtifactId";
+		public const string INVALID_PARAMETERS = "Invalid parameters";
+		public const string OBJECT_IDENTIFIER_APPENDAGE_TEXT = " [Object Identifier]";
+		public const int QUERY_BATCH_SIZE = 10000;
+		public const string RELATIVITY_CUSTOMPAGE_GUID = "dcf6e9d1-22b6-4da3-98f6-41381e93c30c";
+		public const string RELATIVITY_PROVIDER_CONFIGURATION = "RelativityProviderConfiguration";
 		public const string RELATIVITY_PROVIDER_GUID = "423b4d43-eae9-4e14-b767-17d629de4bb2";
 		public const string RELATIVITY_PROVIDER_NAME = "Relativity";
-		internal const string RELATIVITY_PROVIDER_VIEW = "RelativityProvider";
-		internal const string SAVED_SEARCH_ID = "Saved Search ID";
-		internal const string TARGET_WORKSPACE_ID = "Target Workspace ID";
-		internal const string DOC_OBJ_GUID = "15C36703-74EA-4FF8-9DFB-AD30ECE7530D";
+		public const string RELATIVITY_PROVIDER_VIEW = "RelativityProvider";
+		public const string SAVED_SEARCH_ID = "Saved Search ID";
+		public const string TARGET_WORKSPACE_ID = "Target Workspace ID";
+		public const string DOC_OBJ_GUID = "15C36703-74EA-4FF8-9DFB-AD30ECE7530D";
 
-		internal static class Fields
+		public static class Fields
 		{
-			internal static string Name = "Name";
-			internal static string Choices = "Choices";
-			internal static string ObjectTypeArtifactTypeId = "Object Type Artifact Type ID";
-			internal static string FieldType = "Field Type";
-			internal static string FieldTypeId = "Field Type ID";
-			internal static string FieldTypeName = "Field Type Name";
-			internal static string IsIdentifier = "Is Identifier";
+			public static string Name = "Name";
+			public static string Choices = "Choices";
+			public static string ObjectTypeArtifactTypeId = "Object Type Artifact Type ID";
+			public static string FieldType = "Field Type";
+			public static string FieldTypeId = "Field Type ID";
+			public static string FieldTypeName = "Field Type Name";
+			public static string IsIdentifier = "Is Identifier";
 		}
 	}
 }
