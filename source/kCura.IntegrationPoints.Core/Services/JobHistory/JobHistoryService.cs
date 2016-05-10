b529165dd4c7f2715e940d5d133f198b0e522dcc
Michael Gonnella <mgonnella@kcura.com>
2016-05-10 13:27:50 -0500

    fix merge error

--- source/kCura.IntegrationPoints.Core/Services/JobHistory/JobHistoryService.cs
+++ source/kCura.IntegrationPoints.Core/Services/JobHistory/JobHistoryService.cs
@@ -172,16 +172,10 @@ namespace kCura.IntegrationPoints.Core.Services
 			_caseServiceContext.RsapiService.JobHistoryLibrary.Delete(jobHistoryId);
 		}
 
-		protected List<FieldValue> GetFields()
+		protected List<FieldValue> GetFields<T>()
 		{
 			return (from field in (BaseRdo.GetFieldMetadata(typeof(Data.JobHistory)).Values).ToList()
 					select new FieldValue(field.FieldGuid)).ToList();
-		}
-
-		private List<FieldValue> GetJobHistoryErrorFields()
-		{
-			return (from field in (BaseRdo.GetFieldMetadata(typeof (T)).Values).ToList()
-				select new FieldValue(field.FieldGuid)).ToList();
 		}
 	}
 }
