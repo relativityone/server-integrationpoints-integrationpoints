1817d93548878a4ebf507a11d367616330872985
Artur Mitianiec <amitianiec@kcura.com>
2016-05-12 08:19:18 -0500

    KRK-694 [QA] [automated] Create functional test

---
+++ source/kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration/Process/ExportProcessRunnerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.IntegrationPoint.Tests.Core;
+using kCura.IntegrationPoints.FilesDestinationProvider.Core;
+using kCura.IntegrationPoints.FilesDestinationProvider.Core.Process;
+using kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration.Helpers;
+using NUnit.Framework;
+
+namespace kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration.Process
+{
+
+	public class ExportProcessRunnerTest
+	{
+		private ExportProcessRunner _objectToTest;
+		private ExportSettings _settings;
+		private WorkspaceService _service;
+
+		[SetUp]
+		public void Init()
+		{
+			
+			_objectToTest = new ExportProcessRunner();
+			_service = new WorkspaceService(new Helper());
+			_settings = DefaultExportConfigLoader.Create();
+		}
+
+		[Test]
+		[Ignore("Integration Test")]
+		public void it_should_export_saved_search()
+		{
+			var dt = _service.GetDocumentDataTable();
+			_service.ImportDocument(1016890, dt.CreateDataReader());
+			//var obj = _service.GetWorkspaceId("Test1");
+			//_objectToTest.StartWith(_settings);
+
+		}
+	}
+}
