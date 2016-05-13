4e29e2346138a120da7a6a88ce4e61301a7dce9a
Bryan Boyer <bboyer@kcura.com>
2016-05-12 20:04:26 -0500

    A025 - Update Error Status work, added a lot of logic, but am still missing something that is keeping mass edit from working

--- source/kCura.IntegrationPoints.Core/Managers/Implementations/JobHistoryErrorManager.cs
+++ source/kCura.IntegrationPoints.Core/Managers/Implementations/JobHistoryErrorManager.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using kCura.IntegrationPoints.Contracts.Models;
 using kCura.IntegrationPoints.Data;
 using kCura.IntegrationPoints.Data.Factories;
 using kCura.IntegrationPoints.Data.Repositories;
+using kCura.ScheduleQueue.Core;
 
 namespace kCura.IntegrationPoints.Core.Managers.Implementations
 {
@@ -14,20 +17,26 @@ namespace kCura.IntegrationPoints.Core.Managers.Implementations
 			_repositoryFactory = repositoryFactory;
 		}
 
-		/// <summary>
-		/// Necessary for JobHistoryErrorManager for Batch Status changes
-		/// </summary>
-		internal JobHistoryErrorManager()
+		public JobHistoryErrorDTO.UpdateStatusType StageForUpdatingErrors(Job job, Relativity.Client.Choice jobType, string uniqueJobId)
 		{
+			IJobHistoryErrorRepository jobHistoryErrorRepository = _repositoryFactory.GetJobHistoryErrorRepository(job.WorkspaceID);
+			List<int> jobLevelErrors = GetLastJobHistoryErrorArtifactIds(job.WorkspaceID, job.RelatedObjectArtifactID, ErrorTypeChoices.JobHistoryErrorJob);
+			List<int> itemLevelErrors = GetLastJobHistoryErrorArtifactIds(job.WorkspaceID, job.RelatedObjectArtifactID, ErrorTypeChoices.JobHistoryErrorItem);
+
+			JobHistoryErrorDTO.UpdateStatusType updateStatusType = jobHistoryErrorRepository.DetermineUpdateStatusType(jobType, jobLevelErrors.Any(), itemLevelErrors.Any());
+
+			jobHistoryErrorRepository.CreateErrorListTempTables(jobLevelErrors, itemLevelErrors, updateStatusType, uniqueJobId);
+
+			return updateStatusType;
 		}
 
-		public List<JobHistoryError> GetLastJobHistoryErrors(int workspaceArtifactId, int integrationPointArtifactId)
+		private List<int> GetLastJobHistoryErrorArtifactIds(int workspaceArtifactId, int integrationPointArtifactId, Relativity.Client.Choice errorType)
 		{
 			IJobHistoryRepository jobHistoryRepository = _repositoryFactory.GetJobHistoryRepository(workspaceArtifactId);
 			IJobHistoryErrorRepository jobHistoryErrorRepository = _repositoryFactory.GetJobHistoryErrorRepository(workspaceArtifactId);
-			int jobHistoryArtifactId = jobHistoryRepository.GetLastJobHistoryArtifactId(integrationPointArtifactId);
+			int jobHistoryArtifactId = jobHistoryRepository.GetLastTwoJobHistoryArtifactId(integrationPointArtifactId)[1]; //Grab the second in this list as the new job is the first entry
 
-			return jobHistoryErrorRepository.RetreiveJobHistoryErrors(jobHistoryArtifactId);
+			return jobHistoryErrorRepository.RetreiveJobHistoryErrorArtifactIds(jobHistoryArtifactId, errorType);
 		}
 	}
 }
