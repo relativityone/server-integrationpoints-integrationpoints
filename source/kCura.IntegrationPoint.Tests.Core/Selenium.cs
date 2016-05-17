f47e59edfb611878572f322fce1d36f128a3579b
Gerron Thurman <gthurman@kcura.com>
2016-05-17 14:01:14 -0500

    Adding viewerrors tests

---
+++ source/kCura.IntegrationPoint.Tests.Core/Selenium.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kCura.IntegrationPoint.Tests.Core
+{
+	public static class Selenium
+	{
+		public static void GoToUrl()
+		{
+			//IWebDriver driver = new ChromeDriver();
+
+		}
+
+	}
+}
---
+++ source/kCura.IntegrationPoints.EventHandlers.Tests/Integration/ViewErrors.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using kCura.IntegrationPoints.Services;
+using kCura.IntegrationPoint.Tests.Core;
+using kCura.IntegrationPoint.Tests.Core.Models;
+using kCura.IntegrationPoint.Tests.Core.Templates;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace kCura.IntegrationPoints.EventHandlers.Tests.Integration
+{
+	public class ViewErrors : WorkspaceDependentTemplate
+	{
+		public ViewErrors() : base("ViewErrorsSource", "ViewErrorsSourceDestination")
+		{
+		}
+
+		[Test]
+		[Explicit]
+		public void ExpectDisabledViewErrorsLinkOnIntegrationPointCreation()
+		{
+			//Arrange
+			UserModel user = new UserModel
+			{
+				EmailAddress = SharedVariables.RelativityUserName,
+				Password = SharedVariables.RelativityPassword
+			};
+
+			string response = kCura.IntegrationPoint.Tests.Core.IntegrationPoint.CreateIntegrationPoint(Guid.NewGuid().ToString(),
+				SourceWorkspaceArtifactId,
+				TargetWorkspaceArtifactId,
+				1039795,
+				FieldOverlayBehavior.UseFieldSettings,
+				ImportOverwriteMode.AppendOverlay,
+				false,
+				false,
+				user);
+
+			IntegrationPointModel integrationPoint = JsonConvert.DeserializeObject<IntegrationPointModel>(response);
+
+			//Act
+			bool ranIntegrationPoint = kCura.IntegrationPoint.Tests.Core.IntegrationPoint.RunIntegrationPoint(SourceWorkspaceArtifactId, integrationPoint.ArtifactId, user);
+
+			//Assert
+			Assert.IsTrue(ranIntegrationPoint);
+
+
+		}
+	}
+}
