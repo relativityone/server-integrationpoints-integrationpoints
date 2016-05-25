21a826e18e557608632484c17d5f16c660578eb8
David T Biedrzycki <dbiedrzycki@kcura.com>
2016-05-25 18:34:44 -0500

    Fixed incorrect batchin in JobHistoryErrorManager.CreateErrorListTempTablesForItemLevelErrors

--- source/kCura.IntegrationPoints.Core.Tests/Unit/Managers/JobHistoryErrorManagerTests.cs
+++ source/kCura.IntegrationPoints.Core.Tests/Unit/Managers/JobHistoryErrorManagerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using kCura.IntegrationPoints.Contracts.Models;
 using kCura.IntegrationPoints.Core.Managers;
 using kCura.IntegrationPoints.Core.Managers.Implementations;
@@ -7,6 +8,7 @@ using kCura.IntegrationPoints.Data.Factories;
 using kCura.IntegrationPoints.Data.Repositories;
 using kCura.ScheduleQueue.Core;
 using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 using NUnit.Framework;
 
 namespace kCura.IntegrationPoints.Core.Tests.Unit.Managers
@@ -20,6 +22,11 @@ namespace kCura.IntegrationPoints.Core.Tests.Unit.Managers
 		private IJobHistoryErrorRepository _jobHistoryErrorRepository;
 		private IJobHistoryRepository _jobHistoryRepository;
 		private ISavedSearchRepository _savedSearchRepository;
+		private IScratchTableRepository _jobHistoryErrorJobStart;
+		private IScratchTableRepository _jobHistoryErrorJobComplete;
+		private IScratchTableRepository _jobHistoryErrorItemStart;
+		private IScratchTableRepository _jobHistoryErrorItemComplete;
+		private IScratchTableRepository _jobHistoryErrorItemStartOther;
 		
 		private const int _workspaceArtifactId = 102448;
 		private const int _integrationPointArtifactId = 4651358;
@@ -43,11 +50,22 @@ namespace kCura.IntegrationPoints.Core.Tests.Unit.Managers
 			_jobHistoryErrorRepository = Substitute.For<IJobHistoryErrorRepository>();
 			_jobHistoryRepository = Substitute.For<IJobHistoryRepository>();
 			_savedSearchRepository = Substitute.For<ISavedSearchRepository>();
-			_testInstance = new JobHistoryErrorManager(_repositoryFactory, _workspaceArtifactId, _uniqueJobId);
+			_jobHistoryErrorJobStart = Substitute.For<IScratchTableRepository>();
+			_jobHistoryErrorJobComplete = Substitute.For<IScratchTableRepository>();
+			_jobHistoryErrorItemStart = Substitute.For<IScratchTableRepository>();
+			_jobHistoryErrorItemComplete = Substitute.For<IScratchTableRepository>();
+			_jobHistoryErrorItemStartOther = Substitute.For<IScratchTableRepository>();
 
 			_repositoryFactory.GetJobHistoryErrorRepository(_workspaceArtifactId).Returns(_jobHistoryErrorRepository);
 			_repositoryFactory.GetJobHistoryRepository(_workspaceArtifactId).Returns(_jobHistoryRepository);
-			
+			_repositoryFactory.GetScratchTableRepository(_workspaceArtifactId, Data.Constants.TEMPORARY_JOB_HISTORY_ERROR_TABLE_JOB_START, _uniqueJobId).Returns(_jobHistoryErrorJobStart);
+			_repositoryFactory.GetScratchTableRepository(_workspaceArtifactId, Data.Constants.TEMPORARY_JOB_HISTORY_ERROR_TABLE_JOB_COMPLETE, _uniqueJobId).Returns(_jobHistoryErrorJobComplete);
+			_repositoryFactory.GetScratchTableRepository(_workspaceArtifactId, Data.Constants.TEMPORARY_JOB_HISTORY_ERROR_TABLE_ITEM_START, _uniqueJobId).Returns(_jobHistoryErrorItemStart);
+			_repositoryFactory.GetScratchTableRepository(_workspaceArtifactId, Data.Constants.TEMPORARY_JOB_HISTORY_ERROR_TABLE_ITEM_COMPLETE, _uniqueJobId).Returns(_jobHistoryErrorItemComplete);
+			_repositoryFactory.GetScratchTableRepository(_workspaceArtifactId, Data.Constants.TEMPORARY_JOB_HISTORY_ERROR_TABLE_ITEM_START_OTHER, _uniqueJobId).Returns(_jobHistoryErrorItemStartOther);
+
+			_testInstance = new JobHistoryErrorManager(_repositoryFactory, _workspaceArtifactId, _uniqueJobId);
+
 			_job = new Job(_workspaceArtifactId, _integrationPointArtifactId, _submittedByArtifactId);
 		}
 
@@ -102,7 +120,7 @@ namespace kCura.IntegrationPoints.Core.Tests.Unit.Managers
 			//Arrange
 			_jobHistoryErrorRepository.RetrieveJobHistoryErrorArtifactIds(0, ErrorTypeChoices.JobHistoryErrorJob).Returns(new List<int>());
 			_jobHistoryErrorRepository.RetrieveJobHistoryErrorArtifactIds(0, ErrorTypeChoices.JobHistoryErrorItem).Returns(_sampleItemErrors);
-
+			
 			//Act
 			_testInstance.StageForUpdatingErrors(_job, JobTypeChoices.JobHistoryRunNow);
 
@@ -295,6 +313,171 @@ namespace kCura.IntegrationPoints.Core.Tests.Unit.Managers
 			_scratchTableRepository.Received(1).AddArtifactIdsIntoTempTable(Arg.Is<List<int>>(x => x.Count == 1 && x[0] == error1));
 			_scratchTableRepository.Received(1).AddArtifactIdsIntoTempTable(Arg.Is<List<int>>(x => x.Count == 1 && x[0] == error2));
 
+		}
+
+
+		[Test]
+		public void CreateErrorListTempTablesForItemLevelErrors_MultipleBatches_NoExpiredItemLevelErrors()
+		{
+			// Arrange
+			const int lastJobHistoryId = 2322133;
+			const int savedSearchId = 2321393;
+			const int artifactTypeId = 10;
+			const int documentId1 = 100501;
+			const int documentId2 = 100502;
+			const int error1 = 424324;
+			const int error2 = 234234;
+			const string controlNumber1 = "REL0000000179.0001";
+			const string controlNumber2 = "REL0000000179.0002";
+			var itemLevelErrorsAndSourceUniqueIds = new Dictionary<int, string>()
+			{
+				{error1, controlNumber1},
+				{error2, controlNumber2}
+			};
+
+			var artifactDtoBatchOne = new ArtifactDTO[]
+			{
+				new ArtifactDTO(documentId1, artifactTypeId, controlNumber1, new ArtifactFieldDTO[0]),
+			};
+
+			var artifactDtoBatchTwo = new ArtifactDTO[]
+			{
+				new ArtifactDTO(documentId2, artifactTypeId, controlNumber2, new ArtifactFieldDTO[0]),
+			};
+
+			_jobHistoryRepository.GetLastJobHistoryArtifactId(_integrationPointArtifactId).Returns(lastJobHistoryId);
+			_jobHistoryErrorRepository.RetrieveJobHistoryErrorIdsAndSourceUniqueIds(lastJobHistoryId, ErrorTypeChoices.JobHistoryErrorItem).Returns(itemLevelErrorsAndSourceUniqueIds);
+			_repositoryFactory.GetSavedSearchRepository(_workspaceArtifactId, savedSearchId).Returns(_savedSearchRepository);
+			_savedSearchRepository.AllDocumentsRetrieved().Returns(false, false, true);
+			_savedSearchRepository.RetrieveNextDocuments().Returns(x => artifactDtoBatchOne, x => artifactDtoBatchTwo, x => new ArtifactDTO[0]);
+
+			// Act
+			_testInstance.CreateErrorListTempTablesForItemLevelErrors(_job, savedSearchId);
+
+			// Assert
+			_repositoryFactory.Received(1).GetJobHistoryErrorRepository(_workspaceArtifactId);
+			_repositoryFactory.Received(1).GetJobHistoryRepository(_workspaceArtifactId);
+			_jobHistoryRepository.Received(1).GetLastJobHistoryArtifactId(_integrationPointArtifactId);
+			_jobHistoryErrorRepository.Received(1).RetrieveJobHistoryErrorIdsAndSourceUniqueIds(lastJobHistoryId, ErrorTypeChoices.JobHistoryErrorItem);
+			_repositoryFactory.Received(1).GetSavedSearchRepository(_workspaceArtifactId, savedSearchId);
+			_savedSearchRepository.Received(3).AllDocumentsRetrieved();
+			_savedSearchRepository.Received(2).RetrieveNextDocuments();
+
+			_jobHistoryErrorItemStart.Received(1)
+				.AddArtifactIdsIntoTempTable(Arg.Is<IList<int>>(y => itemLevelErrorsAndSourceUniqueIds.Keys.SequenceEqual(y)));
+			_jobHistoryErrorItemComplete.Received(1)
+				.AddArtifactIdsIntoTempTable(Arg.Is<IList<int>>(y => itemLevelErrorsAndSourceUniqueIds.Keys.SequenceEqual(y)));
+			_jobHistoryErrorItemStartOther.DidNotReceiveWithAnyArgs().AddArtifactIdsIntoTempTable(null);
+		}
+
+		[Test]
+		public void CreateErrorListTempTablesForItemLevelErrors_MultipleBatches_AllExpiredItemLevelErrors()
+		{
+			// Arrange
+			const int lastJobHistoryId = 2322133;
+			const int savedSearchId = 2321393;
+			const int artifactTypeId = 10;
+			const int documentId1 = 100501;
+			const int documentId2 = 100502;
+			const int error1 = 424324;
+			const int error2 = 234234;
+			const string controlNumber1 = "REL0000000179.0001";
+			const string controlNumber2 = "REL0000000179.0002";
+			var itemLevelErrorsAndSourceUniqueIds = new Dictionary<int, string>()
+			{
+				{error1, "NEVER"},
+				{error2, "GUNNA"}
+			};
+
+			var artifactDtoBatchOne = new ArtifactDTO[]
+			{
+				new ArtifactDTO(documentId1, artifactTypeId, controlNumber1, new ArtifactFieldDTO[0]),
+			};
+
+			var artifactDtoBatchTwo = new ArtifactDTO[]
+			{
+				new ArtifactDTO(documentId2, artifactTypeId, controlNumber2, new ArtifactFieldDTO[0]),
+			};
+
+			_jobHistoryRepository.GetLastJobHistoryArtifactId(_integrationPointArtifactId).Returns(lastJobHistoryId);
+			_jobHistoryErrorRepository.RetrieveJobHistoryErrorIdsAndSourceUniqueIds(lastJobHistoryId, ErrorTypeChoices.JobHistoryErrorItem).Returns(itemLevelErrorsAndSourceUniqueIds);
+			_repositoryFactory.GetSavedSearchRepository(_workspaceArtifactId, savedSearchId).Returns(_savedSearchRepository);
+			_savedSearchRepository.AllDocumentsRetrieved().Returns(false, false, true);
+			_savedSearchRepository.RetrieveNextDocuments().Returns(x => artifactDtoBatchOne, x => artifactDtoBatchTwo, x => new ArtifactDTO[0]);
+
+			// Act
+			_testInstance.CreateErrorListTempTablesForItemLevelErrors(_job, savedSearchId);
+
+			// Assert
+			_repositoryFactory.Received(1).GetJobHistoryErrorRepository(_workspaceArtifactId);
+			_repositoryFactory.Received(1).GetJobHistoryRepository(_workspaceArtifactId);
+			_jobHistoryRepository.Received(1).GetLastJobHistoryArtifactId(_integrationPointArtifactId);
+			_jobHistoryErrorRepository.Received(1).RetrieveJobHistoryErrorIdsAndSourceUniqueIds(lastJobHistoryId, ErrorTypeChoices.JobHistoryErrorItem);
+			_repositoryFactory.Received(1).GetSavedSearchRepository(_workspaceArtifactId, savedSearchId);
+			_savedSearchRepository.Received(3).AllDocumentsRetrieved();
+			_savedSearchRepository.Received(2).RetrieveNextDocuments();
+
+			_jobHistoryErrorItemStart.DidNotReceiveWithAnyArgs()
+				.AddArtifactIdsIntoTempTable(null);
+			_jobHistoryErrorItemComplete.DidNotReceiveWithAnyArgs()
+				.AddArtifactIdsIntoTempTable(null);
+			_jobHistoryErrorItemStartOther.Received(1).AddArtifactIdsIntoTempTable(Arg.Is<IList<int>>(y => itemLevelErrorsAndSourceUniqueIds.Keys.SequenceEqual(y)));
+		}
+
+		[Test]
+		public void CreateErrorListTempTablesForItemLevelErrors_MultipleBatches_CurrentAndExpiredItemsLevelErrors()
+		{
+			// Arrange
+			const int lastJobHistoryId = 2322133;
+			const int savedSearchId = 2321393;
+			const int artifactTypeId = 10;
+			const int documentId1 = 100501;
+			const int documentId2 = 100502;
+			const int error1 = 424324;
+			const int error2 = 234234;
+			const int error3 = 89324;
+			const int error4 = 999234;
+			const string controlNumber1 = "REL0000000179.0001";
+			const string controlNumber2 = "REL0000000179.0002";
+			var itemLevelErrorsAndSourceUniqueIds = new Dictionary<int, string>()
+			{
+				{error1, "NEVER"},
+				{error2, controlNumber1},
+				{error3, controlNumber2},
+				{error4, "GUNNA"},
+			};
+
+			var artifactDtoBatchOne = new ArtifactDTO[]
+			{
+				new ArtifactDTO(documentId1, artifactTypeId, controlNumber1, new ArtifactFieldDTO[0]),
+			};
+
+			var artifactDtoBatchTwo = new ArtifactDTO[]
+			{
+				new ArtifactDTO(documentId2, artifactTypeId, controlNumber2, new ArtifactFieldDTO[0]),
+			};
+
+			_jobHistoryRepository.GetLastJobHistoryArtifactId(_integrationPointArtifactId).Returns(lastJobHistoryId);
+			_jobHistoryErrorRepository.RetrieveJobHistoryErrorIdsAndSourceUniqueIds(lastJobHistoryId, ErrorTypeChoices.JobHistoryErrorItem).Returns(itemLevelErrorsAndSourceUniqueIds);
+			_repositoryFactory.GetSavedSearchRepository(_workspaceArtifactId, savedSearchId).Returns(_savedSearchRepository);
+			_savedSearchRepository.AllDocumentsRetrieved().Returns(false, false, true);
+			_savedSearchRepository.RetrieveNextDocuments().Returns(x => artifactDtoBatchOne, x => artifactDtoBatchTwo, x => new ArtifactDTO[0]);
+
+			// Act
+			_testInstance.CreateErrorListTempTablesForItemLevelErrors(_job, savedSearchId);
+
+			// Assert
+			_repositoryFactory.Received(1).GetJobHistoryErrorRepository(_workspaceArtifactId);
+			_repositoryFactory.Received(1).GetJobHistoryRepository(_workspaceArtifactId);
+			_jobHistoryRepository.Received(1).GetLastJobHistoryArtifactId(_integrationPointArtifactId);
+			_jobHistoryErrorRepository.Received(1).RetrieveJobHistoryErrorIdsAndSourceUniqueIds(lastJobHistoryId, ErrorTypeChoices.JobHistoryErrorItem);
+			_repositoryFactory.Received(1).GetSavedSearchRepository(_workspaceArtifactId, savedSearchId);
+			_savedSearchRepository.Received(3).AllDocumentsRetrieved();
+			_savedSearchRepository.Received(2).RetrieveNextDocuments();
+
+			_jobHistoryErrorItemStart.Received(1).AddArtifactIdsIntoTempTable(Arg.Is<IList<int>>(y => y.SequenceEqual(new [] {error2, error3})));
+			_jobHistoryErrorItemComplete.Received(1).AddArtifactIdsIntoTempTable(Arg.Is<IList<int>>(y => y.SequenceEqual(new [] {error2, error3})));
+			_jobHistoryErrorItemStartOther.Received(1).AddArtifactIdsIntoTempTable(Arg.Is<IList<int>>(y => y.SequenceEqual(new [] {error1, error4})));
 		}
 	}
 }
