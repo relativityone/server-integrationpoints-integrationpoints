72f9f9fb205a89e5952536bfc71fe81b44272264
David T Biedrzycki <dbiedrzycki@kcura.com>
2016-06-03 13:04:55 -0500

    Added UserHasArtifactTypePermission and UserHasArtifactInstancePermission unit tests

--- source/kCura.IntegrationPoints.Data.Tests/Unit/Repositories/PermissionRepositoryTests.cs
+++ source/kCura.IntegrationPoints.Data.Tests/Unit/Repositories/PermissionRepositoryTests.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using kCura.IntegrationPoints.Data.Repositories;
 using kCura.IntegrationPoints.Data.Repositories.Implementations;
 using kCura.Relativity.Client;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using NUnit.Framework;
 using Relativity.API;
 using Relativity.Services;
@@ -72,6 +74,39 @@ namespace kCura.IntegrationPoints.Data.Tests.Unit.Repositories
 					x => x.Count() == 1
 					     && x.First().ArtifactType.ID == expectedPermissionRef.ArtifactType.ID
 					     && x.First().PermissionType == expectedPermissionRef.PermissionType),
+				Arg.Is(_WORKSPACE_ID));
+		}
+
+		[Test]
+		public void UserHasPermissionToAccessWorkspace_PermissionRequestExcepts_ReturnsFalse()
+		{
+			// Arrange
+			var expectedPermissionRef = new PermissionRef()
+			{
+				ArtifactType = new ArtifactTypeIdentifier((int)ArtifactType.Case),
+				PermissionType = PermissionType.View
+			};
+
+			_permissionManager.GetPermissionSelectedAsync(
+				Arg.Is(-1),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+					     && x.First().ArtifactType.ID == expectedPermissionRef.ArtifactType.ID
+					     && x.First().PermissionType == expectedPermissionRef.PermissionType),
+				Arg.Is(_WORKSPACE_ID))
+				.Throws(new Exception("LEEEDDLE LEEEDDLE LEEELE"));
+
+			// Act
+			bool result = _instance.UserHasPermissionToAccessWorkspace();
+
+			// Assert
+			Assert.IsFalse(result);
+			_permissionManager.Received(1).GetPermissionSelectedAsync(
+				Arg.Is(-1),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+						 && x.First().ArtifactType.ID == expectedPermissionRef.ArtifactType.ID
+						 && x.First().PermissionType == expectedPermissionRef.PermissionType),
 				Arg.Is(_WORKSPACE_ID));
 		}
 
@@ -152,5 +187,228 @@ namespace kCura.IntegrationPoints.Data.Tests.Unit.Repositories
 						 && x.First().PermissionType == expectedPermissionRef.PermissionType),
 				Arg.Is(_WORKSPACE_ID));
 		}
+
+		[Test]
+		public void UserHasArtifactInstancePermission_UserHasPermissions_ReturnsTrue()
+		{
+			// Arrange
+			const int artifactId = 84903;
+			Guid artifactGuid = Guid.NewGuid();
+			var expectedPermissionRef = new PermissionRef()
+			{
+				ArtifactType = new ArtifactTypeIdentifier(artifactGuid),
+				PermissionType = PermissionType.View
+			};
+
+			var permissionValues = new List<PermissionValue>()
+			{
+				new PermissionValue()
+				{
+					Selected = true
+				}
+			};
+
+			_permissionManager.GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+						 && x.First().ArtifactType.Guids.SequenceEqual(expectedPermissionRef.ArtifactType.Guids)
+						 && x.First().PermissionType == expectedPermissionRef.PermissionType),
+				Arg.Is(artifactId))
+				.Returns(permissionValues);
+
+			// Act
+			bool result = _instance.UserHasArtifactInstancePermission(artifactGuid, artifactId, ArtifactPermission.View);
+
+			// Assert
+			Assert.IsTrue(result);
+			_permissionManager.Received(1).GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+						 && x.First().ArtifactType.Guids.SequenceEqual(expectedPermissionRef.ArtifactType.Guids)
+						 && x.First().PermissionType == expectedPermissionRef.PermissionType),
+				Arg.Is(artifactId));
+		}
+
+		[Test]
+		public void UserHasArtifactInstancePermission_PermissionRequestExcepts_ReturnsFalse()
+		{
+			// Arrange
+			const int artifactId = 84903;
+			Guid artifactGuid = Guid.NewGuid();
+			var expectedPermissionRef = new PermissionRef()
+			{
+				ArtifactType = new ArtifactTypeIdentifier(artifactGuid),
+				PermissionType = PermissionType.View
+			};
+
+			_permissionManager.GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+					     && x.First().ArtifactType.Guids.SequenceEqual(expectedPermissionRef.ArtifactType.Guids)
+					     && x.First().PermissionType == expectedPermissionRef.PermissionType),
+				Arg.Is(artifactId))
+				.Throws(new Exception("SQUAREPANTS"));
+
+			// Act
+			bool result = _instance.UserHasArtifactInstancePermission(artifactGuid, artifactId, ArtifactPermission.View);
+
+			// Assert
+			Assert.IsFalse(result);
+			_permissionManager.Received(1).GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+						 && x.First().ArtifactType.Guids.SequenceEqual(expectedPermissionRef.ArtifactType.Guids)
+						 && x.First().PermissionType == expectedPermissionRef.PermissionType),
+				Arg.Is(artifactId));
+		}
+
+		[Test]
+		public void UserHasArtifactTypePermissions_UserHasPermissions_ReturnsTrue()
+		{
+			// Arrange
+			const int artifactTypeId = 49203;
+			var expectedPermissionRef = new PermissionRef()
+			{
+				ArtifactType = new ArtifactTypeIdentifier(artifactTypeId),
+				PermissionType = PermissionType.View
+			};
+
+			var permissionValues = new List<PermissionValue>()
+			{
+				new PermissionValue()
+				{
+					Selected = true
+				}
+			};
+
+			_permissionManager.GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+						 && x.First().ArtifactType.ID == expectedPermissionRef.ArtifactType.ID
+						 && x.First().PermissionType == expectedPermissionRef.PermissionType))
+				.Returns(permissionValues);
+
+			// Act
+			bool result = _instance.UserHasArtifactTypePermissions(artifactTypeId, new [] { ArtifactPermission.View });
+
+			// Assert
+			Assert.IsTrue(result);
+			_permissionManager.Received(1).GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+					     && x.First().ArtifactType.ID == expectedPermissionRef.ArtifactType.ID
+					     && x.First().PermissionType == expectedPermissionRef.PermissionType));
+		}
+
+		[Test]
+		public void UserHasArtifactTypePermissions_PermissionRequestExcepts_ReturnsFalse()
+		{
+			// Arrange
+			const int artifactTypeId = 49203;
+			var expectedPermissionRef = new PermissionRef()
+			{
+				ArtifactType = new ArtifactTypeIdentifier(artifactTypeId),
+				PermissionType = PermissionType.View
+			};
+
+			_permissionManager.GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+					     && x.First().ArtifactType.ID == expectedPermissionRef.ArtifactType.ID
+					     && x.First().PermissionType == expectedPermissionRef.PermissionType))
+				.Throws(new Exception("Patrick"));
+
+			// Act
+			bool result = _instance.UserHasArtifactTypePermissions(artifactTypeId, new[] { ArtifactPermission.View });
+
+			// Assert
+			Assert.IsFalse(result);
+			_permissionManager.Received(1).GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+						 && x.First().ArtifactType.ID == expectedPermissionRef.ArtifactType.ID
+						 && x.First().PermissionType == expectedPermissionRef.PermissionType));
+		}
+
+		[Test]
+		public void UserHasArtifactTypePermission_UserHasPermissions_ReturnsTrue()
+		{
+			// Arrange
+			Guid artifactGuid = Guid.NewGuid();
+			var expectedPermissionRef = new PermissionRef()
+			{
+				ArtifactType = new ArtifactTypeIdentifier(artifactGuid),
+				PermissionType = PermissionType.View
+			};
+
+			var permissionValues = new List<PermissionValue>()
+			{
+				new PermissionValue()
+				{
+					Selected = true
+				}
+			};
+
+			_permissionManager.GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+						 && x.First().ArtifactType.Guids.SequenceEqual(expectedPermissionRef.ArtifactType.Guids)
+						 && x.First().PermissionType == expectedPermissionRef.PermissionType))
+				.Returns(permissionValues);
+
+			// Act
+			bool result = _instance.UserHasArtifactTypePermission(artifactGuid, ArtifactPermission.View );
+
+			// Assert
+			Assert.IsTrue(result);
+			_permissionManager.Received(1).GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+						 && x.First().ArtifactType.Guids.SequenceEqual(expectedPermissionRef.ArtifactType.Guids)
+						 && x.First().PermissionType == expectedPermissionRef.PermissionType));
+		}
+
+		[Test]
+		public void UserHasArtifactTypePermission_PermissionRequestExcepts_ReturnsFalse()
+		{
+			// Arrange
+			Guid artifactGuid = Guid.NewGuid();
+			var expectedPermissionRef = new PermissionRef()
+			{
+				ArtifactType = new ArtifactTypeIdentifier(artifactGuid),
+				PermissionType = PermissionType.View
+			};
+
+			_permissionManager.GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+					     && x.First().ArtifactType.Guids.SequenceEqual(expectedPermissionRef.ArtifactType.Guids)
+					     && x.First().PermissionType == expectedPermissionRef.PermissionType))
+				.Throws(new Exception("Squidward"));
+
+			// Act
+			bool result = _instance.UserHasArtifactTypePermission(artifactGuid, ArtifactPermission.View);
+
+			// Assert
+			Assert.IsFalse(result);
+			_permissionManager.Received(1).GetPermissionSelectedAsync(
+				Arg.Is(_WORKSPACE_ID),
+				Arg.Is<List<PermissionRef>>(
+					x => x.Count() == 1
+						 && x.First().ArtifactType.Guids.SequenceEqual(expectedPermissionRef.ArtifactType.Guids)
+						 && x.First().PermissionType == expectedPermissionRef.PermissionType));
+		}
+
 	}
 }
