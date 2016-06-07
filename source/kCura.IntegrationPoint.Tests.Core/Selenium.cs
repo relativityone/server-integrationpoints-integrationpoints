a5d9cf58266e656bae5b39587e75e2f5480eab66
Kezhang Wu <kwu@kcura.com>
2016-06-07 16:36:13 -0500

    Updated tests based on review.

--- source/kCura.IntegrationPoint.Tests.Core/Selenium.cs
+++ source/kCura.IntegrationPoint.Tests.Core/Selenium.cs
@@ -8,36 +8,36 @@ namespace kCura.IntegrationPoint.Tests.Core
 
 	public static class Selenium
 	{
-		public static void GoToUrl(this IWebDriver _driver, string url)
+		public static void GoToUrl(this IWebDriver driver, string url)
 		{
-			_driver.Navigate().GoToUrl(url);
+			driver.Navigate().GoToUrl(url);
 		}
 
-		public static void LogIntoRelativity(this IWebDriver _driver, string username, string password)
+		public static void LogIntoRelativity(this IWebDriver driver, string username, string password)
 		{
 			string relativityUrl = $"http://{SharedVariables.TargetHost}/relativity";
-			_driver.GoToUrl(relativityUrl);
+			driver.GoToUrl(relativityUrl);
 
-			_driver.FindElement(By.Id("_email")).SendKeys(username);
-			_driver.FindElement(By.Id("continue")).Click();
+			driver.FindElement(By.Id("_email")).SendKeys(username);
+			driver.FindElement(By.Id("continue")).Click();
 
-			_driver.FindElement(By.Id("_password__password_TextBox")).SendKeys(password);
-			_driver.FindElement(By.Id("_login")).Click();
+			driver.FindElement(By.Id("_password__password_TextBox")).SendKeys(password);
+			driver.FindElement(By.Id("_login")).Click();
 		}
 
-		public static void GoToWorkspace(this IWebDriver _driver, int artifactId)
+		public static void GoToWorkspace(this IWebDriver driver, int artifactId)
 		{
 			string workspaceXpath = $"//a[@href='/Relativity/RedirectHandler.aspx?defaultCasePage=1&AppID={artifactId}&RootFolderID=1003697']";
 
-			_driver.SwitchTo().DefaultContent();
-			_driver.SwitchTo().Frame("ListTemplateFrame");
+			driver.SwitchTo().DefaultContent();
+			driver.SwitchTo().Frame("ListTemplateFrame");
 
-			_driver.FindElement(By.XPath(workspaceXpath)).Click();
+			driver.FindElement(By.XPath(workspaceXpath)).Click();
 		}
 
-		public static void GoToTab(this IWebDriver _driver, string tabName)
+		public static void GoToTab(this IWebDriver driver, string tabName)
 		{
-			ReadOnlyCollection<IWebElement> webElementCollection = _driver.FindElements(By.Id("horizontal-tabstrip"));
+			ReadOnlyCollection<IWebElement> webElementCollection = driver.FindElements(By.Id("horizontal-tabstrip"));
 			IWebElement navigationList = webElementCollection[0].FindElement(By.XPath("//ul[@class='nav navbar-nav']"));
 			ReadOnlyCollection<IWebElement> listElements = navigationList.FindElements(By.TagName("li"));
 			foreach (IWebElement listElement in listElements)
@@ -55,20 +55,20 @@ namespace kCura.IntegrationPoint.Tests.Core
 			}
 		}
 
-		public static void GoToObjectInstance(this IWebDriver _driver, int workspaceArtifactId, int integrationPointArtifactId, int artifactTypeId)
+		public static void GoToObjectInstance(this IWebDriver driver, int workspaceArtifactId, int integrationPointArtifactId, int artifactTypeId)
 		{
 			string integrationPointUrl = $"http://{SharedVariables.TargetHost}/Relativity/Case/Mask/View.aspx?AppID={workspaceArtifactId}&ArtifactID={integrationPointArtifactId}&ArtifactTypeID={artifactTypeId}";
-			_driver.Navigate().GoToUrl(integrationPointUrl);
+			driver.Navigate().GoToUrl(integrationPointUrl);
 		}
 
-		public static bool PageShouldContain(this IWebDriver _driver, string message)
+		public static bool PageShouldContain(this IWebDriver driver, string message)
 		{
-			return _driver.PageSource.Contains(message);
+			return driver.PageSource.Contains(message);
 		}
 
-		public static void WaitUntilElementExists(this IWebDriver _driver, ElementType elementType, string value, int timeSeconds)
+		public static void WaitUntilElementExists(this IWebDriver driver, ElementType elementType, string value, int timeSeconds)
 		{
-			WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(timeSeconds));
+			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeSeconds));
 			switch (elementType)
 			{
 				case ElementType.Id:
@@ -88,9 +88,9 @@ namespace kCura.IntegrationPoint.Tests.Core
 			}
 		}
 
-		public static void WaitUntilElementIsClickable(this IWebDriver _driver, ElementType elementType, string value, int timeSeconds)
+		public static void WaitUntilElementIsClickable(this IWebDriver driver, ElementType elementType, string value, int timeSeconds)
 		{
-			WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(timeSeconds));
+			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeSeconds));
 			switch (elementType)
 			{
 				case ElementType.Id:
@@ -110,9 +110,9 @@ namespace kCura.IntegrationPoint.Tests.Core
 			}
 		}
 
-		public static void WaitUntilElementIsVisible(this IWebDriver _driver, ElementType elementType, string value, int timeSeconds)
+		public static void WaitUntilElementIsVisible(this IWebDriver driver, ElementType elementType, string value, int timeSeconds)
 		{
-			WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(timeSeconds));
+			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeSeconds));
 			switch (elementType)
 			{
 				case ElementType.Id:
@@ -132,26 +132,26 @@ namespace kCura.IntegrationPoint.Tests.Core
 			}
 		}
 
-		public static void WaitUntilIdExists(this IWebDriver _driver, string id, int timeSeconds)
+		public static void WaitUntilIdExists(this IWebDriver driver, string id, int timeSeconds)
 		{
-			WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(timeSeconds));
+			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeSeconds));
 			wait.Until(ExpectedConditions.ElementExists(By.Id(id)));
 		}
 
-		public static void CloseSeleniumBrowser(this IWebDriver _driver)
+		public static void CloseSeleniumBrowser(this IWebDriver driver)
 		{
 			try
 			{
-				_driver.Quit();
+				driver.Quit();
 			}
 			catch (Exception)
 			{
 			}
 		}
 
-		public static void SelectFromDropdownList(this IWebDriver _driver, string dropdownId, string value)
+		public static void SelectFromDropdownList(this IWebDriver driver, string dropdownId, string value)
 		{
-			IWebElement dropDown = _driver.FindElement(By.Id(dropdownId));
+			IWebElement dropDown = driver.FindElement(By.Id(dropdownId));
 			SelectElement selectValue = new SelectElement(dropDown);
 			selectValue.SelectByText(value);
 		}
