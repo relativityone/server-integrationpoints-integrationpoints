22cf3f1b464094dc01f7295c9f1c15ca0f956216
Art Kelenzon <akelenzon@kcura.com>
2016-06-20 15:04:25 -0500

    fixing SDK build issues.

--- source/DevelopmentScripts/copy.sdk.files.bat
+++ source/DevelopmentScripts/copy.sdk.files.bat
@@ -1,10 +1,11 @@
 IF NOT EXIST "%IPSdkOutput%" (
-	md %IPSdkOutput%
+	md "%IPSdkOutput%"
 	echo create
 )
-xcopy /Y %SDK_TARGET%\kCura.IntegrationPoints.Contracts.dll %IPSdkOutput%
-xcopy /Y %SDK_TARGET%\kCura.IntegrationPoints.Domain.dll %IPSdkOutput%
-xcopy /Y %SDK_TARGET%\kCura.SourceProviderInstaller.dll	%IPSdkOutput%
-xcopy /Y %LDAPSync%\..\example\ProviderIncludes\api\frame-messaging.js %IPSdkOutput%
-xcopy /Y %LDAPSync%\..\example\ProviderIncludes\api\jquery-1.8.2.js %IPSdkOutput%
-xcopy /Y %LDAPSync%\..\example\ProviderIncludes\api\jquery-postMessage.js %IPSdkOutput%
+del /F /S /Q "%IPSdkOutput%\*.*"
+xcopy /Y "%SDK_TARGET%\kCura.IntegrationPoints.Contracts.dll" "%IPSdkOutput%"
+xcopy /Y "%SDK_TARGET%\kCura.IntegrationPoints.Domain.dll" "%IPSdkOutput%"
+xcopy /Y "%SDK_TARGET%\kCura.SourceProviderInstaller.dll" "%IPSdkOutput%"
+xcopy /Y "%LDAPSync%\..\example\ProviderIncludes\api\frame-messaging.js" "%IPSdkOutput%"
+xcopy /Y "%LDAPSync%\..\example\ProviderIncludes\api\jquery-1.8.2.js" "%IPSdkOutput%"
+xcopy /Y "%LDAPSync%\..\example\ProviderIncludes\api\jquery-postMessage.js" "%IPSdkOutput%"
