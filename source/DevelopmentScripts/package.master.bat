e86350101c9bdfd0801f744cdea1758c95f4d1be
Art Kelenzon <akelenzon@kcura.com>
2016-06-20 15:39:44 -0500

    updating final build step to copy over sdk zip file

--- source/DevelopmentScripts/package.master.bat
+++ source/DevelopmentScripts/package.master.bat
@@ -8,6 +8,7 @@ mkdir "%revisionDirectory%"
 mkdir "%revisionDirectory%\web"
 mkdir "%revisionDirectory%\dlls"
 mkdir "%revisionDirectory%\pdbs"
+mkdir "%revisionDirectory%\sdk"
 
 echo f | xcopy "%basePath%\bin\Application\*" "%revisionDirectory%\Application\" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\bin\*.dll" "%revisionDirectory%\dlls" /EY /EXCLUDE:packageCreationExcludeList.txt
@@ -15,6 +16,8 @@ echo f | xcopy "%basePath%\bin\*.config" "%revisionDirectory%\dlls" /EY /EXCLUDE
 echo f | xcopy "%basePath%\bin\*.pdb" "%revisionDirectory%\pdbs" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\publish\bin\web\Release\*" "%revisionDirectory%\web\" /EY /EXCLUDE:packageCreationExcludeList.txt
 echo f | xcopy "%basePath%\nugspec\*.nupkg" "%revisionDirectory%\%buildType% %version% NuGet Packages - INTERNAL USE ONLY" /E /I
+echo f | xcopy "%basePath%\bin\sdk\*" "%revisionDirectory%\dlls\sdk\" /EY /EXCLUDE:packageCreationExcludeList.txt
+echo f | xcopy "%basePath%\bin\IntegrationPoints.SDK.zip" "%revisionDirectory%\sdk\" /EY /EXCLUDE:packageCreationExcludeList.txt
 
 :END
 echo PACKAGEMASTER: END
