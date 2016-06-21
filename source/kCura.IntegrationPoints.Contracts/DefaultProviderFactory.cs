534126448e884af5efc1b9ed443bc6186a7556b6
David T Biedrzycki <dbiedrzycki@kcura.com>
2016-06-21 17:03:04 -0500

    Third party Providers can now supply an IProviderFactory so that they may implement their own DI

--- source/kCura.IntegrationPoints.Contracts/DefaultProviderFactory.cs
+++ source/kCura.IntegrationPoints.Contracts/DefaultProviderFactory.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using kCura.IntegrationPoints.Contracts.Provider;
@@ -10,7 +8,7 @@ namespace kCura.IntegrationPoints.Contracts
 	/// <summary>
 	/// Represents a default provider used to create data source providers.
 	/// </summary>
-	public class DefaultProviderFactory : IProviderFactory
+	internal class DefaultProviderFactory : ProviderFactoryBase
 	{
 		private readonly IWindsorContainer _windsorContainer;
 
@@ -24,48 +22,11 @@ namespace kCura.IntegrationPoints.Contracts
 		}
 
 		/// <summary>
-		/// Creates a new data source provider using the GUID specified as the identifier.
-		/// </summary>
-		/// <param name="identifier"> A GUID decorating the class so that it can be accessed.</param>
-        /// <returns>A new instance of a data source provider.</returns>
-		public IDataSourceProvider CreateProvider(Guid identifier)
-		{
-			Type t = GetType(identifier);
-			return CreateInstance(t);
-		}
-		/// <summary>
-		/// Gets the Type of the current instance.
-		/// </summary>
-		/// <param name="identifer"></param>
-		/// <returns></returns>
-		protected virtual Type GetType(Guid identifer)
-		{
-			List<Type> types = new List<Type>();
-			foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-			{
-				Type[] loadableTypes = assembly.GetLoadableTypes();
-				types.AddRange(loadableTypes.Where(type => type.IsDefined(typeof(DataSourceProviderAttribute), true)));
-			}
-
-			var providerTypes = types.Where(x => x.GetCustomAttributes(typeof(DataSourceProviderAttribute), true)
-													.Cast<DataSourceProviderAttribute>().Any(y => y.Identifier.Equals(identifer))).ToList();
-			if (providerTypes.Count() > 1)
-			{
-				throw new Exception(string.Format(Properties.Resources.MoreThanOneProviderFound, providerTypes.Count(), identifer));
-			}
-			if (!providerTypes.Any())
-			{
-				throw new Exception(string.Format(Properties.Resources.NoProvidersFound, identifer));
-			}
-			return providerTypes.First();
-		}
-
-		/// <summary>
         /// Creates a new instance of the provider type using the Activator with an empty constructor.
 		/// </summary>
 		/// <param name="providerType">The type of the provider to create.</param>
         /// <returns>A new instance of a data source provider.</returns>
-		protected virtual IDataSourceProvider CreateInstance(Type providerType)
+		public override IDataSourceProvider CreateInstance(Type providerType)
 		{
 			IDataSourceProvider provider = null;
 			string assemblyQualifiedName = providerType.AssemblyQualifiedName;
---
+++ source/kCura.IntegrationPoints.Contracts/ProviderFactoryBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using kCura.IntegrationPoints.Contracts.Provider;
+
+namespace kCura.IntegrationPoints.Contracts
+{
+	public abstract class ProviderFactoryBase : IProviderFactory
+	{
+		public abstract IDataSourceProvider CreateInstance(Type providerType);
+
+		/// <summary>
+		/// Creates a new data source provider using the GUID specified as the identifier.
+		/// </summary>
+		/// <param name="identifier"> A GUID decorating the class so that it can be accessed.</param>
+		/// <returns>A new instance of a data source provider.</returns>
+		IDataSourceProvider IProviderFactory.CreateProvider(Guid identifier)
+		{
+			Type t = GetType(identifier);
+			return CreateInstance(t);
+		}
+
+		/// <summary>
+		/// Gets the Type of the current instance.
+		/// </summary>
+		/// <param name="identifer"></param>
+		/// <returns></returns>
+		private Type GetType(Guid identifer)
+		{
+			List<Type> types = new List<Type>();
+			foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+			{
+				Type[] loadableTypes = assembly.GetLoadableTypes();
+				types.AddRange(loadableTypes.Where(type => type.IsDefined(typeof(DataSourceProviderAttribute), true)));
+			}
+
+			var providerTypes = types.Where(x => x.GetCustomAttributes(typeof(DataSourceProviderAttribute), true)
+													.Cast<DataSourceProviderAttribute>().Any(y => y.Identifier.Equals(identifer))).ToList();
+			if (providerTypes.Count() > 1)
+			{
+				throw new Exception(string.Format(Properties.Resources.MoreThanOneProviderFound, providerTypes.Count(), identifer));
+			}
+			if (!providerTypes.Any())
+			{
+				throw new Exception(string.Format(Properties.Resources.NoProvidersFound, identifer));
+			}
+			return providerTypes.First();
+		}
+	}
+}
