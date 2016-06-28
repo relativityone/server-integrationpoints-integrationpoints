7176a8a39f34c29a86a9b21eec5f489b624128df
Patryk Stepien <pstepien@kcura.com>
2016-06-28 14:30:30 +0200

    KRK-10 Summary:Create summary page for export configuration
    Add custom page as partial view for summary page:
    * Add debug logging when loading partial view (for investigating KRK-735)

---
+++ source/kCura.IntegrationPoints.Web/Scripts/Export/export-details-helper.js
@@ -0,0 +1,15 @@
+﻿var ExportDetailsHelper = {};
+
+ExportDetailsHelper.modifySummaryPage = function (settings, $rootField) {
+    IP.utils.getViewField(IP.overwriteFields).closest('.editTableColumn').parent().hide();
+    IP.utils.getViewField(IP.sourceConfiguration).closest('.editTableColumn').parent().hide();
+    IP.utils.getViewField(IP.destinationid).closest('.editTableColumn').parent().hide();
+    IP.data.ajax({
+        url: IP.utils.generateWebURL('IntegrationPoints', 'ExportDetails'),
+        type: 'post',
+        data: settings,
+        dataType: 'html'
+    }).then(function (result) {
+        $rootField.closest('.innerTabTable').children('tbody').append(result);
+    });
+}
