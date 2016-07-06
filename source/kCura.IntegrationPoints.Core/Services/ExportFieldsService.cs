a13761c1db439f27313c77f676bd9be3586f4e92
Stanislaw Wolny <swolny@kcura.com>
2016-07-06 17:44:41 +0200

    [KRK-13] added fields mapping

---
+++ source/kCura.IntegrationPoints.Core/Services/ExportFieldsService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using kCura.IntegrationPoints.Contracts.Models;
+using kCura.IntegrationPoints.Data.Contexts;
+using kCura.IntegrationPoints.Data.Extensions;
+using Relativity.Core;
+using Relativity.Core.Service;
+using Relativity.Query;
+
+namespace kCura.IntegrationPoints.Core.Services
+{
+	public class ExportFieldsService : IExportFieldsService
+	{
+		private readonly IOnBehalfOfUserClaimsPrincipalFactory _claimsPrincipalFactory;
+
+		public ExportFieldsService(IOnBehalfOfUserClaimsPrincipalFactory claimsPrincipalFactory)
+		{
+			_claimsPrincipalFactory = claimsPrincipalFactory;
+		}
+
+		public FieldEntry[] GetAllExportableFields(int workspaceArtifactID, int artifactTypeID)
+		{
+			ClaimsPrincipal principal = _claimsPrincipalFactory.CreateClaimsPrincipal(9); // TODO: get current user id, admin for now
+			ICoreContext context = principal.GetUnversionContext(workspaceArtifactID);
+
+			kCura.Data.DataView dataview = (new SearchQuery()).RetrieveAllExportableViewFields(context, artifactTypeID);
+
+			var fields = new List<FieldEntry>();
+			for (int i = 0; i < dataview.Count; i++)
+			{
+				fields.Add(new FieldEntry
+				{
+					FieldIdentifier = dataview[i]["AvfId"].ToString(),
+					DisplayName = dataview[i]["DisplayName"].ToString()
+				});
+			}
+
+			return fields.ToArray();
+		}
+
+		public FieldEntry[] GetAllViewFields(int workspaceArtifactID, int viewArtifactID, int artifactTypeID)
+		{
+			ClaimsPrincipal principal = _claimsPrincipalFactory.CreateClaimsPrincipal(9); // TODO: get current user id, admin for now
+			ICoreContext context = principal.GetUnversionContext(workspaceArtifactID);
+
+			IPermissionsMatrix permissions = UserPermissionsMatrixFactory.Instance.CreateUserPermissionsMatrix(context);
+			var fieldDTOs = ViewHelper.GetViewFields(context as BaseServiceContext, viewArtifactID, artifactTypeID, permissions);
+
+			var fields = new List<FieldEntry>();
+			for (int i = 0; i < fieldDTOs.Count; i++)
+			{
+				fields.Add(new FieldEntry
+				{
+					FieldIdentifier = fieldDTOs[i].ArtifactViewFieldID.ToString(),
+					DisplayName = fieldDTOs[i].HeaderName
+				});
+			}
+
+			return fields.ToArray();
+		}
+	}
+}
---
+++ source/kCura.IntegrationPoints.Core/Services/IExportFieldsService.cs
@@ -0,0 +1,11 @@
+﻿using kCura.IntegrationPoints.Contracts.Models;
+
+namespace kCura.IntegrationPoints.Core.Services
+{
+	public interface IExportFieldsService
+	{
+		FieldEntry[] GetAllExportableFields(int workspaceArtifactID, int artifactTypeID);
+
+		FieldEntry[] GetAllViewFields(int workspaceArtifactID, int viewArtifactID, int artifactTypeID);
+	}
+}
