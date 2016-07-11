46bbdc95aaa51648c823758520f3a8e1a17fb74a
Sorawit Amornborvornwong <samornborvornwong@kcura.com>
2016-07-11 16:38:23 -0500

    Try Out on adding dependencies project

---
+++ source/Utilities/BuildPropertiesScrubber/BuildPropertiesScrubber/BuildPropertiesScrubber/Models/Packages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace BuildPropertiesScrubber.Models
+{
+	[XmlRoot("packages"), Serializable]
+	public class Packages
+	{
+		[XmlElement("package")]
+		public Package[] Items;
+	}
+}
