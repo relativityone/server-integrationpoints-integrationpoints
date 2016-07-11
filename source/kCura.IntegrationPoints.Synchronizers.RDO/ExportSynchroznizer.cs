bf71d001208080a689cef550c1120430e5a456cc
David T Biedrzycki <dbiedrzycki@kcura.com>
2016-07-11 13:09:08 -0500

    Merge fix

--- source/kCura.IntegrationPoints.Synchronizers.RDO/ExportSynchroznizer.cs
+++ source/kCura.IntegrationPoints.Synchronizers.RDO/ExportSynchroznizer.cs
@@ -1,10 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Diagnostics;
-using System.Linq;
 using kCura.IntegrationPoints.Contracts.Models;
 using kCura.IntegrationPoints.Data.Queries;
+using kCura.IntegrationPoints.Domain.Models;
 using kCura.IntegrationPoints.Domain.Synchronizer;
 
 namespace kCura.IntegrationPoints.Synchronizers.RDO
