8a1307106bde5de5c5067f2fff19fe919d19a695
Dan Nelson <dnelson@kcura.com>
2016-07-26 15:29:09 -0500

    [Z037] - Update stop confirmation message to be more impartial to RDO type.

--- source/kCura.IntegrationPoints.Web/Scripts/EventHandlers/integration-points-view.js
+++ source/kCura.IntegrationPoints.Web/Scripts/EventHandlers/integration-points-view.js
@@ -28,7 +28,7 @@
     };
 
     root.stopJob = function (artifactId, appId) {
-    	var confirmationMessage = "Stopping this transfer will not remove any documents that were transfered. When re-running this transfer, make sure that your overwrite settings will return expected results.";
+    	var confirmationMessage = "Stopping this transfer will not remove any data that was transferred. When re-running this transfer, make sure that your overwrite settings will return expected results.";
     	var failureMessage = "The transfer cannot be stopped at this point in the process.";
 
     	window.Dragon.dialogs.showConfirm({
