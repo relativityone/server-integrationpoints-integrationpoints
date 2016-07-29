b0e77cb4f6996a7d94ab67dcdd8e3f9f177a2bbb
Andrzej Korczyński <akorczynski@kcura.com>
2016-07-29 15:25:09 +0200

    REL-96989 [Fix subdirectory prefix info when exporting images or natives are not selected] Summary:Create summary page for export configuration

--- source/kCura.IntegrationPoints.Web/Scripts/EventHandlers/export-details-view.js
+++ source/kCura.IntegrationPoints.Web/Scripts/EventHandlers/export-details-view.js
@@ -3,105 +3,108 @@
 IP.isFileshareProvider = true;
 
 ExportDetailsView.downloadSummaryPage = function () {
-    var dataContainer = new DataContainer();
-    dataContainer.hideContainer();
+	var dataContainer = new DataContainer();
+	dataContainer.hideContainer();
 
-    IP.data.ajax({
-        url: IP.utils.generateWebURL('Fileshare', 'ExportDetails'),
-        type: 'get',
-        dataType: 'html'
-    }).then(function (result) {
-        IP.utils.getViewField(IP.nameId).closest('.innerTabTable').replaceWith(result);
+	IP.data.ajax({
+		url: IP.utils.generateWebURL('Fileshare', 'ExportDetails'),
+		type: 'get',
+		dataType: 'html'
+	}).then(function (result) {
+		IP.utils.getViewField(IP.nameId).closest('.innerTabTable').replaceWith(result);
 
-        var viewModel = new Model(dataContainer);
-        ko.applyBindings(viewModel, document.getElementById('exportSummaryPage'));
-    });
+		var viewModel = new Model(dataContainer);
+		ko.applyBindings(viewModel, document.getElementById('exportSummaryPage'));
+	});
 }
 
 var DataContainer = function () {
-    this.hasErrors = IP.utils.getViewField(IP.hasErrorsId).siblings('.dynamicViewFieldValue').text();
-    this.logErrors = IP.utils.getViewField(IP.logErrorsId).siblings('.dynamicViewFieldValue').text();
-    this.emailNotification = IP.utils.getViewField(IP.emailNotificationId).siblings('.dynamicViewFieldValue').text();
-    this.name = IP.utils.getViewField(IP.nameId).siblings('.dynamicViewFieldValue').text();
-    this.settings = JSON.parse(IP.utils.getViewField(IP.sourceConfiguration).siblings('.dynamicViewFieldValue').text());
+	this.hasErrors = IP.utils.getViewField(IP.hasErrorsId).siblings('.dynamicViewFieldValue').text();
+	this.logErrors = IP.utils.getViewField(IP.logErrorsId).siblings('.dynamicViewFieldValue').text();
+	this.emailNotification = IP.utils.getViewField(IP.emailNotificationId).siblings('.dynamicViewFieldValue').text();
+	this.name = IP.utils.getViewField(IP.nameId).siblings('.dynamicViewFieldValue').text();
+	this.settings = JSON.parse(IP.utils.getViewField(IP.sourceConfiguration).siblings('.dynamicViewFieldValue').text());
 
-    this.hideContainer = function () {
-        IP.utils.getViewField(IP.sourceConfiguration).parent().hide();
-        IP.utils.getViewField(IP.overwriteFieldsId).parent().hide();
-        IP.utils.getViewField(IP.sourceProviderId).parent().hide();
-        IP.utils.getViewField(IP.destinationid).parent().hide();
-        IP.utils.getViewField(IP.destinationProviderid).parent().hide();
-    };
+	this.hideContainer = function () {
+		IP.utils.getViewField(IP.sourceConfiguration).parent().hide();
+		IP.utils.getViewField(IP.overwriteFieldsId).parent().hide();
+		IP.utils.getViewField(IP.sourceProviderId).parent().hide();
+		IP.utils.getViewField(IP.destinationid).parent().hide();
+		IP.utils.getViewField(IP.destinationProviderid).parent().hide();
+	};
 }
 
 var Model = function (dataContainer) {
-    var self = this;
+	var self = this;
 
-    this.hasErrors = dataContainer.hasErrors;
-    this.logErrors = dataContainer.logErrors;
-    this.emailNotification = dataContainer.emailNotification;
-    this.name = dataContainer.name;
-    this.settings = dataContainer.settings;
+	this.hasErrors = dataContainer.hasErrors;
+	this.logErrors = dataContainer.logErrors;
+	this.emailNotification = dataContainer.emailNotification;
+	this.name = dataContainer.name;
+	this.settings = dataContainer.settings;
 
-    this.volumeInfo = function () {
-        return self.settings.VolumePrefix + "; " + self.settings.VolumeStartNumber + "; " + self.settings.VolumeDigitPadding + "; " + self.settings.VolumeMaxSize;
-    };
+	this.volumeInfo = function () {
+		return self.settings.VolumePrefix + "; " + self.settings.VolumeStartNumber + "; " + self.settings.VolumeDigitPadding + "; " + self.settings.VolumeMaxSize;
+	};
 
-    this.subdirectoryInfo = function () {
-    	return self.settings.SubdirectoryImagePrefix + "; " + self.settings.SubdirectoryNativePrefix + "; " + self.settings.SubdirectoryTextPrefix + "; " + self.settings.SubdirectoryStartNumber + "; " + self.settings.SubdirectoryDigitPadding + "; " + self.settings.SubdirectoryMaxFiles;
-    };
+	this.subdirectoryInfo = function () {
 
-    this.exportType = function () {
-        return "Load file"
-        + (self.settings.ExportImagesChecked ? "; Images" : "")
-        + (self.settings.CopyFileFromRepository ? "; Natives" : "");
-    };
+		return (self.settings.ExportImagesChecked ? self.settings.SubdirectoryImagePrefix + "; " : "")
+			+ (self.settings.CopyFileFromRepository ? self.settings.SubdirectoryNativePrefix + "; " : "")
+			+ self.settings.SubdirectoryTextPrefix + "; " + self.settings.SubdirectoryStartNumber + "; " + self.settings.SubdirectoryDigitPadding + "; " + self.settings.SubdirectoryMaxFiles;
+	};
 
-    this.filePath = function () {
-        var filePathType = "";
-        for (var i = 0; i < ExportEnums.FilePathType.length; i++) {
-            if (ExportEnums.FilePathType[i].value == self.settings.FilePath) {
-                filePathType = ExportEnums.FilePathType[i].key;
-            }
-        }
-        return (self.settings.IncludeNativeFilesPath ? "Include" : "Do not include")
-        + ("; " + filePathType)
-        + (self.settings.FilePath == ExportEnums.FilePathTypeEnum.UserPrefix ? (": " + self.settings.UserPrefix) : "");
-    };
+	this.exportType = function () {
+		return "Load file"
+		+ (self.settings.ExportImagesChecked ? "; Images" : "")
+		+ (self.settings.CopyFileFromRepository ? "; Natives" : "");
+	};
 
-    this.loadFileInfo = function () {
-        var fileFormat = "";
-        for (var i = 0; i < ExportEnums.DataFileFormats.length; i++) {
-            if (ExportEnums.DataFileFormats[i].value == self.settings.SelectedDataFileFormat) {
-                fileFormat = ExportEnums.DataFileFormats[i].key;
-            }
-        }
-        return fileFormat + "; " + self.settings.DataFileEncodingType.toUpperCase();
-    };
+	this.filePath = function () {
+		var filePathType = "";
+		for (var i = 0; i < ExportEnums.FilePathType.length; i++) {
+			if (ExportEnums.FilePathType[i].value == self.settings.FilePath) {
+				filePathType = ExportEnums.FilePathType[i].key;
+			}
+		}
+		return (self.settings.IncludeNativeFilesPath ? "Include" : "Do not include")
+		+ ("; " + filePathType)
+		+ (self.settings.FilePath == ExportEnums.FilePathTypeEnum.UserPrefix ? (": " + self.settings.UserPrefix) : "");
+	};
 
-    this.imageFileType = function () {
-    	if (self.settings.ExportImagesChecked) {
-    		for (var i = 0; i < ExportEnums.ImageFileTypes.length; i++) {
-    			if (ExportEnums.ImageFileTypes[i].value == self.settings.SelectedImageFileType) {
-    				return ExportEnums.ImageFileTypes[i].key;
-    			}
-    		}
-	    }
-	    return "";
-    };
+	this.loadFileInfo = function () {
+		var fileFormat = "";
+		for (var i = 0; i < ExportEnums.DataFileFormats.length; i++) {
+			if (ExportEnums.DataFileFormats[i].value == self.settings.SelectedDataFileFormat) {
+				fileFormat = ExportEnums.DataFileFormats[i].key;
+			}
+		}
+		return fileFormat + "; " + self.settings.DataFileEncodingType.toUpperCase();
+	};
 
-    this.imageDataFileFormat = function () {
-	    if (self.settings.ExportImagesChecked) {
-		    for (var i = 0; i < ExportEnums.ImageDataFileFormats.length; i++) {
-			    if (ExportEnums.ImageDataFileFormats[i].value == self.settings.SelectedImageDataFileFormat) {
-				    return ExportEnums.ImageDataFileFormats[i].key;
-			    }
-		    }
-	    }
-	    return "";
-    };
+	this.imageFileType = function () {
+		if (self.settings.ExportImagesChecked) {
+			for (var i = 0; i < ExportEnums.ImageFileTypes.length; i++) {
+				if (ExportEnums.ImageFileTypes[i].value == self.settings.SelectedImageFileType) {
+					return ExportEnums.ImageFileTypes[i].key;
+				}
+			}
+		}
+		return "";
+	};
+
+	this.imageDataFileFormat = function () {
+		if (self.settings.ExportImagesChecked) {
+			for (var i = 0; i < ExportEnums.ImageDataFileFormats.length; i++) {
+				if (ExportEnums.ImageDataFileFormats[i].value == self.settings.SelectedImageDataFileFormat) {
+					return ExportEnums.ImageDataFileFormats[i].key;
+				}
+			}
+		}
+		return "";
+	};
 };
 
 $(function () {
-    ExportDetailsView.downloadSummaryPage();
+	ExportDetailsView.downloadSummaryPage();
 });
