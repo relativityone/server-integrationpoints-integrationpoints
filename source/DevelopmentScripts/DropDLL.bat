a0b00f936ff9da75096611756b8be3696fe18f76
Dan Nelson <dnelson@kcura.com>
2016-08-08 09:18:49 -0500

    Removing unused files.

--- source/DevelopmentScripts/DropDLL.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SET Root=%LDAP%
-echo.Dropping kCura.IntegrationPoints.dll into localhost
-echo.
-
-	pushd %LDAP%\Dependencies
-	RelativityDLLDropper.exe localhost ..\bin\Debug\kCura.IntegrationPoints.dll eddsdbo edds DCF6E9D1-22B6-4DA3-98F6-41381E93C30C"
-	popd
