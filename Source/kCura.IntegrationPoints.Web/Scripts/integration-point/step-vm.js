b9d2011a3aa7e514118449256f2f3bf94b02b726
dwickowski <dwickowski@kcura.com>
2016-08-23 03:40:38 -0500

    REL-100916 Streamline workflow when creating/editing RIPs - bug fix

--- Source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-vm.js
+++ Source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-vm.js
@@ -17,41 +17,35 @@
 
 
 		this.goToStep = function (step, model) {
-			var _updateStepProgressDOM = function () {
-				if (step === 0) {
-					IP.stepDefinitionProvider.init();
+
+			if (step === 0) {
+				IP.stepDefinitionProvider.init();
+			}
+			else if (step === 1) {
+				if (model.destinationProviderGuid === "1D3AD995-32C5-48FE-BAA5-5D97089C8F18") {
+					IP.stepDefinitionProvider.loadOverride([
+							{
+								text: 'Setup'
+							},
+							{
+								text: 'Source Information'
+							},
+							{
+								text: 'Destination Information'
+							}
+					]);
 				}
-				else if (step === 1) {
-					if (model.destinationProviderGuid === "1D3AD995-32C5-48FE-BAA5-5D97089C8F18") {
-						IP.stepDefinitionProvider.loadOverride([
-								{
-									text: 'Setup'
-								},
-								{
-									text: 'Source Information'
-								},
-								{
-									text: 'Destination Information'
-								}
-						]);
-					}
-					else {
-						IP.stepDefinitionProvider.loadDefaults();
-					}
+				else {
+					IP.stepDefinitionProvider.loadDefaults();
 				}
 			}
-			_updateStepProgressDOM();
+
 			var totalSteps = 2; // hardcoded to hide additional steps //this.steps().length - 1;
 			if (step > totalSteps) {
 				return totalSteps;
 			}
 			if (step < 0) {
 				return 0;
-			}
-			if (step === 1) {
-				$('#stepProgress').stepProgress({
-					steps: IP.stepDefinitionProvider.stepsOptions['defaultsteps']
-				});
 			}
 			var nextStep = this.steps()[step];
 			if (nextStep.loadModel) {
@@ -75,7 +69,7 @@
 		}).then(function (result) {
 			vm = new viewModel();
 			if (result.scheduler && result.scheduler.scheduledTime) {
-				
+
 				var time = helper.utcToLocal(result.scheduler.scheduledTime.split(':'), "HH:mm");
 				var timeSplit = time.split(':');
 				var hour = parseInt(timeSplit[0], 10);
@@ -92,7 +86,7 @@
 					result.scheduler.scheduledTime = hour + ":" + timeSplit[1];
 					result.scheduler.selectedTimeFormat = 'AM';
 				}
-				
+
 			}
 			vm.goToStep(0, result);
 			artifactID = result.artifactID;
@@ -126,18 +120,18 @@
 			_next().then(function (result) {
 				if (result.scheduler && result.scheduler.scheduledTime) {
 					var timeSplit = result.scheduler.scheduledTime.split(':');
-					var time = result.scheduler.scheduledTime; 
+					var time = result.scheduler.scheduledTime;
 					if (result.scheduler.selectedTimeFormat == "AM") {
-						result.scheduler.scheduledTime = timeSplit[0] == 12 ? helper.timeLocalToUtc(0+':'+timeSplit[1]) : helper.timeLocalToUtc(time);
+						result.scheduler.scheduledTime = timeSplit[0] == 12 ? helper.timeLocalToUtc(0 + ':' + timeSplit[1]) : helper.timeLocalToUtc(time);
 					} else {
 						var hour = 12;
 						if (parseInt(timeSplit[0], 10) < 12) {
 							hour = parseInt(timeSplit[0], 10) + 12;
 						}
 
-						result.scheduler.scheduledTime = helper.timeLocalToUtc(hour+':'+timeSplit[1]);
+						result.scheduler.scheduledTime = helper.timeLocalToUtc(hour + ':' + timeSplit[1]);
 					}
-					
+
 				}
 				IP.messaging.publish('saveComplete', result);
 			}, function (error) {
@@ -147,21 +141,21 @@
 		});
 
 		IP.messaging.subscribe('saveComplete', function (model) {
-		    var save = $('#save').attr('save');
-		    if (typeof (save) != 'undefined') {
-		        return;
-		    }
+			var save = $('#save').attr('save');
+			if (typeof (save) != 'undefined') {
+				return;
+			}
 			IP.data.ajax({ type: 'POST', url: IP.utils.generateWebAPIURL('IntegrationPointsAPI'), data: JSON.stringify(model) }).then(function (result) {
-			    //redirect to page!!
-			
-			    $('#save').attr('save', 'true');
+				//redirect to page!!
+
+				$('#save').attr('save', 'true');
 				var prefix = window.top.location.protocol + "//" + window.top.location.host;
 				window.top.location = prefix + result.returnURL;
 			}, function (error) {
 				IP.message.error.raise(error);
 			});
 		});
-		
+
 		IP.messaging.subscribe('back', function () {
 			if (typeof (vm.currentStep().back) !== "function") {
 				vm.currentStep().back = function () {
