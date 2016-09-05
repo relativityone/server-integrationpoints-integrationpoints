ec5b99f33262f07dcbceba0098647d43a030daee
Łukasz Starzyk <lstarzyk@kcura.com>
2016-09-05 09:40:33 +0200

    Fixed integrations tests failing because of problems with ProductionsAPI

---
+++ source/kCura.IntegrationPoint.Tests.Core/Placeholder.cs
@@ -0,0 +1,44 @@
+﻿using Relativity.Services.Production;
+
+namespace kCura.IntegrationPoint.Tests.Core
+{
+	public class Placeholder
+	{
+		private const string _PRODUCTION_PLACEHOLDER_SERVICE_BASE =
+			@"/Relativity.REST/api/Relativity.Productions.Services.IProductionModule/Production%20Placeholder%20Manager/";
+
+		private const string _CREATE_SINGLE_SERVICE =
+			_PRODUCTION_PLACEHOLDER_SERVICE_BASE + "CreateSingleAsync";
+
+		public static int Create(int workspaceId, string fileData)
+		{
+			var json =
+				$@"
+					{{
+					  ""workspaceArtifactID"": {workspaceId},
+					  ""placeholder"":
+						{{
+							""PlaceholderType"": ""Image"", 
+							""FileData"": ""{fileData}"",
+							""Filename"": ""DefaultPlaceholder.tif"",
+							""Name"": ""CustomPlaceholder""
+						}}
+					}}";
+
+			//var json =
+			//	$@"
+			//		{{
+			//		  ""workspaceArtifactID"": {workspaceId},
+			//		  ""placeholder"":
+			//			{{
+			//				""PlaceholderType"": ""Custom"",
+			//				""CustomText"": ""<div style=\""text-align:center;\""><span style=\""font-weight:bold;font-size:24px;font-family:Arial;\"">No image available for this record.</span></div>"",
+			//				""Name"": ""CustomPlaceholder""
+			//			}}
+			//		}}";
+
+			string output = Rest.PostRequestAsJson(_CREATE_SINGLE_SERVICE, false, json);
+			return int.Parse(output);
+		}
+	}
+}
