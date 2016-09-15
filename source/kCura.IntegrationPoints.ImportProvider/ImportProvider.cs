f64175f698b3251be822efbc84fe38ed5e61b478
Bob Pacione <bpacione@kcura.com>
2016-09-15 10:45:48 -0500

    Adding initial Import Provider Project

---
+++ source/kCura.IntegrationPoints.ImportProvider/ImportProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.IntegrationPoints.Contracts.Models;
+
+namespace kCura.IntegrationPoints.ImportProvider
+{
+    public class ImportProvider : kCura.IntegrationPoints.Contracts.Provider.IDataSourceProvider
+    {
+        public IDataReader GetBatchableIds(FieldEntry identifier, string options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataReader GetData(IEnumerable<FieldEntry> fields, IEnumerable<string> entryIds, string options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FieldEntry> GetFields(string options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
