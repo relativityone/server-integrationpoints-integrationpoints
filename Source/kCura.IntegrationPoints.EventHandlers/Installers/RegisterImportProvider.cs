57bac456bf02cf6d7ee2e521af08850ed4a0bffd
Bob Pacione <bpacione@kcura.com>
2016-09-16 10:58:27 -0500

    adding guids to RegisterImportProvider; adding ImportProvider dll to schema and build script so it gets built into the RAP; adding postInstallEvent to register import provider to schema

--- Source/kCura.IntegrationPoints.EventHandlers/Installers/RegisterImportProvider.cs
+++ Source/kCura.IntegrationPoints.EventHandlers/Installers/RegisterImportProvider.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using kCura.IntegrationPoints.SourceProviderInstaller;
 
 namespace kCura.IntegrationPoints.EventHandlers.Installers
 {
+    [kCura.EventHandler.CustomAttributes.Description("Register the RIP Import Provider")]
+    [kCura.EventHandler.CustomAttributes.RunOnce(false)]
+    [Guid("01E26CBC-98CA-48A6-942E-FD546E2D5F7E")]
     public class RegisterImportProvider : IntegrationPointSourceProviderInstaller
     {
         public override IDictionary<Guid, SourceProvider> GetSourceProviders()
@@ -18,7 +19,7 @@ namespace kCura.IntegrationPoints.EventHandlers.Installers
                     new SourceProvider()
                     {
                         Name = "Relativity Import",
-                        Url = "/%applicationpath%/CustomPages/DCF6E9D1-22B6-4DA3-98F6-41381E93C30C/ImportProvider/GetDefaultFtpSettings"
+                        Url = "/%applicationpath%/CustomPages/DCF6E9D1-22B6-4DA3-98F6-41381E93C30C/ImportProvider/ImportSettings"
                         //TODO: add api controller to view the source configuration data
                         //ViewDataUrl = "/%applicationpath%/CustomPages/DCF6E9D1-22B6-4DA3-98F6-41381E93C30C/%appId%/api/FtpProviderAPI/view"
                     }
