e9c3e61c8315b60d8e29d38a6af4d6d3efbc68b2
jaycdave88 <jaycdave@gmail.com>
2016-09-19 16:22:41 -0500

    added javascript functionallity and pass model back to db.

--- source/kCura.IntegrationPoints.Web/Scripts/Import/import-provider-settings-step.js
+++ source/kCura.IntegrationPoints.Web/Scripts/Import/import-provider-settings-step.js
@@ -1,1 +1,54 @@
-﻿console.log("If you are seeing this message, the file has been linked up properly.");
+﻿(function () {
+
+    //Create a new communication object that talks to the host page.
+    var message = IP.frameMessaging();
+
+    var _getModel = function () {
+        var model = {
+            ImportType: $("input:radio[name=import-provider-import-detail-radio]").val(),
+            LoadFile: $("#import-loadfile-textfield").val(),
+            HasStartLine: $("#import-hascolumnnames-checkbox").attr("checked") ? true : false,
+            LineNumber: HashChangeEvent ? $("#import-columnname-numbers").val() : null
+        };
+
+        console.log(model);
+        return JSON.stringify(model);
+    };
+
+    //An event raised when the user has clicked the Next or Save button.
+    message.subscribe('submit', function () {
+        //Execute save logic that persists the state.
+        var localModel = _getModel();
+        var parsedModel = JSON.parse(localModel);
+
+        this.publish("saveState", localModel);
+
+        if (parsedModel.CsvFilePath === '') {
+            IP.frameMessaging().dFrame.IP.message.error.raise('Please select a load file to continue.');
+        } else {
+            //Communicate to the host page that it to continue.
+            this.publish('saveComplete', localModel);
+        }
+    });
+
+    ////An event raised when a user clicks the Back button.
+    //message.subscribe('back', function () {
+    //    //Execute save logic that persists the state.
+    //    this.publish('saveState', _getModel());
+    //});
+
+    ////An event raised when the host page has loaded the current settings page.
+    //message.subscribe('load', function (model) {
+    //    if (model != '') {
+    //        var sourceConfig = JSON.parse(model);
+    //        windowObj.o365.StorageRoot = sourceConfig.StorageRoot;
+    //        windowObj.o365.SelectedFolderPath = sourceConfig.CsvFilePath;
+    //    }
+    //    else {
+    //        windowObj.o365.StorageRoot = '';
+    //        windowObj.o365.SelectedFolderPath = '';
+    //    }
+    //    windowObj.o365.IPFrameMessagingLoadEvent = true;
+    //});
+
+})(this);
