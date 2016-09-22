5edea58d54bbfe33413a2fd1b78addaaf171e85d
aarnesen <aarnesen@claritycon.com>
2016-09-22 12:37:29 -0500

    set up a ImportProvider.Parser project; ImportExport nuget package added.

---
+++ Source/kCura.IntegrationPoints.ImportProvider.Parser/FieldParserFactory.cs
@@ -0,0 +1,19 @@
+﻿using kCura.IntegrationPoints.ImportProvider.Parser.Interfaces;
+
+namespace kCura.IntegrationPoints.ImportProvider.Parser
+{
+    public class FieldParserFactory : IFieldParserFactory
+    {
+        public IFieldParser GetFieldParser(string options)
+        {
+            /*
+            using (kCura.Relativity.Client.IRSAPIClient client = 
+     Relativity.CustomPages.ConnectionHelper.Helper().GetServicesManager().CreateProxy<kCura.Relativity.Client.IRSAPIClient>(Relativity.API.ExecutionIdentity.System))
+            {
+
+            }
+            */
+            return null;
+        }
+    }
+}
---
+++ Source/kCura.IntegrationPoints.ImportProvider.Parser/LoadFileFieldParser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using kCura.IntegrationPoints.ImportProvider.Parser.Interfaces;
+
+namespace kCura.IntegrationPoints.ImportProvider.Parser
+{
+    public class LoadFileFieldParser : IFieldParser
+    {
+        private kCura.WinEDDS.LoadFileReader _loadFileReader;
+        private kCura.WinEDDS.LoadFile _config;
+
+        public LoadFileFieldParser(kCura.WinEDDS.LoadFile loadFile)
+        {
+            _config = loadFile;
+            _loadFileReader = new kCura.WinEDDS.LoadFileReader(_config, false);
+        }
+
+        public List<string> GetFields()
+        {
+            return new List<string>(_loadFileReader.GetColumnNames(_config));
+        }
+    }
+}
