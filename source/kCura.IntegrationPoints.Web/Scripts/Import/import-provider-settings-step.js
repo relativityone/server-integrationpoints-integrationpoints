8bfc06795d1288d3247aa068c59e90aa9160023c
jaycdave88 <jaycdave@gmail.com>
2016-09-23 14:40:17 -0500

    Changed the data-binding for the Processing Location dropdown to match the export code

--- source/kCura.IntegrationPoints.Web/Scripts/Import/import-provider-settings-step.js
+++ source/kCura.IntegrationPoints.Web/Scripts/Import/import-provider-settings-step.js
@@ -4,76 +4,77 @@
     //Create a new communication object that talks to the host page.
     var message = IP.frameMessaging();
 
-    var ViewModel = function () {
+    var viewModel = function () {
 
         var self = this;
+
         self.ImportTypeChoiceValue = ko.observable();
         self.ImportTypeChoiceValue("document");
 
         this.ProcessingSourceLocationList = ko.observableArray([]);
         this.ProcessingSourceLocationArtifactId = this.ProcessingSourceLocation || 0;
 
-        this.ProcessingSourceLocation = ko.observable(self.ProcessingSourceLocationArtifactId).extend({
-            required: true
-        });
-    };
-    ko.applyBindings(new ViewModel());
-
-    $.get(root.utils.generateWebAPIURL("ResourcePool/GetProcessingSourceLocationStructure"), function (data) {
-        $.each(data,
-            function (index, dataObject) {
-                $("#processingSources").append($("<option />").val(dataObject.artifactId).text(dataObject.location));
+        this.ProcessingSourceLocation = ko.observable(self.ProcessingSourceLocationArtifactId)
+            .extend({
+                required: true
             });
-    });
 
-    var _getModel = function () {
-        var model = {
-            InputType: $('input:radio[name=import-type]:checked').val(),
-            //ProcessingSource: windowObj.import.StorageRoot ? windowObj.import.StorageRoot : "",
-            //LoadDataFrom: windowObj.import.SelectedFolderPath ? windowObj.import.SelectedFolderPath : "",
-            HasStartLine: $("#import-hascolumnnames-checkbox").attr("checked") ? true : false,
-            LineNumber: $("#import-columnname-numbers").val(),
-            LoadFile: $("#import-loadFile-text").val()
+
+        $.get(root.utils.generateWebAPIURL("ResourcePool/GetProcessingSourceLocationStructure"), function (data) {
+            self.ProcessingSourceLocationList(data);
+        });
+
+        var _getModel = function () {
+            var model = {
+                InputType: $('input:radio[name=import-type]:checked').val(),
+                //ProcessingSource: windowObj.import.StorageRoot ? windowObj.import.StorageRoot : "",
+                //LoadDataFrom: windowObj.import.SelectedFolderPath ? windowObj.import.SelectedFolderPath : "",
+                HasStartLine: $("#import-hascolumnnames-checkbox").attr("checked") ? true : false,
+                LineNumber: $("#import-columnname-numbers").val(),
+                LoadFile: $("#import-loadFile-text").val()
+            };
+
+            console.log(model);
+            return JSON.stringify(model);
         };
 
-        console.log(model);
-        return JSON.stringify(model);
+        //An event raised when the user has clicked the Next or Save button.
+        message.subscribe('submit',
+            function () {
+                //Execute save logic that persists the root.
+                var localModel = _getModel();
+                var parsedModel = JSON.parse(localModel);
+
+                this.publish("saveState", localModel);
+
+                if (parsedModel.CsvFilePath === '') {
+                    IP.frameMessaging().dFrame.IP.message.error.raise('Please select a load file to continue.');
+                } else {
+                    //Communicate to the host page that it to continue.
+                    this.publish('saveComplete', localModel);
+                }
+            });
+
+        ////An event raised when a user clicks the Back button.
+        //message.subscribe('back', function () {
+        //    //Execute save logic that persists the root.
+        //    this.publish('saveState', _getModel());
+        //});
+
+        ////An event raised when the host page has loaded the current settings page.
+        //message.subscribe('load', function (model) {
+        //if (model != '') {
+        //    var sourceConfig = JSON.parse(model);
+        //    windowObj.import.StorageRoot = sourceConfig.StorageRoot;
+        //    windowObj.import.SelectedFolderPath = sourceConfig.CsvFilePath;
+        //}
+        //else {
+        //    windowObj.import.StorageRoot = "";
+        //    windowObj.import.SelectedFolderPath = "";
+        //}
+        //windowObj.import.IPFrameMessagingLoadEvent = true;
+        //});
     };
 
-    //An event raised when the user has clicked the Next or Save button.
-    message.subscribe('submit', function () {
-        //Execute save logic that persists the state.
-        var localModel = _getModel();
-        var parsedModel = JSON.parse(localModel);
-
-        this.publish("saveState", localModel);
-
-        if (parsedModel.CsvFilePath === '') {
-            IP.frameMessaging().dFrame.IP.message.error.raise('Please select a load file to continue.');
-        } else {
-            //Communicate to the host page that it to continue.
-            this.publish('saveComplete', localModel);
-        }
-    });
-
-    ////An event raised when a user clicks the Back button.
-    //message.subscribe('back', function () {
-    //    //Execute save logic that persists the state.
-    //    this.publish('saveState', _getModel());
-    //});
-
-    ////An event raised when the host page has loaded the current settings page.
-    //message.subscribe('load', function (model) {
-    //if (model != '') {
-    //    var sourceConfig = JSON.parse(model);
-    //    windowObj.import.StorageRoot = sourceConfig.StorageRoot;
-    //    windowObj.import.SelectedFolderPath = sourceConfig.CsvFilePath;
-    //}
-    //else {
-    //    windowObj.import.StorageRoot = "";
-    //    windowObj.import.SelectedFolderPath = "";
-    //}
-    //windowObj.import.IPFrameMessagingLoadEvent = true;
-    //});
-
+    ko.applyBindings(new viewModel());
 })(this, IP, ko);
