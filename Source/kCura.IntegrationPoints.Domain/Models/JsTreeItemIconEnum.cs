4f72f416fd59619f83e728cc8c7c3b9c46701010
Stanislaw Wolny <swolny@kcura.com>
2016-10-04 14:01:48 +0200

    REL-108104 updated jstree ux implementation

---
+++ Source/kCura.IntegrationPoints.Domain/Models/JsTreeItemIconEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace kCura.IntegrationPoints.Domain.Models
+{
+    public enum JsTreeItemIconEnum
+    {
+        [Description("jstree-folder-root")]
+        Root = 0,
+
+        [Description("jstree-folder")]
+        Folder = 10,
+
+        [Description("jstree-folder-search")]
+        SavedSearchFolder = 20,
+
+        [Description("jstree-search")]
+        SavedSearch = 30,
+
+        [Description("jstree-search-personal")]
+        SavedSearchPersonal = 40
+    }
+}
--- source/kCura.IntegrationPoints.Domain/Extensions/ArtifactExtensions.cs
+++ source/kCura.IntegrationPoints.Domain/Extensions/ArtifactExtensions.cs
@@ -3,16 +3,17 @@ using kCura.Relativity.Client;
 
 namespace kCura.IntegrationPoints.Domain.Extensions
 {
-	public static class ArtifactExtensions
-	{
-		public static JsTreeItemWithParentIdDTO ToTreeItemWithParentIdDTO(this Artifact artifact)
-		{
-			return new JsTreeItemWithParentIdDTO
-			{
-				Id = artifact.ArtifactID.ToString(),
-				ParentId = artifact.ParentArtifactID.ToString(),
-				Text = artifact.Name
-			};
-		}
-	}
+    public static class ArtifactExtensions
+    {
+        public static JsTreeItemWithParentIdDTO ToTreeItemWithParentIdDTO(this Artifact artifact)
+        {
+            return new JsTreeItemWithParentIdDTO
+            {
+                Id = artifact.ArtifactID.ToString(),
+                ParentId = artifact.ParentArtifactID.ToString(),
+                Text = artifact.Name,
+                Icon = JsTreeItemIconEnum.Folder.GetDescription()
+            };
+        }
+    }
 }
