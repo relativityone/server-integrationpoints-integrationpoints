891da6207435116eca5df6cef85be72c0a902a66
aarnesen <aarnesen@claritycon.com>
2016-10-04 12:27:32 -0500

    Now parsing fields in ImportProvider using WinEDDS.LoadFileReader

---
+++ Source/kCura.IntegrationPoints.ImportProvider.Parser/Models/ImportProviderSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kCura.IntegrationPoints.ImportProvider.Parser.Models
+{
+    public class ImportProviderSettings
+    {
+        public string InputType { get; set; }
+        public string HasStartLine { get; set; }
+        public string LineNumber { get; set; }
+        public string LoadFile { get; set; }
+    }
+}
