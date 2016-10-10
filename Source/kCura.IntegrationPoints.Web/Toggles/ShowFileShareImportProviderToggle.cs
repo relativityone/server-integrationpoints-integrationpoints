a63d3dcae6f84b7aeebc5ba8d71b8ba9c7ff2b9f
Bob Pacione <bpacione@kcura.com>
2016-10-10 17:20:35 -0500

    Adding toggle to hide Import provider

---
+++ Source/kCura.IntegrationPoints.Web/Toggles/ShowFileShareImportProviderToggle.cs
@@ -0,0 +1,9 @@
+﻿using Relativity.Toggles;
+
+namespace kCura.IntegrationPoints.Web.Toggles
+{
+    [DefaultValue(false)]
+    public class ShowFileShareImportProviderToggle : IToggle
+    {
+    }
+}
