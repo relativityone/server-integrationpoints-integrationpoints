6f39c5c6dea7ef8a0a68e31f9bf0dc5f01879d18
Stanislaw Wolny <swolny@kcura.com>
2016-10-12 10:02:59 +0200

    REL-198853 added exception logging to Kepler services

---
+++ Source/kCura.IntegrationPoints.Services.Tests/DocumentManagerLoggingTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using NSubstitute;
+using NUnit.Framework;
+using Relativity.Logging;
+
+namespace kCura.IntegrationPoints.Services.Tests
+{
+    [TestFixture]
+    public class DocumentManagerLoggingTests
+    {
+        [Test]
+        public void ItShouldLogErrorFromGetPercentagePushedToReviewAsync()
+        {
+            // arrange
+            var logger = Substitute.For<ILog>();
+            var manager = new DocumentManager(logger);
+            var request = new PercentagePushedToReviewRequest();
+
+            // act & assert
+            Assert.Throws<AggregateException>(() =>
+            {
+                PercentagePushedToReviewModel actual = manager.GetPercentagePushedToReviewAsync(request).Result;
+            });
+
+            logger.Received().LogError(Arg.Any<Exception>(), Arg.Any<string>(), Arg.Any<object[]>());
+        }
+
+        [Test]
+        public void ItShouldLogErrorFromGetCurrentPromotionStatusAsync()
+        {
+            // arrange
+            var logger = Substitute.For<ILog>();
+            var manager = new DocumentManager(logger);
+            var request = new CurrentPromotionStatusRequest();
+
+            // act & assert
+            Assert.Throws<AggregateException>(() =>
+            {
+                CurrentPromotionStatusModel actual = manager.GetCurrentPromotionStatusAsync(request).Result;
+            });
+
+            logger.Received().LogError(Arg.Any<Exception>(), Arg.Any<string>(), Arg.Any<object[]>());
+        }
+
+        [Test]
+        public void ItShouldLogErrorFromGetHistoricalPromotionStatusAsync()
+        {
+            // arrange
+            var logger = Substitute.For<ILog>();
+            var manager = new DocumentManager(logger);
+            var request = new HistoricalPromotionStatusRequest();
+
+            // act & assert
+            Assert.Throws<AggregateException>(() =>
+            {
+                HistoricalPromotionStatusSummaryModel actual = manager.GetHistoricalPromotionStatusAsync(request).Result;
+            });
+
+            logger.Received().LogError(Arg.Any<Exception>(), Arg.Any<string>(), Arg.Any<object[]>());
+        }
+    }
+}
