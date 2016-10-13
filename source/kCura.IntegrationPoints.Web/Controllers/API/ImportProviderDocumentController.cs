155f325ecfa9aea739523f7cccff741109724fce
aarnesen <aarnesen@claritycon.com>
2016-10-13 15:23:56 -0500

    REL-108418 now injecting field parser factory into ImportProviderDocument controller
    (cherry picked from commit dbc9f816aeaa0dda7ae2ea56867aef69cf3390f9)

--- source/kCura.IntegrationPoints.Web/Controllers/API/ImportProviderDocumentController.cs
+++ source/kCura.IntegrationPoints.Web/Controllers/API/ImportProviderDocumentController.cs
@@ -3,10 +3,18 @@ using System.Linq;
 using System.Web.Http;
 using kCura.IntegrationPoints.Web.Attributes;
 
+using kCura.IntegrationPoints.ImportProvider.Parser.Interfaces;
+
 namespace kCura.IntegrationPoints.Web.Controllers.API
 {
     public class ImportProviderDocumentController : ApiController
     {
+        private IFieldParserFactory _fieldParserFactory;
+        public ImportProviderDocumentController(IFieldParserFactory fieldParserFactory)
+        {
+            _fieldParserFactory = fieldParserFactory;
+        }
+
         [HttpGet]
         [LogApiExceptionFilter(Message = "Unable to retrieve list of Ascii Delimiters.")]
         public IHttpActionResult GetAsciiDelimiters()
