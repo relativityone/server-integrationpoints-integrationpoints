5667db41d98333db56a8866bdeda09a2336755b0
Dan Nelson <dnelson@kcura.com>
2016-10-13 15:22:44 -0500

    Addressing code review feedback
    - Updating comments
    - Changing var to type

--- Source/JsonLoader/Eventhandlers/Uninstaller.cs
+++ Source/JsonLoader/Eventhandlers/Uninstaller.cs
@@ -7,7 +7,7 @@ namespace JsonLoader.Eventhandlers
 	[Guid("A4AF9126-A773-4B62-B155-7B54C6BDDC19")]
 	public class Uninstaller : IntegrationPointSourceProviderUninstaller
 	{
-		// Intentionally empty
-		// Custom actions only
+		// This uninstaller removes the provider from the Integration Points drop-down.
+		// This class is intentionally left empty. Any custom uninstall logic must be created in its own separate PreUninstallEventHandlers or PostUninstallEventHandlers.
 	}
 }
--- Source/JsonLoader/JsonHelper.cs
+++ Source/JsonLoader/JsonHelper.cs
@@ -7,19 +7,19 @@ namespace JsonLoader
 	{
 		public virtual string ReadFields(string options)
 		{
-			var settings = GetSettings(options);
+			JsonSettings settings = GetSettings(options);
 			return File.ReadAllText(settings.FieldLocation);
 		}
 
 		public virtual string ReadData(string options)
 		{
-			var settings = GetSettings(options);
+			JsonSettings settings = GetSettings(options);
 			return File.ReadAllText(settings.DataLocation);
 		}
 
 		public virtual JsonSettings GetSettings(string options)
 		{
-			var settings = Newtonsoft.Json.JsonConvert.DeserializeObject<JsonSettings>(options);
+			JsonSettings settings = Newtonsoft.Json.JsonConvert.DeserializeObject<JsonSettings>(options);
 			return settings;
 		}
 	}
