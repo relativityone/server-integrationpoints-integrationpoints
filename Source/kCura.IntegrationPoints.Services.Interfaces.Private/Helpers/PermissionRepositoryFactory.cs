cd5de9f24e5837c8ee4608b4dc6576e089296b76
Patryk Stepien <pstepien@kcura.com>
2016-10-14 13:10:04 +0200

    REL-100952 [Security] Unauthorized user can make calls to IntegrationPointsModule.Integration Point Manager service

---
+++ Source/kCura.IntegrationPoints.Services.Interfaces.Private/Helpers/PermissionRepositoryFactory.cs
@@ -0,0 +1,14 @@
+﻿using kCura.IntegrationPoints.Data.Repositories;
+using kCura.IntegrationPoints.Data.Repositories.Implementations;
+using Relativity.API;
+
+namespace kCura.IntegrationPoints.Services.Interfaces.Private.Helpers
+{
+	public class PermissionRepositoryFactory : IPermissionRepositoryFactory
+	{
+		public IPermissionRepository Create(IHelper helper, int workspaceArtifactId)
+		{
+			return new PermissionRepository(helper, workspaceArtifactId);
+		}
+	}
+}
--- Source/kCura.IntegrationPoints.Services.Interfaces.Private/KeplerServiceBase.cs
+++ Source/kCura.IntegrationPoints.Services.Interfaces.Private/KeplerServiceBase.cs
@@ -1,25 +1,56 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using kCura.IntegrationPoints.Data;
+using kCura.IntegrationPoints.Services.Interfaces.Private.Helpers;
 using Relativity.Logging;
 
 namespace kCura.IntegrationPoints.Services
 {
-    public abstract class KeplerServiceBase : IKeplerService
-    {
-        private readonly ILog _logger;
+	public abstract class KeplerServiceBase : IKeplerService
+	{
+		private const string _NO_ACCESS_EXCEPTION_MESSAGE = "You do not have permission to access this service.";
 
-        public ILog Logger
-        {
-            get { return _logger; }
-        }
+		private readonly IPermissionRepositoryFactory _permissionRepositoryFactory;
 
-        public KeplerServiceBase(ILog logger)
-        {
-            _logger = logger;
-        }
+		/// <summary>
+		///     Since we cannot register any dependencies for Kepler Service we have to create separate constructors for runtime
+		///     and for testing
+		/// </summary>
+		/// <param name="logger"></param>
+		/// <param name="permissionRepositryFactory"></param>
+		protected KeplerServiceBase(ILog logger, IPermissionRepositoryFactory permissionRepositryFactory)
+		{
+			_permissionRepositoryFactory = permissionRepositryFactory;
+			Logger = logger;
+		}
 
-        public async Task<bool> PingAsync()
-        {
-            return await Task.Run(() => true).ConfigureAwait(false);
-        }
-    }
+		protected KeplerServiceBase(ILog logger) : this(logger, new PermissionRepositoryFactory())
+		{
+		}
+
+		public ILog Logger { get; }
+
+		public async Task<bool> PingAsync()
+		{
+			return await Task.Run(() => true).ConfigureAwait(false);
+		}
+
+		protected void CheckPermissions(int workspaceId)
+		{
+			try
+			{
+				var permissionRepository = _permissionRepositoryFactory.Create(global::Relativity.API.Services.Helper, workspaceId);
+				if (permissionRepository.UserHasPermissionToAccessWorkspace() &&
+					permissionRepository.UserHasArtifactTypePermission(new Guid(ObjectTypeGuids.IntegrationPoint), ArtifactPermission.View))
+				{
+					return;
+				}
+			}
+			catch (Exception e)
+			{
+				Logger.LogError(e, "Failed to validate permissions for Integration Points Kepler Service. Denying access.");
+			}
+			throw new Exception(_NO_ACCESS_EXCEPTION_MESSAGE);
+		}
+	}
 }
