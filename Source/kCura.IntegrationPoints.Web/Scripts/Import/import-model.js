bddcf036d31765d1edcaebee9f6c83a0bbe830a2
jaycdave88 <jaycdave@gmail.com>
2016-10-18 12:09:30 -0500

    Refactored import-modal
    (cherry picked from commit 075f0b1779dc1fa6bf6e9a7bced15e531306d01b)

--- Source/kCura.IntegrationPoints.Web/Scripts/Import/import-model.js
+++ Source/kCura.IntegrationPoints.Web/Scripts/Import/import-model.js
@@ -11,18 +11,14 @@
 
     var viewModel = function () {
         //TODO: refactor viewmodel
-        //- make self / this usage consistent
-        //- consider making selectedImportType a computed observable that indexes into importTypes, and pulling the string from the 'value' field. would need a new variable like 'activeImportType'
-        //- assignment to  ProcessingSourceLocationArtifactId can never be true, so always defaults to 0; why is the || used?
-        //- assignment to HasBeenRun references a non-existent self.hasBeenRun; again, can never be present so always defaults to false
         //- why are we passing self.Fileshare into the ko.observable function that defines... this.Fileshare? understand required / onlyIf; is this knockout or integration points?
 
         var self = this;
         self.selectedImportType = ko.observable("document");
         self.importTypes = ko.observableArray([
-            new ImportTypeModel({ id: "1", value: "document", name: "Document Load File" }),
-            new ImportTypeModel({ id: "2", value: "image", name: "Image Load File" }),
-            new ImportTypeModel({ id: "3", value: "production", name: "Production Load File" })
+            new ImportTypeModel({ value: "document", name: "Document Load File" }),
+            new ImportTypeModel({ value: "image", name: "Image Load File" }),
+            new ImportTypeModel({ value: "production", name: "Production Load File" })
         ]);
 
         self.populateFileColumnHeaders = ko.observable();
@@ -65,8 +61,8 @@
         };
 
         self.ProcessingSourceLocationList = ko.observableArray([]);
-        self.ProcessingSourceLocationArtifactId = this.ProcessingSourceLocation || 0;
-        self.HasBeenRun = ko.observable(self.hasBeenRun || false);
+        self.ProcessingSourceLocationArtifactId = 0;
+        self.HasBeenRun = ko.observable(false);
         self.ProcessingSourceLocation = ko.observable(self.ProcessingSourceLocationArtifactId)
             .extend({
                 required: true
