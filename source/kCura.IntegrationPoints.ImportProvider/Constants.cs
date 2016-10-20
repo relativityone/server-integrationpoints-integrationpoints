72e38c411163fe5296a51652783d12040c57be1a
aarnesen <aarnesen@claritycon.com>
2016-10-20 10:28:48 -0500

    code cleanup; switching from storing Guid type in Constants to storing Guid string representation

--- source/kCura.IntegrationPoints.ImportProvider/Constants.cs
+++ source/kCura.IntegrationPoints.ImportProvider/Constants.cs
@@ -10,7 +10,7 @@ namespace kCura.IntegrationPoints.ImportProvider
     {
         public class Guids
         {
-            public static Guid RelativityInterationPoints_Relativity_Application = new Guid("DCF6E9D1-22B6-4DA3-98F6-41381E93C30C");
+            public const String RelativityInterationPoints_Relativity_Application = "DCF6E9D1-22B6-4DA3-98F6-41381E93C30C";
             public const String ImportProviderEventHandler = "548F0873-8E5E-4DA6-9F27-5F9CDA764636";
         }
     }
