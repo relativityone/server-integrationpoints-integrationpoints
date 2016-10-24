8c530f832366c37675ae115ca9178b52c4646ca9
aarnesen <aarnesen@claritycon.com>
2016-10-24 12:57:05 -0500

    REL-109879 added unit test for get fields

--- Source/kCura.IntegrationPoints.ImportProvider.Tests/ImportProviderTests.cs
+++ Source/kCura.IntegrationPoints.ImportProvider.Tests/ImportProviderTests.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using NSubstitute;
 
+using kCura.IntegrationPoints.Contracts.Models;
 using kCura.IntegrationPoints.ImportProvider;
 using kCura.IntegrationPoints.ImportProvider.Parser.Interfaces;
 
@@ -15,6 +16,8 @@ namespace kCura.IntegrationPoints.ImportProvider.Tests
     [TestFixture]
     public class ImportProviderTests
     {
+        private int MAX_COLS = 100;
+
         private IFieldParser _fieldParser;
         private IFieldParserFactory _fieldParserFactory;
         private IDataReaderFactory _dataReaderFactory;
@@ -30,9 +33,51 @@ namespace kCura.IntegrationPoints.ImportProvider.Tests
         }
 
         [Test]
-        public void ImportProviderConstructorRuns()
+        public void ImportProviderCanGetFields()
         {
-            ImportProvider p = new ImportProvider(_fieldParserFactory, _dataReaderFactory, _enumerableParserFactory);
+            List<string> testData = TestHeaders((new Random()).Next(MAX_COLS));
+            IEnumerator<string> tdEnum = testData.GetEnumerator();
+            tdEnum.MoveNext();
+
+            _fieldParserFactory.GetFieldParser("").ReturnsForAnyArgs(_fieldParser);
+            _fieldParser.GetFields().Returns(testData);
+
+            ImportProvider ip = new ImportProvider(_fieldParserFactory, _dataReaderFactory, _enumerableParserFactory);
+            IEnumerable<FieldEntry> ipFields = ip.GetFields(string.Empty);
+
+            Assert.AreEqual(testData.Count, ipFields.Count());
+
+            int tdIndex = 0;
+            foreach (FieldEntry ipEntry in  ipFields)
+            {
+                Assert.AreEqual(tdEnum.Current, ipEntry.DisplayName);
+                Assert.AreEqual(tdIndex, Int32.Parse(ipEntry.FieldIdentifier));
+                tdIndex++;
+                tdEnum.MoveNext();
+            }
+        }
+
+        private List<string> TestHeaders(int fieldCount)
+        {
+            return
+                Enumerable
+                .Range(0, fieldCount)
+                .Select(x => string.Format("col-{0}", x))
+                .ToList();
+        }
+
+        private List<List<string>> TestData(int fieldCount, int rowCount)
+        {
+            return
+                Enumerable
+                .Range(0, rowCount)
+                .Select(row =>
+                        Enumerable
+                        .Range(0, fieldCount)
+                        .Select(col => string.Format("r{0}c{1}", row, col))
+                        .ToList())
+                .ToList();
         }
     }
 }
+
