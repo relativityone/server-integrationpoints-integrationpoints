d886486bdaab32ed12bcb38094e39f7acb0efe01
Patryk Stepien <pstepien@kcura.com>
2016-10-27 13:50:20 +0200

    Fix build error after merging pull request

--- Source/kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration/TestCases/ItShouldExportProductionSetWithImages.cs
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration/TestCases/ItShouldExportProductionSetWithImages.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using kCura.IntegrationPoints.Core.Models;
 using kCura.IntegrationPoints.FilesDestinationProvider.Core;
 using kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration.Helpers;
 using kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration.Model;
