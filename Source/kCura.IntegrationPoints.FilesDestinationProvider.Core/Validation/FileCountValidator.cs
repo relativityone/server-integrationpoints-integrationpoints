a2e382a9dfe083b6116d4dceb2a38958f51a719d
Patryk Stepien <pstepien@kcura.com>
2016-10-28 09:42:57 +0200

    REL-110293 Check documents count on export "Save" action
    Add proper warning message

--- Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/Validation/FileCountValidator.cs
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/Validation/FileCountValidator.cs
@@ -4,12 +4,14 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.Validation
 {
 	public class FileCountValidator : IFileCountValidator
 	{
+		private const string _FILE_COUNT_WARNING = "There are no items to export. Verify your source location.";
+
 		public ExportSettingsValidationResult Validate(int totalDocCount)
 		{
 			var result = new ExportSettingsValidationResult {IsValid = totalDocCount > 0};
 			if (!result.IsValid)
 			{
-				result.Message = "....";
+				result.Message = _FILE_COUNT_WARNING;
 			}
 			return result;
 		}
