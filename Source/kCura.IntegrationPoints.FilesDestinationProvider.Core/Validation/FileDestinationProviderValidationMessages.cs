8ce00702c2527c7506ff3be63f4315986b663ab0
Stanislaw Wolny <swolny@kcura.com>
2016-11-30 19:19:13 +0100

    REL-105011 added folder artifact validation

--- Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/Validation/FileDestinationProviderValidationMessages.cs
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/Validation/FileDestinationProviderValidationMessages.cs
@@ -8,7 +8,7 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.Validation
 		public static readonly string FILE_COUNT_WARNING = "There are no items to export. Verify your source location.";
 		public static readonly string PRODUCTION_NOT_EXIST = "Production does not exists";
 		public static readonly string VIEW_NOT_EXIST = "View does not exists";
-
+		public static readonly string ARTIFACT_NOT_EXIST = "artifact does not exists";
 		public static readonly string RDO_INVALID_EXPORT_NATIVE_OPTION 
 			= "Export Natives or/and Include Native Files options should not be selected for RDO that does not contain File type column";
 	}
