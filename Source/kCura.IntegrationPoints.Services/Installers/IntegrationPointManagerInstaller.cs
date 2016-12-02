d26f0924b9bb0bfa3e3474e2e8a6794beab6c544
Patryk Stepien <pstepien@kcura.com>
2016-12-02 14:04:16 +0100

    Fix merge

--- Source/kCura.IntegrationPoints.Services/Installers/IntegrationPointManagerInstaller.cs
+++ Source/kCura.IntegrationPoints.Services/Installers/IntegrationPointManagerInstaller.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
@@ -7,20 +6,18 @@ using kCura.IntegrationPoints.Core.Installers;
 using kCura.IntegrationPoints.Core.Services.ServiceContext;
 using kCura.IntegrationPoints.Data;
 using kCura.IntegrationPoints.Data.Installers;
+using kCura.IntegrationPoints.Services.Repositories;
 using kCura.Relativity.Client;
 using Relativity.API;
 
-namespace kCura.IntegrationPoints.Services
+namespace kCura.IntegrationPoints.Services.Installers
 {
-	public class ServiceInstaller
+	public class IntegrationPointManagerInstaller : IInstaller
 	{
-		private readonly int _caseArtifactId;
 		private readonly List<IWindsorInstaller> _dependencies;
 
-		public ServiceInstaller(int caseArtifactId)
+		public IntegrationPointManagerInstaller()
 		{
-			_caseArtifactId = caseArtifactId;
-
 			_dependencies = new List<IWindsorInstaller>
 			{
 				new QueryInstallers(),
@@ -29,33 +26,36 @@ namespace kCura.IntegrationPoints.Services
 			};
 		}
 
-		public void Install(IWindsorContainer container, IConfigurationStore store)
+		public void Install(IWindsorContainer container, IConfigurationStore store, int workspaceId)
 		{
-			container.Register(Component.For<IServiceHelper>().UsingFactoryMethod(k => global::Relativity.API.Services.Helper, managedExternally: true));
-			container.Register(Component.For<IHelper>().UsingFactoryMethod(k => k.Resolve<IServiceHelper>(), managedExternally: true));
+			container.Register(Component.For<IServiceHelper>().UsingFactoryMethod(k => global::Relativity.API.Services.Helper, true));
+			container.Register(Component.For<IHelper>().UsingFactoryMethod(k => k.Resolve<IServiceHelper>(), true));
+			container.Register(Component.For<IUserInfo>().UsingFactoryMethod(k => k.Resolve<IServiceHelper>().GetAuthenticationManager().UserInfo, true));
+
 			container.Register(Component.For<IServiceContextHelper>()
 				.UsingFactoryMethod(k =>
 				{
 					IServiceHelper helper = k.Resolve<IServiceHelper>();
-					return new ServiceContextHelperForKelperService(helper, _caseArtifactId);
+					return new ServiceContextHelperForKelperService(helper, workspaceId);
 				}));
 			container.Register(
 				Component.For<IWorkspaceDBContext>()
 					.ImplementedBy<WorkspaceContext>()
-					.UsingFactoryMethod(k => new WorkspaceContext(k.Resolve<IHelper>().GetDBContext(_caseArtifactId)))
+					.UsingFactoryMethod(k => new WorkspaceContext(k.Resolve<IHelper>().GetDBContext(workspaceId)))
 					.LifeStyle.Transient);
 
 			container.Register(
 				Component.For<IRSAPIClient>()
-				.UsingFactoryMethod(k =>
-				{
-					IRSAPIClient client = container.Resolve<IServiceHelper>().GetServicesManager().CreateProxy<IRSAPIClient>(ExecutionIdentity.CurrentUser);
-					client.APIOptions.WorkspaceID = _caseArtifactId;
-					return client;
-				})
-				.LifeStyle.Transient);
+					.UsingFactoryMethod(k =>
+					{
+						IRSAPIClient client = container.Resolve<IServiceHelper>().GetServicesManager().CreateProxy<IRSAPIClient>(ExecutionIdentity.CurrentUser);
+						client.APIOptions.WorkspaceID = workspaceId;
+						return client;
+					})
+					.LifeStyle.Transient);
 
 			container.Register(Component.For<IServicesMgr>().UsingFactoryMethod(k => global::Relativity.API.Services.Helper.GetServicesManager()));
+			container.Register(Component.For<IIntegrationPointRepository>().ImplementedBy<IntegrationPointRepository>().LifestyleTransient());
 
 			foreach (IWindsorInstaller dependency in _dependencies)
 			{
