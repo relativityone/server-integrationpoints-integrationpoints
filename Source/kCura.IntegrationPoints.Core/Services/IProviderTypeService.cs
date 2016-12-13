337cec6443640322236a522314b5b0a01bafd89d
Patryk Stepien <pstepien@kcura.com>
2016-12-13 13:28:44 +0100

    REL-117186 Create Provider Enum for all providers

---
+++ Source/kCura.IntegrationPoints.Core/Services/IProviderTypeService.cs
@@ -0,0 +1,9 @@
+﻿using kCura.IntegrationPoints.Core.Models;
+
+namespace kCura.IntegrationPoints.Core.Services
+{
+	public interface IProviderTypeService
+	{
+		ProviderType GetProviderType(int sourceProviderId, int destinationProviderId);
+	}
+}
