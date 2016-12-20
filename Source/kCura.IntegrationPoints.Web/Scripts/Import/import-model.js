72df8ecd7b8e76210df111b3c071b7894dce0741
Bob Pacione <bpacione@kcura.com>
2016-12-20 15:29:01 -0600

    Merge pull request #69 in IN/integrationpoints from REL-117566 to develop
    Squashed commit of the following:
    
    commit b4f147f332f30fa147dcfab80801a2f451fcb5a3
    Merge: 50484b2 1a15a5d
    Author: Bob Pacione <bpacione@kcura.com>
    Date:   Tue Dec 20 15:27:14 2016 -0600
    
        Merge branch 'develop' into REL-117566
    
    commit 50484b253df1f61f0963d2f35bbb43ce777fff39
    Author: Bob Pacione <bpacione@kcura.com>
    Date:   Tue Dec 20 11:08:08 2016 -0600
    
        REL-117566 updating logic to parseInt and exclude negative values if they are manually typed into the text box
    
    commit 6c0b85252b05dd24abd28d6bbfe4dde27a3152f1
    Merge: 67a4333 5f61c43
    Author: Bob Pacione <bpacione@kcura.com>
    Date:   Mon Dec 19 09:55:38 2016 -0600
    
        mergind in develop and fixing conflicts
    
    commit 67a4333e8cf2847175866b6d6b40a4deb38757c0
    Author: jaycdave88 <jaycdave@gmail.com>
    Date:   Thu Dec 15 13:30:33 2016 -0600
    
        REL-117566: Added client-side validation around the Start Line for Import Settings step2.

--- Source/kCura.IntegrationPoints.Web/Scripts/Import/import-model.js
+++ Source/kCura.IntegrationPoints.Web/Scripts/Import/import-model.js
@@ -1,164 +1,172 @@
 ﻿'use strict';
 (function (windowObj, root, ko) {
+	ko.validation.rules.pattern.message = 'Invalid.';
+	ko.validation.registerExtenders();
 
-    ko.validation.rules.pattern.message = 'Invalid.';
-    ko.validation.registerExtenders();
+	ko.validation.configure({
+		insertMessages: false,
+	}, true);
 
-    ko.validation.configure({
-        insertMessages: false,
-    }, true);
+	ko.extenders.deferValidation = function (target, option) {
+		if (option) {
+			target.subscribe(function () {
+				target.isModified(false);
+			});
+		}
+		return target;
+	};
 
-    ko.extenders.deferValidation = function (target, option) {
-        if (option) {
-            target.subscribe(function () {
-                target.isModified(false);
-            });
-        }
-        return target;
-    };
+	var ImportTypeEnum = {
+		Document: 0,
+		Image: 1,
+		Production: 2
+	};
 
-    var ImportTypeEnum = {
-    	Document: 0,
-    	Image: 1,
-    	Production: 2
-    };
+	var viewModel = function () {
+		var self = this;
 
-    var viewModel = function () {
-        var self = this;
-
-        self.selectedImportType = ko.observable();
-        self.setSelectedImportType = function(data) {
-	        self.selectedImportType(data);
-        }
+		self.selectedImportType = ko.observable();
+		self.setSelectedImportType = function(data) {
+			self.selectedImportType(data);
+		}
 
 
-	    self.importTypes = ko.observableArray([]);
-	    $.getJSON(root.utils.generateWebAPIURL("/ImportProviderDocument/GetImportTypes"), function (data) {
-		    self.importTypes(data);
-		    self.setSelectedImportType(ImportTypeEnum.Document);
-	    });
+		self.importTypes = ko.observableArray([]);
+		$.getJSON(root.utils.generateWebAPIURL("/ImportProviderDocument/GetImportTypes"), function (data) {
+			self.importTypes(data);
+			self.setSelectedImportType(ImportTypeEnum.Document);
+		});
 
-        self.populateFileColumnHeaders = ko.observable();
-        self.setPopulateFileColumnHeaders = function (data) {
-            self.populateFileColumnHeaders(data);
-        };
+		self.populateFileColumnHeaders = ko.observable();
+		self.setPopulateFileColumnHeaders = function (data) {
+			self.populateFileColumnHeaders(data);
+		};
 
-        self.startLine = ko.observable("1");
+		self.startLine = ko.observable("1").extend({
+			validation: {
+				validator: function (val) {
+					var intVal = parseInt(val);
+					return intVal > 0;
+				},
+				message: 'The field must be greater than 0.'
+			},
+			required: true
+		});
 
-        self.fileContainsColumn = ko.observable("true");
+		self.fileContainsColumn = ko.observable("true");
 
 		//image/production import knockout bindings for Numbering section.
-        self.loadFilePageIdSelected = ko.observable();
-        self.autoNumberPagesSelected = ko.observable();
-	    self.copyFilesToDocumentRepository = ko.observable();
+		self.loadFilePageIdSelected = ko.observable();
+		self.autoNumberPagesSelected = ko.observable();
+		self.copyFilesToDocumentRepository = ko.observable();
 
-        self.OverwriteOptions = ko.observableArray(['Append Only', 'Overlay Only', 'Append/Overlay']);
-        self.SelectedOverwrite = ko.observable(self.SelectedOverwrite || 'Append Only');
+		self.OverwriteOptions = ko.observableArray(['Append Only', 'Overlay Only', 'Append/Overlay']);
+		self.SelectedOverwrite = ko.observable(self.SelectedOverwrite || 'Append Only');
 
-	    self.overlayIdentifiers = ko.observableArray([]);
-        self.productionSets = ko.observableArray([]);
+		self.overlayIdentifiers = ko.observableArray([]);
+		self.productionSets = ko.observableArray([]);
 
-        self.asciiDelimiters = ko.observableArray([]);
-        self.setAsciiDelimiters = function (data) {
-            self.asciiDelimiters(data);
-        };
+		self.asciiDelimiters = ko.observableArray([]);
+		self.setAsciiDelimiters = function (data) {
+			self.asciiDelimiters(data);
+		};
 
-        self.selectedColumnAsciiDelimiter = ko.observable();
-        self.setSelectedColumnAsciiDelimiters = function (data) {
-            self.selectedColumnAsciiDelimiter(data);
-        };
+		self.selectedColumnAsciiDelimiter = ko.observable();
+		self.setSelectedColumnAsciiDelimiters = function (data) {
+			self.selectedColumnAsciiDelimiter(data);
+		};
 
-        self.selectedQuoteAsciiDelimiter = ko.observable();
-        self.setSelectedQuoteAsciiDelimiters = function (data) {
-            self.selectedQuoteAsciiDelimiter(data);
-        };
+		self.selectedQuoteAsciiDelimiter = ko.observable();
+		self.setSelectedQuoteAsciiDelimiters = function (data) {
+			self.selectedQuoteAsciiDelimiter(data);
+		};
 
-        self.selectedNewLineAsciiDelimiter = ko.observable();
-        self.setSelectedNewLineAsciiDelimiters = function (data) {
-            self.selectedNewLineAsciiDelimiter(data);
-        };
+		self.selectedNewLineAsciiDelimiter = ko.observable();
+		self.setSelectedNewLineAsciiDelimiters = function (data) {
+			self.selectedNewLineAsciiDelimiter(data);
+		};
 
-        self.selectedMultiLineAsciiDelimiter = ko.observable();
-        self.setSelectedMultiLineAsciiDelimiters = function (data) {
-            self.selectedMultiLineAsciiDelimiter(data);
-        };
+		self.selectedMultiLineAsciiDelimiter = ko.observable();
+		self.setSelectedMultiLineAsciiDelimiters = function (data) {
+			self.selectedMultiLineAsciiDelimiter(data);
+		};
 
-        self.selectedNestedValueAsciiDelimiter = ko.observable();
-        self.setSelectedNestedValueAsciiDelimiters = function (data) {
-            self.selectedNestedValueAsciiDelimiter(data);
-        };
+		self.selectedNestedValueAsciiDelimiter = ko.observable();
+		self.setSelectedNestedValueAsciiDelimiters = function (data) {
+			self.selectedNestedValueAsciiDelimiter(data);
+		};
 
-        self.ProcessingSourceLocationArtifactId = 0;
+		self.ProcessingSourceLocationArtifactId = 0;
 
-        self.ProcessingSourceLocationList = ko.observableArray([]);
+		self.ProcessingSourceLocationList = ko.observableArray([]);
 
-        self.HasBeenRun = ko.observable(false);
+		self.HasBeenRun = ko.observable(false);
 
-        self.ProcessingSourceLocation = ko.observable(self.ProcessingSourceLocationArtifactId).extend({
-            required: true,
-            deferValidation: true
-        });
+		self.ProcessingSourceLocation = ko.observable(self.ProcessingSourceLocationArtifactId).extend({
+			required: true,
+			deferValidation: true
+		});
 
-        self.Fileshare = ko.observable().extend({
-            required: true,
-            deferValidation: true
-        });
+		self.Fileshare = ko.observable().extend({
+			required: true,
+			deferValidation: true
+		});
 
-        self.GetSelectedProcessingSourceLocationPath = function (artifactId) {
-            var selectedPath = ko.utils.arrayFirst(self.ProcessingSourceLocationList(), function (item) {
-                if (item.artifactId === artifactId) {
-                    return item;
-                }
-            });
-            return selectedPath;
-        };
+		self.GetSelectedProcessingSourceLocationPath = function (artifactId) {
+			var selectedPath = ko.utils.arrayFirst(self.ProcessingSourceLocationList(), function (item) {
+				if (item.artifactId === artifactId) {
+					return item;
+				}
+			});
+			return selectedPath;
+		};
 
-        self.DataFileEncodingTypeValue = "Select...";
+		self.DataFileEncodingTypeValue = "Select...";
 
-        self.DataFileEncodingType = ko.observable(self.DataFileEncodingTypeValue).extend({
-            required: true
-        });
+		self.DataFileEncodingType = ko.observable(self.DataFileEncodingTypeValue).extend({
+			required: true
+		});
 
-        //Populate file encoding dropdown
-        self.FileEncodingTypeList = ko.observableArray([]);
-        self._UpdateFileEncodingTypeList = function () {
-            IP.data.ajax({ type: 'get', url: IP.utils.generateWebAPIURL('GetAvailableEncodings') }).then(function (result) {
-                function Group(label, children) {
-                    this.label = ko.observable(label);
-                    this.children = ko.observableArray(children);
-                };
-                function Option(displayName, name) {
-                    this.displayName = ko.observable(displayName);
-                    this.name = ko.observable(name);
-                };
+		//Populate file encoding dropdown
+		self.FileEncodingTypeList = ko.observableArray([]);
+		self._UpdateFileEncodingTypeList = function () {
+			IP.data.ajax({ type: 'get', url: IP.utils.generateWebAPIURL('GetAvailableEncodings') }).then(function (result) {
+				function Group(label, children) {
+					this.label = ko.observable(label);
+					this.children = ko.observableArray(children);
+				};
+				function Option(displayName, name) {
+					this.displayName = ko.observable(displayName);
+					this.name = ko.observable(name);
+				};
 
-                var favorite = [];
-                var others = [];
+				var favorite = [];
+				var others = [];
 
-                for (var i = 0; i < result.length; i++) {
-                    var option = new Option(result[i].displayName, result[i].name);
+				for (var i = 0; i < result.length; i++) {
+					var option = new Option(result[i].displayName, result[i].name);
 
-                    if ($.inArray(result[i].name, ['utf-16', 'utf-16BE', 'utf-8', 'Windows-1252']) >= 0) {
-                        favorite.push(option);
-                    } else {
-                        others.push(option);
-                    }
-                }
+					if ($.inArray(result[i].name, ['utf-16', 'utf-16BE', 'utf-8', 'Windows-1252']) >= 0) {
+						favorite.push(option);
+					} else {
+						others.push(option);
+					}
+				}
 
-                // By default user should see only 4 default options: Unicode, Unicode (Big-Endian), Unicode (UTF-8), Western European (Windows) as in RDC
-                //self.FileEncodingTypeList([new Group("", [new Option("Select...", "")]), new Group("Favorite", favorite), new Group("Others", others)]);
+				// By default user should see only 4 default options: Unicode, Unicode (Big-Endian), Unicode (UTF-8), Western European (Windows) as in RDC
+				//self.FileEncodingTypeList([new Group("", [new Option("Select...", "")]), new Group("Favorite", favorite), new Group("Others", others)]);
 
-                self.FileEncodingTypeList([new Group("", [new Option("Unicode (UTF-8)", "utf-8")]), new Group("Favorite", favorite), new Group("Others", others)]);
+				self.FileEncodingTypeList([new Group("", [new Option("Unicode (UTF-8)", "utf-8")]), new Group("Favorite", favorite), new Group("Others", others)]);
 
-                self.DataFileEncodingType(self.DataFileEncodingTypeValue);
-                self.DataFileEncodingType.isModified(false);
-            });
-        }
-        self._UpdateFileEncodingTypeList();
+				self.DataFileEncodingType(self.DataFileEncodingTypeValue);
+				self.DataFileEncodingType.isModified(false);
+			});
+		}
+		self._UpdateFileEncodingTypeList();
 
-    }
-    windowObj.RelativityImport.koModel = new viewModel();
-    windowObj.RelativityImport.koErrors = ko.validation.group(windowObj.RelativityImport.koModel);
-    ko.applyBindings(windowObj.RelativityImport.koModel);
+	}
+	windowObj.RelativityImport.koModel = new viewModel();
+	windowObj.RelativityImport.koErrors = ko.validation.group(windowObj.RelativityImport.koModel);
+	ko.applyBindings(windowObj.RelativityImport.koModel);
 
 })(this, IP, ko);
