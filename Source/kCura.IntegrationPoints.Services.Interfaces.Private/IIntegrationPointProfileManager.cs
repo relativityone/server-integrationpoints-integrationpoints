588d18e0d79b083e3abcea7d718e1d73bf06e113
Patryk Stepien <pstepien@kcura.com>
2016-12-22 12:57:09 +0100

    REL-114902 Add IntegrationPointProfileManager
    Add comments to public API

--- Source/kCura.IntegrationPoints.Services.Interfaces.Private/IIntegrationPointProfileManager.cs
+++ Source/kCura.IntegrationPoints.Services.Interfaces.Private/IIntegrationPointProfileManager.cs
@@ -10,12 +10,33 @@ namespace kCura.IntegrationPoints.Services
 	[ServiceAudience(Audience.Private)]
 	public interface IIntegrationPointProfileManager : IKeplerService, IDisposable
 	{
+		/// <summary>
+		/// Create integration point profile from the given model
+		/// </summary>
+		/// <param name="request">Integration point profile model to create integration point object.</param>
+		/// <returns>Integration point model</returns>
 		Task<IntegrationPointModel> CreateIntegrationPointProfileAsync(CreateIntegrationPointRequest request);
 
+		/// <summary>
+		/// Update integration point profile from the given model
+		/// </summary>
+		/// <param name="request">Integration point profile model to update integration point's object.</param>
+		/// <returns>Integration point model</returns>
 		Task<IntegrationPointModel> UpdateIntegrationPointProfileAsync(CreateIntegrationPointRequest request);
 
+		/// <summary>
+		/// Retrieve integration point profile object
+		/// </summary>
+		/// <param name="workspaceArtifactId">The workspace's id where the requested integration point profile object lives</param>
+		/// <param name="integrationPointProfileArtifactId">The integration point profile's artifact id</param>
+		/// <returns>Integration point model</returns>
 		Task<IntegrationPointModel> GetIntegrationPointProfileAsync(int workspaceArtifactId, int integrationPointProfileArtifactId);
 
+		/// <summary>
+		/// Get all available integration point profile objects in the workspace.
+		/// </summary>
+		/// <param name="workspaceArtifactId">An artifact id of the workspace.</param>
+		/// <returns>A list of integration point profile objects</returns>
 		Task<IList<IntegrationPointModel>> GetAllIntegrationPointProfilesAsync(int workspaceArtifactId);
 
 		/// <summary>
