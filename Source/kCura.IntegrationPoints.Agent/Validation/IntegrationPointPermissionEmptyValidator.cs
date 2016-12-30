4c8c73d6ea006f83c13da06c9b5ea9035aa06b9f
Patryk Stepien <pstepien@kcura.com>
2016-12-30 09:03:55 +0100

    REL-114159 Fix permission issue in agent (missing EmptyValidator registration)

---
+++ Source/kCura.IntegrationPoints.Agent/Validation/IntegrationPointPermissionEmptyValidator.cs
@@ -0,0 +1,33 @@
+﻿using kCura.IntegrationPoints.Core.Models;
+using kCura.IntegrationPoints.Core.Validation.Abstract;
+using kCura.IntegrationPoints.Data;
+using kCura.IntegrationPoints.Domain.Models;
+
+namespace kCura.IntegrationPoints.Agent.Validation
+{
+	public class IntegrationPointPermissionEmptyValidator : IIntegrationPointPermissionValidator
+	{
+		public ValidationResult Validate(IntegrationPointModelBase model, SourceProvider sourceProvider, DestinationProvider destinationProvider,
+			IntegrationPointType integrationPointType)
+		{
+			return new ValidationResult();
+		}
+
+		public ValidationResult ValidateSave(IntegrationPointModelBase model, SourceProvider sourceProvider, DestinationProvider destinationProvider,
+			IntegrationPointType integrationPointType)
+		{
+			return new ValidationResult();
+		}
+
+		public ValidationResult ValidateViewErrors(int workspaceArtifactId)
+		{
+			return new ValidationResult();
+		}
+
+		public ValidationResult ValidateStop(IntegrationPointModelBase model, SourceProvider sourceProvider, DestinationProvider destinationProvider,
+			IntegrationPointType integrationPointType)
+		{
+			return new ValidationResult();
+		}
+	}
+}
