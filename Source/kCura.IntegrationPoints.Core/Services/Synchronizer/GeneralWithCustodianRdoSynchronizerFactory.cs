30a189eaf3ff7ebf45f3e6cab5b0fa6fa185d0b0
Patryk Stepien <pstepien@kcura.com>
2016-12-30 14:43:31 +0100

    Remove ExportSynchroznizer class

--- Source/kCura.IntegrationPoints.Core/Services/Synchronizer/GeneralWithCustodianRdoSynchronizerFactory.cs
+++ Source/kCura.IntegrationPoints.Core/Services/Synchronizer/GeneralWithCustodianRdoSynchronizerFactory.cs
@@ -30,10 +30,7 @@ namespace kCura.IntegrationPoints.Core.Services.Synchronizer
 		{
 			var json = JsonConvert.DeserializeObject<ImportSettings>(options);
 			var rdoObjectType = _query.GetObjectType(json.ArtifactTypeId);
-
-            if (json.DestinationProviderType != null && json.DestinationProviderType.ToLower() == "load file")
-                return _container.Kernel.Resolve<IDataSynchronizer>(typeof(ExportSynchroznizer).AssemblyQualifiedName);
-
+			
             if (json.Provider != null && json.Provider.ToLower() == "relativity")
 			{ 
 				IRSAPIClient client = _container.Resolve<IRSAPIClient>();
