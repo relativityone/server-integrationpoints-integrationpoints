1442eedaa115a9681fc83c88e2b0c54cece94f09
Lukasz Starzyk <lstarzyk@kcura.com>
2017-01-24 10:24:55 +0100

    Reverted ImportService changes

--- Source/kCura.IntegrationPoints.SourceProviderInstaller/IntegrationPointSourceProviderUninstaller.cs
+++ Source/kCura.IntegrationPoints.SourceProviderInstaller/IntegrationPointSourceProviderUninstaller.cs
@@ -126,7 +126,7 @@ namespace kCura.IntegrationPoints.SourceProviderInstaller
 			{
 				if (_importService == null)
 				{
-					_importService = new ImportService(this.CaseServiceContext, DeleteIntegrationPoints, base.Helper);
+					_importService = new ImportService(this.CaseServiceContext, this.EddsServiceContext, this.DeleteIntegrationPoints, base.Helper);
 				}
 
 				return _importService;
