2b18cbb7c30a0dfef246027064a2f4ece26b0809
Artur Mitianiec <amitianiec@kcura.com>
2017-01-30 08:31:35 -0600

    REL-122272 Integration Points - Error during Field Mapping - validation logic in RIP expected identifier filed name to be the same in the source and destination workspace -> this rule has been removed

--- Source/kCura.IntegrationPoints.Core/Validation/RelativityProviderValidator/Parts/FieldsMappingValidator.cs
+++ Source/kCura.IntegrationPoints.Core/Validation/RelativityProviderValidator/Parts/FieldsMappingValidator.cs
@@ -54,7 +54,6 @@ namespace kCura.IntegrationPoints.Core.Validation.RelativityProviderValidator.Pa
 			}
 
 			result.Add(ValidateUniqueIdentifierIsMapped(mappedIdentifier));
-			result.Add(ValidateAllRequiredFieldsMapped(fieldsMap, destinationWorkpaceFields));
 			result.Add(ValidateSettingsFieldOverlayBehavior(destinationConfiguration));
 			result.Add(ValidateSettingsFolderPathInformation(sourceWorkpaceFields, destinationConfiguration));
 
