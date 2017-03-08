d26300e79373d74ace67769d873a0bef31634b6a
Bob Pacione <bpacione@kcura.com>
2017-03-08 13:00:36 -0600

    Merge pull request #149 in IN/integrationpoints from REL-128524-xss to develop
    Squashed commit of the following:
    
    commit 2fa408136895b37da7e7ba4ae4baf6e04c16df4f
    Merge: 0cbfebd 13defee
    Author: Bob Pacione <bpacione@kcura.com>
    Date:   Wed Mar 8 13:00:08 2017 -0600
    
        Merge branch 'develop' into REL-128524-xss
    
    commit 0cbfebdadc11d0523702b8ca08b30c9751635e50
    Author: Bob Pacione <bpacione@kcura.com>
    Date:   Tue Mar 7 18:31:57 2017 -0600
    
        Tabifying entire Preview Job Tests file; removing unneeded usings
    
    commit 837dad469021c01bd6564b9b6d0886184f1ca239
    Author: Bob Pacione <bpacione@kcura.com>
    Date:   Tue Mar 7 18:29:52 2017 -0600
    
        REL-128524 adding a UT to make sure that values in preview are html encoded
    
    commit b9e8a96f8dfa65d969375428c1a6c4e47806ca20
    Author: Bob Pacione <bpacione@kcura.com>
    Date:   Tue Mar 7 17:56:03 2017 -0600
    
        REL-128524 html encoding values that we read from the load file to prevent cross site scripting vulnerability

--- source/kCura.IntegrationPoints.ImportProvider.Parser.Tests/PreviewJobTests.cs
+++ source/kCura.IntegrationPoints.ImportProvider.Parser.Tests/PreviewJobTests.cs
@@ -1,128 +1,147 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 using NUnit.Framework;
 using NSubstitute;
-using kCura.WinEDDS;
 using kCura.IntegrationPoints.ImportProvider.Parser.Interfaces;
 
 namespace kCura.IntegrationPoints.ImportProvider.Parser.Tests
 {
     [TestFixture, Category("ImportProvider")]
-    public class PreviewJobTests
-    {
-        private const int ColumnCount = 5;
-        private List<object> arrList;
-        private ILoadFilePreviewer _loadFilePreviewerMock;
-        private PreviewJob _subjectUnderTest;
+	public class PreviewJobTests
+	{
+		private const int ColumnCount = 5;
+		private List<object> arrList;
+		private ILoadFilePreviewer _loadFilePreviewerMock;
+		private PreviewJob _subjectUnderTest;
+		private const string ALERT_SCRIPT = "<script>Alert(1)</script>";
 
-        [SetUp]
-        public void SetUp()
-        {
-            _loadFilePreviewerMock = Substitute.For<ILoadFilePreviewer>();
-            _subjectUnderTest = new PreviewJob();
-            _subjectUnderTest._loadFilePreviewer = _loadFilePreviewerMock;
-            arrList = new List<object>();
-        }
+		[SetUp]
+		public void SetUp()
+		{
+			_loadFilePreviewerMock = Substitute.For<ILoadFilePreviewer>();
+			_subjectUnderTest = new PreviewJob();
+			_subjectUnderTest._loadFilePreviewer = _loadFilePreviewerMock;
+			arrList = new List<object>();
+		}
 
-        [Test]
-        public void PreviewJobReturnsProperHeaders()
-        {
-            MockPreviewTable();
+		[Test]
+		public void PreviewJobReturnsProperHeaders()
+		{
+			MockPreviewTable();
 
-            _subjectUnderTest._loadFilePreviewer.ReadFile(false).ReturnsForAnyArgs(arrList);
-            _subjectUnderTest.StartRead();
-            //assert that Preview Job is set to complete
-            Assert.IsTrue(_subjectUnderTest.IsComplete);
+			_subjectUnderTest._loadFilePreviewer.ReadFile(false).ReturnsForAnyArgs(arrList);
+			_subjectUnderTest.StartRead();
+			//assert that Preview Job is set to complete
+			Assert.IsTrue(_subjectUnderTest.IsComplete);
 
-            //assert that we have the expected number of headers
-            Assert.AreEqual(ColumnCount, _subjectUnderTest.PreviewTable.Header.Count);
-        }
+			//assert that we have the expected number of headers
+			Assert.AreEqual(ColumnCount, _subjectUnderTest.PreviewTable.Header.Count);
+		}
 
-        [Test]
-        public void PreviewJobReturnsCorrectErrorsExceptions()
-        {
-            MockPreviewTable();
-            //arrange our return value from ReadFile to include two exceptions
-            arrList.Add(new Exception("test 1"));
-            arrList.Add(new Exception("test 2"));
-            _subjectUnderTest._loadFilePreviewer.ReadFile(false).ReturnsForAnyArgs(arrList);
-            _subjectUnderTest.StartRead();
+		[Test]
+		public void PreviewJobReturnsCorrectErrorsExceptions()
+		{
+			MockPreviewTable();
+			//arrange our return value from ReadFile to include two exceptions
+			arrList.Add(new Exception("test 1"));
+			arrList.Add(new Exception("test 2"));
+			_subjectUnderTest._loadFilePreviewer.ReadFile(false).ReturnsForAnyArgs(arrList);
+			_subjectUnderTest.StartRead();
 
-            //assert that we have the expected number errors
-            Assert.AreEqual(2, _subjectUnderTest.PreviewTable.ErrorRows.Count);
-        }
+			//assert that we have the expected number errors
+			Assert.AreEqual(2, _subjectUnderTest.PreviewTable.ErrorRows.Count);
+		}
 
-        [Test]
-        public void PreviewJobReturnsCorrectErrors()
-        {
-            MockPreviewTable();
-            MockPreviewTableErrorColumn();
-            
-            //arrange our return value from ReadFile to include an exception column
-            //this returns differently than when the entire row has an exception
-            _subjectUnderTest._loadFilePreviewer.ReadFile(false).ReturnsForAnyArgs(arrList);
-            _subjectUnderTest.StartRead();
+		[Test]
+		public void PreviewJobReturnsCorrectErrors()
+		{
+			MockPreviewTable();
+			MockPreviewTableErrorColumn();
+			
+			//arrange our return value from ReadFile to include an exception column
+			//this returns differently than when the entire row has an exception
+			_subjectUnderTest._loadFilePreviewer.ReadFile(false).ReturnsForAnyArgs(arrList);
+			_subjectUnderTest.StartRead();
 
-            //assert that we have the expected number errors and data rows
-            Assert.AreEqual(1, _subjectUnderTest.PreviewTable.ErrorRows.Count);
-            Assert.AreEqual(2, _subjectUnderTest.PreviewTable.Data.Count);
-        }
+			//assert that we have the expected number errors and data rows
+			Assert.AreEqual(1, _subjectUnderTest.PreviewTable.ErrorRows.Count);
+			Assert.AreEqual(2, _subjectUnderTest.PreviewTable.Data.Count);
+		}
 
-        [Test]
-        public void PreviewJobReturnsNoData()
-        {
-            //Test to make sure that PreviewJob is safely handling a situation where LoadFilePreviewer doesn't find any rows
-            _subjectUnderTest._loadFilePreviewer.ReadFile(false).ReturnsForAnyArgs(arrList);
-            _subjectUnderTest.StartRead();
+		[Test]
+		public void PreviewJobEncodesValues()
+		{
+			MockPreviewTable();
 
-            //assert that we have the expected number errors and data rows
-            Assert.AreEqual(1, _subjectUnderTest.PreviewTable.Header.Count);
+			//arrange our return value from ReadFile to include an value with a script inside it
+			_subjectUnderTest._loadFilePreviewer.ReadFile(false).ReturnsForAnyArgs(arrList);
+			_subjectUnderTest.StartRead();
 
-            //assert that the Read did not fail
-            Assert.IsFalse(_subjectUnderTest.IsFailed);
-        }
+			//assert that we have the expected number of rows
+			Assert.AreEqual(1, _subjectUnderTest.PreviewTable.Data.Count);
+			//Assert that the row and column that contained the Alert script has been safely html encoded
+			string valueWithScript = _subjectUnderTest.PreviewTable.Data[0][1];
+			Assert.AreEqual(HttpUtility.HtmlEncode(ALERT_SCRIPT), valueWithScript);
+		}
 
-        private void MockPreviewTable()
-        {
-            global::Relativity.FieldCategory fieldCat;            
-            WinEDDS.Api.ArtifactField field;
-            List<WinEDDS.Api.ArtifactField> fieldList = new List<WinEDDS.Api.ArtifactField>();
-            for (int i = 1; i <= ColumnCount; i++)
-            {
-                fieldCat = global::Relativity.FieldCategory.Generic;
-                if (i == 1)
-                {
-                    fieldCat = global::Relativity.FieldCategory.Identifier;
-                }
-                field = new WinEDDS.Api.ArtifactField(String.Format("Field{0}", i), i, global::Relativity.FieldTypeHelper.FieldType.Text, fieldCat, -1, -1, -1, false);
-                field.Value = String.Format("Value{0}",i);
-                fieldList.Add(field);
-            }
-            arrList.Add(fieldList.ToArray());
-        }
+		[Test]
+		public void PreviewJobReturnsNoData()
+		{
+			//Test to make sure that PreviewJob is safely handling a situation where LoadFilePreviewer doesn't find any rows
+			_subjectUnderTest._loadFilePreviewer.ReadFile(false).ReturnsForAnyArgs(arrList);
+			_subjectUnderTest.StartRead();
 
-        private void MockPreviewTableErrorColumn()
-        {
-            global::Relativity.FieldCategory fieldCat;
-            WinEDDS.Api.ArtifactField field;
-            List<WinEDDS.Api.ArtifactField> fieldList = new List<WinEDDS.Api.ArtifactField>();
-            for (int i = 1; i <= ColumnCount; i++)
-            {
-                fieldCat = global::Relativity.FieldCategory.Generic;
-                if (i == 1)
-                {
-                    fieldCat = global::Relativity.FieldCategory.Identifier;
-                }
-                field = new WinEDDS.Api.ArtifactField(String.Format("Field{0}", i), i, global::Relativity.FieldTypeHelper.FieldType.Text, fieldCat, -1, -1, -1, false);
-                //insert value with the error keyword that the LoadFilePreviewer will put on columns that contain an error
-                field.Value = String.Format("Error: {0}", i);
-                fieldList.Add(field);
-            }
-            arrList.Add(fieldList.ToArray());
-        }
-    }
+			//assert that we have the expected number errors and data rows
+			Assert.AreEqual(1, _subjectUnderTest.PreviewTable.Header.Count);
+
+			//assert that the Read did not fail
+			Assert.IsFalse(_subjectUnderTest.IsFailed);
+		}
+
+		private void MockPreviewTable()
+		{
+			global::Relativity.FieldCategory fieldCat;
+			WinEDDS.Api.ArtifactField field;
+			List<WinEDDS.Api.ArtifactField> fieldList = new List<WinEDDS.Api.ArtifactField>();
+			for (int i = 1; i <= ColumnCount; i++)
+			{
+				fieldCat = global::Relativity.FieldCategory.Generic;
+				if (i == 1)
+				{
+					fieldCat = global::Relativity.FieldCategory.Identifier;
+				}
+				field = new WinEDDS.Api.ArtifactField(String.Format("Field{0}", i), i, global::Relativity.FieldTypeHelper.FieldType.Text, fieldCat, -1, -1, -1, false);
+				field.Value = String.Format("Value{0}",i);
+
+				//Give one of these columns a value that contains a script
+				if (i == 2)
+				{
+					field.Value = ALERT_SCRIPT;
+				}
+				fieldList.Add(field);
+			}
+			arrList.Add(fieldList.ToArray());
+		}
+
+		private void MockPreviewTableErrorColumn()
+		{
+			global::Relativity.FieldCategory fieldCat;
+			WinEDDS.Api.ArtifactField field;
+			List<WinEDDS.Api.ArtifactField> fieldList = new List<WinEDDS.Api.ArtifactField>();
+			for (int i = 1; i <= ColumnCount; i++)
+			{
+				fieldCat = global::Relativity.FieldCategory.Generic;
+				if (i == 1)
+				{
+					fieldCat = global::Relativity.FieldCategory.Identifier;
+				}
+				field = new WinEDDS.Api.ArtifactField(String.Format("Field{0}", i), i, global::Relativity.FieldTypeHelper.FieldType.Text, fieldCat, -1, -1, -1, false);
+				//insert value with the error keyword that the LoadFilePreviewer will put on columns that contain an error
+				field.Value = String.Format("Error: {0}", i);
+				fieldList.Add(field);
+			}
+			arrList.Add(fieldList.ToArray());
+		}
+	}
 }
