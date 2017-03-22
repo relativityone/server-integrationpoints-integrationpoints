4981241685df1026c9327eb683f86ec9413ca09e
dwickowski <dwickowski@kcura.com>
2017-03-22 03:22:17 -0500

    Set Copy Images UI toggle by default

---
+++ Source/kCura.IntegrationPoints.Data/Resources/ShowImageImportToggle.sql
@@ -0,0 +1,4 @@
+﻿IF NOT EXISTS (SELECT * FROM [eddsdbo].[TOGGLE] WHERE [Name] = 'kCura.IntegrationPoints.Web.Toggles.UI.ShowImageImportToggle')
+BEGIN
+	INSERT INTO [eddsdbo].[Toggle] VALUES ('kCura.IntegrationPoints.Web.Toggles.UI.ShowImageImportToggle', 1)
+END
