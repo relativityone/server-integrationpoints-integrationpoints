c2850700ce589c5a83b9bc24d826c11c1b81eee1
Pawel Nowak <pnowak@kcura.com>
2017-05-23 06:33:58 -0500

    Merge pull request #226 in IN/integrationpoints from REL-141431 to release-9.5-may
    Squashed commit of the following:
    
    commit 1bd5365a51eabf9703a1fa307fc390f78583d1f4
    Author: Pawel Nowak <pnowak@kcura.com>
    Date:   Wed May 17 14:15:27 2017 +0200
    
        REL-141431 Installed ServicesInstaller from Core project in JobHistoryManager

--- Source/kCura.IntegrationPoints.Services/Installers/JobHistoryManagerInstaller.cs
+++ Source/kCura.IntegrationPoints.Services/Installers/JobHistoryManagerInstaller.cs
@@ -7,6 +7,7 @@ using Castle.Windsor;
 using kCura.IntegrationPoints.Core;
 using kCura.IntegrationPoints.Core.Factories;
 using kCura.IntegrationPoints.Core.Factories.Implementations;
+using kCura.IntegrationPoints.Core.Installers;
 using kCura.IntegrationPoints.Core.Services.JobHistory;
 using kCura.IntegrationPoints.Core.Services.ServiceContext;
 using kCura.IntegrationPoints.Data;
@@ -29,9 +30,10 @@ namespace kCura.IntegrationPoints.Services.Installers
 		{
 			_dependencies = new List<IWindsorInstaller>
 			{
-				new QueryInstallers()
-			};
-		}
+                new QueryInstallers(),
+                new ServicesInstaller()
+            };
+        }
 
 		protected override IList<IWindsorInstaller> Dependencies => _dependencies;
 
@@ -39,10 +41,6 @@ namespace kCura.IntegrationPoints.Services.Installers
 		{
 			container.Register(Component.For<IServiceHelper>().UsingFactoryMethod(k => global::Relativity.API.Services.Helper, true));
 			container.Register(Component.For<IHelper>().UsingFactoryMethod(k => k.Resolve<IServiceHelper>(), true));
-			container.Register(Component.For<ITokenProvider>().ImplementedBy<RelativityCoreTokenProvider>().LifestyleTransient());
-			container.Register(Component.For<IHelperFactory>().ImplementedBy<HelperFactory>().LifestyleTransient());
-			container.Register(Component.For<IManagerFactory>().ImplementedBy<ManagerFactory>().LifestyleTransient());
-			container.Register(Component.For<IContextContainerFactory>().ImplementedBy<ContextContainerFactory>().LifestyleTransient());
 			container.Register(Component.For<IDestinationParser>().ImplementedBy<DestinationParser>().LifestyleTransient());
 			container.Register(Component.For<IJobHistoryAccess>().ImplementedBy<JobHistoryAccess>().LifestyleTransient());
 			container.Register(Component.For<IJobHistorySummaryModelBuilder>().ImplementedBy<JobHistorySummaryModelBuilder>().LifestyleTransient());
@@ -55,24 +53,6 @@ namespace kCura.IntegrationPoints.Services.Installers
 					.LifestyleTransient());
 			container.Register(Component.For<ICompletedJobsHistoryRepository>().ImplementedBy<CompletedJobsHistoryRepository>().LifestyleTransient());
 			container.Register(Component.For<IRSAPIService>().UsingFactoryMethod(k => ServiceContextFactory.CreateRSAPIService(k.Resolve<IHelper>(), workspaceId), true));
-			container.Register(Component.For<IIntegrationPointSerializer>().ImplementedBy<IntegrationPointSerializer>().LifestyleSingleton());
-			container.Register(Component.For<IToggleProvider>().Instance(new SqlServerToggleProvider(
-				() =>
-				{
-					SqlConnection connection = container.Resolve<IHelper>().GetDBContext(-1).GetConnection(true);
-
-					return connection;
-				},
-				async () =>
-				{
-					Task<SqlConnection> task = Task.Run(() =>
-					{
-						SqlConnection connection = container.Resolve<IHelper>().GetDBContext(-1).GetConnection(true);
-						return connection;
-					});
-
-					return await task;
-				})).LifestyleTransient());
 		}
 	}
 }
