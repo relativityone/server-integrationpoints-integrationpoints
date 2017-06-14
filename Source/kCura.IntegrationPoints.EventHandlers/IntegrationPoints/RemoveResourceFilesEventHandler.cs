cb56fd10f5869826a78fe99fdbabb34bf3646a4a
Andrzej Korczyński <akorczynski@kcura.com>
2017-06-14 12:45:55 -0500

    Merge pull request #253 in IN/integrationpoints from REL-146810 to release-9.5-june
    Squashed commit of the following:
    
    commit 18d51febb5a980e03c25317d394cad72699a8a00
    Author: Andrzej Korczynski <akorczynski@kcura.com>
    Date:   Wed Jun 14 19:39:47 2017 +0200
    
        REL-146810 Remove old resource files

---
+++ Source/kCura.IntegrationPoints.EventHandlers/IntegrationPoints/RemoveResourceFilesEventHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.EventHandler;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using kCura.EventHandler.CustomAttributes;
+using Relativity.API;
+using Relativity.Core.Service;
+using kCura.IntegrationPoints.Data.Extensions;
+using Relativity.Core;
+
+namespace kCura.IntegrationPoints.EventHandlers.IntegrationPoints
+{
+    [Guid("51BAAFB6-898F-48AC-BB6E-A5B9FCD4EF1F")]
+    [Description("This is an event handler that removes Integration Points Resource Files.")]
+    [RunTarget(EventHandler.Helper.RunTargets.Instance)]
+    [RunOnce(true)]
+    public class RemoveResourceFileEventHandler : PreInstallEventHandler
+    {
+        private string[] assemblyNames =
+        {
+            "kCura.Image.Viewer.dll",
+            "kCura.Print.dll"
+        };
+
+        public override Response Execute()
+        {
+            Guid applicationGuid = new Guid(Domain.Constants.IntegrationPoints.APPLICATION_GUID_STRING);
+
+            BaseServiceContext baseServiceContext = ClaimsPrincipal.Current.GetUnversionContext(-1);
+            try
+            {
+                AssemblyManager am = new AssemblyManager();
+
+                foreach (var assemblyName in assemblyNames)
+                {
+                    var assembly = am.Read(baseServiceContext, assemblyName, applicationGuid);
+                    int assemblyArtifactId = assembly.ArtifactID;
+
+                    if (assemblyArtifactId > 0)
+                    {
+                        am.Delete(baseServiceContext, assemblyArtifactId);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    Exception = ex,
+                    Message = ex.Message,
+                    Success = false
+                };
+            }
+
+            return new Response
+            {
+                Message = "Resource Files were removed successfully",
+                Success = true
+            };
+        }
+    }
+}
