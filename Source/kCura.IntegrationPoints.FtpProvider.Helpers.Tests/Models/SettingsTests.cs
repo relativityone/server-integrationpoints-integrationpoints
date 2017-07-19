79053c90358fb449c6ab147ddf92382a734474b3
Lukasz Starzyk <lstarzyk@kcura.com>
2017-07-19 10:00:51 -0500

    Merge pull request #318 in IN/integrationpoints from REL-154150_ftp_csv_path_validation_fails to release-9.5-july
    Squashed commit of the following:
    
    commit 6f65671f3a33f3ba7d7e6e87a40871c20f320689
    Author: Konrad Blasinski <kblasinski@kcura.com>
    Date:   Wed Jul 19 16:56:17 2017 +0200
    
        REL-154150 fix validation for csv path and add Unit Tests

---
+++ Source/kCura.IntegrationPoints.FtpProvider.Helpers.Tests/Models/SettingsTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using kCura.IntegrationPoints.FtpProvider.Helpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.IntegrationPoint.Tests.Core;
+
+namespace kCura.IntegrationPoints.FtpProvider.Helpers.Models.Tests
+{
+	[TestFixture()]
+	public class SettingsTests : TestBase
+	{
+		private Settings _settings;
+
+		[Test()]
+		[TestCase("test.csv", true)]
+		[TestCase("folder/test.csv", true)]
+		[TestCase("", false)]
+		public void ValidateCSVNameTest(string csvName, bool expectedResult)
+		{
+			_settings.Filename_Prefix = csvName;
+			bool result = _settings.ValidateCSVName();
+			Assert.AreEqual(expectedResult, result);
+		}
+
+		[Test()]
+		[TestCase("justname", true)]
+		[TestCase("relativity.com", true)]
+		[TestCase("http://toomuchforahostname.com/", false)]
+		[TestCase("ftp://toomuchforahostname.com/", false)]
+		[TestCase("8.8.8.8", true)]
+		[TestCase("", false)]
+		[TestCase("#hostwithinvalidchar", false)]
+		public void ValidateHostTest(string host, bool expectedResult)
+		{
+			_settings.Host = host;
+			bool result = _settings.ValidateHost();
+			Assert.AreEqual(expectedResult, result);
+		}
+
+		[SetUp]
+		public override void SetUp()
+		{
+			_settings = new Settings();
+		}
+	}
+}
