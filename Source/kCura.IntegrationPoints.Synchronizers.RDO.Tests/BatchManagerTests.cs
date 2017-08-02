ff322de682f8decc3f645a11096615470dcdbc46
Krzysztof Dabrowski <kdabrowski@kcura.com>
2017-08-02 09:54:42 -0500

    Merge pull request #337 in IN/integrationpoints from REL-133492-unit-test-batchManager to develop
    Squashed commit of the following:
    
    commit a3f77ca3a9f5b98e435a2fd09c313a5732941409
    Merge: 079a75c ec2617f
    Author: Krzysztof Dabrowski <kdabrowski@kcura.com>
    Date:   Wed Aug 2 16:53:40 2017 +0200
    
        Merge remote-tracking branch 'origin/develop' into REL-133492-unit-test-batchManager
    
    commit 079a75c8b957848b086d2514896c2f55d2793bbf
    Author: Krzysztof Dabrowski <kdabrowski@kcura.com>
    Date:   Wed Aug 2 16:53:25 2017 +0200
    
        cosmetic changes
    
    commit d69913adea21c048904f4adf4040049675731e00
    Author: Krzysztof Dabrowski <kdabrowski@kcura.com>
    Date:   Mon Jul 31 10:56:06 2017 +0200
    
        cleanup
    
    commit a90ee87f7e0769839e5483e7498c546b10f020b9
    Author: Krzysztof Dabrowski <kdabrowski@kcura.com>
    Date:   Mon Jul 31 09:34:47 2017 +0200
    
        Added new unit test for clear data source
    
    commit 8d304e68dde57b7afede738fcb511c4f7b1850c4
    Author: Krzysztof Dabrowski <kdabrowski@kcura.com>
    Date:   Mon Jul 31 09:29:16 2017 +0200
    
        improved test for getbatchdata
    
    commit 4465e8da90d7b61d9c8542d395d604011fa6a8c0
    Author: Krzysztof Dabrowski <kdabrowski@kcura.com>
    Date:   Mon Jul 31 09:18:06 2017 +0200
    
        added new test for getbatchdata
    
    commit af6d877328813abef8c20ef80daf4331371db98e
    Author: Krzysztof Dabrowski <kdabrowski@kcura.com>
    Date:   Fri Jul 28 13:32:43 2017 +0200
    
        Added test for IsBatchFull method
    
    commit d15b44ac22567a910a0e6a568ce2c14ada3d72a7
    Author: Krzysztof Dabrowski <kdabrowski@kcura.com>
    Date:   Fri Jul 28 10:58:31 2017 +0200
    
        Added test for add menthod

--- Source/kCura.IntegrationPoints.Synchronizers.RDO.Tests/BatchManagerTests.cs
+++ Source/kCura.IntegrationPoints.Synchronizers.RDO.Tests/BatchManagerTests.cs
@@ -17,6 +17,98 @@ namespace kCura.IntegrationPoints.Synchronizers.RDO.Tests
 		}
 
 		[Test]
+		public void Add_RaisesOnBatchCreateEvent()
+		{
+			//ARRANGE
+			BatchManager batchManager = new BatchManager();
+			bool eventFired = false;
+			
+			batchManager.OnBatchCreate += delegate ( int batchSize )
+			{
+				eventFired = true;
+			};
+
+			var fileDataToAdd = new Dictionary<string, object>();
+			fileDataToAdd.Add("F1", 111);
+
+			//ACT
+			batchManager.Add(fileDataToAdd);
+
+			//ASSERT
+			Assert.IsTrue( eventFired );
+			Assert.AreEqual(1, batchManager.CurrentSize );
+		}
+
+		[Test]
+		public void IsBatchFull_ReturnsTrueWhenCountIsEqualOrGreaterThanMinBatchSize()
+		{
+			BatchManager batchManager = new BatchManager(2);
+
+			//less than 2
+			batchManager.Add(new Dictionary<string, object>());
+			Assert.IsFalse(batchManager.IsBatchFull());
+
+			// equal to 2
+			batchManager.Add(new Dictionary<string, object>());
+			Assert.IsTrue(batchManager.IsBatchFull());
+
+			// greater than 2
+			batchManager.Add(new Dictionary<string, object>());
+			Assert.IsTrue(batchManager.IsBatchFull());
+		}
+
+		[Test]
+		public void GetBatchData_ReturnsDataReaderEquivalentToConfigureTable()
+		{
+			//ARRANGE
+			HashSet<string> columnNamesSet = new HashSet<string>() { "F1","F2","F3" };
+			IEnumerable<string> columnNames = columnNamesSet;
+			var fileData = new Dictionary<string, object>()
+			{
+				{"F1", 111},
+				{"F2", DateTime.Parse("11/22/2014 11:22:33")},
+				{"F3", "Hello"}
+			};
+
+			BatchManager batchManager = new BatchManager() { ColumnNames = columnNamesSet };
+			batchManager.Add(fileData);
+
+			//ACT
+			var result = batchManager.GetBatchData();
+			var expected = batchManager.ConfigureTable(columnNames, new List<IDictionary<string, object>> {fileData})
+				.CreateDataReader();
+
+			//ASSERT
+			Assert.AreEqual(expected.FieldCount, result.FieldCount);
+			Assert.AreEqual(expected.Read(), result.Read());
+			Assert.AreEqual(expected.GetValue(0), result.GetValue(0)); 
+			Assert.AreEqual(expected.GetValue(1), result.GetValue(1)); 
+			Assert.AreEqual(expected.GetValue(2), result.GetValue(2)); 
+			Assert.IsFalse(result.Read());
+		}
+
+		[Test]
+		public void ClearDataSource_Pass()
+		{
+			//ARRANGE
+			var fileData = new Dictionary<string, object>()
+			{
+				{"F1", 111},
+				{"F2", DateTime.Parse("11/22/2014 11:22:33")},
+				{"F3", "Hello"}
+			};
+
+			BatchManager batchManager = new BatchManager();
+			batchManager.Add(fileData);
+
+			//ACT
+			batchManager.ClearDataSource();
+
+			//ASSERT
+			Assert.AreEqual(0, batchManager.CurrentSize);
+		}
+
+		[Test]
 		public void ConfigureTable_Pass()
 		{
 			//ARRANGE
