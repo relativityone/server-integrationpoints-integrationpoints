8090a33a6ba6d1c93bd0beb77e6f512f87983119
Krzysztof Dabrowski <krzysztof.dabrowski@relativity.com>
2017-09-06 04:27:24 -0500

    Merge pull request #398 in IN/integrationpoints from REL-115111-push-ss-picker to develop
    Squashed commit of the following:
    
    commit 54efd50cd89bfaa46c1d5af53294ea9cf507a3ca
    Author: Krzysztof Dabrowski <kdabrowski@kcura.com>
    Date:   Wed Sep 6 11:23:39 2017 +0200
    
        sorted searches alfabetically

--- Source/kCura.IntegrationPoints.Web/Scripts/Export/saved-search-flat.js
+++ Source/kCura.IntegrationPoints.Web/Scripts/Export/saved-search-flat.js
@@ -17,6 +17,20 @@ var FlatSavedSearches = function(tree) {
         }
     };
 
+    var _sort = function(a, b) {
+        var nameA = a.displayName.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.displayName.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+
+        // names must be equal
+        return 0;
+    };
+
     _iterate(tree, 0);
-    return _searches;
-};
+    return _searches.sort(_sort);
+}
