ba1907a93b1df08da885367a8687bd691ce39064
Krzysztof Dabrowski <krzysztof.dabrowski@relativity.com>
2017-12-15 03:31:28 -0600

    Merge pull request #494 in IN/integrationpoints from REL-185023-fix to develop
    Squashed commit of the following:
    
    commit f4b482cb4236df0add0b3584ebf51b124e41c544
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Fri Dec 15 10:21:58 2017 +0100
    
        added context restorer to each method in DataReader decorator

--- Source/kCura.IntegrationPoints.Core/Services/Domain/DataReaderWithLogContextDecorator.cs
+++ Source/kCura.IntegrationPoints.Core/Services/Domain/DataReaderWithLogContextDecorator.cs
@@ -23,162 +23,212 @@ namespace kCura.IntegrationPoints.Core.Services.Domain
 
 		public string GetName(int i)
 		{
-			return _dataReaderImplementation.GetName(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetName(i);
 		}
 
 		public string GetDataTypeName(int i)
 		{
-			return _dataReaderImplementation.GetDataTypeName(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetDataTypeName(i);
 		}
 
 		public Type GetFieldType(int i)
 		{
-			return _dataReaderImplementation.GetFieldType(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetFieldType(i);
 		}
 
 		public object GetValue(int i)
 		{
-			return _dataReaderImplementation.GetValue(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetValue(i);
 		}
 
 		public int GetValues(object[] values)
 		{
-			return _dataReaderImplementation.GetValues(values);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetValues(values);
 		}
 
 		public int GetOrdinal(string name)
 		{
-			return _dataReaderImplementation.GetOrdinal(name);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetOrdinal(name);
 		}
 
 		public bool GetBoolean(int i)
 		{
-			return _dataReaderImplementation.GetBoolean(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetBoolean(i);
 		}
 
 		public byte GetByte(int i)
 		{
-			return _dataReaderImplementation.GetByte(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetByte(i);
 		}
 
 		public long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length)
 		{
-			return _dataReaderImplementation.GetBytes(i, fieldOffset, buffer, bufferoffset, length);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetBytes(i, fieldOffset, buffer, bufferoffset, length);
 		}
 
 		public char GetChar(int i)
 		{
-			return _dataReaderImplementation.GetChar(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetChar(i);
 		}
 
 		public long GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length)
 		{
-			return _dataReaderImplementation.GetChars(i, fieldoffset, buffer, bufferoffset, length);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetChars(i, fieldoffset, buffer, bufferoffset, length);
 		}
 
 		public Guid GetGuid(int i)
 		{
-			return _dataReaderImplementation.GetGuid(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetGuid(i);
 		}
 
 		public short GetInt16(int i)
 		{
-			return _dataReaderImplementation.GetInt16(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetInt16(i);
 		}
 
 		public int GetInt32(int i)
 		{
-			return _dataReaderImplementation.GetInt32(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetInt32(i);
 		}
 
 		public long GetInt64(int i)
 		{
-			return _dataReaderImplementation.GetInt64(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetInt64(i);
 		}
 
 		public float GetFloat(int i)
 		{
-			return _dataReaderImplementation.GetFloat(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetFloat(i);
 		}
 
 		public double GetDouble(int i)
 		{
-			return _dataReaderImplementation.GetDouble(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetDouble(i);
 		}
 
 		public string GetString(int i)
 		{
-			return _dataReaderImplementation.GetString(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetString(i);
 		}
 
 		public decimal GetDecimal(int i)
 		{
-			return _dataReaderImplementation.GetDecimal(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetDecimal(i);
 		}
 
 		public DateTime GetDateTime(int i)
 		{
-			return _dataReaderImplementation.GetDateTime(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetDateTime(i);
 		}
 
 		public IDataReader GetData(int i)
 		{
-			return _dataReaderImplementation.GetData(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetData(i);
 		}
 
 		public bool IsDBNull(int i)
 		{
-			return _dataReaderImplementation.IsDBNull(i);
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.IsDBNull(i);
 		}
 
 		public int FieldCount
 		{
-			get { return _dataReaderImplementation.FieldCount; }
+			get
+			{
+				using (new SerilogContextRestorer())
+					return _dataReaderImplementation.FieldCount;
+			}
 		}
 
 		object IDataRecord.this[int i]
 		{
-			get { return _dataReaderImplementation[i]; }
+			get
+			{
+				using (new SerilogContextRestorer())
+					return _dataReaderImplementation[i];
+			}
 		}
 
 		object IDataRecord.this[string name]
 		{
-			get { return _dataReaderImplementation[name]; }
+			get
+			{
+				using (new SerilogContextRestorer())
+					return _dataReaderImplementation[name];
+			}
 		}
 
 		public void Close()
 		{
-			_dataReaderImplementation.Close();
+			using (new SerilogContextRestorer())
+				_dataReaderImplementation.Close();
 		}
 
 		public DataTable GetSchemaTable()
 		{
-			return _dataReaderImplementation.GetSchemaTable();
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.GetSchemaTable();
 		}
 
 		public bool NextResult()
 		{
-			return _dataReaderImplementation.NextResult();
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.NextResult();
 		}
 
 		public bool Read()
 		{
-			return _dataReaderImplementation.Read();
+			using (new SerilogContextRestorer())
+				return _dataReaderImplementation.Read();
 		}
 
 		public int Depth
 		{
-			get { return _dataReaderImplementation.Depth; }
+			get
+			{
+				using (new SerilogContextRestorer())
+					return _dataReaderImplementation.Depth;
+			}
 		}
 
 		public bool IsClosed
 		{
-			get { return _dataReaderImplementation.IsClosed; }
+			get
+			{
+				using (new SerilogContextRestorer())
+					return _dataReaderImplementation.IsClosed;
+			}
 		}
 
 		public int RecordsAffected
 		{
-			get { return _dataReaderImplementation.RecordsAffected; }
+			get
+			{
+				using (new SerilogContextRestorer())
+					return _dataReaderImplementation.RecordsAffected;
+			}
 		}
 	}
 }
