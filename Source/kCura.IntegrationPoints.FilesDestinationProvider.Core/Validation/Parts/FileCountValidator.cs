c76d62abf6b3b6e8531043366ec6cc808dbfa291
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2017-12-20 05:55:24 -0600

    Merge pull request #501 in IN/integrationpoints from FileCountValidator-changeIntToLong to develop
    Squashed commit of the following:
    
    commit 1981807b2ed3218429cb4e5ae1e5f465aa13220f
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Wed Dec 20 11:42:28 2017 +0100
    
        fix

--- Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/Validation/Parts/FileCountValidator.cs
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/Validation/Parts/FileCountValidator.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using kCura.IntegrationPoints.Core.Validation.Abstract;
+﻿using kCura.IntegrationPoints.Core.Validation.Abstract;
 using kCura.IntegrationPoints.Domain.Models;
 
 namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.Validation
 {
-	public class FileCountValidator : BasePartsValidator<int>
+	public class FileCountValidator : BasePartsValidator<long>
 	{
-		public override ValidationResult Validate(int value)
+		public override ValidationResult Validate(long value)
 		{
 			return (value > 0) ? new ValidationResult() : new ValidationResult(false, FileDestinationProviderValidationMessages.FILE_COUNT_WARNING);
 		}
