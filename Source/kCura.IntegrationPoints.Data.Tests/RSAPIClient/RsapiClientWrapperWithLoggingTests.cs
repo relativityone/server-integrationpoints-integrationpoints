ea10fc004da8d9c8ebed3e1fa925a86cf582b3c0
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2018-02-09 03:45:37 -0600

    Merge pull request #557 in IN/integrationpoints from REL-192960-RsapiClientWrapper to develop
    Squashed commit of the following:
    
    commit 9948befa1017967394d9a56c0d64bf058462070f
    Merge: e0ef754 b8509ec
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Fri Feb 9 10:38:49 2018 +0100
    
        Merge remote-tracking branch 'origin/develop' into REL-192960-RsapiClientWrapper
    
    commit e0ef75411e9fb1d9fb01e03c967294a43843aed0
    Merge: 0a26eea 414e901
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Fri Feb 9 10:38:21 2018 +0100
    
        Merge branch 'develop' into REL-192960-RsapiClientWrapper
    
    commit 0a26eea76712ff26d2cc2aa5548d12ebe3848660
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Fri Feb 9 10:38:10 2018 +0100
    
        changes after code review
    
    commit 562203b6ec44209279669434be684950c123d5d0
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Thu Feb 8 11:29:36 2018 +0100
    
        logging for events
    
    commit 0f7c0802096fc5db2037247dbaacdc50c64f66d9
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Thu Feb 8 10:26:46 2018 +0100
    
        tests for logging in events and properties
    
    commit daa0012e99ece71caf6a50fa4b928b51c82106d0
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Thu Feb 8 09:41:20 2018 +0100
    
        logging and rethrowing exceptions from RsapiClient
    
    commit 59bbd5cebc70eab5b6c9a43a4d4099ff0a55e9aa
    Merge: 8979e5f e25a0d3
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Thu Feb 8 08:54:17 2018 +0100
    
        Merge branch 'develop' into REL-192960-RsapiClientWrapper
    
    commit 8979e5febfbba87916757441bae7ace4437743a4
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Wed Feb 7 12:59:26 2018 +0100
    
        created RsapiClient wrapper

---
+++ Source/kCura.IntegrationPoints.Data.Tests/RSAPIClient/RsapiClientWrapperWithLoggingTests.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using kCura.IntegrationPoints.Data.RSAPIClient;
+using kCura.IntegrationPoints.Domain.Exceptions;
+using kCura.Relativity.Client;
+using NSubstitute;
+using NUnit.Framework;
+using Relativity.API;
+
+namespace kCura.IntegrationPoints.Data.Tests.RSAPIClient
+{
+	[TestFixture]
+	public class RsapiClientWrapperWithLoggingTests
+	{
+		[Test]
+		public void ItShouldCallMethodsAndPropertiesAndEventsOfDecoratedObject()
+		{
+			IRSAPIClient decoratedObject = Substitute.For<IRSAPIClient>();
+			IAPILog logger = Substitute.For<IAPILog>();
+			var decorator = new RsapiClientWrapperWithLogging(decoratedObject, logger);
+
+			Type interfaceType = typeof(IRSAPIClient);
+			MethodInfo[] methodsFromInterface = GetAllMethodsAndPropertiesFromInterfaceAndDerivedInterfaces(interfaceType).ToArray();
+
+			var methodEmptyParams = new Dictionary<MethodInfo, object[]>();
+
+			foreach (MethodInfo methodInfo in methodsFromInterface)
+			{
+				object[] parameters = methodInfo
+					.GetParameters()
+					.Select(x => x.ParameterType)
+					.Select(GetDefaultValue)
+					.ToArray();
+
+				methodEmptyParams[methodInfo] = parameters;
+			}
+
+			// Act
+			foreach (MethodInfo methodInfo in methodsFromInterface)
+			{
+				methodInfo.Invoke(decorator, methodEmptyParams[methodInfo]);
+			}
+
+			// Assert
+			foreach (MethodInfo methodInfo in methodsFromInterface)
+			{
+				IRSAPIClient receivedValidator = decoratedObject.ReceivedWithAnyArgs(); // new instance of validator have to be created for each method
+				methodInfo.Invoke(receivedValidator, methodEmptyParams[methodInfo]);
+			}
+		}
+
+		[Test]
+		public void ItShouldLogAndRethrowExceptionInMethodsOfDecoratedObject()
+		{
+			IRSAPIClient decoratedObject = Substitute.For<IRSAPIClient>();
+			IAPILog logger = Substitute.For<IAPILog>();
+			logger.ForContext<RsapiClientWrapperWithLogging>().Returns(logger);
+			var decorator = new RsapiClientWrapperWithLogging(decoratedObject, logger);
+
+			Type interfaceType = typeof(IRSAPIClient);
+			MethodInfo[] methodsFromInterface = GetAllMethodsFromInterfaceAndDerivedInterfaces(interfaceType).ToArray();
+
+			var methodEmptyParams = new Dictionary<MethodInfo, object[]>();
+
+			foreach (MethodInfo methodInfo in methodsFromInterface)
+			{
+				object[] parameters = methodInfo
+					.GetParameters()
+					.Select(x => x.ParameterType)
+					.Select(GetDefaultValue)
+					.ToArray();
+
+				methodEmptyParams[methodInfo] = parameters;
+			}
+
+			foreach (MethodInfo methodInfo in methodsFromInterface)
+			{
+				decoratedObject.When(x => methodInfo.Invoke(x, methodEmptyParams[methodInfo])).Do(x => { throw new Exception(); });
+			}
+
+			foreach (MethodInfo methodInfo in methodsFromInterface)
+			{
+				try
+				{
+					methodInfo.Invoke(decorator, methodEmptyParams[methodInfo]);
+					Assert.Fail(); // Assert exception is rethrown
+				}
+				catch (TargetInvocationException e) when (e.InnerException is IntegrationPointsException) { }
+
+				logger.Received().LogError(Arg.Any<Exception>(), Arg.Any<string>(), methodInfo.Name); // Asser error is logged
+			}
+		}
+
+		[Test]
+		public void ItShouldLogAndRethrowExceptionInPropertiesOfDecoratedObject()
+		{
+			IRSAPIClient decoratedObject = Substitute.For<IRSAPIClient>();
+			IAPILog logger = Substitute.For<IAPILog>();
+			logger.ForContext<RsapiClientWrapperWithLogging>().Returns(logger);
+			var decorator = new RsapiClientWrapperWithLogging(decoratedObject, logger);
+
+			Type interfaceType = typeof(IRSAPIClient);
+			PropertyInfo[] propertiesFromInterface = GetAllPropertiesFromInterfaceAndDerivedInterfaces(interfaceType).ToArray();
+
+			var propertyEmptyValues = new Dictionary<PropertyInfo, object>();
+			foreach (PropertyInfo methodInfo in propertiesFromInterface)
+			{
+				object parameters = GetDefaultValue(methodInfo.PropertyType);
+				propertyEmptyValues[methodInfo] = parameters;
+			}
+
+			foreach (PropertyInfo propertyInfo in propertiesFromInterface)
+			{
+				if (propertyInfo.CanRead)
+				{
+					decoratedObject.When(x => propertyInfo.GetValue(x)).Do(x => { throw new Exception(); });
+				}
+				if (propertyInfo.CanWrite)
+				{
+					decoratedObject.When(x => propertyInfo.SetValue(x, propertyEmptyValues[propertyInfo]))
+						.Do(x => { throw new Exception(); });
+				}
+			}
+
+			foreach (PropertyInfo propertyInfo in propertiesFromInterface)
+			{
+				if (propertyInfo.CanRead)
+				{
+					try
+					{
+						propertyInfo.GetValue(decorator);
+						Assert.Fail(); // Assert exception is rethrown
+					}
+					catch (TargetInvocationException e) when (e.InnerException is IntegrationPointsException)
+					{
+					}
+
+					logger.Received().LogError(Arg.Any<Exception>(), Arg.Any<string>(), propertyInfo.Name); // Asser error is logged
+				}
+
+				if (propertyInfo.CanWrite)
+				{
+					try
+					{
+						propertyInfo.SetValue(decorator, propertyEmptyValues[propertyInfo]);
+						Assert.Fail(); // Assert exception is rethrown
+					}
+					catch (TargetInvocationException e) when (e.InnerException is IntegrationPointsException)
+					{
+					}
+
+					logger.Received().LogError(Arg.Any<Exception>(), Arg.Any<string>(), propertyInfo.Name); // Asser error is logged
+				}
+			}
+		}
+
+		[Test]
+		public void ItShouldLogAndRethrowExceptionInEventsOfDecoratedObject()
+		{
+			IRSAPIClient decoratedObject = Substitute.For<IRSAPIClient>();
+			IAPILog logger = Substitute.For<IAPILog>();
+			logger.ForContext<RsapiClientWrapperWithLogging>().Returns(logger);
+			var decorator = new RsapiClientWrapperWithLogging(decoratedObject, logger);
+
+			Type interfaceType = typeof(IRSAPIClient);
+			EventInfo[] eventsFromInterface = GetAllEventsFromInterfaceAndDerivedInterfaces(interfaceType).ToArray();
+
+			foreach (EventInfo eventInfo in eventsFromInterface)
+			{
+				decoratedObject.When(x => eventInfo.AddEventHandler(x, Arg.Any<Delegate>())).Do(x => { throw new Exception(); });
+				decoratedObject.When(x => eventInfo.RemoveEventHandler(x, Arg.Any<Delegate>())).Do(x => { throw new Exception(); });
+			}
+
+			foreach (EventInfo propertyInfo in eventsFromInterface)
+			{
+				try
+				{
+					propertyInfo.AddEventHandler(decorator, null);
+					Assert.Fail(); // Assert exception is rethrown
+				}
+				catch (TargetInvocationException e) when (e.InnerException is IntegrationPointsException)
+				{
+				}
+
+				logger.Received().LogError(Arg.Any<Exception>(), Arg.Any<string>(), propertyInfo.Name); // Asser error is logged
+
+
+				try
+				{
+					propertyInfo.RemoveEventHandler(decorator, null);
+					Assert.Fail(); // Assert exception is rethrown
+				}
+				catch (TargetInvocationException e) when (e.InnerException is IntegrationPointsException)
+				{
+				}
+
+				logger.Received().LogError(Arg.Any<Exception>(), Arg.Any<string>(), propertyInfo.Name); // Asser error is logged
+			}
+		}
+
+		private IEnumerable<PropertyInfo> GetAllPropertiesFromInterfaceAndDerivedInterfaces(Type interfaceType)
+		{
+			IEnumerable<Type> allInterfaces = GetAllInterfaces(interfaceType);
+
+			return allInterfaces
+				.Select(x => (IEnumerable<PropertyInfo>)x.GetProperties())
+				.Aggregate((x, y) => x.Concat(y));
+		}
+		private IEnumerable<EventInfo> GetAllEventsFromInterfaceAndDerivedInterfaces(Type interfaceType)
+		{
+			IEnumerable<Type> allInterfaces = GetAllInterfaces(interfaceType);
+
+			return allInterfaces
+				.Select(x => (IEnumerable<EventInfo>)x.GetEvents())
+				.Aggregate((x, y) => x.Concat(y));
+		}
+
+		private IEnumerable<MethodInfo> GetAllMethodsFromInterfaceAndDerivedInterfaces(Type interfaceType)
+		{
+			return GetAllMethodsAndPropertiesFromInterfaceAndDerivedInterfaces(interfaceType).Where(x => !x.IsSpecialName);
+		}
+
+		private IEnumerable<MethodInfo> GetAllMethodsAndPropertiesFromInterfaceAndDerivedInterfaces(Type interfaceType)
+		{
+			IEnumerable<Type> allInterfaces = GetAllInterfaces(interfaceType);
+
+			return allInterfaces
+				.Select(x => (IEnumerable<MethodInfo>)x.GetMethods())
+				.Aggregate((x, y) => x.Concat(y));
+		}
+
+		private IEnumerable<Type> GetAllInterfaces(Type t)
+		{
+			IEnumerable<Type> allImplementedInterfeces = new List<Type> { t };
+			foreach (Type type in t.GetInterfaces())
+			{
+				allImplementedInterfeces = allImplementedInterfeces.Concat(GetAllInterfaces(type));
+			}
+			return allImplementedInterfeces;
+		}
+
+		private object GetDefaultValue(Type t)
+		{
+			return t.IsValueType ? Activator.CreateInstance(t) : null;
+		}
+	}
+}
+
---
+++ Source/kCura.IntegrationPoints.Data/RSAPIClient/RsapiClientWrapperWithLogging.cs
@@ -0,0 +1,470 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using kCura.IntegrationPoints.Domain.Exceptions;
+using kCura.Relativity.Client;
+using kCura.Relativity.Client.Repositories;
+using Relativity.API;
+
+namespace kCura.IntegrationPoints.Data.RSAPIClient
+{
+	public class RsapiClientWrapperWithLogging : IRSAPIClient
+	{
+		private readonly IRSAPIClient _rsapiClient;
+		private readonly IAPILog _logger;
+		public RsapiClientWrapperWithLogging(IRSAPIClient rsapiClient, IAPILog logger)
+		{
+			_rsapiClient = rsapiClient;
+			_logger = logger.ForContext<RsapiClientWrapperWithLogging>();
+
+			Failure += OnFailure;
+			ProcessFailure += OnProcessFailure;
+			RSAPIClientServiceOperationFailed += OnRsapiClientServiceOperationFailed;
+			ProcessCompleteWithError += OnProcessCompleteWithError;
+		}
+
+		public string Login()
+		{
+			return LogAndRethrowException(() =>
+				_rsapiClient.Login()
+			);
+		}
+
+		public string TokenLogin(string sessionToken)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.TokenLogin(sessionToken)
+			);
+		}
+
+		public ReadResult GenerateRelativityAuthenticationToken(APIOptions apiOpt)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.GenerateRelativityAuthenticationToken(apiOpt)
+			);
+		}
+
+		public ReadResult GetAuthenticationToken(APIOptions apiOpt, string onBehalfOfUserName)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.GetAuthenticationToken(apiOpt, onBehalfOfUserName)
+			);
+		}
+
+		public void Logout(APIOptions apiOpt)
+		{
+			LogAndRethrowException(() => 
+				_rsapiClient.Logout(apiOpt)
+			);
+		}
+
+		public string LoginWithCredentials(string username, string password)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.LoginWithCredentials(username, password)
+			);
+		}
+
+		public ResultSet Create(APIOptions apiOpt, List<ArtifactRequest> artifactRequests)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.Create(apiOpt, artifactRequests)
+			);
+		}
+
+		public ReadResultSet Read(APIOptions apiOpt, List<ArtifactRequest> artifactRequests)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.Read(apiOpt, artifactRequests)
+			);
+		}
+
+		public ResultSet Update(APIOptions apiOpt, List<ArtifactRequest> artifactRequests)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.Update(apiOpt, artifactRequests)
+			);
+		}
+
+		public ResultSet Delete(APIOptions apiOpt, List<ArtifactRequest> artifactRequests)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.Delete(apiOpt, artifactRequests)
+			);
+		}
+
+		public Dictionary<AdminChoice, int> GetAdminChoiceTypes(APIOptions apiOpt)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.GetAdminChoiceTypes(apiOpt)
+			);
+		}
+
+		public MassEditResult MassEdit(APIOptions apiOpt, ArtifactRequest templateArtifactRequest, List<int> artifactIDs)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.MassEdit(apiOpt, templateArtifactRequest, artifactIDs)
+			);
+		}
+
+		public MassCreateResult MassCreate(APIOptions apiOpt, ArtifactRequest templateArtifactRequest, List<ArtifactRequest> artifactRequests)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.MassCreate(apiOpt, templateArtifactRequest, artifactRequests)
+			);
+		}
+
+		public MassCreateResult MassCreateWithDetails(APIOptions apiOpt, ArtifactRequest templateArtifactRequest,
+			List<ArtifactRequest> artifactRequests)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.MassCreateWithDetails(apiOpt, templateArtifactRequest, artifactRequests)
+			);
+		}
+
+		public MassCreateResult MassCreateWithAPIParameters(APIOptions apiOpt, ArtifactRequest templateArtifactRequest,
+			List<ArtifactRequest> artifactRequests, List<APIParameters> apiParms)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.MassCreateWithAPIParameters(apiOpt, templateArtifactRequest, artifactRequests, apiParms)
+			);
+		}
+
+		public ResultSet MassDelete(APIOptions apiOpt, MassDeleteOptions options, List<int> artifactIDs)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.MassDelete(apiOpt, options, artifactIDs)
+			);
+		}
+
+		public ResultSet MassDeleteAllObjects(APIOptions apiOpt, MassDeleteOptions options)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.MassDeleteAllObjects(apiOpt, options)
+			);
+		}
+
+		public ResultSet MassDeleteDocuments(APIOptions apiOpt, DocumentMassDeleteOptions options, List<int> artifactIDs)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.MassDeleteDocuments(apiOpt, options, artifactIDs)
+			);
+		}
+
+		public ResultSet MassDeleteAllDocuments(APIOptions apiOpt, DocumentMassDeleteOptions options)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.MassDeleteAllDocuments(apiOpt, options)
+			);
+		}
+
+		public ProcessInformation GetProcessState(APIOptions apiOpt, Guid processID)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.GetProcessState(apiOpt, processID)
+			);
+		}
+
+		public ProcessOperationResult FlagProcessForCancellationAsync(APIOptions apiOpt, Guid processID)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.FlagProcessForCancellationAsync(apiOpt, processID)
+			);
+		}
+
+		public ProcessOperationResult CreateBatchesForBatchSetAsync(APIOptions apiOpt, int batchSetArtifactID)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.CreateBatchesForBatchSetAsync(apiOpt, batchSetArtifactID)
+			);
+		}
+
+		public ProcessOperationResult PurgeBatchesOfBatchSetAsync(APIOptions apiOpt, int batchSetArtifactID)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.PurgeBatchesOfBatchSetAsync(apiOpt, batchSetArtifactID)
+			);
+		}
+
+		public SerialLicense GetLicense(APIOptions apiOpt, Guid appGuid, string password)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.GetLicense(apiOpt, appGuid, password)
+			);
+		}
+
+		public ProcessOperationResult InstallApplication(APIOptions apiOpt, AppInstallRequest appInstallRequest)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.InstallApplication(apiOpt, appInstallRequest)
+			);
+		}
+
+		public ResultSet InstallLibraryApplication(APIOptions apiOpt, AppInstallRequest appInstallRequest)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.InstallLibraryApplication(apiOpt, appInstallRequest)
+			);
+		}
+
+		public ResultSet ExportApplication(APIOptions apiOpt, AppExportRequest appExportRequest)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.ExportApplication(apiOpt, appExportRequest)
+			);
+		}
+
+		public ResultSet PushResourceFiles(APIOptions apiOpt, List<ResourceFileRequest> rfPushRequests)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.PushResourceFiles(apiOpt, rfPushRequests)
+			);
+		}
+
+		public void Clear(FileRequest fileRequest)
+		{
+			LogAndRethrowException(() => 
+				_rsapiClient.Clear(fileRequest)
+			);
+		}
+
+		public KeyValuePair<DownloadResponse, Stream> Download(FileRequest fileRequest)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.Download(fileRequest)
+			);
+		}
+
+		public DownloadResponse Download(FileRequest fileRequest, string outputPath)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.Download(fileRequest, outputPath)
+			);
+		}
+
+		public void Upload(UploadRequest uploadRequest)
+		{
+			LogAndRethrowException(() => 
+				_rsapiClient.Upload(uploadRequest)
+			);
+		}
+
+		public DownloadURLResponse GetFileFieldDownloadURL(DownloadURLRequest downloadUrlRequest)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.GetFileFieldDownloadURL(downloadUrlRequest)
+			);
+		}
+
+		public event CancelEventHandler Cancel
+		{
+			add { LogAndRethrowException(() => _rsapiClient.Cancel += value); }
+			remove { LogAndRethrowException(() => _rsapiClient.Cancel -= value); }
+		}
+
+		public event DownloadCompleteEventHandler DownloadComplete
+		{
+			add { LogAndRethrowException(() => _rsapiClient.DownloadComplete += value); }
+			remove { LogAndRethrowException(() => _rsapiClient.DownloadComplete -= value); }
+		}
+
+		public event FailureEventHandler Failure
+		{
+			add { LogAndRethrowException(() => _rsapiClient.Failure += value); }
+			remove { LogAndRethrowException(() => _rsapiClient.Failure -= value); }
+		}
+
+		public event ProgressEventHandler Progress
+		{
+			add { LogAndRethrowException(() => _rsapiClient.Progress += value); }
+			remove { LogAndRethrowException(() => _rsapiClient.Progress -= value); }
+		}
+
+		public event UploadCompleteEventHandler UploadComplete
+		{
+			add { LogAndRethrowException(() => _rsapiClient.UploadComplete += value); }
+			remove { LogAndRethrowException(() => _rsapiClient.UploadComplete -= value); }
+		}
+
+		public QueryResult Query(APIOptions apiOpt, Query queryObject, int length = 0)
+		{
+			return LogAndRethrowException(() => _rsapiClient.Query(apiOpt, queryObject, length));
+		}
+
+		public QueryResult QuerySubset(APIOptions apiOpt, string queryToken, int start, int length)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.QuerySubset(apiOpt, queryToken, start, length)
+			);
+		}
+
+		public ExecuteBatchResultSet ExecuteBatch(APIOptions apiOpt, List<Command> commands, TransactionType transType)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.ExecuteBatch(apiOpt, commands, transType)
+			);
+		}
+
+		public RelativityScriptResult ExecuteRelativityScript(APIOptions apiOpt, int scriptArtifactID, List<RelativityScriptInput> inputs)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.ExecuteRelativityScript(apiOpt, scriptArtifactID, inputs)
+			);
+		}
+
+		public List<RelativityScriptInputDetails> GetRelativityScriptInputs(APIOptions apiOpt, int scriptArtifactID)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.GetRelativityScriptInputs(apiOpt, scriptArtifactID)
+			);
+		}
+
+		public ProcessOperationResult MonitorProcessState(APIOptions apiOpt, Guid processID)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.MonitorProcessState(apiOpt, processID)
+			);
+		}
+
+		public event ProcessCancelEventHandler ProcessCancelled
+		{
+			add { LogAndRethrowException(() => _rsapiClient.ProcessCancelled += value); }
+			remove { LogAndRethrowException(() => _rsapiClient.ProcessCancelled -= value); }
+		}
+
+		public event ProcessFailureEventHandler ProcessFailure
+		{
+			add { LogAndRethrowException(() => _rsapiClient.ProcessFailure += value); }
+			remove { LogAndRethrowException(() => _rsapiClient.ProcessFailure -= value); }
+		}
+
+		public event ProcessProgressEventHandler ProcessProgress
+		{
+			add { LogAndRethrowException(() => _rsapiClient.ProcessProgress += value); }
+			remove { LogAndRethrowException(() => _rsapiClient.ProcessProgress -= value); }
+		}
+
+		public event ProcessCompleteEventHandler ProcessComplete
+		{
+			add { LogAndRethrowException(() => _rsapiClient.ProcessComplete += value); }
+			remove { LogAndRethrowException(() => _rsapiClient.ProcessComplete -= value); }
+		}
+
+		public event ProcessCompleteWithErrorEventHandler ProcessCompleteWithError
+		{
+			add { LogAndRethrowException(() => _rsapiClient.ProcessCompleteWithError += value); }
+			remove { LogAndRethrowException(() => _rsapiClient.ProcessCompleteWithError -= value); }
+		}
+
+		public void Dispose()
+		{
+			LogAndRethrowException(() => 
+				_rsapiClient.Dispose()
+			);
+		}
+
+		public OperationResult ValidateEndpoint()
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.ValidateEndpoint()
+			);
+		}
+
+		public List<RelativityScriptInput> ConvertToScriptInputList(List<RelativityScriptInputDetails> inputDetails)
+		{
+			return LogAndRethrowException(() => 
+				_rsapiClient.ConvertToScriptInputList(inputDetails)
+			);
+		}
+
+		public AuthenticationType AuthType
+		{
+			get { return LogAndRethrowException(() => _rsapiClient.AuthType); }
+			set { LogAndRethrowException(() => _rsapiClient.AuthType = value); }
+		}
+
+		public Uri EndpointUri
+		{
+			get { return LogAndRethrowException(() => _rsapiClient.EndpointUri); }
+			set { LogAndRethrowException(() => _rsapiClient.EndpointUri = value); }
+		}
+
+		public APIOptions APIOptions
+		{
+			get { return LogAndRethrowException(() => _rsapiClient.APIOptions); }
+			set { LogAndRethrowException(() => _rsapiClient.APIOptions = value); }
+		}
+
+		public RepositoryGroup Repositories
+		{
+			get { return LogAndRethrowException(() => _rsapiClient.Repositories); }
+		}
+
+		public event RSAPIClientServiceOperationFailedHandler RSAPIClientServiceOperationFailed
+		{
+			add { LogAndRethrowException(() => _rsapiClient.RSAPIClientServiceOperationFailed += value); }
+			remove { LogAndRethrowException(() => _rsapiClient.RSAPIClientServiceOperationFailed -= value); }
+		}
+
+		private T LogAndRethrowException<T>(Func<T> wrappedFunction, [CallerMemberName] string callerName = "")
+		{
+			try
+			{
+				return wrappedFunction();
+			}
+			catch (Exception e)
+			{
+				throw LogAndCreateException(e, callerName);
+			}
+		}
+
+		private void LogAndRethrowException(Action wrappedAction, [CallerMemberName] string callerName = "")
+		{
+			try
+			{
+				wrappedAction();
+			}
+			catch (Exception e)
+			{
+				throw LogAndCreateException(e, callerName);
+			}
+		}
+
+		private IntegrationPointsException LogAndCreateException(Exception e, string callerName)
+		{
+			_logger.LogError(e, "Exception occured in IRSAPIClient implementation of {callerName} method", callerName);
+			return new IntegrationPointsException($"Exception occured in IRSAPIClient implementation of {callerName} method", e)
+			{
+				ExceptionSource = IntegrationPointsExceptionSource.RSAPI,
+				ShouldAddToErrorsTab = true
+			};
+		}
+
+		private void OnRsapiClientServiceOperationFailed(object sender, ServiceOperationFailedEventArgs eventArgs)
+		{
+			string operationName = eventArgs.OperationName;
+			string serviceType = eventArgs.ServiceType.ToString();
+			_logger.LogError(eventArgs.OperationException, "OnRsapiClientServiceOperationFailed event of RsapiClient was raised. Operation name: {operationName}, service type: {serviceType}",
+				operationName, serviceType);
+		}
+
+		private void OnProcessFailure(object sender, ProcessFailureEventArgs eventArgs)
+		{
+			ProcessInformation processInfo = eventArgs.ProcessInformation;
+			_logger.LogWarning("OnProcessFailure event of RsapiClient was raised. Process information: {@processInfo}", processInfo);
+		}
+
+		private void OnFailure(FailureEventArgs eventArgs)
+		{
+			TargetField targetField = eventArgs.TargetField;
+			_logger.LogError(eventArgs.Exception, "OnFailure event of RsapiClient was raised. Target fields: {@targetField}", targetField);
+		}
+
+		private void OnProcessCompleteWithError(object sender, ProcessCompleteWithErrorEventArgs eventArgs)
+		{
+			ProcessInformation processInfo = eventArgs.ProcessInformation;
+			_logger.LogWarning("OnProcessCompleteWithError event of RsapiClient was raised. Process information: {@processInfo}", processInfo);
+		}
+	}
+}
