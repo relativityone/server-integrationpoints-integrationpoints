c6820255e38a99dde6029d8213764f4e0108a4fc
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2018-03-20 02:48:07 -0500

    Merge pull request #661 in IN/integrationpoints from REL-208244_Filtering_workspaces_by_name to develop
    Squashed commit of the following:
    
    commit c7d09a13f0f99e1d4337e3fc530ed050d26b273f
    Merge: b0b2c9a 4059aec
    Author: Artur Jaskiewicz <artur.jaskiewicz@relativity.com>
    Date:   Tue Mar 20 08:47:08 2018 +0100
    
        Merge remote-tracking branch 'remotes/origin/develop' into REL-208244_Filtering_workspaces_by_name
    
    commit b0b2c9a40c5de410045137aea642dc0e8e6eeffe
    Author: Artur Jaskiewicz <artur.jaskiewicz@relativity.com>
    Date:   Mon Mar 19 15:27:32 2018 +0100
    
        REL-208244 Filtering workspaces by name
        Refactor after code review
    
    commit 025dd12a35cfd8867e9561eb2fe78d8bace42b96
    Author: Artur Jaskiewicz <artur.jaskiewicz@relativity.com>
    Date:   Mon Mar 19 14:35:39 2018 +0100
    
        REL-208244
        Filtering workspaces by name

--- Source/kCura.IntegrationPoints.UITests/Pages/GeneralPage.cs
+++ Source/kCura.IntegrationPoints.UITests/Pages/GeneralPage.cs
@@ -1,71 +1,115 @@
-﻿using OpenQA.Selenium;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using kCura.IntegrationPoint.Tests.Core;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
 
 namespace kCura.IntegrationPoints.UITests.Pages
 {
-    public class GeneralPage : Page
-    {
-        
-        // TODO Move to some "SthBar", "Navigator" or something similar
-        [FindsBy(How = How.Id, Using = "GetNavigateHomeScript")]
-        protected IWebElement NavigateHome;
+	public class GeneralPage : Page
+	{
 
-        [FindsBy(How = How.ClassName, Using = "headerUpperRow")]
-        protected IWebElement Header;
+		// TODO Move to some "SthBar", "Navigator" or something similar
+		[FindsBy(How = How.Id, Using = "GetNavigateHomeScript")]
+		protected IWebElement NavigateHome;
 
-        [FindsBy(How = How.Id, Using = "horizontal-tabstrip")]
-        protected IWebElement MainMenu;
+		[FindsBy(How = How.ClassName, Using = "headerUpperRow")]
+		protected IWebElement Header;
 
-        [FindsBy(How = How.ClassName, Using = "quickNavIcon")]
-        protected IWebElement QuickNavigation;
+		[FindsBy(How = How.Id, Using = "horizontal-tabstrip")]
+		protected IWebElement MainMenu;
 
-        [FindsBy(How = How.CssSelector, Using = ".quickNavTextbox")]
-        protected IWebElement QuickNavigationInput;
+		[FindsBy(How = How.ClassName, Using = "quickNavIcon")]
+		protected IWebElement QuickNavigation;
 
-        [FindsBy(How = How.XPath, Using =
-                "//div[@class='quickNavOuterContainer']/ul/li[@class='quickNavResultItem ui-menu-item']/a[@title='Integration Points']")]
-        protected IWebElement QuickNavigationResult;
+		[FindsBy(How = How.CssSelector, Using = ".quickNavTextbox")]
+		protected IWebElement QuickNavigationInput;
 
-        [FindsBy(How = How.CssSelector, Using = "span[title = 'User Dropdown Menu']")]
-        protected IWebElement UserDropdownMenu;
+		[FindsBy(How = How.XPath, Using =
+				"//div[@class='quickNavOuterContainer']/ul/li[@class='quickNavResultItem ui-menu-item']/a[@title='Integration Points']")]
+		protected IWebElement QuickNavigationResult;
 
-        public GeneralPage(RemoteWebDriver driver) : base(driver)
-        {
-            PageFactory.InitElements(driver, this);
-        }
+		[FindsBy(How = How.CssSelector, Using = "span[title = 'User Dropdown Menu']")]
+		protected IWebElement UserDropdownMenu;
 
-        public LoginPage LogOut()
-        {
-            UserDropdownMenu.Click();
-            IWebElement logOutLink = Driver.FindElement(By.LinkText("Logout"));
-            logOutLink.Click();
-            return new LoginPage(Driver);
-        }
-        
-        public GeneralPage ChooseWorkspace(string name)
-        {
-            NavigateHome.Click();
-            MainMenu.FindElement(By.LinkText("Workspaces")).Click();
-	        IWebElement viewMenu = Driver.SwitchTo().Frame("externalPage").FindElement(By.Id("viewMenu"));
+		public GeneralPage(RemoteWebDriver driver) : base(driver)
+		{
+			PageFactory.InitElements(driver, this);
+		}
+
+		public LoginPage LogOut()
+		{
+			UserDropdownMenu.Click();
+			IWebElement logOutLink = Driver.FindElement(By.LinkText("Logout"));
+			logOutLink.Click();
+			return new LoginPage(Driver);
+		}
+
+		public GeneralPage ChooseWorkspace(string name)
+		{
+			NavigateHome.Click();
+			MainMenu.FindElement(By.LinkText("Workspaces")).Click();
+			IWebElement workspaceLink = GetWorkspaceLink(name);
+
+			//IWebElement viewMenu = Driver.SwitchTo().Frame("externalPage").FindElement(By.Id("viewMenu"));
 			//var viewMenuSelect = new SelectElement(viewMenu);
 			//viewMenuSelect.SelectByText("All Case Templates");
 
-	        IWebElement element = Driver.FindElement(By.LinkText(name));
-            element.Click();
-            return this;
-        }
+			workspaceLink.Click();
+			return this;
+		}
 
-        public IntegrationPointsPage GoToIntegrationPointsPage()
-        {
+		private IWebElement GetWorkspaceLink(string workspaceName)
+		{
+			IWebDriver externalPage = Driver.SwitchTo().Frame("externalPage");
+			IWebElement nameSearchFilter = FindFilterTextboxInColumn("Name", externalPage);
+			nameSearchFilter.SendKeys(workspaceName);
+			nameSearchFilter.SendKeys(Keys.Return);
 			WaitForPage();
-            QuickNavigation.Click();
-            QuickNavigationInput.SendKeys("Integration Points" + Keys.Enter);
+			IWebElement workspaceLink = Driver.FindElement(By.LinkText(workspaceName));
+			return workspaceLink;
+		}
+
+		private IWebElement FindFilterTextboxInColumn(string title, IWebDriver driver)
+		{
+			// We assume that column exists in current view
+			IWebElement table = driver.FindElement(By.ClassName("ui-jqgrid-htable"));
+			ReadOnlyCollection<IWebElement> rows = table.FindElements(By.TagName("tr"));
+
+			int columnIndex = FindColumnIndex(title, rows[0]);
+			if (columnIndex >= 0)
+			{
+				ReadOnlyCollection<IWebElement> thElements = rows[1].FindElements(By.TagName("th"));
+				IWebElement input = thElements[columnIndex].FindElement(By.TagName("input"));
+				return input;
+			}
+			else
+			{
+				throw new TestException($"Could not find column '{title}' in Workspaces tab.");
+			}
+		}
+
+		private int FindColumnIndex(string columnTitle, IWebElement webElement)
+		{
+			return webElement
+				.FindElements(By.TagName("th"))
+				.ToList()
+				.FindIndex(x => x.GetAttribute("title").Equals(columnTitle));
+		}
+
+		public IntegrationPointsPage GoToIntegrationPointsPage()
+		{
+			WaitForPage();
+			QuickNavigation.Click();
+			QuickNavigationInput.SendKeys("Integration Points" + Keys.Enter);
 			Sleep(300);
 			QuickNavigationInput.SendKeys(Keys.Enter);
-            //QuickNavigationResult.Click();
-            return new IntegrationPointsPage(Driver);
-        }
-        
-    }
+			//QuickNavigationResult.Click();
+			return new IntegrationPointsPage(Driver);
+		}
+
+	}
 }
