f9b8e385018d93830455ea913f30be8c729e17a9
Andrzej Korczyński <andrzej.korczynski@relativity.com>
2018-04-06 09:49:06 -0500

    Merge pull request #688 in IN/integrationpoints from REL-206390 to develop
    Squashed commit of the following:
    
    commit 55323c5881952dced3216694ecfa5ff8b958917d
    Merge: 1f50cdd 93123ca
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Fri Apr 6 16:41:21 2018 +0200
    
        Merge branch 'develop' into REL-206390
    
    commit 1f50cddd97c5d46042dc640f69547ca02973a8cc
    Merge: 80827f5 1d471a0
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Tue Apr 3 14:04:14 2018 +0200
    
        Merge branch 'develop' into REL-206390
    
    commit 80827f561dd22dc5ca4d25946f96942e2a26930d
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Tue Apr 3 13:51:02 2018 +0200
    
        REL-206390 [Added MessageService library to schema] Create foundational SUM metric framework
    
    commit 8e204cad54e39b008d428dadd5af1f48f18ede93
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Fri Mar 30 16:18:19 2018 +0200
    
        REL-206390 [Fix build] Create foundational SUM metric framework
    
    commit 439fd22500bf832b28901492a982514c220748c0
    Merge: b211125 f75522e
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Fri Mar 30 15:59:29 2018 +0200
    
        Merge branch 'develop' into REL-206390
    
    commit b2111251a52b040663ea06638582491a2437db1b
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Fri Mar 30 15:29:51 2018 +0200
    
        REL-206390 [Updated Message Service nuget] Create foundational SUM metric framework
    
    commit ac17100713bea03605b4c86636ee74719e55b19f
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Fri Mar 30 14:07:34 2018 +0200
    
        REL-206390 [Added Message Service nuget to RIP] Create foundational SUM metric framework
    
    commit da57b154b9d5b79241e7b652add6c12cbfce2e07
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Sat Mar 24 18:28:19 2018 +0100
    
        REL-206390 Create foundational SUM metric framework
    
    commit 3dd4f8e971806443e339962ac99e1d651bb8a63b
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Sat Mar 24 17:56:30 2018 +0100
    
        REL-206390 Create foundational SUM metric framework
    
    commit 38625d8fe770f0b9b3ec9e48aa1b1c77576e2d39
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Sat Mar 24 17:50:50 2018 +0100
    
        REL-206390 Create foundational SUM metric framework
    
    commit eb72d505b1e347b41621e4b2a226a3a5a9357155
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Thu Mar 22 14:42:24 2018 +0100
    
        REL-206390 Create foundational SUM metric framework
    
    commit b09d071a8fed8d6650f1311df1cb06e1de7092d7
    Author: Andrzej Korczyński <andrzej.korczynski@relativity.com>
    Date:   Tue Mar 13 17:49:43 2018 +0100
    
        REL-206390 Create foundational SUM metric framework

---
+++ Source/kCura.IntegrationPoints.Core/Monitoring/MetricsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kCura.IntegrationPoints.Core.Monitoring
+{
+	public static class MetricsRepository
+	{
+		public static string TotalItemsTemplate = "IntegrationPoints.Usage.NumberOfRecords.{0}";
+	}
+}
