94f0cf8b2592e13b4cf4c742c93c402d2395c955
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2018-04-13 10:33:53 +0200

    Created class for copying entire directories

---
+++ Source/kCura.IntegrationPoints.UITests/Auxiliary/FileHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+
+namespace kCura.IntegrationPoints.UITests.Auxiliary
+{
+	public class FileHelper
+	{
+		public static void CopyDirectory(string originalPath, string destinationPath)
+		{
+			CopyFilesInDirectory(originalPath, destinationPath);
+		}
+
+		private static void CopyFilesInDirectory(string originalPath, string destinationPath)
+		{
+			if (!Directory.Exists(destinationPath))
+			{
+				Directory.CreateDirectory(destinationPath);
+			}
+
+			var directory = new DirectoryInfo(originalPath);
+
+			FileInfo[] files = directory.GetFiles();
+			foreach (var file in files)
+			{
+				string fileName = file.Name;
+				string originalFilePath = $"{originalPath}\\{fileName}";
+				string destinationFilePath = $"{destinationPath}\\{fileName}";
+				CopyFile(originalFilePath, destinationFilePath);
+			}
+
+			DirectoryInfo[] subdirectories = directory.GetDirectories();
+			foreach (var subdirectory in subdirectories)
+			{
+				string subdirectoryName = subdirectory.Name;
+				string originalSubdirectoryPath = $"{originalPath}\\{subdirectoryName}";
+				string destinationSubdirectoryPath = $"{destinationPath}\\{subdirectoryName}";
+				CopyFilesInDirectory(originalSubdirectoryPath, destinationSubdirectoryPath);
+			}
+		}
+
+		public static void CopyFile(string originalLocation, string destinationLocation)
+		{
+			File.Copy(originalLocation, destinationLocation, true);
+		}
+	}
+}
---
+++ Source/kCura.IntegrationPoints.UITests/Tests/ImportFromLoadFile/ImportFromLoadFileTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace kCura.IntegrationPoints.UITests.Tests.ImportFromLoadFile
+{
+	public class ImportFromLoadFileTest : UiTest
+	{
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			CopyFilesToFileshare();
+		}
+
+		private void CopyFilesToFileshare()
+		{
+			// TODO
+		}
+
+		[Test, Order(1)]
+		public void DocumentImportFromLoadFile_TC_ILF_DOC_1()
+		{
+			// TODO
+		}
+	}
+}
