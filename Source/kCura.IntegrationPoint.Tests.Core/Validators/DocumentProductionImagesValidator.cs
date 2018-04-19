fa0ee7a8cdccd1cfcb9212bca40a0fed83916245
Grzegorz Ziemiański <grzegorz.ziemianski@relativity.com>
2018-04-19 05:49:27 -0500

    Merge pull request #725 in IN/integrationpoints from REL-214470-push-ui-tests-stabilization to develop
    Squashed commit of the following:
    
    commit d1d63dca1e1dcc60303386280e29950d1b7fed1a
    Author: Grzegorz Ziemianski <grzegorz.ziemianski@relativity.com>
    Date:   Thu Apr 19 11:54:49 2018 +0200
    
        REL-214470
        Update relativity packages
    
    commit bd44fa8912e40f3241594fcd58241635ba235eca
    Merge: 42eb3b1 313397c
    Author: Grzegorz Ziemianski <grzegorz.ziemianski@relativity.com>
    Date:   Thu Apr 19 11:25:41 2018 +0200
    
        Merge branch 'develop' into REL-214470-push-ui-tests-stabilization
    
    commit 42eb3b1ed68853eb67dabcb6db7613c98b0e173c
    Author: Artur Jaskiewicz <artur.jaskiewicz@relativity.com>
    Date:   Thu Apr 19 09:36:11 2018 +0200
    
        Revert "Merge pull request #721 in IN/integrationpoints from REL-214470-push-ui-tests-stabilization to develop"
    
        This reverts commit 86d4d61479a836c97a75ffa6446faf75791242d3.
    
    commit 1d9d80a6bf8ef18f7793e838fdf40641dd838457
    Merge: 7071079 86d4d61
    Author: Artur Jaskiewicz <artur.jaskiewicz@relativity.com>
    Date:   Wed Apr 18 14:15:27 2018 +0200
    
        Merge remote-tracking branch 'remotes/origin/develop' into REL-214470-push-ui-tests-stabilization
    
    commit 707107952087e586206baf80b0acb9dbb2f190c1
    Author: Artur Jaskiewicz <artur.jaskiewicz@relativity.com>
    Date:   Wed Apr 18 13:34:03 2018 +0200
    
        Revert "Merge pull request #720 in IN/integrationpoints from REL-214470-push-ui-tests-stabilization to develop"
    
        This reverts commit c9b3d1ef69e47df951e98cb3122daf4e8b234c58.
    
    commit 9b6dce17932c401ca2f1b2b330806ac5965e8ec5
    Merge: 94e4d92 c9b3d1e
    Author: Artur Jaskiewicz <artur.jaskiewicz@relativity.com>
    Date:   Wed Apr 18 13:33:35 2018 +0200
    
        Merge branch 'REL-214470-push-ui-tests-stabilization' of ssh://git.kcura.com:7999/in/integrationpoints into REL-214470-push-ui-tests-stabilization
    
    commit 94e4d92d958f857953c8838744e04f6d9a8163dc
    Merge: d3f76f4 acb3f83
    Author: Artur Jaskiewicz <artur.jaskiewicz@relativity.com>
    Date:   Wed Apr 18 11:08:36 2018 +0200
    
        Merge remote-tracking branch 'remotes/origin/develop' into REL-214470-push-ui-tests-stabilization
    
    commit d3f76f4a99d38c735523de3074294f2e5587eeed
    Author: Grzegorz Ziemianski <grzegorz.ziemianski@relativity.com>
    Date:   Tue Apr 17 15:00:50 2018 +0200
    
        REL-214470
        Update tests
    
    commit f14cb6922653d89a7957f03892be9db2196b696a
    Author: Grzegorz Ziemianski <grzegorz.ziemianski@relativity.com>
    Date:   Tue Apr 17 11:57:29 2018 +0200
    
        REL-214470
        Update tests
    
    commit fb44b1ab06c8e1c8868da475003f4598eaa1b510
    Author: Grzegorz Ziemianski <grzegorz.ziemianski@relativity.com>
    Date:   Tue Apr 17 08:35:46 2018 +0200
    
        REL-214470
        Update logging packages
    
    commit 97d8c03a7cec1800752dbd771e3fe1e4fe6dbe7e
    Merge: e2fb82e 7158bb2
    Author: Grzegorz Ziemianski <grzegorz.ziemianski@relativity.com>
    Date:   Mon Apr 16 17:57:48 2018 +0200
    
        Merge branch 'develop' into REL-214470-push-ui-tests-stabilization
    
    commit e2fb82e6233b86af12f28bb7dc460d0864b744f7
    Author: Grzegorz Ziemianski <grzegorz.ziemianski@relativity.com>
    Date:   Mon Apr 16 17:56:53 2018 +0200
    
        REL-214470
        Use GetProductionImageFiles
    
    commit 4302ffbfcdf641776510a5f80d55aabba0f5d020
    Author: Artur Jaskiewicz <artur.jaskiewicz@relativity.com>
    Date:   Mon Apr 16 12:27:25 2018 +0200
    
        Add support for RetrieveProducedImagesForDocument web api method in ui tests

---
+++ Source/kCura.IntegrationPoint.Tests.Core/Validators/DocumentProductionImagesValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using kCura.IntegrationPoint.Tests.Core.TestHelpers;
+using NUnit.Framework;
+using Relativity.Core.DTO;
+using Document = kCura.Relativity.Client.DTOs.Document;
+
+namespace kCura.IntegrationPoint.Tests.Core.Validators
+{
+	public class DocumentProductionImagesValidator : IDocumentValidator
+	{
+		private readonly IProductionImagesService _productionImagesService;
+		private readonly int _destinationWorkspaceId;
+		private readonly bool _expectInRepository;
+
+		public DocumentProductionImagesValidator(IProductionImagesService productionImagesService, int destinationWorkspaceId, bool expectInRepository)
+		{
+			_productionImagesService = productionImagesService;	
+			_destinationWorkspaceId = destinationWorkspaceId;
+			_expectInRepository = expectInRepository;
+		}
+
+		public void ValidateDocument(Document destinationDocument, Document sourceDocument)
+		{
+			IList<File> destinationImages = _productionImagesService.GetProductionImagesFileInfo(_destinationWorkspaceId, destinationDocument.ArtifactID);
+			Assert.That(destinationImages, Is.Not.Null, $"Could not find production image file for document {destinationDocument.ArtifactID}");
+
+			int expectedNumberOfImages = Math.Max(sourceDocument.RelativityImageCount.GetValueOrDefault(), 1);
+			Assert.That(destinationImages.Count, Is.EqualTo(expectedNumberOfImages), $"Number of produced images is different than expected for document {destinationDocument.ArtifactID}");
+
+			foreach (File destinationImage in destinationImages)
+			{
+				Assert.That(destinationImage.InRepository, Is.EqualTo(_expectInRepository), $"Destination production image {destinationImage.FileID} does not have InRepository flag set to {_expectInRepository}");
+			}
+		}
+	}
+}
