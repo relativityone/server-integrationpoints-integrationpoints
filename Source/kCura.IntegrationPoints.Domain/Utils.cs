0c3c7d3bac59e5b26d9e996418463dae11849321
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2018-05-17 07:41:19 -0500

    Merge pull request #749 in IN/integrationpoints from REL-224816-fix-for-june-release to release-9.6-june
    Squashed commit of the following:
    
    commit 73acfca0177e7127d43ff7a262bed03632086c34
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Wed May 16 10:44:30 2018 +0200
    
        fixed issue with DestinationWorkspace update

--- Source/kCura.IntegrationPoints.Domain/Utils.cs
+++ Source/kCura.IntegrationPoints.Domain/Utils.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Text;
-
-namespace kCura.IntegrationPoints.Domain
+﻿namespace kCura.IntegrationPoints.Domain
 {
-	/// <summary>
-	/// 
-	/// </summary>
 	public static class Utils
 	{
 		public static string GetFormatForWorkspaceOrJobDisplay(string name, int? id)
 		{
-		    return id.HasValue ? $"{name} - {id}" : name;
+			return id.HasValue ? $"{name} - {id}" : name;
 		}
 
-		public static string GetFormatForWorkspaceOrJobDisplay(string prefix, string name, int id)
+		public static string GetFormatForWorkspaceOrJobDisplay(string prefix, string name, int? id)
 		{
 			return $"{prefix} - {GetFormatForWorkspaceOrJobDisplay(name, id)}";
 		}
