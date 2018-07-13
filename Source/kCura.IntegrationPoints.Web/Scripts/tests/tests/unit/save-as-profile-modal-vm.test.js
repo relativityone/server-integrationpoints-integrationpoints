ab153a0dc5ad6ed2808ca8fc718681e79cd79113
Bartłomiej Wesołowski <bartlomiej.wesolowski@relativity.com>
2018-07-13 11:05:27 +0200

    Changed variable types from var to let, const

--- Source/kCura.IntegrationPoints.Web/Scripts/tests/tests/unit/save-as-profile-modal-vm.test.js
+++ Source/kCura.IntegrationPoints.Web/Scripts/tests/tests/unit/save-as-profile-modal-vm.test.js
@@ -3,14 +3,14 @@ const { suite, test, beforeEach } = intern.getInterface('tdd');
 const { assert } = intern.getPlugin('chai');
 
 suite('save-as-profile-modal-vm.js', () => {
-    var callbackResult = '';
-    var dialogResult = '';
+    let callbackResult = '';
+    let dialogResult = '';
 
-    var okCallback = function(profileName){
+    const okCallback = function(profileName){
         callbackResult = profileName;    
     };
-    
-    var view = {
+
+    const view = {
         dialog: function (dialogMsg) {
             dialogResult = dialogMsg;
         },
@@ -23,42 +23,42 @@ suite('save-as-profile-modal-vm.js', () => {
     })
 
     function setUpProfileView(profileName){
-        var profile = new SaveAsProfileModalViewModel(okCallback);
+        const profile = new SaveAsProfileModalViewModel(okCallback);
         profile.construct(view);
         profile.open(profileName);
         return profile;
     }
 
     test('Example good data 1', () => {
-        var saveAsProfileModalViewModel = setUpProfileView("Example data 1");
+        const saveAsProfileModalViewModel = setUpProfileView("Example data 1");
         saveAsProfileModalViewModel.ok();
         assert.strictEqual(callbackResult, "Example data 1");
         assert.strictEqual(dialogResult, "close");
     });
 
     test('Example wrong ?? data 123', () => {      
-        var saveAsProfileModalViewModel = setUpProfileView("Example wrong ?? data 123");
+        const saveAsProfileModalViewModel = setUpProfileView("Example wrong ?? data 123");
         saveAsProfileModalViewModel.ok();
         assert.strictEqual(callbackResult, '');
         assert.strictEqual(dialogResult, "open");
     });
 
     test('N/A', () => {
-        var saveAsProfileModalViewModel = setUpProfileView('');
+        const saveAsProfileModalViewModel = setUpProfileView('');
         saveAsProfileModalViewModel.ok();
         assert.strictEqual(callbackResult, '');
         assert.strictEqual(dialogResult, "open");
     });
 
     test('null', () => {
-        var saveAsProfileModalViewModel = setUpProfileView(null);
+        const saveAsProfileModalViewModel = setUpProfileView(null);
         saveAsProfileModalViewModel.ok();
         assert.strictEqual(callbackResult, '');
         assert.strictEqual(dialogResult, "open");
     });
 
     test('Other good data 2', () => {
-        var saveAsProfileModalViewModel = setUpProfileView("Other good data 2");
+        const saveAsProfileModalViewModel = setUpProfileView("Other good data 2");
         saveAsProfileModalViewModel.ok();
         assert.strictEqual(callbackResult, "Other good data 2");
         assert.strictEqual(dialogResult, "close");
