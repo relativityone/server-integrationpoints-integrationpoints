b4610bbde7fd54c32ac1ef0e3828fc04de8c9fcb
Grzegorz Ziemiański <grzegorz.ziemianski@relativity.com>
2018-08-14 06:56:35 -0500

    Merge pull request #864 in IN/integrationpoints from REL-247172-rip-failed-to-upgrade to release-9.7-bluestem
    Squashed commit of the following:
    
    commit f8b73090dcf0421d9e514444f713c443375e1e03
    Author: Grzegorz Ziemiański <grzegorz.ziemianski@relativity.com>
    Date:   Tue Aug 14 13:09:56 2018 +0200
    
        REL-247172
        Failed to update RIP(bluestem)

--- Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/SharedLibrary/LoadFileExportConfig.cs
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/SharedLibrary/LoadFileExportConfig.cs
@@ -7,16 +7,17 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.SharedLibrary
 {
 	public class LoadFileExportConfig : ConfigBase, IExportConfig
 	{
-        private const int _DEF_EXPORT_BATCH_SIZE = 1000;
+		private const int _DEF_EXPORT_BATCH_SIZE = 1000;
 		private const int _DEF_EXPORT_THREAD_COUNT = 4;
 		private const int _EXPORT_LOADFILE_ERROR_RETRIES_NUMBER = 2;
 		private const int _EXPORT_LOADFILE_ERROR_WAIT_TIME = 10;
 		private const int _EXPORT_LOADFILE_IO_ERROR_RETRIES_NUMBER = 2;
 		private const int _EXPORT_MAX_NUMBER_OF_TASKS = 2;
+		private const bool _DEF_FORCE_PARALLELISM_IN_NEW_EXPORT = false;
 
 		private const int _EXPORT_LOADFILE_IO_ERROR_WAIT_TIME = 20;
 		
-        private const string _EXPORT_BATCH_SIZE_SETTING_NAME = "ExportBatchSize";
+		private const string _EXPORT_BATCH_SIZE_SETTING_NAME = "ExportBatchSize";
 		private const string _EXPORT_LOADFILE_ERROR_RETRIES_NUMBER_NAME = "Export.LoadFile.ErrorNumberOfRetries";
 		private const string _EXPORT_LOADFILE_ERROR_WAIT_TIME_NAME = "Export.LoadFile.ErrorWaitTime";
 		private const string _EXPORT_LOADFILE_IO_ERROR_RETRIES_NUMBER_NAME = "Export.LoadFile.IOErrorNumberOfRetries";
@@ -24,16 +25,17 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.SharedLibrary
 		private const string _EXPORT_LOADFILE_IO_ERROR_WAIT_TIME_NAME = "Export.LoadFile.IOErrorWaitTime";
 		private const string _EXPORT_THREAD_COUNT_SETTING_NAME = "ExportThreadCount";
 		private const string _EXPORT_MAX_NUMBER_OF_TASKS_SETTING_NAME = "MaxNumberOfFileExportTasks";
+		private const string _FORCE_PARALLELISM_IN_NEW_EXPORT = "Export.ForceParallelismInNewExport";
 
 		private readonly IToggleProvider _toggleProvider;
-        
-	    public bool UseOldExport => _toggleProvider.IsEnabled<UseOldExportVolumeManagerToggle>();
 
-        public int ExportBatchSize => GetValue(_EXPORT_BATCH_SIZE_SETTING_NAME, _DEF_EXPORT_BATCH_SIZE);
+		public bool UseOldExport => _toggleProvider.IsEnabled<UseOldExportVolumeManagerToggle>();
+
+		public int ExportBatchSize => GetValue(_EXPORT_BATCH_SIZE_SETTING_NAME, _DEF_EXPORT_BATCH_SIZE);
 
 		public int ExportThreadCount => GetValue(_EXPORT_THREAD_COUNT_SETTING_NAME, _DEF_EXPORT_THREAD_COUNT);
 
-	    public int ExportIOErrorWaitTime => GetValue(_EXPORT_LOADFILE_IO_ERROR_WAIT_TIME_NAME, _EXPORT_LOADFILE_IO_ERROR_WAIT_TIME);
+		public int ExportIOErrorWaitTime => GetValue(_EXPORT_LOADFILE_IO_ERROR_WAIT_TIME_NAME, _EXPORT_LOADFILE_IO_ERROR_WAIT_TIME);
 
 		public int ExportIOErrorNumberOfRetries => GetValue(_EXPORT_LOADFILE_IO_ERROR_RETRIES_NUMBER_NAME, _EXPORT_LOADFILE_IO_ERROR_RETRIES_NUMBER);
 
@@ -42,6 +44,8 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.SharedLibrary
 		public int ExportErrorWaitTime => GetValue(_EXPORT_LOADFILE_ERROR_WAIT_TIME_NAME, _EXPORT_LOADFILE_ERROR_WAIT_TIME);
 
 		public int MaxNumberOfFileExportTasks => GetValue(_EXPORT_MAX_NUMBER_OF_TASKS_SETTING_NAME, _EXPORT_MAX_NUMBER_OF_TASKS);
+
+		public bool ForceParallelismInNewExport => GetValue(_FORCE_PARALLELISM_IN_NEW_EXPORT, _DEF_FORCE_PARALLELISM_IN_NEW_EXPORT);
 
 		public LoadFileExportConfig(IToggleProvider toggleProvider)
 		{
