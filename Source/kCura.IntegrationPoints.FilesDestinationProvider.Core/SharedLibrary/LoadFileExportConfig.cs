ab50c97302fd26bf4465e342f4d79193d5270d48
Grzegorz Ziemiański <grzegorz.ziemianski@relativity.com>
2018-08-23 01:25:11 -0500

    Merge pull request #879 in IN/integrationpoints from REL-246866-merge-changes-to-develop to develop
    Squashed commit of the following:
    
    commit bbfd4327f18ca9b6e9da0e2fc025a848cc978c55
    Merge: a8f26f0 4ce89d3
    Author: Grzegorz Ziemiański <grzegorz.ziemianski@relativity.com>
    Date:   Wed Aug 22 15:18:56 2018 +0200
    
        Merge branch 'develop' into REL-246866-merge-changes-to-develop
    
        # Conflicts:
        #	paket.dependencies
        #	paket.lock
    
    commit a8f26f085b6dba27aba41ea4145c33552a635bbe
    Author: Grzegorz Ziemiański <grzegorz.ziemianski@relativity.com>
    Date:   Wed Aug 22 15:09:06 2018 +0200
    
        REL-246866
        Update RIP with parallel configuration

--- Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/SharedLibrary/LoadFileExportConfig.cs
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/SharedLibrary/LoadFileExportConfig.cs
@@ -13,6 +13,8 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.SharedLibrary
 		private const int _EXPORT_LOADFILE_ERROR_WAIT_TIME = 10;
 		private const int _EXPORT_LOADFILE_IO_ERROR_RETRIES_NUMBER = 2;
 		private const int _EXPORT_MAX_NUMBER_OF_TASKS = 2;
+		private const bool _DEF_FORCE_PARALLELISM_IN_NEW_EXPORT = true;
+
 
 		private const int _EXPORT_LOADFILE_IO_ERROR_WAIT_TIME = 20;
 		
@@ -24,6 +26,8 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.SharedLibrary
 		private const string _EXPORT_LOADFILE_IO_ERROR_WAIT_TIME_NAME = "Export.LoadFile.IOErrorWaitTime";
 		private const string _EXPORT_THREAD_COUNT_SETTING_NAME = "ExportThreadCount";
 		private const string _EXPORT_MAX_NUMBER_OF_TASKS_SETTING_NAME = "MaxNumberOfFileExportTasks";
+		private const string _FORCE_PARALLELISM_IN_NEW_EXPORT = "Export.ForceParallelismInNewExport";
+
 
 		private readonly IToggleProvider _toggleProvider;
         
@@ -42,6 +46,8 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.SharedLibrary
 		public int ExportErrorWaitTime => GetValue(_EXPORT_LOADFILE_ERROR_WAIT_TIME_NAME, _EXPORT_LOADFILE_ERROR_WAIT_TIME);
 
 		public int MaxNumberOfFileExportTasks => GetValue(_EXPORT_MAX_NUMBER_OF_TASKS_SETTING_NAME, _EXPORT_MAX_NUMBER_OF_TASKS);
+
+		public bool ForceParallelismInNewExport => GetValue(_FORCE_PARALLELISM_IN_NEW_EXPORT, _DEF_FORCE_PARALLELISM_IN_NEW_EXPORT);
 
 		public LoadFileExportConfig(IToggleProvider toggleProvider)
 		{
