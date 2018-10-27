722997b3d256cfc2655dab7c10a7c34919fc2cb3
Patryk Stepien <patryk.stepien@relativity.com>
2018-10-27 21:44:52 +0200

    Refactor scripts and add restoring packages

--- default.ps1
+++ default.ps1
@@ -1,5 +1,7 @@
 #Requires -Version 5.0
 
+FormatTaskName "------- Executing Task: {0} -------"
+
 properties {
     $root
     $toolsDir
@@ -8,14 +10,20 @@ properties {
     $buildConfig
     $buildType
     $version
+    $nugetExe
+    $paketExe
 }
 
-task default -Depends build
+task default -depends build
 
-task getCertificate {
+task sign {
     $global:certThumbprint = & (Join-Path $scriptsDir "get-certificate-thumbprint.ps1")
 }
 
-task build {
+task restorePackages {
+    & (Join-Path $scriptsDir "restore-packages.ps1") -paketExe $paketExe
+}
+
+task build -depends restorePackages {
     & (Join-Path $scriptsDir "build-solution.ps1") -buildConf $buildConfig -version $version -sourceDir $sourceDir -certThumbprint $global:certThumbprint
 }
