68458c5c25f34ef373f9358174c7300d8d99f823
Patryk Stepien <patryk.stepien@relativity.com>
2018-10-27 19:18:43 +0200

    Change signing task

--- default.ps1
+++ default.ps1
@@ -1,22 +1,21 @@
 #Requires -Version 5.0
 
 properties {
-    $root
-    $toolsDir
-    $scriptsDir
-    $sourceDir    
-    $buildConfig
-    $buildType
-    $version
+    # $root
+    # $toolsDir
+    # $scriptsDir
+    # $sourceDir    
+    # $buildConfig
+    # $buildType
+    # $version
 }
 
 task default -Depends build
 
-task buildAndSign {
-    $certThumbprint = & (Join-Path $scriptsDir "get-certificate-thumbprint.ps1")
-    & (Join-Path $scriptsDir "build-solution.ps1") -buildConf $buildConfig -version $version -sourceDir $sourceDir
+task getCertificate {
+    $global:certThumbprint = & (Join-Path $scriptsDir "get-certificate-thumbprint.ps1")
 }
 
 task build {
-    & (Join-Path $scriptsDir "build-solution.ps1") -buildConf $buildConfig -version $version -sourceDir $sourceDir
+    & (Join-Path $scriptsDir "build-solution.ps1") -buildConf $buildConfig -version $version -sourceDir $sourceDir -certThumbprint $global:certThumbprint
 }
