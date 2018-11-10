fb1a508ec7fbefab1b6685b83056d06ff4e3bd01
Patryk Stepien <patryk.stepien@relativity.com>
2018-11-10 10:52:25 +0100

    Treat code analysis warnings as errors

--- Source/Relativity.Sync.Tests.Performance/GlobalSuppressions.cs
+++ Source/Relativity.Sync.Tests.Performance/GlobalSuppressions.cs
@@ -1,1 +1,2 @@
-﻿
+﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1016:MarkAssembliesWithAssemblyVersion", Justification = "Version is being added during build")]
