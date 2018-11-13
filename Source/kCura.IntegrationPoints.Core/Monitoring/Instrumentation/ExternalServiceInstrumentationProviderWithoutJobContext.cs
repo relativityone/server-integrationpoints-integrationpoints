00ff5e8d004caaf02c22efe4dbbc52a5288a4a9b
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2018-11-13 14:55:36 +0100

    added instrumentation provider for calls without job context

---
+++ Source/kCura.IntegrationPoints.Core/Monitoring/Instrumentation/ExternalServiceInstrumentationProviderWithoutJobContext.cs
@@ -0,0 +1,27 @@
+﻿using kCura.IntegrationPoints.Common.Monitoring.Instrumentation;
+using kCura.IntegrationPoints.Core.Monitoring.Instrumentation.Model;
+using Relativity.API;
+
+namespace kCura.IntegrationPoints.Core.Monitoring.Instrumentation
+{
+	public class ExternalServiceInstrumentationProviderWithoutJobContext : IExternalServiceInstrumentationProvider
+	{
+		private IAPILog _logger;
+
+		public ExternalServiceInstrumentationProviderWithoutJobContext(IAPILog logger)
+		{
+			_logger = logger;
+		}
+
+		public IExternalServiceInstrumentation Create(string serviceType, string serviceName, string operationName)
+		{
+			var callContext = new InstrumentationServiceCallContext(serviceType, serviceName, operationName);
+			return new ExternalServiceLogsInstrumentation(callContext, _logger);
+		}
+
+		public IExternalServiceSimpleInstrumentation CreateSimple(string serviceType, string serviceName, string operationName)
+		{
+			return new ExternalServiceSimpleInstrumentation(Create(serviceType, serviceName, operationName));
+		}
+	}
+}
