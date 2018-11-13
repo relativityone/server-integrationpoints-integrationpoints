0772f5a580093b5e2495dca007c47d31ee27062a
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2018-11-13 06:09:18 -0600

    Merge pull request #982 in IN/integrationpoints from REL-269609-implement-simple-external-service-instrumentation to REL-267291-extend-rip-metrics-framework-to-log-external-services-calls
    Squashed commit of the following:
    
    commit caeea3868ea8ad69f72949ffb92bd7878c5cb949
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Thu Nov 8 13:59:15 2018 +0100
    
        REL-269609 implemented simple version of external service instrumentation

---
+++ Source/kCura.IntegrationPoints.Core.Tests/Monitoring/Instrumentation/ExternalServiceSimpleInstrumentationTests.cs
@@ -0,0 +1,127 @@
+﻿using kCura.IntegrationPoints.Common.Monitoring.Instrumentation;
+using kCura.IntegrationPoints.Core.Monitoring.Instrumentation;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+
+namespace kCura.IntegrationPoints.Core.Tests.Monitoring.Instrumentation
+{
+	[TestFixture]
+	public class ExternalServiceSimpleInstrumentationTests
+	{
+		private IExternalServiceInstrumentation _instrumentation;
+		private IExternalServiceInstrumentationStarted _startedInstrumentation;
+		private ExternalServiceSimpleInstrumentation _sut;
+
+		private const int _EXPECTED_RETURN_VALUE = 4324324;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_startedInstrumentation = Substitute.For<IExternalServiceInstrumentationStarted>();
+			_instrumentation = Substitute.For<IExternalServiceInstrumentation>();
+			_instrumentation.Started().Returns(_startedInstrumentation);
+			_sut = new ExternalServiceSimpleInstrumentation(_instrumentation);
+		}
+
+		[Test]
+		public void ItShouldCallStartedBeforeActionWasExecuted()
+		{
+			// act & assert
+			_sut.Execute(ValidateActionExecution);
+		}
+		
+		[Test]
+		public void ItShouldCallCompletedWhenActionExecutedWithoutException()
+		{
+			// act
+			_sut.Execute(() => { });
+
+			// assert
+			_startedInstrumentation.Received(1).Completed();
+		}
+
+		[Test]
+		public void ItShouldCallFailedAndRethrowExceptionWhenActionThrowsException()
+		{
+			// arrange
+			var expectedException = new ArgumentException();
+			
+			// act
+			try
+			{
+				_sut.Execute(() => { throw expectedException; });
+			}
+			catch (Exception ex)
+			{
+				// assert
+				Assert.AreEqual(expectedException, ex);
+			}
+
+			// assert
+			_startedInstrumentation.Received(1).Failed(expectedException);
+		}
+
+		[Test]
+		public void ItShouldCallStartedBeforeFunctionWasExecuted()
+		{
+			// act & assert
+			_sut.Execute(ValidateFunctionExecution);
+		}
+
+		[Test]
+		public void ItShouldCallCompletedWhenFunctionWasExecutedWithoutException()
+		{
+			// act
+			_sut.Execute(() => _EXPECTED_RETURN_VALUE);
+
+			// assert
+			_startedInstrumentation.Received(1).Completed();
+		}
+
+		[Test]
+		public void ItShouldReturnValueWhenFunctionWasExecutedWithoutException()
+		{
+			// act
+			int result = _sut.Execute(() => _EXPECTED_RETURN_VALUE);
+
+			// assert
+			Assert.AreEqual(_EXPECTED_RETURN_VALUE, result);
+		}
+
+		[Test]
+		public void ItShouldCallFailedAndRethrowExceptionWhenFunctionThrowsException()
+		{
+			// arrange
+			var expectedException = new ArgumentException();
+			Func<string> functionToExecute = () => { throw expectedException; };
+			// act
+			try
+			{
+				_sut.Execute(functionToExecute);
+			}
+			catch (Exception ex)
+			{
+				// assert
+				Assert.AreEqual(expectedException, ex);
+			}
+
+			// assert
+			_startedInstrumentation.Received(1).Failed(expectedException);
+		}
+
+		private int ValidateFunctionExecution()
+		{
+			ValidateActionExecution();
+			return _EXPECTED_RETURN_VALUE;
+		}
+
+		private void ValidateActionExecution()
+		{
+			_instrumentation.Received(1).Started();
+			_startedInstrumentation.DidNotReceiveWithAnyArgs().Completed();
+			_startedInstrumentation.DidNotReceiveWithAnyArgs().Failed(Arg.Any<string>());
+			_startedInstrumentation.DidNotReceiveWithAnyArgs().Failed(Arg.Any<Exception>());
+		}
+	}
+}
---
+++ Source/kCura.IntegrationPoints.Core/Monitoring/Instrumentation/ExternalServiceSimpleInstrumentation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using kCura.IntegrationPoints.Common.Monitoring.Instrumentation;
+
+namespace kCura.IntegrationPoints.Core.Monitoring.Instrumentation
+{
+	internal class ExternalServiceSimpleInstrumentation : IExternalServiceSimpleInstrumentation 
+	{
+		private readonly IExternalServiceInstrumentation _instrumentation;
+
+		public ExternalServiceSimpleInstrumentation(IExternalServiceInstrumentation instrumentation)
+		{
+			_instrumentation = instrumentation;
+		}
+
+		public T Execute<T>(Func<T> functionToExecute)
+		{
+			IExternalServiceInstrumentationStarted startedInstrumentation = _instrumentation.Started();
+			try
+			{
+				T result = functionToExecute();
+				startedInstrumentation.Completed();
+				return result;
+			}
+			catch (Exception ex)
+			{
+				startedInstrumentation.Failed(ex);
+				throw;
+			}
+		}
+
+		public void Execute(Action actionToExecute)
+		{
+			IExternalServiceInstrumentationStarted startedInstrumentation = _instrumentation.Started();
+			try
+			{
+				actionToExecute();
+				startedInstrumentation.Completed();
+			}
+			catch (Exception ex)
+			{
+				startedInstrumentation.Failed(ex);
+				throw;
+			}
+		}
+	}
+}
