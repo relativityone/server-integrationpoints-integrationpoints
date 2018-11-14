6b10588e4f5ec9fae3c34e2549b9502c84d64f72
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2018-11-14 08:17:34 -0600

    Merge pull request #992 in IN/integrationpoints from REL-269807-develop-logging to develop
    Squashed commit of the following:
    
    commit 314973c90980e372a32bbd5a4c2dfb2b01ce350e
    Author: Piotr Szmigielski <piotr.szmigielski@relativity.com>
    Date:   Wed Nov 14 14:17:57 2018 +0100
    
        Fixed failing build
    
    commit 04386371b7de094dd34342e43f62d0039540feff
    Author: Piotr Szmigielski <piotr.szmigielski@relativity.com>
    Date:   Wed Nov 14 14:12:17 2018 +0100
    
        Added checking for null reference exception and throwing exception
    
    commit 20bf484bf1d69dccbaf7e582b4a400704a3a4cc5
    Author: Piotr Szmigielski <piotr.szmigielski@relativity.com>
    Date:   Fri Nov 9 16:23:13 2018 +0100
    
        Implemented logging error while updating job history status on job completion

--- Source/kCura.IntegrationPoints.Core/JobHistoryBatchUpdateStatus.cs
+++ Source/kCura.IntegrationPoints.Core/JobHistoryBatchUpdateStatus.cs
@@ -5,23 +5,29 @@ using kCura.IntegrationPoints.Core.Services.JobHistory;
 using kCura.IntegrationPoints.Data;
 using kCura.ScheduleQueue.Core;
 using kCura.ScheduleQueue.Core.Core;
+using Relativity.API;
 
 namespace kCura.IntegrationPoints.Core
 {
 	public class JobHistoryBatchUpdateStatus : IBatchStatus
 	{
+		private const string _JOB_HISTORY_NULL = "Failed to retrieve job history. job ID: {0}.";
+		private const string _JOB_UPDATE_ERROR_MESSAGE_TEMPLATE = "Failed to update finished job error status. Current status: {0}, target status: {1}, job ID: {2}, job history artifact ID: {3}.";
+
 		private readonly IJobStatusUpdater _updater;
 		private readonly IJobHistoryService _jobHistoryService;
 		private readonly IJobService _jobService;
 		private readonly ISerializer _serializer;
+		private readonly IAPILog _logger;
 
 	    public JobHistoryBatchUpdateStatus(IJobStatusUpdater jobStatusUpdater, IJobHistoryService jobHistoryService,
-	        IJobService jobService, ISerializer serializer)
+	        IJobService jobService, ISerializer serializer, IAPILog logger)
 	    {
 	        _updater = jobStatusUpdater;
 	        _jobHistoryService = jobHistoryService;
 	        _jobService = jobService;
 	        _serializer = serializer;
+		    _logger = logger;
 	    }
 
 	    public void OnJobStart(Job job)
@@ -38,9 +44,28 @@ namespace kCura.IntegrationPoints.Core
 		public void OnJobComplete(Job job)
 		{
 			JobHistory result = GetHistory(job);
+			if (result == null)
+			{
+				long jobId = job?.JobId ?? -1;
+				string message = string.Format(_JOB_HISTORY_NULL, jobId);
+				NullReferenceException exception = new NullReferenceException(message);
+				_logger.LogError(exception, _JOB_HISTORY_NULL, jobId);
+				throw exception;
+			}
+			int artifactId = result.ArtifactId;
+			string oldStatusName = result.JobStatus.Name;
 			result.JobStatus = _updater.GenerateStatus(result, job.WorkspaceID);
+			string newStatusName = result.JobStatus.Name;
 			result.EndTimeUTC = DateTime.UtcNow;
-			_jobHistoryService.UpdateRdo(result);
+			try
+			{
+				_jobHistoryService.UpdateRdo(result);
+			}
+			catch (Exception exception)
+			{
+				_logger.LogError(exception, _JOB_UPDATE_ERROR_MESSAGE_TEMPLATE, oldStatusName, newStatusName, job.JobId, artifactId);
+				throw;
+			}
 		}
 
 		private JobHistory GetHistory(Job job)
