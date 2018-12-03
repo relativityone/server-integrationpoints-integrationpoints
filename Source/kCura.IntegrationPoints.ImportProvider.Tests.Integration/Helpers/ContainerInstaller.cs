3af3a0c3e2566368a841c959f46f05de6c31c12f
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2018-12-03 03:44:11 -0600

    Merge pull request #1021 in IN/integrationpoints from REL-273764-hotfix-june-update-2 to release-9.6-june-update-2-hot-fix
    Squashed commit of the following:
    
    commit 159b22d3b2caa68a5efa9ef94c57c351056a49bb
    Author: Piotr Szmigielski <piotr.szmigielski@relativity.com>
    Date:   Wed Nov 28 18:53:35 2018 +0100
    
        Backported WebAPI session fix in RIP

--- Source/kCura.IntegrationPoints.ImportProvider.Tests.Integration/Helpers/ContainerInstaller.cs
+++ Source/kCura.IntegrationPoints.ImportProvider.Tests.Integration/Helpers/ContainerInstaller.cs
@@ -12,6 +12,7 @@ using kCura.IntegrationPoints.Core.Authentication;
 using kCura.IntegrationPoints.Core.Services.JobHistory;
 using kCura.IntegrationPoints.Data.Logging;
 using kCura.IntegrationPoints.Domain;
+using kCura.IntegrationPoints.Domain.Authentication;
 using kCura.IntegrationPoints.Domain.Managers;
 using kCura.IntegrationPoints.ImportProvider.Parser;
 using kCura.IntegrationPoints.ImportProvider.Parser.Interfaces;
@@ -120,6 +121,7 @@ namespace kCura.IntegrationPoints.ImportProvider.Tests.Integration.Helpers
 
 		private static void RegisterDomainClasses(WindsorContainer windsorContainer)
 		{
+            windsorContainer.Register(Component.For<IAuthTokenGenerator>().Instance(Substitute.For<IAuthTokenGenerator>()));
 			windsorContainer.Register(Component.For<ITokenProvider>().Instance(Substitute.For<ITokenProvider>()));
 			windsorContainer.Register(Component.For<IFederatedInstanceManager>().Instance(Substitute.For<IFederatedInstanceManager>()));
 		}
