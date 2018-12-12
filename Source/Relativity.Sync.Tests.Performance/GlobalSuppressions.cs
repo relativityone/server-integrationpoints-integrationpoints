c13c9bd95f728f92286c017e6999709b1f77cb18
Patryk Stepien <patryk.stepien@relativity.com>
2018-12-12 11:32:54 +0100

    Clean up

--- Source/Relativity.Sync.Tests.Performance/GlobalSuppressions.cs
+++ Source/Relativity.Sync.Tests.Performance/GlobalSuppressions.cs
@@ -1,2 +1,1 @@
-﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1016:MarkAssembliesWithAssemblyVersion", Justification = "Version is being added during build")]
+﻿
