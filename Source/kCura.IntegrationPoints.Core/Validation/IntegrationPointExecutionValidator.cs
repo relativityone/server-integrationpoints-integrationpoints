db2c700694eca83251b675b6672feb0b15396a32
Damian Janas <damian.janas@relativity.com>
2018-12-19 10:10:46 -0600

    Merge pull request #1062 in IN/integrationpoints from REL-280778-cherry-pick-changes-from-rel-250732 to release-9.6-june-update-1-hot-fix
    Squashed commit of the following:
    
    commit fcfb8616330adc88a7a4958ca845ace5f9db08b2
    Author: Damian Janas <damian.janas@relativity.com>
    Date:   Wed Dec 19 05:39:43 2018 -0600
    
        Merge pull request #1059 in IN/integrationpoints from REL-279101-fix-myfirstprovider-web.config-rip-sdk-9.6 to release-9.6-june-update-2-hot-fix
    
        Squashed commit of the following:
    
        commit 9b6f11bd73fec93db98ef3c2d87f253211f8314d
        Author: Damian Janas <damian.janas@relativity.com>
        Date:   Wed Dec 19 12:27:12 2018 +0100
    
            web configs updated
    
    commit 23e55186111325216323b58a44c1046ad1080343
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Wed Aug 29 11:01:03 2018 +0200
    
        REL-250721 - use providerWrapper in ProviderFactoryBase
    
    commit 5bd8e0dee887dbd09577a960628f1fca38ad8017
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Wed Aug 29 11:00:12 2018 +0200
    
        REL-250721 - add missing dlls to jsonloader
    
    commit 4ff544ebfcac3916e44597ff92581beebb431fa4
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Mon Aug 27 07:15:54 2018 -0500
    
        Merge pull request #881 in IN/integrationpoints from REL-250400-fix-sdk-and-myfirstprovider to release-9.6-june
    
        Squashed commit of the following:
    
        commit 388d955232b03e05ba747cc36032d8fca9363e40
        Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
        Date:   Fri Aug 24 12:39:48 2018 +0200
    
            fixed issue with failing unit test
    
        commit a78e138148dfff60a1581cc0a8c8866630334db9
        Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
        Date:   Fri Aug 24 12:28:04 2018 +0200
    
            REL-250400 fixed MyFirstProvider app
    
        commit 91cf234fe63ee313d782458c88c0b5bfac5bf3cb
        Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
        Date:   Fri Aug 24 12:27:47 2018 +0200
    
            REL-250400 fixed RIP custom providers handling
    
    commit 5b71409d510206cee24d393dab517b345acc8259
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Wed Aug 22 05:23:04 2018 -0500
    
        Merge pull request #877 in IN/integrationpoints from REL-249093-fix-rip-sdk to release-9.6-june
    
        Squashed commit of the following:
    
        commit 0bbe5a82099a6933d4e859c03b4c9649a21ad671
        Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
        Date:   Wed Aug 22 10:37:29 2018 +0200
    
            mark DataSourceProvicerConfiguration as serializable

--- Source/kCura.IntegrationPoints.Core/Validation/IntegrationPointExecutionValidator.cs
+++ Source/kCura.IntegrationPoints.Core/Validation/IntegrationPointExecutionValidator.cs
@@ -4,49 +4,53 @@ using kCura.IntegrationPoints.Core.Contracts.Configuration;
 using kCura.IntegrationPoints.Core.Models;
 using kCura.IntegrationPoints.Core.Validation.Parts;
 using kCura.IntegrationPoints.Core.Validation.RelativityProviderValidator;
+using kCura.IntegrationPoints.Domain.Exceptions;
 using kCura.IntegrationPoints.Domain.Models;
 using Newtonsoft.Json;
 
 namespace kCura.IntegrationPoints.Core.Validation
 {
-    public class IntegrationPointExecutionValidator : IIntegrationPointExecutionValidator
-    {
-        private readonly ISerializer _serializer;
-        private readonly IRelativityProviderValidatorsFactory _validatorsFactory;
+	public class IntegrationPointExecutionValidator : IIntegrationPointExecutionValidator
+	{
+		private readonly ISerializer _serializer;
+		private readonly IRelativityProviderValidatorsFactory _validatorsFactory;
 
-        public IntegrationPointExecutionValidator(ISerializer serializer, IRelativityProviderValidatorsFactory validatorsFactory)
-        {
-            _serializer = serializer;
-            _validatorsFactory = validatorsFactory;
-        }
+		public IntegrationPointExecutionValidator(ISerializer serializer, IRelativityProviderValidatorsFactory validatorsFactory)
+		{
+			_serializer = serializer;
+			_validatorsFactory = validatorsFactory;
+		}
 
-        public ValidationResult Validate(IntegrationPointModel integrationModel)
-        {
-            var result = new ValidationResult();
-            SourceConfiguration sourceConfiguration;
+		public ValidationResult Validate(IntegrationPointModel integrationModel)
+		{
+			var result = new ValidationResult();
+			SourceConfiguration sourceConfiguration;
 
-            try
-            {
-                sourceConfiguration =
-                    _serializer.Deserialize<SourceConfiguration>(integrationModel.SourceConfiguration);
+			try
+			{
+				sourceConfiguration =
+					_serializer.Deserialize<SourceConfiguration>(integrationModel.SourceConfiguration);
+			}
+			catch (IntegrationPointsException ex) when (ex.InnerException is JsonReaderException) //handle case of encrypted sourceConfiguration
+			{
+				return result;
+			}
+			catch (JsonReaderException) //handle case of encrypted sourceConfiguration
+			{
+				return result;
+			}
 
-            }
-            catch (JsonReaderException) //handle case of encrypted sourceConfiguration
-            {
-                return result;
-            }
+			if (sourceConfiguration.SavedSearchArtifactId != 0)
+			{
+				SavedSearchValidator savedSearchValidator =
+					_validatorsFactory.CreateSavedSearchValidator(sourceConfiguration.SourceWorkspaceArtifactId,
+						sourceConfiguration.SavedSearchArtifactId);
+				result.Add(savedSearchValidator.Validate(sourceConfiguration.SavedSearchArtifactId));
+			}
 
-            if (sourceConfiguration.SavedSearchArtifactId != 0)
-            {
-                SavedSearchValidator savedSearchValidator =
-                    _validatorsFactory.CreateSavedSearchValidator(sourceConfiguration.SourceWorkspaceArtifactId,
-                        sourceConfiguration.SavedSearchArtifactId);
-                result.Add(savedSearchValidator.Validate(sourceConfiguration.SavedSearchArtifactId));
-            }
+			//ToDo: Add other validators e.g. for production
 
-            //ToDo: Add other validators e.g. for production
-
-            return result;
-        }
-    }
+			return result;
+		}
+	}
 }
