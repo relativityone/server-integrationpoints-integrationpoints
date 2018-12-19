a540094ea3ae9f62bd3b56c2316f953ec93306ea
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2018-12-19 14:19:57 +0100

    Fix double health check
    # Conflicts:
    #	Source/kCura.IntegrationPoints.Core.Tests/JobHistoryBatchUpdateStatusTests.cs
    #	Source/kCura.IntegrationPoints.Web.Tests/kCura.IntegrationPoints.Web.Tests.csproj
    #	Source/kCura.IntegrationPoints.sln

--- Source/kCura.IntegrationPoints.Core/Services/JobStatusUpdater.cs
+++ Source/kCura.IntegrationPoints.Core/Services/JobStatusUpdater.cs
@@ -4,14 +4,16 @@ using kCura.IntegrationPoints.Core.Services.JobHistory;
 using kCura.IntegrationPoints.Data;
 using kCura.IntegrationPoints.Data.Extensions;
 using kCura.IntegrationPoints.Data.Queries;
+using kCura.Relativity.Client.DTOs;
 using Relativity.Telemetry.APM;
+using Client = Relativity.Telemetry.APM.Client;
 
 namespace kCura.IntegrationPoints.Core.Services
 {
 	public class JobStatusUpdater : IJobStatusUpdater
 	{
-		private readonly JobHistoryErrorQuery _service;
 		private readonly IJobHistoryService _jobHistoryService;
+		private readonly JobHistoryErrorQuery _service;
 
 		public JobStatusUpdater(JobHistoryErrorQuery service, IJobHistoryService jobHistoryService)
 		{
@@ -19,18 +21,19 @@ namespace kCura.IntegrationPoints.Core.Services
 			_jobHistoryService = jobHistoryService;
 		}
 
-		public Relativity.Client.DTOs.Choice GenerateStatus(Guid batchId, long wkspId)
+		public Choice GenerateStatus(Guid batchId)
 		{
 			Data.JobHistory result = _jobHistoryService.GetRdo(batchId);
-			return GenerateStatus(result, wkspId);
+			return GenerateStatus(result);
 		}
 
-		public Relativity.Client.DTOs.Choice GenerateStatus(Data.JobHistory jobHistory, long wkspId)
+		public Choice GenerateStatus(Data.JobHistory jobHistory)
 		{
 			if (jobHistory == null)
 			{
 				throw new ArgumentNullException(nameof(jobHistory));
 			}
+
 			if (jobHistory.JobStatus.EqualsToChoice(JobStatusChoices.JobHistoryStopping))
 			{
 				return JobStatusChoices.JobHistoryStopped;
@@ -39,28 +42,41 @@ namespace kCura.IntegrationPoints.Core.Services
 			JobHistoryError recent = _service.GetJobErrorFailedStatus(jobHistory.ArtifactId);
 			if (recent != null)
 			{
-				if (recent.ErrorType.EqualsToChoice(Data.ErrorTypeChoices.JobHistoryErrorItem))
+				if (recent.ErrorType.EqualsToChoice(ErrorTypeChoices.JobHistoryErrorItem))
 				{
-					return Data.JobStatusChoices.JobHistoryCompletedWithErrors;
+					return JobStatusChoices.JobHistoryCompletedWithErrors;
 				}
-				if (recent.ErrorType.EqualsToChoice(Data.ErrorTypeChoices.JobHistoryErrorJob))
-				{
-					IHealthMeasure healthcheck = Client.APMClient.HealthCheckOperation(Constants.IntegrationPoints.Telemetry.APM_HEALTHCHECK, 
-						() => HealthCheck.CreateJobFailedMetric(jobHistory, wkspId));
-					healthcheck.Write();
 
+				if (recent.ErrorType.EqualsToChoice(ErrorTypeChoices.JobHistoryErrorJob))
+				{
 					return jobHistory.JobStatus.EqualsToChoice(JobStatusChoices.JobHistoryValidationFailed)
-						? JobStatusChoices.JobHistoryValidationFailed : JobStatusChoices.JobHistoryErrorJobFailed;
+						? JobStatusChoices.JobHistoryValidationFailed
+						: JobStatusChoices.JobHistoryErrorJobFailed;
 				}
 			}
 			else
 			{
 				if (jobHistory.ItemsWithErrors.GetValueOrDefault(0) > 0)
 				{
-					return Data.JobStatusChoices.JobHistoryCompletedWithErrors;
+					return JobStatusChoices.JobHistoryCompletedWithErrors;
 				}
 			}
-			return Data.JobStatusChoices.JobHistoryCompleted;
+
+			return JobStatusChoices.JobHistoryCompleted;
+		}
+
+		public void SendHealthCheck(Data.JobHistory jobHistory, long workspaceID)
+		{
+			if (IsJobFailed(jobHistory.JobStatus))
+			{
+				IHealthMeasure healthcheck = Client.APMClient.HealthCheckOperation(Constants.IntegrationPoints.Telemetry.APM_HEALTHCHECK, () => HealthCheck.CreateJobFailedMetric(jobHistory, workspaceID));
+				healthcheck.Write();
+			}
+		}
+
+		private static bool IsJobFailed(Choice jobStatusChoice)
+		{
+			return jobStatusChoice.EqualsToChoice(JobStatusChoices.JobHistoryValidationFailed) || jobStatusChoice.EqualsToChoice(JobStatusChoices.JobHistoryErrorJobFailed);
 		}
 	}
 }
