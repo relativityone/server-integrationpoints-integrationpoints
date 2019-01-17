e2a32f4c733ed57776b74efaaffe6af3c82407d5
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2019-01-17 15:41:01 +0100

    REL-285938 terrible hack for missing Polly dll during custom provider installation

---
+++ Source/kCura.IntegrationPoints.Data/IRetryHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace kCura.IntegrationPoints.Data
+{
+	internal interface IRetryHandler
+	{
+		T ExecuteWithRetries<T>(Func<Task<T>> function, [CallerMemberName] string callerName = "");
+
+		Task<T> ExecuteWithRetriesAsync<T>(Func<Task<T>> function, [CallerMemberName] string callerName = "");
+	}
+}
