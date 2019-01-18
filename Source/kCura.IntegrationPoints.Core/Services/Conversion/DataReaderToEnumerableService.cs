e9300f1367c65915d4dff6f0f23132fd1d3b352c
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2019-01-18 03:45:30 -0600

    Merge pull request #1116 in IN/integrationpoints from REL-285962-rip-larkspur1 to release-10.0-larkspur1
    Squashed commit of the following:
    
    commit 7fa2c8d2502b741999195455da0d08d72da1f82c
    Author: Piotr Szmigielski <piotr.szmigielski@relativity.com>
    Date:   Thu Jan 17 15:13:45 2019 +0100
    
        Fixed error with dispose being called two times after import job ends

--- Source/kCura.IntegrationPoints.Core/Services/Conversion/DataReaderToEnumerableService.cs
+++ Source/kCura.IntegrationPoints.Core/Services/Conversion/DataReaderToEnumerableService.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using kCura.IntegrationPoints.Core.Services.Conversion;
-using kCura.IntegrationPoints.Synchronizers.RDO.ImportAPI;
 
-namespace kCura.IntegrationPoints.Core.Conversion
+namespace kCura.IntegrationPoints.Core.Services.Conversion
 {
 	public class DataReaderToEnumerableService
 	{
@@ -13,19 +11,13 @@ namespace kCura.IntegrationPoints.Core.Conversion
 		{
 			_objectBuilder = objectBuilder;
 		}
+
 		public IEnumerable<T> GetData<T>(IDataReader reader)
 		{
-			try
+			List<string> columns = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList();
+			while (reader.Read())
 			{
-				List<string> columns = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList();
-				while (reader.Read())
-				{
-					yield return _objectBuilder.BuildObject<T>(reader, columns);
-				}
-			}
-			finally
-			{
-				reader.Dispose();
+				yield return _objectBuilder.BuildObject<T>(reader, columns);
 			}
 		}
 	}
