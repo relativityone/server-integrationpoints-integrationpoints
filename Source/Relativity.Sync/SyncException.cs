770be508fd1f943277d6f8d3a7599f79d3b701e8
Patryk Stepien <patryk.stepien@relativity.com>
2019-01-21 17:07:21 +0100

    REL-282939 [SYNC] Add GetObjectData to SyncException

--- Source/Relativity.Sync/SyncException.cs
+++ Source/Relativity.Sync/SyncException.cs
@@ -1,42 +1,69 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.Serialization;
+using System.Security.Permissions;
 
 namespace Relativity.Sync
 {
 	/// <summary>
 	///     Represents Sync exception
 	/// </summary>
-	[ExcludeFromCodeCoverage]
 	[Serializable]
 	public sealed class SyncException : Exception
 	{
 		/// <summary>
-		///     Constructor
+		///     Sync job correlation ID
 		/// </summary>
+		public string CorrelationId { get; }
+
+		/// <inheritdoc />
 		public SyncException()
 		{
 		}
 
-		/// <summary>
-		///     Constructor
-		/// </summary>
+		/// <inheritdoc />
 		private SyncException(SerializationInfo info, StreamingContext context) : base(info, context)
 		{
+			CorrelationId = info.GetString(nameof(CorrelationId));
 		}
 
-		/// <summary>
-		///     Constructor
-		/// </summary>
+		/// <inheritdoc />
 		public SyncException(string message) : base(message)
 		{
 		}
 
-		/// <summary>
-		///     Constructor
-		/// </summary>
+		/// <inheritdoc />
 		public SyncException(string message, Exception innerException) : base(message, innerException)
 		{
+		}
+
+		/// <summary>
+		///     Constructor with correlation ID
+		/// </summary>
+		public SyncException(string message, string correlationId) : base(message)
+		{
+			CorrelationId = correlationId;
+		}
+
+		/// <summary>
+		///     Constructor with correlation ID
+		/// </summary>
+		public SyncException(string message, Exception innerException, string correlationId) : base(message, innerException)
+		{
+			CorrelationId = correlationId;
+		}
+
+		/// <summary>
+		/// </summary>
+		[SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+		public override void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			if (info == null)
+			{
+				throw new ArgumentNullException(nameof(info));
+			}
+
+			info.AddValue(nameof(CorrelationId), CorrelationId);
+			base.GetObjectData(info, context);
 		}
 	}
 }
