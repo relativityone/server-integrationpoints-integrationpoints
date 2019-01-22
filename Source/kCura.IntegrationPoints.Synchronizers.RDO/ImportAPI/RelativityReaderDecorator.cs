9aa1cd43c601464ef24ad63c7cf52c22b7b2c80c
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-01-22 16:55:00 +0100

    Fix issues after backport

--- Source/kCura.IntegrationPoints.Synchronizers.RDO/ImportAPI/RelativityReaderDecorator.cs
+++ Source/kCura.IntegrationPoints.Synchronizers.RDO/ImportAPI/RelativityReaderDecorator.cs
@@ -61,6 +61,7 @@ namespace kCura.IntegrationPoints.Synchronizers.RDO.ImportAPI
 			RegisterSpecialField(columns, Constants.SPECIAL_NATIVE_FILE_LOCATION_FIELD_NAME, Constants.SPECIAL_NATIVE_FILE_LOCATION_FIELD);
 			RegisterSpecialField(columns, Constants.SPECIAL_FILE_TYPE_FIELD_NAME, Constants.SPECIAL_FILE_TYPE_FIELD);
 			RegisterSpecialField(columns, Constants.SPECIAL_FILE_SUPPORTED_BY_VIEWER_FIELD_NAME, Constants.SPECIAL_FILE_SUPPORTED_BY_VIEWER_FIELD);
+			RegisterSpecialField(columns, Constants.SPECIAL_NATIVE_FILE_SIZE_FIELD_NAME, Constants.SPECIAL_NATIVE_FILE_SIZE_FIELD);
 			RegisterSpecialField(columns, Constants.SPECIAL_FOLDERPATH_FIELD_NAME, Constants.SPECIAL_FOLDERPATH_FIELD);
 			RegisterSpecialField(columns, Constants.SPECIAL_FOLDERPATH_DYNAMIC_FIELD_NAME, Constants.SPECIAL_FOLDERPATH_DYNAMIC_FIELD);
 
