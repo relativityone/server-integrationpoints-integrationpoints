cf3a1afece934954747c576c38f225dcbe1ab90f
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-01-22 08:19:24 +0100

    Add InternalsVisibleTo

--- Source/Relativity.Sync/AssemblyInfo.cs
+++ Source/Relativity.Sync/AssemblyInfo.cs
@@ -1,2 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("kCura.IntegrationPoints.RelativitySync")]
+[assembly: InternalsVisibleTo("kCura.IntegrationPoints.RelativitySync.Tests")]
