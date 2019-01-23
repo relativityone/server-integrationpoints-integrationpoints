3c6ef6a2de493db2072a72e1adf6ddef144ea951
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2019-01-23 15:18:29 +0100

    removed usused code

--- Source/kCura.IntegrationPoints.UITests/Pages/IntegrationPointDetailsPage.cs
+++ Source/kCura.IntegrationPoints.UITests/Pages/IntegrationPointDetailsPage.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using kCura.IntegrationPoint.Tests.Core;
-using kCura.IntegrationPoint.Tests.Core.Models;
+﻿using kCura.IntegrationPoint.Tests.Core.Models;
 using kCura.IntegrationPoints.UITests.Components;
 using kCura.IntegrationPoints.UITests.Driver;
 using OpenQA.Selenium;
@@ -11,43 +9,19 @@ namespace kCura.IntegrationPoints.UITests.Pages
 {
 	public class IntegrationPointDetailsPage : GeneralPage
 	{
-		[FindsBy(How = How.LinkText, Using = "Edit")]
-		protected IWebElement EditButton;
-
-		[FindsBy(How = How.LinkText, Using = "Delete")]
-		protected IWebElement DeleteButton;
-
-		[FindsBy(How = How.LinkText, Using = "Back")]
-		protected IWebElement BackButton;
-
-		[FindsBy(How = How.LinkText, Using = "Edit Permissions")]
-		protected IWebElement EditPermissionsButton;
-
-		[FindsBy(How = How.LinkText, Using = "View Audit")]
-		protected IWebElement ViewAuditButton;
-
 		[FindsBy(How = How.LinkText, Using = "Run")]
 		protected IWebElement RunButton;
-
-		[FindsBy(How = How.LinkText, Using = "Save as a Profile")]
-		protected IWebElement SaveProfileButton;
-
-		[FindsBy(How = How.LinkText, Using = "General")]
-		protected IWebElement GeneralTabLink;
 
 		public IntegrationPointDetailsPage(RemoteWebDriver driver) : base(driver)
 		{
 			WaitForPage();
 			PageFactory.InitElements(driver, this);
-			//Driver.SwitchTo().Frame("externalPage");
 		}
 
 		public IntegrationPointDetailsPage RunIntegrationPoint()
 		{
 			RunButton.ClickEx();
-
 			ClickOkOnConfirmationDialog();
-
 			return this;
 		}
 
@@ -55,13 +29,6 @@ namespace kCura.IntegrationPoints.UITests.Pages
 		{
 			WaitForPage();
 			var t = new PropertiesTable(Driver.FindElementById("summaryPage"), "General");
-			t.Select();
-			return t;
-		}
-
-		public PropertiesTable SelectSchedulingPropertiesTable()
-		{
-			var t = new PropertiesTable(Driver.FindElementById("schedulerSummaryPage"), "Scheduling");
 			t.Select();
 			return t;
 		}
@@ -74,17 +41,8 @@ namespace kCura.IntegrationPoints.UITests.Pages
 
 		private static JobHistoryModel BuildLatestJobHistory(JobStatusTable jobStatusTable)
 		{
-			//TODO implement remaining methods and assing JobHistoryModel remaining properties
 			var jobHistoryModel = new JobHistoryModel
 			{
-				//JobId = int.Parse(Parent.FindElement(By.ClassName("itemTable")).FindElements(By.XPath("tbody/tr/td"))[1].Text),
-				//StartTime = DateTime.Parse(Parent.FindElement(By.ClassName("itemTable")).FindElements(By.XPath("tbody/tr/td"))[2].Text, CultureInfo.GetCultureInfo(1033)),
-				//ArtifactId = int.Parse(Parent.FindElement(By.ClassName("itemTable")).FindElements(By.XPath("tbody/tr/td"))[3].Text),
-				//Name = Parent.FindElement(By.ClassName("itemTable")).FindElements(By.XPath("tbody/tr/td"))[4].Text,
-				//IntegrationPoint = Parent.FindElement(By.ClassName("itemTable")).FindElements(By.XPath("tbody/tr/td"))[5].Text,
-				//JobType = Parent.FindElement(By.ClassName("itemTable")).FindElements(By.XPath("tbody/tr/td"))[6].Text,
-				//DestinationWorkspace = Parent.FindElement(By.ClassName("itemTable")).FindElements(By.XPath("tbody/tr/td"))[8].Text,
-				//DestinationInstance = Parent.FindElement(By.ClassName("itemTable")).FindElements(By.XPath("tbody/tr/td"))[9].Text,
 				JobStatus = jobStatusTable.GetLatestJobStatus(),
 				ItemsWithErrors = jobStatusTable.GetItemsWithErrors(),
 				ItemsTransferred = jobStatusTable.GetItemsTransfered(),
