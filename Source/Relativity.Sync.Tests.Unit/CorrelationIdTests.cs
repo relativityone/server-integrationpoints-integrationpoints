2d9520d4d20ea4a91f5b067281576a3fca76c0e8
Patryk Stepien <patryk.stepien@relativity.com>
2019-01-23 13:03:20 +0100

    REL-282030 [SYNC] Add error handling on SyncJob level

---
+++ Source/Relativity.Sync.Tests.Unit/CorrelationIdTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Relativity.Sync.Tests.Unit
+{
+	[TestFixture]
+	public sealed class CorrelationIdTests
+	{
+		[Test]
+		public void ItShouldSetCorrelationIdValue()
+		{
+			const string value = "cid";
+
+			CorrelationId correlationId = new CorrelationId(value);
+
+			// ASSERT
+			correlationId.Value.Should().Be(value);
+		}
+
+		[Test]
+		public void ItShouldOverrideToStringWithCorrelationIdValue()
+		{
+			const string value = "cid";
+
+			CorrelationId correlationId = new CorrelationId(value);
+
+			// ASSERT
+			correlationId.ToString().Should().Be(value);
+		}
+	}
+}
--- Source/Relativity.Sync/SyncJob.cs
+++ Source/Relativity.Sync/SyncJob.cs
@@ -9,11 +9,15 @@ namespace Relativity.Sync
 	{
 		private readonly INode<SyncExecutionContext> _pipeline;
 		private readonly ISyncExecutionContextFactory _executionContextFactory;
+		private readonly CorrelationId _correlationId;
+		private readonly ISyncLog _logger;
 
-		public SyncJob(INode<SyncExecutionContext> pipeline, ISyncExecutionContextFactory executionContextFactory)
+		public SyncJob(INode<SyncExecutionContext> pipeline, ISyncExecutionContextFactory executionContextFactory, CorrelationId correlationId, ISyncLog logger)
 		{
 			_pipeline = pipeline;
 			_executionContextFactory = executionContextFactory;
+			_correlationId = correlationId;
+			_logger = logger;
 		}
 
 		public async Task ExecuteAsync(CancellationToken token)
@@ -23,11 +27,30 @@ namespace Relativity.Sync
 
 		public async Task ExecuteAsync(IProgress<SyncProgress> progress, CancellationToken token)
 		{
-			IExecutionContext<SyncExecutionContext> executionContext = _executionContextFactory.Create(progress, token);
-			NodeResult executionResult = await _pipeline.ExecuteAsync(executionContext).ConfigureAwait(false);
+			NodeResult executionResult;
+			try
+			{
+				IExecutionContext<SyncExecutionContext> executionContext = _executionContextFactory.Create(progress, token);
+				executionResult = await _pipeline.ExecuteAsync(executionContext).ConfigureAwait(false);
+			}
+			catch (OperationCanceledException e)
+			{
+				_logger.LogWarning(e, "Operation cancelled.");
+				throw;
+			}
+			catch (SyncException)
+			{
+				throw;
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e, "Error occured during Sync job execution.");
+				throw new SyncException("Error occured during Sync job execution. See inner exception for more details.", e, _correlationId.Value);
+			}
+
 			if (executionResult.Status != NodeResultStatus.Succeeded)
 			{
-				throw new SyncException("Sync job failed", executionResult.Exception);
+				throw new SyncException("Sync job failed. See inner exceptions for more details.", new AggregateException(executionResult.GetFailExceptions()), _correlationId.Value);
 			}
 		}
 
