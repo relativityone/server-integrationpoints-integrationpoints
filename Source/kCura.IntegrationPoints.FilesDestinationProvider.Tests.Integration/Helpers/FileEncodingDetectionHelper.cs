8f3f51fd5b0f46ac1352f4e4118d1c00f9eb35b0
Michal Grygierzec <michal.grygierzec@relativity.com>
2019-01-23 10:22:26 +0100

    Fix encoding verification helper for integration tests

--- Source/kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration/Helpers/FileEncodingDetectionHelper.cs
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration/Helpers/FileEncodingDetectionHelper.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration.Helpers
 {
@@ -16,21 +12,43 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration.Hel
 		/// <returns></returns>
 		public static Encoding GetFileEncoding(string srcFile)
 		{
-			byte[] buffer = new byte[4];
-			using (FileStream file = new FileStream(srcFile, FileMode.Open))
-			{
-				file.Read(buffer, 0, 4);
-			}
-			if (buffer[0] == 0xEF && buffer[1] == 0xBB && buffer[2] == 0xBF)
-				return Encoding.UTF8;
-			if (buffer[0] == 0xFF && buffer[1] == 0xFE)
-				return Encoding.Unicode;
-			if (buffer[0] == 0xFE && buffer[1] == 0xFF)
-				return Encoding.BigEndianUnicode;
-			if (buffer[0] == 0xFE && buffer[1] == 0x43)
-				return Encoding.Default;
+			// *** Use Default of Encoding.Default (Ansi CodePage)
+			Encoding enc = Encoding.Default;
 
-			throw new Exception($"Not supported encoding type found in file: {srcFile}");
+			// *** Detect byte order mark if any - otherwise assume default
+			byte[] buffer = new byte[10];
+			FileStream file = new FileStream(srcFile, FileMode.Open);
+			file.Read(buffer, 0, 10);
+			file.Close();
+			
+			if (buffer[0] == 0xEF && buffer[1] == 0xBB && buffer[2] == 0xBF)
+			{
+				enc = Encoding.UTF8;
+			}
+			else if (buffer[0] == 0xFE && buffer[1] == 0xFF)
+			{
+				enc = Encoding.Unicode;
+			}
+			else if (buffer[0] == 0 && buffer[1] == 0 && buffer[2] == 0xFE && buffer[3] == 0xFF)
+			{
+				enc = Encoding.UTF32;
+			}
+			else if (buffer[0] == 0x2B && buffer[1] == 0x2F && buffer[2] == 0x76)
+			{
+				enc = Encoding.UTF7;
+			}
+			else if (buffer[0] == 0xFE && buffer[1] == 0xFF)
+			{
+				// 1201 unicodeFFFE Unicode (Big-Endian)
+				enc = Encoding.GetEncoding(1201);
+			}
+			else if (buffer[0] == 0xFF && buffer[1] == 0xFE)
+			{
+				// 1200 utf-16 Unicode
+				enc = Encoding.GetEncoding(1200);
+			}
+
+			return enc;
 		}
 	}
 
