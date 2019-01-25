4ad5128828d9fef7663b191d9016b6481fbba789
Michal Grygierzec <michal.grygierzec@relativity.com>
2019-01-25 16:03:16 +0100

    Refactor FileEncodingDetectionHelper class

--- Source/kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration/Helpers/FileEncodingDetectionHelper.cs
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration/Helpers/FileEncodingDetectionHelper.cs
@@ -1,10 +1,13 @@
 ﻿using System.IO;
 using System.Text;
+using kCura.IntegrationPoint.Tests.Core;
 
 namespace kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration.Helpers
 {
 	internal static class FileEncodingDetectionHelper
 	{
+		private const short _BUFFER_SIZE = 4;
+
 		/// <summary>
 		/// Method that performs simple check of the file encoding based on BOM 
 		/// </summary>
@@ -12,28 +15,63 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Tests.Integration.Hel
 		/// <returns></returns>
 		public static Encoding GetFileEncoding(string srcFile)
 		{
-			Encoding enc = Encoding.Default;
-
-			byte[] buffer = new byte[4];
-			FileStream file = new FileStream(srcFile, FileMode.Open);
-			file.Read(buffer, 0, 4);
-			file.Close();
-			
-			if (buffer[0] == 0xEF && buffer[1] == 0xBB && buffer[2] == 0xBF)
+			byte[] buffer = new byte[_BUFFER_SIZE];
+			using (FileStream file = new FileStream(srcFile, FileMode.Open))
 			{
-				enc = Encoding.UTF8;
-			}
-			else if (buffer[0] == 0xFF && buffer[1] == 0xFE)
-			{
-				enc = Encoding.Unicode;
-			}
-			else if (buffer[0] == 0xFE && buffer[1] == 0xFF)
-			{
-				enc = Encoding.BigEndianUnicode;
+				file.Read(buffer, 0, _BUFFER_SIZE);
+				file.Close();
 			}
 
-			return enc;
+			if (IsUTF8(buffer))
+			{
+				return Encoding.UTF8;
+			}
+			if (IsUnicode(buffer))
+			{
+				return Encoding.Unicode;
+			}
+			if (IsBigEndiancUnicode(buffer))
+			{
+				return Encoding.BigEndianUnicode;
+			}
+
+			return Encoding.Default;
 		}
+
+		private static bool IsUTF8(byte[] buffer)
+		{
+			const short requiredBufferLength = 4;
+
+			ValidateBufferLength(requiredBufferLength, buffer);
+
+			return buffer[0] == 0xEF && buffer[1] == 0xBB && buffer[2] == 0xBF;
+		}
+		private static bool IsUnicode(byte[] buffer)
+		{
+			const short requiredBufferLength = 2;
+
+			ValidateBufferLength(requiredBufferLength, buffer);
+
+			return buffer[0] == 0xFF && buffer[1] == 0xFE;
+		}
+
+		private static bool IsBigEndiancUnicode(byte[] buffer)
+		{
+			const short requiredBufferLength = 2;
+
+			ValidateBufferLength(requiredBufferLength, buffer);
+
+			return buffer[0] == 0xFE && buffer[1] == 0xFF;
+		}
+
+		private static void ValidateBufferLength(short requiredBufferLength, byte[] buffer)
+		{
+			if (buffer == null || buffer.Length < requiredBufferLength)
+			{
+				throw new TestException($"Buffer should have required {requiredBufferLength} bytes length or more!");
+			}
+		}
+
 	}
 
 }
