6b8fcc7a449251dceea24fcfee292fe4b5fd853c
Damian Janas <damian.janas@relativity.com>
2019-01-31 16:08:33 +0100

    documentation updated

--- Source/kCura.IntegrationPoint.Tests.Core/TestCategories/TestQuarantineState.cs
+++ Source/kCura.IntegrationPoint.Tests.Core/TestCategories/TestQuarantineState.cs
@@ -2,32 +2,35 @@
 {
     /// <summary>
     /// The main purpose of these states is to help in monitoring of 
-    /// unstable tests placed in the quarantine. A test should be mark
-    /// with a specific state if it met following conditions:
-    /// 
-    /// - UnderObservation - initial state of a test in the quarantine. 
-    /// 
-    /// Test should stay with it no longer than duration of a single 
-    /// sprint (two weeks). After two weeks, we have enough data to 
-    /// decide what action should be made to each test in the quarantine.
-    /// 
-    /// - FindsDefect - particular test still fails because of the defect
-    /// in a product and should be fixed as soon as possible. After fixing, 
-    /// it should be marked with SeemsToBeStable.
-    /// 
-    /// - ShowsInstability - a test randomly fails because of some instability
-    /// issues in itself. The reason of instability should be described.
-    /// After fixing, it should be marked with SeemsToBeStable.
-    /// 
-    /// - SeemsToBeStable - a test still succeeds and looks like is stable.
-    /// On the next round of reviewing the quarantine, it should be moved
-    /// back to stable tests if
+    /// unstable tests placed in the quarantine. A test should be marked
+    /// with a specific state if it meets following conditions.
     /// </summary>
 	public enum TestQuarantineState
 	{
-		UnderObservation,
-        FindsDefect,
+        /// <summary>
+        /// Initial state of a test in the quarantine. 
+        /// A test should stay with it no longer than duration of a single 
+        /// sprint (two weeks). After two weeks, we have enough data to 
+        /// decide what action should be made to each test in the quarantine.
+        /// </summary>
+        UnderObservation,
+        /// <summary>
+        /// Particular test still fails because of the defect
+        /// in a product and should be fixed as soon as possible. After fixing, 
+        /// it should be marked with SeemsToBeStable.
+        /// </summary>
+        DetectsDefect,
+        /// <summary>
+        /// A test randomly fails because of some instability
+        /// issues in itself. The reason of instability should be described.
+        /// After fixing, it should be marked with SeemsToBeStable.
+        /// </summary>
         ShowsInstability,
-		SeemsToBeStable
-	}
+        /// <summary>
+        /// A test still succeeds and looks like stable.
+        /// On the next round of reviewing the quarantine, it should be moved
+        /// back to stable tests if reviewer does not find any failure in a history.
+        /// </summary>
+        SeemsToBeStable
+    }
 }
