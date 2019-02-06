7c00a0168758715d17a8807a2e47a1a12326db90
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-02-06 14:15:13 +0100

    Add integration tests for Validation

--- Source/Relativity.Sync.Tests.Integration/GlobalSuppressions.cs
+++ Source/Relativity.Sync.Tests.Integration/GlobalSuppressions.cs
@@ -13,5 +13,6 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "<Pending>", Scope = "type", Target = "~T:Relativity.Sync.Tests.Integration.DestinationWorkspaceObjectTypesCreationStepTests")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "<Pending>", Scope = "type", Target = "~T:Relativity.Sync.Tests.Integration.DestinationWorkspaceTagsCreationStepTests")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "<Pending>", Scope = "type", Target = "~T:Relativity.Sync.Tests.Integration.SourceWorkspaceTagsCreationStepTests")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "<Pending>", Scope = "type", Target = "~T:Relativity.Sync.Tests.Integration.ValidationStepTests")]
 
 #pragma warning restore RG2001 // Character Per Line
--- Source/Relativity.Sync.Tests.Integration/ValidationStepTests.cs
+++ Source/Relativity.Sync.Tests.Integration/ValidationStepTests.cs
@@ -1,53 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Autofac;
-using FluentAssertions;
 using NUnit.Framework;
 using Relativity.Sync.Configuration;
-using Relativity.Sync.Tests.Integration.Stubs;
 
 namespace Relativity.Sync.Tests.Integration
 {
 	[TestFixture]
-	public sealed class ValidationStepTests
+	internal sealed class ValidationStepTests : FailingStepsBase<IValidationConfiguration>
 	{
-		private SyncJobFactory _syncJobFactory;
-		private List<Type> _executorTypes;
-		private ContainerBuilder _containerBuilder;
-
-		[SetUp]
-		public void SetUp()
+		protected override void AssertExecutedSteps(List<Type> executorTypes)
 		{
-			_executorTypes = new List<Type>();
-			_syncJobFactory = new SyncJobFactory();
-			_containerBuilder = IntegrationTestsContainerBuilder.CreateContainerBuilder(_executorTypes);
+			// no need to check other steps
 		}
 
-		[Test]
-		public void ItShouldHandleExceptionAndStopExecutionAfterStepExecutionFails()
+		protected override int ExpectedNumberOfExecutedSteps()
 		{
-			IExecutor<IValidationConfiguration> executor = new FailingExecutorStub<IValidationConfiguration>();
-
-			_containerBuilder.RegisterInstance(executor).As<IExecutor<IValidationConfiguration>>();
-
-			ISyncJob syncJob = _syncJobFactory.Create(_containerBuilder.Build(), new SyncJobParameters(1, 1));
-
-			// ACT
-			Func<Task> action = async () => await syncJob.ExecuteAsync(CancellationToken.None).ConfigureAwait(false);
-
-			// ASSERT
-			action.Should().Throw<SyncException>();
-			// validation, notification
+			// permissions, notification
 			const int expectedNumberOfExecutedSteps = 2;
-			_executorTypes.Count.Should().Be(expectedNumberOfExecutedSteps);
-		}
-
-		[Test]
-		public void ItShouldHandleExceptionAndStopExecutionAfterStepConstrainsCheckFails()
-		{
-			// TODO
+			return expectedNumberOfExecutedSteps;
 		}
 	}
 }
