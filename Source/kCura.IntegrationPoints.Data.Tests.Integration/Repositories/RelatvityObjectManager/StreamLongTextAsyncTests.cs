5b5fd1b1d493e434b9fb3e7114e6ff25566cbf41
Michal Grygierzec <michal.grygierzec@relativity.com>
2019-02-18 09:59:04 +0100

    Remove unnecessary ConfigureAwait

--- Source/kCura.IntegrationPoints.Data.Tests.Integration/Repositories/RelatvityObjectManager/StreamLongTextAsyncTests.cs
+++ Source/kCura.IntegrationPoints.Data.Tests.Integration/Repositories/RelatvityObjectManager/StreamLongTextAsyncTests.cs
@@ -101,9 +101,8 @@ namespace kCura.IntegrationPoints.Data.Tests.Integration.Repositories.Relativity
 					_relativityObjectManager.StreamLongTextAsync(
 						documentArtifactID,
 						new FieldRef {Name = _EXTRACTED_TEXT_FIELD_NAME})
-						.ConfigureAwait(false)
-						.GetAwaiter()
-						.GetResult();
+							.GetAwaiter()
+							.GetResult();
 				var actualExtractedTextStreamReader = new StreamReader(actualExtractedTextStream, Encoding.UTF8);
 				string actualExtractedTextString = actualExtractedTextStreamReader.ReadToEnd();
 
