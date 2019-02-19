2571b8d7fbe196757e5dd641e915071931105e5a
Michal Grygierzec <michal.grygierzec@relativity.com>
2019-02-19 14:55:13 +0100

    Move missing method to RIPPipeline class

--- DevelopmentScripts/JenkinsHelpers.groovy
+++ DevelopmentScripts/JenkinsHelpers.groovy
@@ -52,22 +52,22 @@ class RIPPipeline
         echo "RIPPipeline::getVersion set commonBuildArgs to: $commonBuildArgs"
     }
 
-}
+    /**
+    * Return the current build version in the TeamCity versioning database & increment
+    * for the next build if necessary. Basically a pass-through to the New-TeamCityBuildVersion.ps1
+    * script. Examine that script for info about how CI build versioning works.
+    *
+    * @param packageName Name of the package being versioned. Equivalent to the "Product" for purposes of build versioning or the folder in the bld-pkgs Packages folder.
+    * @param buildType   Build type of the current build, e.g. 'DEV', 'GOLD', etc. Affects how the next version is incremented.
+    * @return String indicating the current build version, e.g. "10.2.1.3".
+    */
+    def incrementBuildVersion(String packageName, String buildType)
+    {
+        def versionOutput = powershell(returnStdout: true, script: ".\\DevelopmentScripts\\New-TeamCityBuildVersion.ps1 -Product '$packageName' -Project 'Development' -ServerType 'Jenkins' -BuildType '$buildType'")
+        versionNumber = versionOutput.tokenize()[0]
+        return versionNumber
+    }
 
-/**
-* Return the current build version in the TeamCity versioning database & increment
-* for the next build if necessary. Basically a pass-through to the New-TeamCityBuildVersion.ps1
-* script. Examine that script for info about how CI build versioning works.
-*
-* @param packageName Name of the package being versioned. Equivalent to the "Product" for purposes of build versioning or the folder in the bld-pkgs Packages folder.
-* @param buildType   Build type of the current build, e.g. 'DEV', 'GOLD', etc. Affects how the next version is incremented.
-* @return String indicating the current build version, e.g. "10.2.1.3".
-*/
-def incrementBuildVersion(String packageName, String buildType)
-{
-    def versionOutput = powershell(returnStdout: true, script: ".\\DevelopmentScripts\\New-TeamCityBuildVersion.ps1 -Product '$packageName' -Project 'Development' -ServerType 'Jenkins' -BuildType '$buildType'")
-    versionNumber = versionOutput.tokenize()[0]
-    return versionNumber
 }
 
 /**
