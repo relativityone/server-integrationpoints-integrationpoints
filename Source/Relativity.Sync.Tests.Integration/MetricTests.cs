957403a898b3042cf746428577ee7145add6043a
Patryk Stepien <patryk.stepien@relativity.com>
2019-03-11 17:51:31 +0100

    Add integration test for Metric serialization

---
+++ Source/Relativity.Sync.Tests.Integration/MetricTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentAssertions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Relativity.Sync.Telemetry;
+
+namespace Relativity.Sync.Tests.Integration
+{
+	[TestFixture]
+	public sealed class MetricTests
+	{
+		private Metric _metric;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_metric = Metric.TimedOperation("metric name", TimeSpan.FromMilliseconds(1), CommandExecutionStatus.Completed, "correlation ID");
+		}
+
+		[Test]
+		public void ItShouldSerializeMetricWithoutCustomData()
+		{
+			// ACT
+			string serializedObject = JsonConvert.SerializeObject(_metric);
+
+			// ASSERT
+			serializedObject.Should().NotBeNullOrWhiteSpace();
+		}
+
+		[Test]
+		public void ItShouldSerializeMetricWithCustomData()
+		{
+			_metric.Metadata.Add("custom data int", 1);
+			_metric.Metadata.Add("custom data object", new object());
+			_metric.Metadata.Add("custom data null", null);
+
+			// ACT
+			string serializedObject = JsonConvert.SerializeObject(_metric);
+
+			// ASSERT
+			serializedObject.Should().NotBeNullOrWhiteSpace();
+		}
+	}
+}
