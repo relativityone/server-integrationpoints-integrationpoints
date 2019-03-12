962bfd4505bd20d0c41d86008599d4684e4431ed
Patryk Spytkowski <patryk.spytkowski@relativity.com>
2019-03-12 16:35:39 +0100

    REL-290142 Created constrains class for DestinationWorkspaceTagsCreation step

---
+++ Source/Relativity.Sync.Tests.Unit/ExecutionConstrains/DestinationWorkspaceTagsCreationExecutionConstrainsTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Relativity.Sync.Configuration;
+using Relativity.Sync.ExecutionConstrains;
+
+namespace Relativity.Sync.Tests.Unit.ExecutionConstrains
+{
+	[TestFixture]
+	public sealed class DestinationWorkspaceTagsCreationExecutionConstrainsTests
+	{
+		[Test]
+		public async Task ItShouldAlwaysReturnTrue()
+		{
+			DestinationWorkspaceTagsCreationExecutionConstrains instance = new DestinationWorkspaceTagsCreationExecutionConstrains();
+
+			bool result = await instance.CanExecuteAsync(It.IsAny<IDestinationWorkspaceTagsCreationConfiguration>(), CancellationToken.None).ConfigureAwait(false);
+
+			result.Should().BeTrue();
+		}
+	}
+}
