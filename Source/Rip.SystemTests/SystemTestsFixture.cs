582ca9adb7dc03800b55299a7371a563c525e047
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2019-03-13 14:15:12 +0100

    Fixed white spaces and converted WorkspaceID to a property with a private setter in SystemTextFixture

--- Source/Rip.SystemTests/SystemTestsFixture.cs
+++ Source/Rip.SystemTests/SystemTestsFixture.cs
@@ -4,27 +4,27 @@ using kCura.IntegrationPoint.Tests.Core;
 
 namespace Rip.SystemTests
 {
-    [SetUpFixture]
-    public class SystemTestsFixture
-    {
-	    private const string _RELATIVITY_STARTER_TEMPLATE_NAME = "Relativity Starter Template";
+	[SetUpFixture]
+	public class SystemTestsFixture
+	{
+		private const string _RELATIVITY_STARTER_TEMPLATE_NAME = "Relativity Starter Template";
 
-		public static int WorkspaceID;
+		public static int WorkspaceID { get; private set; }
 
-        [OneTimeSetUp]
-        public void InitializeFixture()
-        {
-	        WorkspaceID = Workspace.CreateWorkspace(
-		        workspaceName: $"CoreSearchManager-{DateTime.Now.Ticks}",
-		        templateName: _RELATIVITY_STARTER_TEMPLATE_NAME
-	        );
+		[OneTimeSetUp]
+		public void InitializeFixture()
+		{
+			WorkspaceID = Workspace.CreateWorkspace(
+				workspaceName: $"CoreSearchManager-{DateTime.Now.Ticks}",
+				templateName: _RELATIVITY_STARTER_TEMPLATE_NAME
+			);
 		}
 
-        [OneTimeTearDown]
-        public void TearDownFixture()
-        {
-	        Workspace.DeleteWorkspace(WorkspaceID);
+		[OneTimeTearDown]
+		public void TearDownFixture()
+		{
+			Workspace.DeleteWorkspace(WorkspaceID);
 		}
 
-    }
+	}
 }
