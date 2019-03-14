d62bfbcdbad5fe055db1437eb8665ea479c33c2e
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-03-14 16:03:23 +0100

    REL-290425 Add SourceWorkspaceTagsCreationStep

---
+++ Source/Relativity.Sync.Tests.Unit/SourceWorkspaceTagsCreationStepTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using Relativity.Sync.Configuration;
+using Relativity.Sync.Nodes;
+
+namespace Relativity.Sync.Tests.Unit
+{
+	[TestFixture]
+	public sealed class SourceWorkspaceTagsCreationStepTests
+	{
+		private SourceWorkspaceTagsCreationStep _sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_sut = new SourceWorkspaceTagsCreationStep();
+		}
+
+		[Test]
+		public async Task ItShouldAlwaysCanExecute()
+		{
+			bool canExecute = await _sut.CanExecuteAsync(It.IsAny<ISourceWorkspaceTagsCreationConfiguration>(), It.IsAny<CancellationToken>()).ConfigureAwait(false);
+
+			Assert.True(canExecute);
+		}
+	}
+}
---
+++ Source/Relativity.Sync/Nodes/SourceWorkspaceTagsCreationStep.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Relativity.Sync.Configuration;
+
+namespace Relativity.Sync.Nodes
+{
+	internal sealed class SourceWorkspaceTagsCreationStep : IExecutor<ISourceWorkspaceTagsCreationConfiguration>, IExecutionConstrains<ISourceWorkspaceTagsCreationConfiguration>
+	{
+		public SourceWorkspaceTagsCreationStep()
+		{
+		}
+
+		public Task<bool> CanExecuteAsync(ISourceWorkspaceTagsCreationConfiguration configuration, CancellationToken token)
+		{
+			return Task.FromResult(true);
+		}
+
+		public Task ExecuteAsync(ISourceWorkspaceTagsCreationConfiguration configuration, CancellationToken token)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
