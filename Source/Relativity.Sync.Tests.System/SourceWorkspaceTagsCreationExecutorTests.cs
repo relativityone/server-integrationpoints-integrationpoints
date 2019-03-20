024cce5219a6edd18f54f9bcbd5bfdc87f271eed
Matt Shanahan <matt.shanahan@relativity.com>
2019-03-20 15:55:50 +0100

    [REL-290425] Update usage of DestinationInstanceArtifactID to conform to RIP + finish SourceWorkspaceTagsCreationStep system tests

--- Source/Relativity.Sync.Tests.System/SourceWorkspaceTagsCreationExecutorTests.cs
+++ Source/Relativity.Sync.Tests.System/SourceWorkspaceTagsCreationExecutorTests.cs
@@ -6,11 +6,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Autofac;
 using kCura.Relativity.Client.DTOs;
-using Moq;
 using NUnit.Framework;
 using Relativity.Services.ApplicationInstallManager;
-using Relativity.Services.Exceptions;
-using Relativity.Services.LibraryApplicationsManager;
 using Relativity.Services.Objects;
 using Relativity.Services.Objects.DataContracts;
 using Relativity.Sync.Configuration;
@@ -18,9 +15,6 @@ using Relativity.Sync.ExecutionConstrains;
 using Relativity.Sync.Executors;
 using Relativity.Sync.Tests.Integration.Stubs;
 using Relativity.Sync.Tests.System.Stubs;
-using ObjectType = kCura.Relativity.Client.DTOs.ObjectType;
-using Field = kCura.Relativity.Client.DTOs.Field;
-using FieldType = kCura.Relativity.Client.FieldType;
 using QueryResult = Relativity.Services.Objects.DataContracts.QueryResult;
 
 namespace Relativity.Sync.Tests.System
@@ -31,16 +25,15 @@ namespace Relativity.Sync.Tests.System
 		private Workspace _sourceWorkspace;
 		private Workspace _destinationWorkspace;
 
-		private readonly Guid _jobHistoryObjectTypeGuid = Guid.Parse("08F4B1F7-9692-4A08-94AB-B5F3A88B6CC9");
-		private readonly Guid _destinationWorkspaceObjectTypeGuid = Guid.Parse("3F45E490-B4CF-4C7D-8BB6-9CA891C0C198");
+		private static readonly Guid _JOB_HISTORY_GUID = Guid.Parse("08F4B1F7-9692-4A08-94AB-B5F3A88B6CC9");
+		private static readonly Guid _DESTINATION_WORKSPACE_GUID = Guid.Parse("3F45E490-B4CF-4C7D-8BB6-9CA891C0C198");
+		private static readonly Guid _INTEGRATION_POINT_GUID = Guid.Parse("DCF6E9D1-22B6-4DA3-98F6-41381E93C30C");
 
-		private readonly Guid _jobHistoryFieldInDestinationWorkspaceGuid = Guid.Parse("07B8A468-DEC8-45BD-B50A-989A35150BE2");
-		private readonly Guid _destinationWorkspaceArtifactIdFieldInDestinationWorkspaceGuid = Guid.Parse("207E6836-2961-466B-A0D2-29974A4FAD36");
-		private readonly Guid _destinationWorkspaceNameFieldInDestinationWorkspaceGuid = Guid.Parse("348D7394-2658-4DA4-87D0-8183824ADF98");
-		private readonly Guid _destinationInstanceNameFieldInDestinationWorkspaceGuid = Guid.Parse("909ADC7C-2BB9-46CA-9F85-DA32901D6554");
-		private readonly Guid _destinationInstanceArtifactIdFieldInDestinationWorkspaceGuid = Guid.Parse("323458DB-8A06-464B-9402-AF2516CF47E0");
-
-		private readonly Guid _integrationPointsGuid = Guid.Parse("DCF6E9D1-22B6-4DA3-98F6-41381E93C30C");
+		private static readonly Guid _DESTINATION_WORKSPACE_JOB_HISTORY_FIELD_GUID = Guid.Parse("07B8A468-DEC8-45BD-B50A-989A35150BE2");
+		private static readonly Guid _DESTINATION_WORKSPACE_DESTINATION_WORKSPACE_ARTIFACTID_FIELD_GUID = Guid.Parse("207E6836-2961-466B-A0D2-29974A4FAD36");
+		private static readonly Guid _DESTINATION_WORKSPACE_DESTINATION_WORKSPACE_NAME_FIELD_GUID = Guid.Parse("348D7394-2658-4DA4-87D0-8183824ADF98");
+		private static readonly Guid _DESTINATION_WORKSPACE_DESTINATION_INSTANCE_NAME_FIELD_GUID = Guid.Parse("909ADC7C-2BB9-46CA-9F85-DA32901D6554");
+		private static readonly Guid _DESTINATION_WORKSPACE_DESTINATION_INSTANCE_ARTIFACTID_FIELD_GUID = Guid.Parse("323458DB-8A06-464B-9402-AF2516CF47E0");
 
 		[SetUp]
 		public async Task SetUp()
@@ -54,11 +47,79 @@ namespace Relativity.Sync.Tests.System
 		{
 			using (var applicationInstallManager = ServiceFactory.CreateProxy<IApplicationInstallManager>())
 			{
-				await applicationInstallManager.InstallLibraryApplicationByGuid(workspaceArtifactId, _integrationPointsGuid).ConfigureAwait(false);
+				await applicationInstallManager.InstallLibraryApplicationByGuid(workspaceArtifactId, _INTEGRATION_POINT_GUID).ConfigureAwait(false);
 			}
 		}
 
-		public async Task<int> CreateJobHistoryInstance(int workspaceId)
+		[Test]
+		public async Task ItShouldCreateTagIfItDoesNotExist()
+		{
+			int jobHistoryArtifactId = await CreateJobHistoryInstance(_sourceWorkspace.ArtifactID).ConfigureAwait(false);
+			const int relativityAdminUserId = 9;
+
+			ConfigurationStub configuration = new ConfigurationStub
+			{
+				DestinationWorkspaceArtifactId = _destinationWorkspace.ArtifactID,
+				SourceWorkspaceArtifactId = _sourceWorkspace.ArtifactID,
+				JobArtifactId = jobHistoryArtifactId,
+				ExecutingUserId = relativityAdminUserId
+			};
+
+			ISyncJob syncJob = CreateSyncJob(configuration);
+
+			// ACT
+			await syncJob.ExecuteAsync(CancellationToken.None).ConfigureAwait(false);
+
+			// ASSERT
+			RelativityObject tag = await QueryForCreatedTag(configuration.DestinationWorkspaceTagArtifactId)
+				.ConfigureAwait(false);
+
+			Assert.AreEqual(_destinationWorkspace.ArtifactID, tag.FieldValues.First(x => x.Field.Guids.Contains(_DESTINATION_WORKSPACE_DESTINATION_WORKSPACE_ARTIFACTID_FIELD_GUID)).Value);
+			Assert.AreEqual(_destinationWorkspace.Name, tag.FieldValues.First(x => x.Field.Guids.Contains(_DESTINATION_WORKSPACE_DESTINATION_WORKSPACE_NAME_FIELD_GUID)).Value);
+
+			var relativityObjectValues = (List<RelativityObjectValue>)tag.FieldValues
+				.First(x => x.Field.Guids.Contains(_DESTINATION_WORKSPACE_JOB_HISTORY_FIELD_GUID)).Value;
+			Assert.AreEqual(1, relativityObjectValues.Count);
+			Assert.AreEqual(jobHistoryArtifactId, relativityObjectValues.First().ArtifactID);
+		}
+
+		[Test]
+		public async Task ItShouldUpdateTagIfItDoesExist()
+		{
+			int jobHistoryArtifactId = await CreateJobHistoryInstance(_sourceWorkspace.ArtifactID).ConfigureAwait(false);
+			const int userId = 9;
+
+			ConfigurationStub configuration = new ConfigurationStub
+			{
+				DestinationWorkspaceArtifactId = _destinationWorkspace.ArtifactID,
+				SourceWorkspaceArtifactId = _sourceWorkspace.ArtifactID,
+				JobArtifactId = jobHistoryArtifactId,
+				ExecutingUserId = userId
+			};
+
+			ISyncJob syncJob = CreateSyncJob(configuration);
+
+			int destinationWorkspaceTagArtifactId = await CreateDestinationWorkspaceTag(_sourceWorkspace.ArtifactID, "whatever", "Wrong Workspace Name", _destinationWorkspace.ArtifactID).ConfigureAwait(false);
+
+			// ACT
+			await syncJob.ExecuteAsync(CancellationToken.None).ConfigureAwait(false);
+
+			// ASSERT
+			Assert.AreEqual(destinationWorkspaceTagArtifactId, configuration.DestinationWorkspaceTagArtifactId);
+
+			RelativityObject tag = await QueryForCreatedTag(configuration.DestinationWorkspaceTagArtifactId)
+				.ConfigureAwait(false);
+
+			Assert.AreEqual(_destinationWorkspace.ArtifactID, tag.FieldValues.First(x => x.Field.Guids.Contains(_DESTINATION_WORKSPACE_DESTINATION_WORKSPACE_ARTIFACTID_FIELD_GUID)).Value);
+			Assert.AreEqual(_destinationWorkspace.Name, tag.FieldValues.First(x => x.Field.Guids.Contains(_DESTINATION_WORKSPACE_DESTINATION_WORKSPACE_NAME_FIELD_GUID)).Value);
+
+			var relativityObjectValues = (List<RelativityObjectValue>)tag.FieldValues
+				.First(x => x.Field.Guids.Contains(_DESTINATION_WORKSPACE_JOB_HISTORY_FIELD_GUID)).Value;
+			Assert.AreEqual(1, relativityObjectValues.Count);
+			Assert.AreEqual(jobHistoryArtifactId, relativityObjectValues.First().ArtifactID);
+		}
+
+		private async Task<int> CreateJobHistoryInstance(int workspaceId)
 		{
 			using (var objectManager = ServiceFactory.CreateProxy<IObjectManager>())
 			{
@@ -77,7 +138,7 @@ namespace Relativity.Sync.Tests.System
 					},
 					ObjectType = new ObjectTypeRef()
 					{
-						Guid = _jobHistoryObjectTypeGuid
+						Guid = _JOB_HISTORY_GUID
 					}
 				};
 				CreateResult result = await objectManager.CreateAsync(workspaceId, request).ConfigureAwait(false);
@@ -87,7 +148,7 @@ namespace Relativity.Sync.Tests.System
 
 		private ISyncJob CreateSyncJob(ConfigurationStub configuration)
 		{
-			// The setup will be changed once the new Containerbuilder changes are merged in.
+			// TODO: This setup should be changed once the IContainerFactory changes are merged in.
 			ContainerBuilder containerBuilder = new ContainerBuilder();
 			List<IInstaller> installers = Assembly.GetAssembly(typeof(IInstaller))
 				.GetTypes()
@@ -123,28 +184,28 @@ namespace Relativity.Sync.Tests.System
 					{
 						new FieldRef()
 						{
-							Guid = _jobHistoryFieldInDestinationWorkspaceGuid
+							Guid = _DESTINATION_WORKSPACE_JOB_HISTORY_FIELD_GUID
 						},
 						new FieldRef()
 						{
-							Guid = _destinationWorkspaceArtifactIdFieldInDestinationWorkspaceGuid
+							Guid = _DESTINATION_WORKSPACE_DESTINATION_WORKSPACE_ARTIFACTID_FIELD_GUID
 						},
 						new FieldRef()
 						{
-							Guid = _destinationWorkspaceNameFieldInDestinationWorkspaceGuid
+							Guid = _DESTINATION_WORKSPACE_DESTINATION_WORKSPACE_NAME_FIELD_GUID
 						},
 						new FieldRef()
 						{
-							Guid = _destinationInstanceNameFieldInDestinationWorkspaceGuid
+							Guid = _DESTINATION_WORKSPACE_DESTINATION_INSTANCE_NAME_FIELD_GUID
 						},
 						new FieldRef()
 						{
-							Guid = _destinationInstanceArtifactIdFieldInDestinationWorkspaceGuid
+							Guid = _DESTINATION_WORKSPACE_DESTINATION_INSTANCE_ARTIFACTID_FIELD_GUID
 						}
 					},
 					ObjectType = new ObjectTypeRef()
 					{
-						Guid = _destinationWorkspaceObjectTypeGuid
+						Guid = _DESTINATION_WORKSPACE_GUID
 					}
 				};
 				QueryResult result = await objectManager.QueryAsync(_sourceWorkspace.ArtifactID, request, 0, 1)
@@ -156,40 +217,7 @@ namespace Relativity.Sync.Tests.System
 			return tag;
 		}
 
-		[Test]
-		public async Task ItShouldCreateTagIfItDoesNotExist()
-		{
-			int jobHistoryArtifactId = await CreateJobHistoryInstance(_sourceWorkspace.ArtifactID).ConfigureAwait(false);
-			const int userId = 9;
-
-			ConfigurationStub configuration = new ConfigurationStub
-			{
-				DestinationWorkspaceArtifactId = _destinationWorkspace.ArtifactID,
-				SourceWorkspaceArtifactId = _sourceWorkspace.ArtifactID,
-				JobArtifactId = jobHistoryArtifactId,
-				ExecutingUserId = userId
-			};
-
-			ISyncJob syncJob = CreateSyncJob(configuration);
-
-			// ACT
-			await syncJob.ExecuteAsync(CancellationToken.None).ConfigureAwait(false);
-
-			// ASSERT
-			RelativityObject tag = await QueryForCreatedTag(configuration.DestinationWorkspaceTagArtifactId)
-				.ConfigureAwait(false);
-
-			Assert.AreEqual(_destinationWorkspace.ArtifactID, tag.FieldValues.First(x => x.Field.Guids.Contains(_destinationWorkspaceArtifactIdFieldInDestinationWorkspaceGuid)).Value);
-			Assert.AreEqual(_destinationWorkspace.Name, tag.FieldValues.First(x => x.Field.Guids.Contains(_destinationWorkspaceNameFieldInDestinationWorkspaceGuid)).Value);
-
-			var relativityObjectValues = (List<RelativityObjectValue>)tag.FieldValues
-				.First(x => x.Field.Guids.Contains(_jobHistoryFieldInDestinationWorkspaceGuid)).Value;
-			Assert.AreEqual(1, relativityObjectValues.Count);
-			Assert.AreEqual(jobHistoryArtifactId, relativityObjectValues.First().ArtifactID);
-		}
-
-		private async Task<DestinationWorkspaceTag> CreateDestinationWorkspaceTag(string tagName, Workspace destinationWorkspace, string federatedInstanceName, int federatedInstanceId,
-			Workspace sourceWorkspace)
+		private async Task<int> CreateDestinationWorkspaceTag(int workspaceId, string tagName, string destinationWorkspaceName, int destinationWorkspaceArtifactId)
 		{
 			using (IObjectManager objectManager = ServiceFactory.CreateProxy<IObjectManager>())
 			{
@@ -197,7 +225,7 @@ namespace Relativity.Sync.Tests.System
 				{
 					ObjectType = new ObjectTypeRef
 					{
-						Guid = _destinationWorkspaceObjectTypeGuid
+						Guid = _DESTINATION_WORKSPACE_GUID
 					},
 					FieldValues = new[]
 					{
@@ -213,82 +241,40 @@ namespace Relativity.Sync.Tests.System
 						{
 							Field = new FieldRef
 							{
-								Guid = _destinationWorkspaceNameFieldInDestinationWorkspaceGuid
+								Guid = _DESTINATION_WORKSPACE_DESTINATION_WORKSPACE_NAME_FIELD_GUID
 							},
-							Value = destinationWorkspace.Name
+							Value = destinationWorkspaceName
 						},
 						new FieldRefValuePair
 						{
 							Field = new FieldRef
 							{
-								Guid = _destinationWorkspaceArtifactIdFieldInDestinationWorkspaceGuid
+								Guid = _DESTINATION_WORKSPACE_DESTINATION_WORKSPACE_ARTIFACTID_FIELD_GUID
 							},
-							Value = destinationWorkspace.ArtifactID
+							Value = destinationWorkspaceArtifactId
 						},
 						new FieldRefValuePair
 						{
 							Field = new FieldRef
 							{
-								Guid = _destinationInstanceNameFieldInDestinationWorkspaceGuid
+								Guid = _DESTINATION_WORKSPACE_DESTINATION_INSTANCE_NAME_FIELD_GUID
 							},
-							Value = federatedInstanceName
+							Value = "This Instance"
 						},
 						new FieldRefValuePair
 						{
 							Field = new FieldRef
 							{
-								Guid = _destinationInstanceArtifactIdFieldInDestinationWorkspaceGuid
+								Guid = _DESTINATION_WORKSPACE_DESTINATION_INSTANCE_ARTIFACTID_FIELD_GUID
 							},
-							Value = federatedInstanceId
+							Value = null // TODO: This should be changed to -1 in the future. See relevant TODOs in DestinationWorkspaceTagRepository.
 						}
 					}
 				};
 
-				CreateResult result = await objectManager.CreateAsync(sourceWorkspace.ArtifactID, request).ConfigureAwait(false);
-
-				DestinationWorkspaceTag createdTag = new DestinationWorkspaceTag
-				{
-					ArtifactId = result.Object.ArtifactID,
-					DestinationWorkspaceName = destinationWorkspace.Name,
-					DestinationInstanceName = federatedInstanceName,
-					DestinationWorkspaceArtifactId = destinationWorkspace.ArtifactID
-				};
-				return createdTag;
+				CreateResult result = await objectManager.CreateAsync(workspaceId, request).ConfigureAwait(false);
+				return result.Object.ArtifactID;
 			}
-		}
-
-		[Test]
-		public async Task ItShouldUpdateTagIfItDoesExist()
-		{
-			int jobHistoryArtifactId = await CreateJobHistoryInstance(_sourceWorkspace.ArtifactID).ConfigureAwait(false);
-			const int userId = 9;
-
-			ConfigurationStub configuration = new ConfigurationStub
-			{
-				DestinationWorkspaceArtifactId = _destinationWorkspace.ArtifactID,
-				SourceWorkspaceArtifactId = _sourceWorkspace.ArtifactID,
-				JobArtifactId = jobHistoryArtifactId,
-				ExecutingUserId = userId
-			};
-
-			ISyncJob syncJob = CreateSyncJob(configuration);
-
-			DestinationWorkspaceTag destinationWorkspaceTag = await CreateDestinationWorkspaceTag("whatever", _destinationWorkspace, "This Instance", -1, _sourceWorkspace).ConfigureAwait(false);
-
-			// ACT
-			await syncJob.ExecuteAsync(CancellationToken.None).ConfigureAwait(false);
-
-			// ASSERT
-			RelativityObject tag = await QueryForCreatedTag(configuration.DestinationWorkspaceTagArtifactId)
-				.ConfigureAwait(false);
-
-			Assert.AreEqual(_destinationWorkspace.ArtifactID, tag.FieldValues.First(x => x.Field.Guids.Contains(_destinationWorkspaceArtifactIdFieldInDestinationWorkspaceGuid)).Value);
-			Assert.AreEqual(_destinationWorkspace.Name, tag.FieldValues.First(x => x.Field.Guids.Contains(_destinationWorkspaceNameFieldInDestinationWorkspaceGuid)).Value);
-
-			var relativityObjectValues = (List<RelativityObjectValue>)tag.FieldValues
-				.First(x => x.Field.Guids.Contains(_jobHistoryFieldInDestinationWorkspaceGuid)).Value;
-			Assert.AreEqual(1, relativityObjectValues.Count);
-			Assert.AreEqual(jobHistoryArtifactId, relativityObjectValues.First().ArtifactID);
 		}
 	}
 }
