6ab3b3b95fb29ff15b24de68d015775bf9609196
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2019-03-21 20:02:20 +0100

    Backported IntegrationPointRepostory from Data project

---
+++ Source/kCura.IntegrationPoints.Data/Repositories/IIntegrationPointRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace kCura.IntegrationPoints.Data.Repositories
+{
+	public interface IIntegrationPointRepository
+	{
+		Task<IntegrationPoint> ReadAsync(int integrationPointArtifactID);
+		Task<string> GetFieldMappingJsonAsync(int integrationPointArtifactID);
+		string GetSecuredConfiguration(int integrationPointArtifactID);
+		string GetName(int integrationPointArtifactID);
+	}
+}
