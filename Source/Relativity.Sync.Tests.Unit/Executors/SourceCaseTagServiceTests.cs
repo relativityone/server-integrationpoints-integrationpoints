207dc7fe47a252f386b7e3f0b826946d4286d53b
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-03-25 09:22:38 +0100

    Add UT for RelativitySourceCaseTag and SourceCaseTagService

---
+++ Source/Relativity.Sync.Tests.Unit/Executors/SourceCaseTagServiceTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using Relativity.Sync.Configuration;
+using Relativity.Sync.Executors;
+
+namespace Relativity.Sync.Tests.Unit.Executors
+{
+	[TestFixture]
+	public sealed class SourceCaseTagServiceTests
+	{
+		private Mock<IRelativitySourceCaseTagRepository> _relativitySourceCaseTagRepository;
+		private Mock<IWorkspaceNameQuery> _workspaceNameQuery;
+		private Mock<IFederatedInstance> _federatedInstance;
+		private Mock<ITagNameFormatter> _tagNameFormatter;
+
+		private SourceCaseTagService _sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_relativitySourceCaseTagRepository = new Mock<IRelativitySourceCaseTagRepository>();
+			_workspaceNameQuery = new Mock<IWorkspaceNameQuery>();
+			_federatedInstance = new Mock<IFederatedInstance>();
+			_tagNameFormatter = new Mock<ITagNameFormatter>();
+
+			_sut = new SourceCaseTagService(_relativitySourceCaseTagRepository.Object, _workspaceNameQuery.Object, _federatedInstance.Object, _tagNameFormatter.Object);
+		}
+
+		[Test]
+		public async Task ItShouldCreateNewTag()
+		{
+			const string instanceName = "instance name";
+			const string sourceWorkspaceName = "workspace name";
+			const int sourceWorkspaceArtifactId = 1;
+			const int sourceWorkspaceArtifactTypeId = 2;
+			const int destinationWorkspaceArtifactId = 3;
+			_federatedInstance.Setup(x => x.GetInstanceNameAsync()).ReturnsAsync(instanceName);
+			_workspaceNameQuery.Setup(x => x.GetWorkspaceNameAsync(It.IsAny<int>(), CancellationToken.None)).ReturnsAsync(sourceWorkspaceName);
+
+			Mock<IDestinationWorkspaceTagsCreationConfiguration> configuration = new Mock<IDestinationWorkspaceTagsCreationConfiguration>();
+			configuration.Setup(x => x.SourceWorkspaceArtifactId).Returns(sourceWorkspaceArtifactId);
+			configuration.Setup(x => x.DestinationWorkspaceArtifactId).Returns(destinationWorkspaceArtifactId);
+			configuration.Setup(x => x.SourceWorkspaceArtifactTypeId).Returns(sourceWorkspaceArtifactTypeId);
+
+			const int tagArtifactId = 4;
+			const string tagName = "tagname";
+			RelativitySourceCaseTag sourceCaseTag = new RelativitySourceCaseTag()
+			{
+				ArtifactId = tagArtifactId,
+				Name = tagName,
+				SourceInstanceName = instanceName,
+				SourceWorkspaceArtifactId = sourceWorkspaceArtifactId,
+				SourceWorkspaceName = sourceWorkspaceName
+			};
+			_relativitySourceCaseTagRepository.Setup(x => x.CreateAsync(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<RelativitySourceCaseTag>())).ReturnsAsync(sourceCaseTag);
+
+			// act
+			RelativitySourceCaseTag createdSourceCaseTag = await _sut.CreateOrUpdateSourceCaseTagAsync(configuration.Object, CancellationToken.None).ConfigureAwait(false);
+
+			// assert
+			Assert.AreEqual(tagArtifactId, createdSourceCaseTag.ArtifactId);
+			Assert.AreEqual(tagName, createdSourceCaseTag.Name);
+			Assert.AreEqual(instanceName, createdSourceCaseTag.SourceInstanceName);
+			Assert.AreEqual(sourceWorkspaceArtifactId, createdSourceCaseTag.SourceWorkspaceArtifactId);
+			Assert.AreEqual(sourceWorkspaceName, createdSourceCaseTag.SourceWorkspaceName);
+		}
+
+		[Test]
+		public async Task ItShouldUpdateExistingTag()
+		{
+			const string instanceName = "instance name";
+			const int sourceWorkspaceArtifactId = 1;
+			const int sourceWorkspaceArtifactTypeId = 2;
+			const int destinationWorkspaceArtifactId = 3;
+			const string newWorkspaceName = "workspace name";
+			_federatedInstance.Setup(x => x.GetInstanceNameAsync()).ReturnsAsync(instanceName);
+			_workspaceNameQuery.Setup(x => x.GetWorkspaceNameAsync(It.IsAny<int>(), CancellationToken.None)).ReturnsAsync(newWorkspaceName);
+
+			Mock<IDestinationWorkspaceTagsCreationConfiguration> configuration = new Mock<IDestinationWorkspaceTagsCreationConfiguration>();
+			configuration.Setup(x => x.SourceWorkspaceArtifactId).Returns(sourceWorkspaceArtifactId);
+			configuration.Setup(x => x.DestinationWorkspaceArtifactId).Returns(destinationWorkspaceArtifactId);
+			configuration.Setup(x => x.SourceWorkspaceArtifactTypeId).Returns(sourceWorkspaceArtifactTypeId);
+
+			const int tagArtifactId = 4;
+			const string oldTagName = "old tagname";
+			const string oldWSourceWorkspaceName = "old workspace";
+			RelativitySourceCaseTag sourceCaseTag = new RelativitySourceCaseTag()
+			{
+				ArtifactId = tagArtifactId,
+				Name = oldTagName,
+				SourceInstanceName = instanceName,
+				SourceWorkspaceArtifactId = sourceWorkspaceArtifactId,
+				SourceWorkspaceName = oldWSourceWorkspaceName
+			};
+			_relativitySourceCaseTagRepository.Setup(x => x.ReadAsync(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(), CancellationToken.None)).ReturnsAsync(sourceCaseTag);
+
+			// act
+			await _sut.CreateOrUpdateSourceCaseTagAsync(configuration.Object, CancellationToken.None).ConfigureAwait(false);
+
+			// assert
+			_relativitySourceCaseTagRepository.Verify(x => x.UpdateAsync(It.IsAny<int>(), It.IsAny<RelativitySourceCaseTag>()));
+		}
+	}
+}
