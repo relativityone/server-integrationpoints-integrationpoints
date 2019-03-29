ad81caed55d9079c5fdc37cc0f3939e15ec46319
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2019-03-29 10:44:20 +0100

    REL-294344 extract helper components from TestContext class

---
+++ Source/kCura.IntegrationPoints.UITests/Configuration/Helpers/ImportDocumentsHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using kCura.IntegrationPoint.Tests.Core;
+using kCura.IntegrationPoint.Tests.Core.Models;
+using kCura.IntegrationPoints.UITests.Common;
+using kCura.IntegrationPoints.UITests.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace kCura.IntegrationPoints.UITests.Configuration.Helpers
+{
+	internal class ImportDocumentsHelper
+	{
+		private readonly TestContext _testContext;
+
+		private static readonly ILogger Log = LoggerFactory.CreateLogger(typeof(ImportDocumentsHelper));
+
+		public ImportDocumentsHelper(TestContext testContext)
+		{
+			_testContext = testContext;
+		}
+
+		public void ImportDocuments(bool withNatives, DocumentTestDataBuilder.TestDataType testDataType)
+		{
+			Log.Information(@"Importing documents...");
+			string testDir = NUnit.Framework.TestContext.CurrentContext.TestDirectory.Replace("kCura.IntegrationPoints.UITests",
+				"kCura.IntegrationPoint.Tests.Core");
+			Log.Information("TestDir for ImportDocuments '{testDir}'", testDir);
+			DocumentsTestData data = DocumentTestDataBuilder.BuildTestData(testDir, withNatives, testDataType);
+			var importHelper = new ImportHelper();
+			var workspaceService = new WorkspaceService(importHelper);
+			bool importSucceded = workspaceService.ImportData(_testContext.GetWorkspaceId(), data);
+			if (!importSucceded)
+			{
+				string suffix = importHelper.ErrorMessages.Any()
+					? " Error messages: " + string.Join("; ", importHelper.ErrorMessages)
+					: " No error messages.";
+				throw new UiTestException("Import of documents failed." + suffix);
+			}
+
+			if (Log.IsEnabled(LogEventLevel.Verbose))
+			{
+				string suffix = importHelper.Messages.Any() ? " Messages: " + string.Join("; ", importHelper.Messages) : " No messages.";
+				Log.Verbose(@"Documents imported." + suffix);
+			}
+		}
+	}
+}
---
+++ Source/kCura.IntegrationPoints.UITests/Configuration/Helpers/UserHelper.cs
@@ -0,0 +1,54 @@
+﻿using kCura.IntegrationPoint.Tests.Core;
+using kCura.IntegrationPoint.Tests.Core.Models;
+using System.Security.Claims;
+
+namespace kCura.IntegrationPoints.UITests.Configuration.Helpers
+{
+	internal class UserHelper
+	{
+		private RelativityUser _testUser;
+
+		private const int _ADMIN_USER_ID = 9;
+		
+		private readonly TestContext _testContext;
+
+		public UserHelper(TestContext testContext)
+		{
+			_testContext = testContext;
+		}
+
+		public RelativityUser GetOrCreateTestUser(string timeStamp)
+		{
+			if (_testUser != null)
+			{
+				return _testUser;
+			}
+
+			_testUser = SharedVariables.UiSkipUserCreation
+				? new RelativityUser(SharedVariables.RelativityUserName, SharedVariables.RelativityPassword)
+				: CreateNewTestUser(timeStamp);
+
+			return _testUser;
+		}
+
+		public void DeleteUserIfWasCreated()
+		{
+			if (_testUser != null && _testUser.CreatedInTest)
+			{
+				User.DeleteUser(_testUser.ArtifactId);
+			}
+		}
+
+		private RelativityUser CreateNewTestUser(string timeStamp)
+		{
+			ClaimsPrincipal.ClaimsPrincipalSelector += () =>
+			{
+				var factory = new ClaimsPrincipalFactory();
+				return factory.CreateClaimsPrincipal2(_ADMIN_USER_ID, _testContext.Helper);
+			};
+
+			UserModel userModel = User.CreateUser("RIP", $"Test_User_{timeStamp}", $"RIP_Test_User_{timeStamp}@relativity.com");
+			return new RelativityUser(userModel.ArtifactID, userModel.EmailAddress, userModel.Password);
+		}
+	}
+}
