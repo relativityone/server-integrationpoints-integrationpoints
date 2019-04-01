b36a6500c84a90c983443f042b16f3da17fe6d8a
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2019-04-01 05:32:59 -0500

    Merge pull request #1307 in IN/integrationpoints from REL-293795-document-view-not-available-foxglove to release-10.2-foxglove
    Squashed commit of the following:
    
    commit 4c9113f9406a89ec69636c48d1c9f6fdec0924a5
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Fri Mar 29 08:48:04 2019 +0100
    
        REL-293795 rafactoring after code review
    
    commit 2e4f034a0e1f2feaacd5b2242ae573478a4f6991
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Wed Mar 27 13:48:48 2019 +0100
    
        REL-293795 creating view in FolderExportToLoadFileTests

--- Source/kCura.IntegrationPoint.Tests.Core/View.cs
+++ Source/kCura.IntegrationPoint.Tests.Core/View.cs
@@ -1,20 +1,64 @@
-﻿using kCura.Relativity.Client;
+﻿using kCura.IntegrationPoint.Tests.Core.TestHelpers;
+using kCura.Relativity.Client;
 using kCura.Relativity.Client.DTOs;
+using Relativity.Services.Field;
+using Relativity.Services.ItemListView;
+using Relativity.Services.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace kCura.IntegrationPoint.Tests.Core
 {
 	public static class View
 	{
-		public static int QueryView(int workspaceId, string viewName)
+		private static ITestHelper Helper => new TestHelper();
+
+		public static int QueryView(int workspaceID, string viewName)
 		{
-			using (var rsApiClient = Rsapi.CreateRsapiClient())
+			using (IRSAPIClient rsapiClient = Rsapi.CreateRsapiClient())
 			{
-				rsApiClient.APIOptions.WorkspaceID = workspaceId;
+				rsapiClient.APIOptions.WorkspaceID = workspaceID;
 				var viewQuery = new Query<Relativity.Client.DTOs.View>
 				{
 					Condition = new TextCondition(ViewFieldNames.Name, TextConditionEnum.EqualTo, viewName)
 				};
-				return rsApiClient.Repositories.View.Query(viewQuery).Results[0].Artifact.ArtifactID;
+				return rsapiClient.Repositories.View.Query(viewQuery).Results[0].Artifact.ArtifactID;
+			}
+		}
+
+		public static async Task<int> CreateViewAsync(int workspaceID, string viewName, int objectTypeID, IEnumerable<Guid> fieldsGuids)
+		{
+			List<FieldRef> viewFields = await GetViewFieldsReferences(workspaceID, objectTypeID, fieldsGuids).ConfigureAwait(false);
+
+			var viewToCreate = new global::Relativity.Services.View.View
+			{
+				ArtifactTypeID = objectTypeID,
+				Name = viewName,
+				Fields = viewFields,
+				VisibleInDropdown = true,
+				Order = 0
+			};
+
+			using (IViewManager viewManager = Helper.CreateAdminProxy<IViewManager>())
+			{
+				return await viewManager.CreateSingleAsync(workspaceID, viewToCreate).ConfigureAwait(false);
+			}
+		}
+
+		private static async Task<List<FieldRef>> GetViewFieldsReferences(int workspaceID, int objectTypeID, IEnumerable<Guid> fieldsGuids)
+		{
+			using (IItemListViewManager viewManager = Helper.CreateAdminProxy<IItemListViewManager>())
+			{
+				Dictionary<int, ItemListFieldRef> objectTypeViewFields = await viewManager
+					.GetFieldsAsync(workspaceID, objectTypeID)
+					.ConfigureAwait(false);
+
+				return objectTypeViewFields.Values
+					.Where(viewField => viewField.Guids.Intersect(fieldsGuids).Any())
+					.Select(viewField => viewField as FieldRef)
+					.ToList();
 			}
 		}
 	}
