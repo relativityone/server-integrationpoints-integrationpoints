a715387ac0272ae31e99de0f3e832c6dda448c1f
Patryk Stepien <patryk.stepien@relativity.com>
2019-04-01 15:27:25 +0200

    Configuration cache

---
+++ Source/Relativity.Sync/Storage/IConfigurationCache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Relativity.Sync.Storage
+{
+	internal interface IConfigurationCache : IDisposable
+	{
+		T GetFieldValue<T>(Guid guid);
+		Task UpdateFieldValueAsync<T>(Guid guid, T value);
+	}
+}
