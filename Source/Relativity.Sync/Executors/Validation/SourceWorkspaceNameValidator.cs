13d4eb0099c47503b22144a5d8b5d2bd1f803b6b
Patryk Spytkowski <patryk.spytkowski@relativity.com>
2019-04-02 20:04:01 +0200

    Added unit tests for SourceWorkspaceNameValidator

--- Source/Relativity.Sync/Executors/Validation/SourceWorkspaceNameValidator.cs
+++ Source/Relativity.Sync/Executors/Validation/SourceWorkspaceNameValidator.cs
@@ -9,20 +9,23 @@ namespace Relativity.Sync.Executors.Validation
 		private const string _WORKSPACE_INVALID_NAME_MESSAGE = "Source workspace name contains an invalid character.";
 
 		private readonly IWorkspaceNameValidator _workspaceNameValidator;
+		private readonly ISyncLog _logger;
 
-		public SourceWorkspaceNameValidator(IWorkspaceNameValidator workspaceNameValidator)
+		public SourceWorkspaceNameValidator(IWorkspaceNameValidator workspaceNameValidator, ISyncLog logger)
 		{
 			_workspaceNameValidator = workspaceNameValidator;
+			_logger = logger;
 		}
 
 		public async Task<ValidationResult> ValidateAsync(IValidationConfiguration configuration, CancellationToken token)
 		{
+			_logger.LogVerbose("Validating if source workspace does not contain invalid characters. {sourceWorkspaceArtifactId}", configuration.SourceWorkspaceArtifactId);
 			ValidationResult result = new ValidationResult();
 
 			bool isValidName = await _workspaceNameValidator.ValidateWorkspaceNameAsync(configuration.SourceWorkspaceArtifactId, token).ConfigureAwait(false);
 			if (!isValidName)
 			{
-				result.Add($"{_WORKSPACE_INVALID_NAME_MESSAGE}");
+				result.Add(_WORKSPACE_INVALID_NAME_MESSAGE);
 			}
 
 			return result;
