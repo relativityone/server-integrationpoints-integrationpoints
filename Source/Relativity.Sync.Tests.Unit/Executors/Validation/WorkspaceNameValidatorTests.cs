78cee453974cc2baea8c280481c85e47bce5f6ca
Billy Bafia <billy.bafia@relativity.com>
2019-04-04 09:28:00 +0200

    [REL-292388] Fixing workspace name validator tests after updates to workspace name validator.

--- Source/Relativity.Sync.Tests.Unit/Executors/Validation/WorkspaceNameValidatorTests.cs
+++ Source/Relativity.Sync.Tests.Unit/Executors/Validation/WorkspaceNameValidatorTests.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Threading;
-using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
-using Relativity.Sync.Executors;
 using Relativity.Sync.Executors.Validation;
 
 namespace Relativity.Sync.Tests.Unit.Executors.Validation
@@ -14,11 +12,8 @@ namespace Relativity.Sync.Tests.Unit.Executors.Validation
 		private CancellationToken _cancellationToken;
 
 		private Mock<ISyncLog> _syncLog;
-		private Mock<IWorkspaceNameQuery> _workspaceNameQuery;
 
 		private WorkspaceNameValidator _instance;
-
-		private const int _TEST_WORKSPACE_ARTIFACT_ID = 101202;
 
 		[SetUp]
 		public void SetUp()
@@ -26,63 +21,38 @@ namespace Relativity.Sync.Tests.Unit.Executors.Validation
 			_cancellationToken = CancellationToken.None;
 
 			_syncLog = new Mock<ISyncLog>();
-			_workspaceNameQuery = new Mock<IWorkspaceNameQuery>();
 
-			_instance = new WorkspaceNameValidator(_workspaceNameQuery.Object, _syncLog.Object);
+			_instance = new WorkspaceNameValidator(_syncLog.Object);
 		}
 
 		[Test]
-		public async Task ValidateWorkspaceNameAsyncGoldFlowTest()
+		public void ValidateGoldFlowTest()
 		{
 			// Arrange
-			_workspaceNameQuery.Setup(x => x.GetWorkspaceNameAsync(_TEST_WORKSPACE_ARTIFACT_ID, _cancellationToken)).ReturnsAsync("My Good Workspace").Verifiable();
+			string testWorkspaceName = "GoodWorkspace Name";
 
 			// Act
-			bool actualResult = await _instance.ValidateWorkspaceNameAsync(_TEST_WORKSPACE_ARTIFACT_ID, _cancellationToken).ConfigureAwait(false);
+			bool actualResult = _instance.Validate(testWorkspaceName, _cancellationToken);
 
 			// Assert
 			Assert.IsTrue(actualResult);
-
-			Mock.VerifyAll(_workspaceNameQuery);
 			_syncLog.Verify(x => x.LogError(It.IsAny<string>(), It.IsAny<object[]>()), Times.Never);
 		}
 
 		[Test]
-		public async Task ValidateWorkspaceNameAsyncInvalidWorkspaceNameCharacterTest()
+		public void ValidateInvalidWorkspaceNameCharacterTest()
 		{
 			// Arrange
 			string testWorkspaceName = "My Bad; Workspace";
-			_workspaceNameQuery.Setup(x => x.GetWorkspaceNameAsync(_TEST_WORKSPACE_ARTIFACT_ID, _cancellationToken)).ReturnsAsync(testWorkspaceName).Verifiable();
 
 			// Act
-			bool actualResult = await _instance.ValidateWorkspaceNameAsync(_TEST_WORKSPACE_ARTIFACT_ID, _cancellationToken).ConfigureAwait(false);
+			bool actualResult = _instance.Validate(testWorkspaceName, _cancellationToken);
 
 			// Assert
 			Assert.IsFalse(actualResult);
-
-			Mock.VerifyAll(_workspaceNameQuery);
 			_syncLog.Verify(x => x.LogError(
 				It.Is<string>(y => y.StartsWith("Invalid workspace name:", StringComparison.InvariantCulture)),
-				It.Is<object[]>(y => (string)y[0] == testWorkspaceName && (int)y[1] == _TEST_WORKSPACE_ARTIFACT_ID)), Times.Once);
-		}
-
-		[Test]
-		public async Task ValidateWorkspaceNameAsyncGetWorkspaceNameAsyncThrowsExceptionTest()
-		{
-			// Arrange
-			_workspaceNameQuery.Setup(x => x.GetWorkspaceNameAsync(_TEST_WORKSPACE_ARTIFACT_ID, _cancellationToken)).ThrowsAsync(new Exception()).Verifiable();
-
-			// Act
-			bool actualResult = await _instance.ValidateWorkspaceNameAsync(_TEST_WORKSPACE_ARTIFACT_ID, _cancellationToken).ConfigureAwait(false);
-
-			// Assert
-			Assert.IsFalse(actualResult);
-
-			Mock.VerifyAll(_workspaceNameQuery);
-			_syncLog.Verify(x => x.LogError(
-				It.IsAny<Exception>(),
-				It.Is<string>(y => y.StartsWith("Error occurred", StringComparison.InvariantCulture)),
-				It.Is<object[]>(y => (int)y[0] == _TEST_WORKSPACE_ARTIFACT_ID)), Times.Once);
+				It.Is<object[]>(y => (string)y[0] == testWorkspaceName)), Times.Once);
 		}
 	}
 }
