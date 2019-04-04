d3a66351083aafdf633904f2cbd445ace705144f
Bartłomiej Wesołowski <bartlomiej.wesolowski@relativity.com>
2019-04-04 11:24:16 +0200

    Added FileFieldRepository and extension to convert enumerable to dataset

---
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/Repositories/IFileFieldRepository.cs
@@ -0,0 +1,9 @@
+﻿using Relativity.Services.FileField.Models;
+
+namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.Repositories
+{
+	public interface IFileFieldRepository
+	{
+		DynamicFileResponse[] GetFilesForDynamicObjectsAsync(int workspaceID, int fileFieldArtifactID, int[] objectIDs);
+	}
+}
---
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/Repositories/Implementations/FileFieldRepository.cs
@@ -0,0 +1,36 @@
+﻿using kCura.IntegrationPoints.Common.Constants;
+using kCura.IntegrationPoints.Common.Monitoring.Instrumentation;
+using Relativity.Services.FileField;
+using Relativity.Services.FileField.Models;
+
+namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.Repositories.Implementations
+{
+	class FileFieldRepository : IFileFieldRepository
+	{
+		private IFileFieldManager _fileFieldManager;
+		private IExternalServiceInstrumentationProvider _instrumentationProvider;
+
+		public FileFieldRepository(IFileFieldManager fileFieldManager, IExternalServiceInstrumentationProvider instrumentationProvider)
+		{
+			_fileFieldManager = fileFieldManager;
+			_instrumentationProvider = instrumentationProvider;
+		}
+
+		public DynamicFileResponse[] GetFilesForDynamicObjectsAsync(int workspaceID, int fileFieldArtifactID, int[] objectIDs)
+		{
+			IExternalServiceSimpleInstrumentation instrumentation =
+				CreateInstrumentation(nameof(IFileFieldRepository.GetFilesForDynamicObjectsAsync));
+
+			return instrumentation.Execute(() =>
+				_fileFieldManager.GetFilesForDynamicObjectsAsync(workspaceID, fileFieldArtifactID, objectIDs)
+					.GetAwaiter().GetResult());
+		}
+		private IExternalServiceSimpleInstrumentation CreateInstrumentation(string methodName)
+		{
+			return _instrumentationProvider.CreateSimple(
+				ExternalServiceTypes.KEPLER,
+				nameof(IFileFieldManager),
+				methodName);
+		}
+	}
+}
