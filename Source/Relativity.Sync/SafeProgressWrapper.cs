77eded38da9efc7c20c6ba6a91beda100dee3049
Matt Shanahan <matt.shanahan@relativity.com>
2019-04-10 11:28:12 +0200

    [REL-305036] Add back ISyncJob(IProgress, CancellationToken) methods + wrap custom IProgress

---
+++ Source/Relativity.Sync/SafeProgressWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relativity.Sync
+{
+	internal sealed class SafeProgressWrapper<T> : IProgress<T>
+	{
+		private readonly IProgress<T> _progress;
+		private readonly ISyncLog _logger;
+
+		public SafeProgressWrapper(IProgress<T> progress, ISyncLog logger)
+		{
+			_progress = progress;
+			_logger = logger;
+		}
+
+		public void Report(T value)
+		{
+			try
+			{
+				_progress.Report(value);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogWarning(ex, 
+					$"Received error when invoking externally-provided implementation of {nameof(IProgress<T>.Report)} ({{type}})",
+					_progress.GetType().FullName);
+			}
+		}
+	}
+}
