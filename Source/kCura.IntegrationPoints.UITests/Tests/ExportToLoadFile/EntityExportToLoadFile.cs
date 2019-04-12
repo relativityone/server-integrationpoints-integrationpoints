06819970d77e06926254d0e2cbb4bd43fbae8847
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2019-04-12 09:04:41 +0200

    REL-306207 renamed category for tests not working on R1 environments

--- Source/kCura.IntegrationPoints.UITests/Tests/ExportToLoadFile/EntityExportToLoadFile.cs
+++ Source/kCura.IntegrationPoints.UITests/Tests/ExportToLoadFile/EntityExportToLoadFile.cs
@@ -17,7 +17,6 @@ namespace kCura.IntegrationPoints.UITests.Tests.ExportToLoadFile
 {
 	[TestFixture]
 	[Category(TestCategory.EXPORT_TO_LOAD_FILE)]
-	[Category(TestCategory.BROKEN_ON_REGRESSION_ENVIRONMENT)] // REL-294344
 	public class EntityExportToLoadFile : UiTest
 	{
 		private IntegrationPointsAction _integrationPointsAction;
