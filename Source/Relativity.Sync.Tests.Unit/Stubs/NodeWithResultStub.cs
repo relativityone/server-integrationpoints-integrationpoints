441de875a6d291e24dcc651352748276c5304634
Patryk Stepien <patryk.stepien@relativity.com>
2019-04-13 11:17:27 +0200

    Changes after review

--- Source/Relativity.Sync.Tests.Unit/Stubs/NodeWithResultStub.cs
+++ Source/Relativity.Sync.Tests.Unit/Stubs/NodeWithResultStub.cs
@@ -1,16 +1,14 @@
 ﻿using System.Threading.Tasks;
 using Banzai;
-using kCura.Relativity.Client;
 
 namespace Relativity.Sync.Tests.Unit.Stubs
 {
 	internal sealed class NodeWithResultStub : Node<SyncExecutionContext>
 	{
 		public NodeResultStatus ResultStatus { get; set; }
-		
+
 		protected override Task<NodeResultStatus> PerformExecuteAsync(IExecutionContext<SyncExecutionContext> context)
 		{
-			
 			return Task.FromResult(ResultStatus);
 		}
 	}
