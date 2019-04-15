d1763c6ca4445d457d73efefe81a5b975c7992e9
Kasper Kądzielawa <kasper.kadzielawa@relativity.com>
2019-04-15 17:21:55 +0200

    Add exception handling tests

--- Source/Relativity.Sync.Tests.Unit/Executors/TagSavedSearchFolderTests.cs
+++ Source/Relativity.Sync.Tests.Unit/Executors/TagSavedSearchFolderTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
@@ -87,6 +88,120 @@ namespace Relativity.Sync.Tests.Unit.Executors
 
 			// ASSERT
 			Assert.AreEqual(newlyCreatedSearchContainerArtifactId, folderId);
+		}
+
+		[Test]
+		public void ItShouldThrowExceptionOnNonSuccessfulQuery()
+		{
+			// ARRANGE
+			const int workspaceArtifactId = 345678;
+
+			var syncLogMock = new Mock<ISyncLog>();
+			var serviceFactoryForUser = new Mock<IDestinationServiceFactoryForUser>();
+			var searchContainerManager = new Mock<ISearchContainerManager>();
+			
+			var result = new SearchContainerQueryResultSet()
+			{
+				Success = false
+			};
+
+			searchContainerManager
+				.Setup(x => x.QueryAsync(It.Is<int>(y => y == workspaceArtifactId), It.IsAny<Services.Query>()))
+				.ReturnsAsync(result);
+
+			serviceFactoryForUser
+				.Setup(x => x.CreateProxyAsync<ISearchContainerManager>())
+				.ReturnsAsync(searchContainerManager.Object);
+
+			var tagSavedSearchFolder = new TagSavedSearchFolder(serviceFactoryForUser.Object, syncLogMock.Object);
+
+			// ACT
+			// ASSERT
+			Assert.ThrowsAsync<SyncException>(async () => await tagSavedSearchFolder.GetFolderId(workspaceArtifactId).ConfigureAwait(false));
+
+			searchContainerManager.Verify(x => x.CreateSingleAsync(It.IsAny<int>(), It.IsAny<SearchContainer>()), Times.Never);
+		}
+
+		[Test]
+		public void ItShouldThrowExceptionOnCreationFailure()
+		{
+			// ARRANGE
+			const int workspaceArtifactId = 345678;
+
+			var syncLogMock = new Mock<ISyncLog>();
+			var serviceFactoryForUser = new Mock<IDestinationServiceFactoryForUser>();
+			var searchContainerManager = new Mock<ISearchContainerManager>();
+
+			var result = new SearchContainerQueryResultSet()
+			{
+				Success = true,
+				Results = new List<Result<SearchContainer>>()
+			};
+
+			searchContainerManager
+				.Setup(x => x.QueryAsync(It.Is<int>(y => y == workspaceArtifactId), It.IsAny<Services.Query>()))
+				.ReturnsAsync(result);
+
+			searchContainerManager
+				.Setup(x => x.CreateSingleAsync(It.Is<int>(y => y == workspaceArtifactId), It.IsAny<SearchContainer>()))
+				.Throws<Exception>();
+
+			serviceFactoryForUser
+				.Setup(x => x.CreateProxyAsync<ISearchContainerManager>())
+				.ReturnsAsync(searchContainerManager.Object);
+
+			var tagSavedSearchFolder = new TagSavedSearchFolder(serviceFactoryForUser.Object, syncLogMock.Object);
+
+			// ACT
+			// ASSERT
+			Assert.ThrowsAsync<SyncException>(async () => await tagSavedSearchFolder.GetFolderId(workspaceArtifactId).ConfigureAwait(false));
+		}
+
+		[Test]
+		public void ItShouldThrowExceptionOnQueryFailure()
+		{
+			// ARRANGE
+			const int workspaceArtifactId = 345678;
+
+			var syncLogMock = new Mock<ISyncLog>();
+			var serviceFactoryForUser = new Mock<IDestinationServiceFactoryForUser>();
+			var searchContainerManager = new Mock<ISearchContainerManager>();
+
+			searchContainerManager
+				.Setup(x => x.QueryAsync(It.Is<int>(y => y == workspaceArtifactId), It.IsAny<Services.Query>()))
+				.Throws<Exception>();
+
+			serviceFactoryForUser
+				.Setup(x => x.CreateProxyAsync<ISearchContainerManager>())
+				.ReturnsAsync(searchContainerManager.Object);
+
+			var tagSavedSearchFolder = new TagSavedSearchFolder(serviceFactoryForUser.Object, syncLogMock.Object);
+
+			// ACT
+			// ASSERT
+			Assert.ThrowsAsync<Exception>(async () => await tagSavedSearchFolder.GetFolderId(workspaceArtifactId).ConfigureAwait(false));
+
+			searchContainerManager.Verify(x => x.CreateSingleAsync(It.IsAny<int>(), It.IsAny<SearchContainer>()), Times.Never);
+		}
+
+		[Test]
+		public void ItShouldThrowExceptionOnCreateProxyFailure()
+		{
+			// ARRANGE
+			const int workspaceArtifactId = 345678;
+
+			var syncLogMock = new Mock<ISyncLog>();
+			var serviceFactoryForUser = new Mock<IDestinationServiceFactoryForUser>();
+
+			serviceFactoryForUser
+				.Setup(x => x.CreateProxyAsync<ISearchContainerManager>())
+				.Throws<Exception>();
+
+			var tagSavedSearchFolder = new TagSavedSearchFolder(serviceFactoryForUser.Object, syncLogMock.Object);
+
+			// ACT
+			// ASSERT
+			Assert.ThrowsAsync<Exception>(async () => await tagSavedSearchFolder.GetFolderId(workspaceArtifactId).ConfigureAwait(false));
 		}
 	}
 }
