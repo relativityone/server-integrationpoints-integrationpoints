53f8958bd4160d114d030c4773ca3d16426006da
Patryk Stepien <patryk.stepien@relativity.com>
2019-04-19 15:15:43 +0200

    REL-310982 Validate batch size value

--- Source/Relativity.Sync/SyncJobExecutionConfiguration.cs
+++ Source/Relativity.Sync/SyncJobExecutionConfiguration.cs
@@ -1,17 +1,33 @@
-﻿namespace Relativity.Sync
+﻿using System;
+
+namespace Relativity.Sync
 {
 	/// <summary>
 	///     Represents Sync job execution configuration
 	/// </summary>
 	public sealed class SyncJobExecutionConfiguration
 	{
+		private int _batchSize = int.MaxValue;
+
 		/// <summary>
 		///     Defines size of a single batch
 		/// </summary>
 		/// <remarks>
 		///     Set to <see cref="int.MaxValue" /> to ensure only one batch
 		/// </remarks>
-		public int BatchSize { get; set; } = int.MaxValue;
+		public int BatchSize
+		{
+			get => _batchSize;
+			set
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException("Batch size must be greater than 0.");
+				}
+
+				_batchSize = value;
+			}
+		}
 
 		/// <summary>
 		///     Defines how many batches can be run in parallel
