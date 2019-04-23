e580d3c3b9e5557747b52c6ebab32efb538aaa88
Bartłomiej Wesołowski <bartlomiej.wesolowski@relativity.com>
2019-04-23 15:11:51 +0200

    REL-307987 - Substuted TestDBContext for mock

---
+++ Source/kCura.IntegrationPoint.Tests.Core/DBContextMockBuilder.cs
@@ -0,0 +1,18 @@
+﻿using NSubstitute;
+using Relativity.API;
+
+namespace kCura.IntegrationPoint.Tests.Core
+{
+	public class DBContextMockBuilder
+	{
+		public static IDBContext GetMockContext(kCura.Data.RowDataGateway.BaseContext baseContext)
+		{
+			IDBContext context = Substitute.For<IDBContext>();
+			context.ExecuteNonQuerySQLStatement(Arg.Any<string>())
+				.Returns(x => baseContext.ExecuteNonQuerySQLStatement(x.Arg<string>()));
+
+
+			return context;
+		}
+	}
+}
