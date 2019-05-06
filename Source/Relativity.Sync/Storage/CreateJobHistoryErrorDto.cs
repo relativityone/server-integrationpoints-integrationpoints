3f0ace4a5535d12de8c9834403191017d4df5729
Billy Bafia <billy.bafia@relativity.com>
2019-05-06 13:09:04 +0200

    [REL-314630] Adding setters and constructor for job history create dto.

--- Source/Relativity.Sync/Storage/CreateJobHistoryErrorDto.cs
+++ Source/Relativity.Sync/Storage/CreateJobHistoryErrorDto.cs
@@ -2,10 +2,16 @@
 {
 	internal class CreateJobHistoryErrorDto
 	{
-		public string ErrorMessage { get; }
+		public string ErrorMessage { get; set; }
 		public ErrorType ErrorType { get; }
 		public int JobHistoryArtifactId { get; }
-		public string SourceUniqueId { get; }
-		public string StackTrace { get; }
+		public string SourceUniqueId { get; set; }
+		public string StackTrace { get; set; }
+
+		public CreateJobHistoryErrorDto(int jobHistoryArtifactId, ErrorType errorType)
+		{
+			JobHistoryArtifactId = jobHistoryArtifactId;
+			ErrorType = errorType;
+		}
 	}
 }
