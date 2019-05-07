f193dae0149d93f4fbbf6bb8b08680f1081f2302
Kasper Kądzielawa <kasper.kadzielawa@relativity.com>
2019-05-07 14:30:07 +0200

    Remove redundant using

--- Source/Relativity.Sync/Telemetry/Metric.cs
+++ Source/Relativity.Sync/Telemetry/Metric.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using kCura.WinEDDS.Core.Export.VolumeManagerV2.Metadata.Text;
 
 namespace Relativity.Sync.Telemetry
 {
