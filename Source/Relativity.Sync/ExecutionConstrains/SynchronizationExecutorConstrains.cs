c25abed7b3246f77a63983ee750bf247f5f1945f
Billy Bafia <billy.bafia@relativity.com>
2019-05-08 13:20:34 +0200

    [REL-318903] Creating skeleton classes for pushing documents executors.

---
+++ Source/Relativity.Sync/ExecutionConstrains/SynchronizationExecutorConstrains.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Relativity.Sync.Configuration;
+
+namespace Relativity.Sync.ExecutionConstrains
+{
+	internal sealed class SynchronizationExecutorConstrains : IExecutionConstrains<ISynchronizationConfiguration>
+	{
+		public async Task<bool> CanExecuteAsync(ISynchronizationConfiguration configuration, CancellationToken token)
+		{
+			await Task.Yield();
+			return true;
+		}
+	}
+}
