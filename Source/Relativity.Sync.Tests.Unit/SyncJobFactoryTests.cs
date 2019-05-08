d15e343f061b56cf71c592d512dafffc29e52391
Patryk Stepien <patryk.stepien@relativity.com>
2019-05-08 11:42:11 +0200

    REL-303912 Fix UT

--- Source/Relativity.Sync.Tests.Unit/SyncJobFactoryTests.cs
+++ Source/Relativity.Sync.Tests.Unit/SyncJobFactoryTests.cs
@@ -25,7 +25,7 @@ namespace Relativity.Sync.Tests.Unit
 			_container = new Mock<IContainer>();
 
 			_syncJobParameters = new SyncJobParameters(1, 1);
-			_relativityServices = new RelativityServices(Mock.Of<IAPM>(), Mock.Of<IServicesMgr>(), new Uri("localhost"));
+			_relativityServices = new RelativityServices(Mock.Of<IAPM>(), Mock.Of<IServicesMgr>(), new Uri("http://localhost", UriKind.RelativeOrAbsolute));
 			_configuration = new SyncJobExecutionConfiguration();
 			_logger = new EmptyLogger();
 			_instance = new SyncJobFactory(new Mock<IContainerFactory>().Object);
