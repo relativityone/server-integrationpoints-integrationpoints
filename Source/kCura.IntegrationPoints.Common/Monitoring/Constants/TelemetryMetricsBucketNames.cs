8ad7c217d019da069d018b67f69b0dfabbfdf231
Bartłomiej Wesołowski <bartlomiej.wesolowski@relativity.com>
2019-05-14 16:11:02 +0200

    REL-301222 - removed currently unused properties

--- Source/kCura.IntegrationPoints.Common/Monitoring/Constants/TelemetryMetricsBucketNames.cs
+++ Source/kCura.IntegrationPoints.Common/Monitoring/Constants/TelemetryMetricsBucketNames.cs
@@ -3,7 +3,5 @@
 	public class TelemetryMetricsBucketNames
 	{
 		public const string BUCKET_SYNC_SOURCE_DOCUMENTS_TAGGING_DURATION = "IntegrationPoints.Sync.SourceDocumentsTagging.Duration";
-		public const string BUCKET_SYNC_SOURCE_DOCUMENTS_TAGGING_RETRIEVE_IDS_DURATION = "IntegrationPoints.Sync.SourceDocumentsTagging.RetrieveIds.Duration";
-		public const string BUCKET_SYNC_SOURCE_DOCUMENTS_TAGGING_MASS_UPDATE_DURATION = "IntegrationPoints.Sync.SourceDocumentsTagging.MassUpdate.Duration";
 	}
 }
