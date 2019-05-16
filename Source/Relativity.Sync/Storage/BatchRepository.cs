ea1dd6bc003eaac5b1faba3b59df0500c61c4e19
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-05-16 08:30:00 +0200

    REL-318903 Fix after merge conflicts

---
+++ Source/Relativity.Sync/Storage/BatchRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Relativity.Sync.KeplerFactory;
+
+namespace Relativity.Sync.Storage
+{
+	internal sealed class BatchRepository : IBatchRepository
+	{
+		private readonly ISourceServiceFactoryForAdmin _serviceFactory;
+
+		public BatchRepository(ISourceServiceFactoryForAdmin serviceFactory)
+		{
+			_serviceFactory = serviceFactory;
+		}
+
+		public async Task<IBatch> CreateAsync(int workspaceArtifactId, int syncConfigurationArtifactId, int totalItemsCount, int startingIndex)
+		{
+			return await Batch.CreateAsync(_serviceFactory, workspaceArtifactId, syncConfigurationArtifactId, totalItemsCount, startingIndex).ConfigureAwait(false);
+		}
+
+		public async Task<IBatch> GetAsync(int workspaceArtifactId, int artifactId)
+		{
+			return await Batch.GetAsync(_serviceFactory, workspaceArtifactId, artifactId).ConfigureAwait(false);
+		}
+
+		public async Task<IBatch> GetLastAsync(int workspaceArtifactId, int syncConfigurationId)
+		{
+			return await Batch.GetLastAsync(_serviceFactory, workspaceArtifactId, syncConfigurationId).ConfigureAwait(false);
+		}
+
+		public async Task<IEnumerable<int>> GetAllNewBatchesIdsAsync(int workspaceArtifactId, int syncConfigurationId)
+		{
+			return await Batch.GetAllNewBatchIdsAsync(_serviceFactory, workspaceArtifactId, syncConfigurationId).ConfigureAwait(false);
+		}
+	}
+}
