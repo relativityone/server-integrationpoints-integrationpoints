6a85ecd65179c0a95f9e7976c605a74dfe5740f3
Kasper Kądzielawa <kasper.kadzielawa@relativity.com>
2019-05-20 10:56:02 +0200

    Add some tests

--- Source/Relativity.Sync.Tests.Unit/Telemetry/TelemetryManagerTests.cs
+++ Source/Relativity.Sync.Tests.Unit/Telemetry/TelemetryManagerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
 using Relativity.API;
@@ -29,6 +30,52 @@ namespace Relativity.Sync.Tests.Unit.Telemetry
 
 			// ASSERT
 			_logger.Verify(x => x.LogDebug(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
+		}
+
+		[Test]
+		public void ItShouldNotAddNullMetricsProvider()
+		{
+			// ARRANGE
+			var metricsCollectionManager = new Mock<IInternalMetricsCollectionManager>();
+			var categoryTargetList = new List<CategoryTarget> { new CategoryTarget { Category = new CategoryRef { Name = TelemetryConstants.TELEMETRY_CATEGORY } } };
+
+			metricsCollectionManager.Setup(x => x.GetCategoryTargetsAsync())
+				.ReturnsAsync(categoryTargetList);
+
+			_servicesManager.Setup(x => x.CreateProxy<IInternalMetricsCollectionManager>(It.IsAny<ExecutionIdentity>()))
+				.Returns(metricsCollectionManager.Object);
+
+			// ACT
+			_telemetryManager.AddMetricProviders(null);
+			_telemetryManager.InstallMetrics();
+
+			// ASSERT
+			_logger.Verify(x => x.LogError(It.IsAny<NullReferenceException>(), It.IsAny<string>()), Times.Never);
+		}
+
+		[Test]
+		public void ItShouldCreateAndEnableCategoryIfItIsNotRegistered()
+		{
+			// ARRANGE
+			var metricsCollectionManager = new Mock<IInternalMetricsCollectionManager>();
+			var categoryTargetList = new List<CategoryTarget>();
+
+			metricsCollectionManager.Setup(x => x.GetCategoryTargetsAsync())
+				.ReturnsAsync(categoryTargetList);
+
+			metricsCollectionManager.Setup(x => x.CreateCategoryAsync(It.IsAny<Category>(), It.IsAny<bool>()))
+				.ReturnsAsync(0);
+
+			_servicesManager.Setup(x => x.CreateProxy<IInternalMetricsCollectionManager>(It.IsAny<ExecutionIdentity>()))
+				.Returns(metricsCollectionManager.Object);
+
+			// ACT
+			_telemetryManager.InstallMetrics();
+
+			// ASSERT
+			metricsCollectionManager.Verify(x => x.GetCategoryTargetsAsync(), Times.Once);
+			metricsCollectionManager.Verify(x => x.CreateCategoryAsync(It.IsAny<Category>(), It.IsAny<bool>()), Times.Once);
+			metricsCollectionManager.Verify(x => x.UpdateCategoryTargetSingleAsync(It.IsAny<CategoryTarget>()), Times.Once);
 		}
 
 		[Test]
