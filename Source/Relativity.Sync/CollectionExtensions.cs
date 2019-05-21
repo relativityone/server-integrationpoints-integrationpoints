03d45d61183e87666ba6702b4a287840afebbd65
Matt Shanahan <matt.shanahan@relativity.com>
2019-05-21 17:34:32 +0200

    [REL-294358] Fill out typeparam info in SplitList doc comment

--- Source/Relativity.Sync/CollectionExtensions.cs
+++ Source/Relativity.Sync/CollectionExtensions.cs
@@ -10,7 +10,7 @@ namespace Relativity.Sync
 		/// Partitions an <see cref="IEnumerable{T}"/> into a series of <see cref="IList{T}"/> of
 		/// at most <paramref name="batchSize"/> elements.
 		/// </summary>
-		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="T">Type of elements in the source</typeparam>
 		/// <param name="fullCollection">Collection to partition</param>
 		/// <param name="batchSize">Maximum size of each partition</param>
 		/// <returns>A sequence of partitions, each of which will contains <paramref name="batchSize"/>
