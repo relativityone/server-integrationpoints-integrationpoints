8a84286a938d4f6ba8b48ffbb498c1bdc590c380
Bartłomiej Wesołowski <bartlomiej.wesolowski@relativity.com>
2019-05-23 09:29:03 +0200

    REL-320600 - Added RemoveErrorDocuments unit test

--- Source/kCura.IntegrationPoints.Data.Tests/Repositories/Implementations/ScratchTableRepositoryTests.cs
+++ Source/kCura.IntegrationPoints.Data.Tests/Repositories/Implementations/ScratchTableRepositoryTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using kCura.IntegrationPoints.Data.Repositories;
 using kCura.IntegrationPoints.Data.Repositories.Implementations;
@@ -10,34 +12,36 @@ namespace kCura.IntegrationPoints.Data.Tests.Repositories.Implementations
 	[TestFixture]
 	public class ScratchTableRepositoryTests
 	{
-		private Mock<IDocumentRepository> _documentsRepositoryMock;
-		private Mock<IFieldQueryRepository> _fileRepositoryMock;
+		private Mock<IDocumentRepository> _documentRepositoryMock;
+		private Mock<IFieldQueryRepository> _fieldRepositoryMock;
 		private Mock<IResourceDbProvider> _resourceDbProviderMock;
 		private Mock<IWorkspaceDBContext> _workspaceDbContextMock;
 
 		private ScratchTableRepository _sut;
-		private string _SCHEMALESS_RESOURCE_DATABASE_PREPEND;
-		private string _TABLE_NAME;
+		private string _schemalessResourceDatabasePrepend;
+		private string _tableName;
 		private const int _SOURCE_WORKSPACE_ARTIFACT_ID = 1234321;
 		private const string _PREFIX = "prefix";
 		private const string _SUFFIX = "_suffix";
 		private const string _RESOURCE_DB_PREPEND = "[Resource]";
+		private const string _DOCUMENT_ARTIFACT_ID_COLUMN_NAME = "ArtifactID";
+
 		[SetUp]
 		public void SetUp()
 		{
-			_TABLE_NAME = $"{_PREFIX}_{_SUFFIX}";
-			_SCHEMALESS_RESOURCE_DATABASE_PREPEND = $"EDDS{_SOURCE_WORKSPACE_ARTIFACT_ID}";
+			_tableName = $"{_PREFIX}_{_SUFFIX}";
+			_schemalessResourceDatabasePrepend = $"EDDS{_SOURCE_WORKSPACE_ARTIFACT_ID}";
 			_workspaceDbContextMock = new Mock<IWorkspaceDBContext>();
-			_documentsRepositoryMock = new Mock<IDocumentRepository>();
-			_fileRepositoryMock = new Mock<IFieldQueryRepository>();
+			_documentRepositoryMock = new Mock<IDocumentRepository>();
+			_fieldRepositoryMock = new Mock<IFieldQueryRepository>();
 			_resourceDbProviderMock = new Mock<IResourceDbProvider>();
 			_resourceDbProviderMock.Setup(x => x.GetSchemalessResourceDataBasePrepend(It.IsAny<int>())).Returns($"EDDS{_SOURCE_WORKSPACE_ARTIFACT_ID}");
 			_resourceDbProviderMock.Setup(x => x.GetResourceDbPrepend(It.IsAny<int>())).Returns("[Resource]");
 
 			_sut = new ScratchTableRepository(
 				_workspaceDbContextMock.Object,
-				_documentsRepositoryMock.Object,
-				_fileRepositoryMock.Object,
+				_documentRepositoryMock.Object,
+				_fieldRepositoryMock.Object,
 				_resourceDbProviderMock.Object,
 				_PREFIX,
 				_SUFFIX,
@@ -46,43 +50,43 @@ namespace kCura.IntegrationPoints.Data.Tests.Repositories.Implementations
 		}
 
 		[Test]
-		public void DeleteTable_WorkspaceScratchTable()
+		public void DeleteTable_ShouldExecuteProperSqlQuery()
 		{
-			// arrange
+			//ARRANGE
 			string expectedQuery =
 				$@"
-			IF EXISTS (SELECT * FROM EDDS{_SOURCE_WORKSPACE_ARTIFACT_ID}.INFORMATION_SCHEMA.TABLES where TABLE_NAME = '{_TABLE_NAME}') 
-			DROP TABLE [Resource].[{_TABLE_NAME}]
+			IF EXISTS (SELECT * FROM EDDS{_SOURCE_WORKSPACE_ARTIFACT_ID}.INFORMATION_SCHEMA.TABLES where TABLE_NAME = '{_tableName}') 
+			DROP TABLE [Resource].[{_tableName}]
 			";
 
-			// act
+			//ACT
 			_sut.DeleteTable();
 
-			// assert
+			//ASSERT
 			_workspaceDbContextMock.Verify(x => x.ExecuteNonQuerySQLStatement(expectedQuery), Times.Once);
 		}
 
 		[Test]
-		public void GetDocumentIdsDataReaderFromTable_WorkspaceScratchTable()
+		public void GetDocumentIDsDataReaderFromTable_ShouldExecuteProperSqlQuery()
 		{
-			// arrange
+			//ARRANGE
 			string expectedQuery =
 				$@"
-			IF EXISTS (SELECT * FROM {_SCHEMALESS_RESOURCE_DATABASE_PREPEND}.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{_TABLE_NAME}') 
-			SELECT [ArtifactID] FROM {_RESOURCE_DB_PREPEND}.[{_TABLE_NAME}]
+			IF EXISTS (SELECT * FROM {_schemalessResourceDatabasePrepend}.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{_tableName}') 
+			SELECT [ArtifactID] FROM {_RESOURCE_DB_PREPEND}.[{_tableName}]
 			";
 
-			// act
+			//ACT
 			_sut.GetDocumentIDsDataReaderFromTable();
 
-			// assert
+			//ASSERT
 			_workspaceDbContextMock.Verify(x => x.ExecuteSQLStatementAsReader(expectedQuery), Times.Once);
 		}
 
 		[Test]
 		public void ReadDocumentIDs_ShouldExecuteProperSqlQuery()
 		{
-			// arrange
+			//ARRANGE
 			Mock<IDataReader> dataReaderMock = new Mock<IDataReader>();
 			_workspaceDbContextMock.Setup(x =>
 					x.ExecuteSQLStatementAsReader(It.IsAny<string>()))
@@ -92,15 +96,39 @@ namespace kCura.IntegrationPoints.Data.Tests.Repositories.Implementations
 			int size = 5;
 			string expectedQuery =
 				$@"
-			IF EXISTS (SELECT * FROM {_SCHEMALESS_RESOURCE_DATABASE_PREPEND}.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{_TABLE_NAME}') 
-			SELECT [ArtifactID] FROM {_RESOURCE_DB_PREPEND}.[{_TABLE_NAME}]
+			IF EXISTS (SELECT * FROM {_schemalessResourceDatabasePrepend}.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{_tableName}') 
+			SELECT [ArtifactID] FROM {_RESOURCE_DB_PREPEND}.[{_tableName}]
 			ORDER BY [ArtifactID] OFFSET {offset} ROWS FETCH NEXT {size} ROWS ONLY";
 
-			// act
+			//ACT
 			_sut.ReadDocumentIDs(offset, size).ToList();
 
-			// assert
+			//ASSERT
 			_workspaceDbContextMock.Verify(x => x.ExecuteSQLStatementAsReader(expectedQuery), Times.Once);
+		}
+
+		[Test]
+		public void RemoveErrorDocuments_ShouldExecuteProperSqlQuery()
+		{
+			//ARRANGE
+			ICollection<string> documentControlNumbers = new[] { "CN1", "CN2", "CN3" };
+			ICollection<int> documentArtifactIDs = new[] { 1, 2, 3 };
+			string documentArtifactIDsAsString = "(" + String.Join(",", documentArtifactIDs) + ")";
+
+			_documentRepositoryMock.Setup(x =>
+				x.RetrieveDocumentsAsync(
+					It.IsAny<string>(),
+					It.IsAny<ICollection<string>>()
+				)
+			).ReturnsAsync(documentArtifactIDs.ToArray());
+
+			string expectedQuery = $@"DELETE FROM {_RESOURCE_DB_PREPEND}.[{_tableName}] WHERE [{_DOCUMENT_ARTIFACT_ID_COLUMN_NAME}] in {documentArtifactIDsAsString}";
+
+			//ACT
+			_sut.RemoveErrorDocuments(documentControlNumbers);
+
+			//ASSERT
+			_workspaceDbContextMock.Verify(x => x.ExecuteNonQuerySQLStatement(expectedQuery), Times.Once);
 		}
 	}
 }
