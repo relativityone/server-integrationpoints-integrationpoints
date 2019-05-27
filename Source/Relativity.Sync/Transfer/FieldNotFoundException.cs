452d417d4ea9a5f9ce5db500b9b12c22e43e96ce
Matt Shanahan <matt.shanahan@relativity.com>
2019-05-27 10:46:43 +0200

    [REL-322897] Update comments on FieldNotFoundException + add serialization tests for it

--- Source/Relativity.Sync/Transfer/FieldNotFoundException.cs
+++ Source/Relativity.Sync/Transfer/FieldNotFoundException.cs
@@ -2,32 +2,30 @@
 
 namespace Relativity.Sync.Transfer
 {
-	/// <inheritdoc />
+	/// <summary>
+	/// Exception thrown by <see cref="IDocumentFieldRepository"/> when a given field is not found in a workspace.
+	/// </summary>
 	[Serializable]
 	public sealed class FieldNotFoundException : Exception
 	{
 		/// <inheritdoc />
 		public FieldNotFoundException() : base()
 		{
-			//...
 		}
 
 		/// <inheritdoc />
 		public FieldNotFoundException(string message) : base(message)
 		{
-			//...
 		}
 
 		/// <inheritdoc />
 		public FieldNotFoundException(string message, Exception innerException) : base(message, innerException)
 		{
-			//...
 		}
 
 		/// <inheritdoc />
 		private FieldNotFoundException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context)
 		{
-			//...
 		}
 	}
 }
