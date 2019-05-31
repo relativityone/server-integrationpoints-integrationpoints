eff66b3e19fb2fe1607a0185d7cc1ef123d19c3e
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-05-31 11:16:39 +0200

    REL-325028 Add more UTs

--- Source/Relativity.Sync.Tests.Unit/SourceWorkspaceTagsCreationConfigurationTests.cs
+++ Source/Relativity.Sync.Tests.Unit/SourceWorkspaceTagsCreationConfigurationTests.cs
@@ -11,22 +11,19 @@ using IConfiguration = Relativity.Sync.Storage.IConfiguration;
 namespace Relativity.Sync.Tests.Unit
 {
 	[TestFixture]
-	internal sealed class DestinationWorkspaceTagsCreationConfigurationTests
+	internal sealed class SourceWorkspaceTagsCreationConfigurationTests
 	{
 		private Mock<IConfiguration> _cache;
 		private ImportSettingsDto _importSettings;
 		private SyncJobParameters _syncJobParameters;
-		private DestinationWorkspaceTagsCreationConfiguration _config;
+		private SourceWorkspaceTagsCreationConfiguration _config;
 
 		private const int _JOB_ID = 1;
 		private const int _SOURCE_WORKSPACE_ARTIFACT_ID = 2;
 
 		private static readonly Guid JobHistoryGuid = new Guid("5D8F7F01-25CF-4246-B2E2-C05882539BB2");
 		private static readonly Guid DestinationWorkspaceArtifactIdGuid = new Guid("15B88438-6CF7-47AB-B630-424633159C69");
-		private static readonly Guid SourceJobTagArtifactIdGuid = new Guid("C0A63A29-ABAE-4BF4-A3F4-59E5BD87A33E");
-		private static readonly Guid SourceJobTagNameGuid = new Guid("DA0E1931-9460-4A61-9033-A8035697C1A4");
-		private static readonly Guid SourceWorkspaceTagArtifactIdGuid = new Guid("FEAB129B-AEEF-4AA4-BC91-9EAE9A4C35F6");
-		private static readonly Guid SourceWorkspaceTagNameGuid = new Guid("D828B69E-AAAE-4639-91E2-416E35C163B1");
+		private static readonly Guid DestinationWorkspaceTagArtifactIdGuid = new Guid("E2100C10-B53B-43FA-BB1B-51E43DCE8208");
 
 		[SetUp]
 		public void SetUp()
@@ -34,7 +31,7 @@ namespace Relativity.Sync.Tests.Unit
 			_cache = new Mock<IConfiguration>();
 			_importSettings = new ImportSettingsDto();
 			_syncJobParameters = new SyncJobParameters(_JOB_ID, _SOURCE_WORKSPACE_ARTIFACT_ID, _importSettings);
-			_config = new DestinationWorkspaceTagsCreationConfiguration(_cache.Object, _syncJobParameters);
+			_config = new SourceWorkspaceTagsCreationConfiguration(_cache.Object, _syncJobParameters);
 		}
 
 		[Test]
@@ -77,30 +74,14 @@ namespace Relativity.Sync.Tests.Unit
 		public async Task ItShouldSetSourceJobTag()
 		{
 			const int artifactId = 5;
-			const string name = "source job";
 
 			// act
-			await _config.SetSourceJobTagAsync(artifactId, name).ConfigureAwait(false);
+			await _config.SetDestinationWorkspaceTagArtifactIdAsync(artifactId).ConfigureAwait(false);
 
 			// assert
-			_cache.Verify(x => x.UpdateFieldValueAsync(SourceJobTagArtifactIdGuid, artifactId));
-			_cache.Verify(x => x.UpdateFieldValueAsync(SourceJobTagNameGuid, name));
-			_config.IsSourceJobTagSet.Should().BeTrue();
+			_cache.Verify(x => x.UpdateFieldValueAsync(DestinationWorkspaceTagArtifactIdGuid, artifactId));
+			_config.IsDestinationWorkspaceTagArtifactIdSet.Should().BeTrue();
 		}
 
-		[Test]
-		public async Task ItShouldSetSourceWorkspaceTag()
-		{
-			const int artifactId = 6;
-			const string name = "source workspace";
-
-			// act
-			await _config.SetSourceWorkspaceTagAsync(artifactId, name).ConfigureAwait(false);
-
-			// assert
-			_cache.Verify(x => x.UpdateFieldValueAsync(SourceWorkspaceTagArtifactIdGuid, artifactId));
-			_cache.Verify(x => x.UpdateFieldValueAsync(SourceWorkspaceTagNameGuid, name));
-			_config.IsSourceWorkspaceTagSet.Should().BeTrue();
-		}
 	}
 }
---
+++ Source/Relativity.Sync.Tests.Unit/Storage/JobHistoryErrorRepositoryTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Relativity.Services.Objects;
+using Relativity.Services.Objects.DataContracts;
+using Relativity.Sync.KeplerFactory;
+using Relativity.Sync.Storage;
+
+namespace Relativity.Sync.Tests.Unit.Storage
+{
+	[TestFixture]
+	internal sealed class JobHistoryErrorRepositoryTests
+	{
+		private Mock<ISourceServiceFactoryForAdmin> _serviceFactory;
+		private JobHistoryErrorRepository _jobHistoryErrorRepository;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_serviceFactory = new Mock<ISourceServiceFactoryForAdmin>();
+			_jobHistoryErrorRepository = new JobHistoryErrorRepository(_serviceFactory.Object);
+		}
+
+		[Test]
+		public async Task ItShouldCreateJobHistoryError()
+		{
+			const int workspaceArtifactId = 1;
+			const int jobHistoryArtifactId = 2;
+			ErrorType errorType = ErrorType.Item;
+			const string stackTrace = "Stack trace";
+			const string sourceUniqueId = "src unique id";
+			const string errorMessage = "Some message.";
+			CreateJobHistoryErrorDto createJobHistoryErrorDto = new CreateJobHistoryErrorDto(jobHistoryArtifactId, errorType)
+			{
+				ErrorMessage = errorMessage,
+				SourceUniqueId = sourceUniqueId,
+				StackTrace = stackTrace
+			};
+			Mock<IObjectManager> objectManager = new Mock<IObjectManager>();
+			CreateResult createResult = new CreateResult()
+			{
+				Object = new RelativityObject()
+			};
+			objectManager.Setup(x => x.CreateAsync(It.IsAny<int>(), It.IsAny<CreateRequest>())).ReturnsAsync(createResult);
+			_serviceFactory.Setup(x => x.CreateProxyAsync<IObjectManager>()).ReturnsAsync(objectManager.Object);
+
+			// act
+			IJobHistoryError jobHistoryError = await _jobHistoryErrorRepository.CreateAsync(workspaceArtifactId, createJobHistoryErrorDto).ConfigureAwait(false);
+
+			// assert
+			jobHistoryError.ErrorMessage.Should().Be(errorMessage);
+			jobHistoryError.SourceUniqueId.Should().Be(sourceUniqueId);
+			jobHistoryError.StackTrace.Should().Be(stackTrace);
+			jobHistoryError.ErrorType.Should().Be(errorType);
+			jobHistoryError.ErrorStatus.Should().Be(ErrorStatus.New);
+		}
+	}
+}
