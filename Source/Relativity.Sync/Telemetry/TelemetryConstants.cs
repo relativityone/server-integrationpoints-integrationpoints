b707f1e1ff8ea02b48039ef9f7bd41fd4e33c858
Kasper Kądzielawa <kasper.kadzielawa@relativity.com>
2019-06-13 11:30:04 +0200

    Introduce inner static class for metric identifiers

--- Source/Relativity.Sync/Telemetry/TelemetryConstants.cs
+++ Source/Relativity.Sync/Telemetry/TelemetryConstants.cs
@@ -8,14 +8,16 @@
 
 		public const string PROVIDER_NAME = "Sync";
 
-		public static readonly string JOB_START_TYPE = $"{_METRIC_PREPEND}.Job.Start.Type";
+		public static class MetricIdentifiers
+		{
+			public static readonly string JOB_START_TYPE = $"{_METRIC_PREPEND}.Job.Start.Type";
+			public static readonly string JOB_END_STATUS = $"{_METRIC_PREPEND}.Job.End.Status";
 
-		public static readonly string JOB_END_STATUS = $"{_METRIC_PREPEND}.Job.End.Status";
-
-		public static readonly string DATA_RECORDS_TRANSFERRED = $"{_METRIC_PREPEND}.Data.Records.Transferred";
-		public static readonly string DATA_RECORDS_FAILED = $"{_METRIC_PREPEND}.Data.Records.Failed";
-		public static readonly string DATA_RECORDS_TOTAL_REQUESTED = $"{_METRIC_PREPEND}.Data.Records.TotalRequested";
-		public static readonly string DATA_FIELDS_MAPPED = $"{_METRIC_PREPEND}.Data.Fields.Mapped";
-		public static readonly string DATA_FILES_SIZE = $"{_METRIC_PREPEND}.Data.Files.Size";
+			public static readonly string DATA_RECORDS_TRANSFERRED = $"{_METRIC_PREPEND}.Data.Records.Transferred";
+			public static readonly string DATA_RECORDS_FAILED = $"{_METRIC_PREPEND}.Data.Records.Failed";
+			public static readonly string DATA_RECORDS_TOTAL_REQUESTED = $"{_METRIC_PREPEND}.Data.Records.TotalRequested";
+			public static readonly string DATA_FIELDS_MAPPED = $"{_METRIC_PREPEND}.Data.Fields.Mapped";
+			public static readonly string DATA_FILES_SIZE = $"{_METRIC_PREPEND}.Data.Files.Size";
+		}
 	}
 }
