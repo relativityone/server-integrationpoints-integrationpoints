a84d675704e8c9a25221fe60a01498c27662490e
Kasper Kądzielawa <kasper.kadzielawa@relativity.com>
2019-06-13 13:33:22 +0200

    Remove DATA_FILES_SIZE from TelemetryConstants

--- Source/Relativity.Sync/Telemetry/TelemetryConstants.cs
+++ Source/Relativity.Sync/Telemetry/TelemetryConstants.cs
@@ -17,7 +17,6 @@
 			public static readonly string DATA_RECORDS_FAILED = $"{_METRIC_PREPEND}.Data.Records.Failed";
 			public static readonly string DATA_RECORDS_TOTAL_REQUESTED = $"{_METRIC_PREPEND}.Data.Records.TotalRequested";
 			public static readonly string DATA_FIELDS_MAPPED = $"{_METRIC_PREPEND}.Data.Fields.Mapped";
-			public static readonly string DATA_FILES_SIZE = $"{_METRIC_PREPEND}.Data.Files.Size";
 		}
 	}
 }
