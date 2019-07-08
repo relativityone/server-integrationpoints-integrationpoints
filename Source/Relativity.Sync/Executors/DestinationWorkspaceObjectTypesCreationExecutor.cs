bb75cc10b1c2e9b83b8270b2008ab196e4534d84
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-07-08 15:47:40 +0200

    REL-289749 Implement creation of Relativity Source Job object type and fields

--- Source/Relativity.Sync/Executors/DestinationWorkspaceObjectTypesCreationExecutor.cs
+++ Source/Relativity.Sync/Executors/DestinationWorkspaceObjectTypesCreationExecutor.cs
@@ -16,17 +16,29 @@ namespace Relativity.Sync.Executors
 	internal sealed class DestinationWorkspaceObjectTypesCreationExecutor : IExecutor<IDestinationWorkspaceObjectTypesCreationConfiguration>
 	{
 		private const string _SOURCE_WORKSPACE_OBJECT_TYPE_NAME = "Relativity Source Case";
-		private const string _SOURCEWORKSPACE_CASEID_FIELD_NAME = "Source Workspace Artifact ID";
-		private const string _SOURCEWORKSPACE_CASENAME_FIELD_NAME = "Source Workspace Name";
-		private const string _SOURCEWORKSPACE_INSTANCENAME_FIELD_NAME = "Source Instance Name";
+		private const string _SOURCE_WORKSPACE_CASEID_FIELD_NAME = "Source Workspace Artifact ID";
+		private const string _SOURCE_WORKSPACE_CASENAME_FIELD_NAME = "Source Workspace Name";
+		private const string _SOURCE_WORKSPACE_INSTANCENAME_FIELD_NAME = "Source Instance Name";
+
+		private const string _SOURCE_JOB_OBJECT_TYPE_NAME = "Relativity Source Job";
+		private const string _SOURCEJOB_JOBHISTORYID_FIELD_NAME = "Job History Artifact ID";
+		private const string _SOURCEJOB_JOBHISTORYNAME_FIELD_NAME = "Job History Name";
+
 		private readonly ISyncObjectTypeManager _syncObjectTypeManager;
 		private readonly ISyncFieldManager _syncFieldManager;
 		private readonly ISyncLog _logger;
+
 		private static readonly Guid SourceWorkspaceObjectTypeGuid = new Guid("7E03308C-0B58-48CB-AFA4-BB718C3F5CAC");
+		private static readonly Guid SourceJobObjectTypeGuid = new Guid("6f4dd346-d398-4e76-8174-f0cd8236cbe7");
+
 		private static readonly Guid CaseIdFieldNameGuid = new Guid("90c3472c-3592-4c5a-af01-51e23e7f89a5");
 		private static readonly Guid CaseNameFieldNameGuid = new Guid("a16f7beb-b3b0-4658-bb52-1c801ba920f0");
 		private static readonly Guid InstanceNameFieldGuid = new Guid("C5212F20-BEC4-426C-AD5C-8EBE2697CB19");
 		private static readonly Guid SourceWorkspaceFieldOnDocumentGuid = new Guid("2fa844e3-44f0-47f9-abb7-d6d8be0c9b8f");
+
+		private static readonly Guid JobHistoryIdFieldGuid = new Guid("2bf54e79-7f75-4a51-a99a-e4d68f40a231");
+		private static readonly Guid JobHistoryNameFieldGuid = new Guid("0b8fcebf-4149-4f1b-a8bc-d88ff5917169");
+		private static readonly Guid JobHistoryFieldOnDocumentGuid = new Guid("7cc3faaf-cbb8-4315-a79f-3aa882f1997f");
 
 		public DestinationWorkspaceObjectTypesCreationExecutor(ISyncObjectTypeManager syncObjectTypeManager, ISyncFieldManager syncFieldManager, ISyncLog logger)
 		{
@@ -37,18 +49,27 @@ namespace Relativity.Sync.Executors
 
 		public async Task<ExecutionResult> ExecuteAsync(IDestinationWorkspaceObjectTypesCreationConfiguration configuration, CancellationToken token)
 		{
+			int destinationWorkspaceArtifactId = configuration.DestinationWorkspaceArtifactId;
 			try
 			{
-				int workspaceObjectTypeArtifactId = await GetWorkspaceObjectTypeArtifactIdAsync(configuration.DestinationWorkspaceArtifactId).ConfigureAwait(false);
-				ObjectTypeRequest objectTypeRequest = GetObjectTypeRequest(_SOURCE_WORKSPACE_OBJECT_TYPE_NAME, workspaceObjectTypeArtifactId);
-				int sourceCaseObjectTypeArtifactId = await _syncObjectTypeManager.EnsureObjectTypeExistsAsync(configuration.DestinationWorkspaceArtifactId,
-					SourceWorkspaceObjectTypeGuid, objectTypeRequest).ConfigureAwait(false);
-				await _syncFieldManager.EnsureFieldsExistAsync(configuration.DestinationWorkspaceArtifactId, GetSourceWorkspaceRdoFieldsRequests(sourceCaseObjectTypeArtifactId)).ConfigureAwait(false);
-				await _syncFieldManager.EnsureFieldsExistAsync(configuration.DestinationWorkspaceArtifactId, GetSourceWorkspaceDocumentFieldsRequests(sourceCaseObjectTypeArtifactId)).ConfigureAwait(false);
+				int workspaceObjectTypeArtifactId = await GetWorkspaceObjectTypeArtifactIdAsync(destinationWorkspaceArtifactId).ConfigureAwait(false);
+				ObjectTypeRequest sourceCaseObjectTypeRequest = GetObjectTypeRequest(_SOURCE_WORKSPACE_OBJECT_TYPE_NAME, workspaceObjectTypeArtifactId);
+				int sourceCaseObjectTypeArtifactId = await _syncObjectTypeManager.EnsureObjectTypeExistsAsync(destinationWorkspaceArtifactId,
+					SourceWorkspaceObjectTypeGuid, sourceCaseObjectTypeRequest).ConfigureAwait(false);
+				await _syncFieldManager.EnsureFieldsExistAsync(destinationWorkspaceArtifactId, GetSourceWorkspaceRdoFieldsRequests(sourceCaseObjectTypeArtifactId)).ConfigureAwait(false);
+				IDictionary<Guid, BaseFieldRequest> sourceCaseDocumentFieldRequest = GetDocumentFieldRequest(sourceCaseObjectTypeArtifactId, _SOURCE_WORKSPACE_OBJECT_TYPE_NAME, SourceWorkspaceFieldOnDocumentGuid);
+				await _syncFieldManager.EnsureFieldsExistAsync(destinationWorkspaceArtifactId, sourceCaseDocumentFieldRequest).ConfigureAwait(false);
+
+				ObjectTypeRequest sourceJobObjectTypeRequest = GetObjectTypeRequest(_SOURCE_JOB_OBJECT_TYPE_NAME, sourceCaseObjectTypeArtifactId);
+				int sourceJobObjectTypeArtifactId = await _syncObjectTypeManager
+					.EnsureObjectTypeExistsAsync(destinationWorkspaceArtifactId, SourceJobObjectTypeGuid, sourceJobObjectTypeRequest).ConfigureAwait(false);
+				await _syncFieldManager.EnsureFieldsExistAsync(destinationWorkspaceArtifactId, GetSourceJobRdoFieldsRequests(sourceJobObjectTypeArtifactId)).ConfigureAwait(false);
+				IDictionary<Guid, BaseFieldRequest> sourceJobDocumentFieldRequest = GetDocumentFieldRequest(sourceJobObjectTypeArtifactId, _SOURCE_JOB_OBJECT_TYPE_NAME, JobHistoryFieldOnDocumentGuid);
+				await _syncFieldManager.EnsureFieldsExistAsync(destinationWorkspaceArtifactId, sourceJobDocumentFieldRequest).ConfigureAwait(false);
 			}
 			catch (Exception ex)
 			{
-				_logger.LogError(ex, "Failed to create object types in destination workspace Artifact ID: {destinationWorkspaceArtifactId}", configuration.DestinationWorkspaceArtifactId);
+				_logger.LogError(ex, "Failed to create object types in destination workspace Artifact ID: {destinationWorkspaceArtifactId}", destinationWorkspaceArtifactId);
 				return ExecutionResult.Failure(ex);
 			}
 
@@ -87,7 +108,7 @@ namespace Relativity.Sync.Executors
 			};
 		}
 
-		private IDictionary<Guid, BaseFieldRequest> GetSourceWorkspaceDocumentFieldsRequests(int objectTypeArtifactTypeId)
+		private IDictionary<Guid, BaseFieldRequest> GetDocumentFieldRequest(int objectTypeArtifactId, string name, Guid guid)
 		{
 			ObjectTypeIdentifier documentObjectType = new ObjectTypeIdentifier()
 			{
@@ -96,7 +117,7 @@ namespace Relativity.Sync.Executors
 
 			ObjectTypeIdentifier associativeObjectType = new ObjectTypeIdentifier()
 			{
-				ArtifactID = objectTypeArtifactTypeId
+				ArtifactID = objectTypeArtifactId
 			};
 
 			const int width = 100;
@@ -104,10 +125,10 @@ namespace Relativity.Sync.Executors
 			return new Dictionary<Guid, BaseFieldRequest>()
 			{
 				{
-					SourceWorkspaceFieldOnDocumentGuid,
+					guid,
 					new MultipleObjectFieldRequest()
 					{
-						Name = _SOURCE_WORKSPACE_OBJECT_TYPE_NAME,
+						Name = name,
 						ObjectType = documentObjectType,
 						AssociativeObjectType = associativeObjectType,
 						AllowGroupBy = false,
@@ -134,7 +155,7 @@ namespace Relativity.Sync.Executors
 					CaseIdFieldNameGuid,
 					new WholeNumberFieldRequest()
 					{
-						Name = _SOURCEWORKSPACE_CASEID_FIELD_NAME,
+						Name = _SOURCE_WORKSPACE_CASEID_FIELD_NAME,
 						ObjectType = objectType,
 						IsRequired = true,
 						IsLinked = false,
@@ -150,7 +171,7 @@ namespace Relativity.Sync.Executors
 					CaseNameFieldNameGuid,
 					new FixedLengthFieldRequest()
 					{
-						Name = _SOURCEWORKSPACE_CASENAME_FIELD_NAME,
+						Name = _SOURCE_WORKSPACE_CASENAME_FIELD_NAME,
 						ObjectType = objectType,
 						IsRequired = true,
 						IncludeInTextIndex = false,
@@ -170,7 +191,56 @@ namespace Relativity.Sync.Executors
 					InstanceNameFieldGuid,
 					new FixedLengthFieldRequest()
 					{
-						Name = _SOURCEWORKSPACE_INSTANCENAME_FIELD_NAME,
+						Name = _SOURCE_WORKSPACE_INSTANCENAME_FIELD_NAME,
+						ObjectType = objectType,
+						IsRequired = true,
+						IncludeInTextIndex = false,
+						IsLinked = false,
+						AllowHtml = false,
+						AllowSortTally = false,
+						AllowGroupBy = false,
+						AllowPivot = false,
+						OpenToAssociations = false,
+						Width = width,
+						Wrapping = false,
+						HasUnicode = false,
+						Length = length
+					}
+				}
+			};
+		}
+
+		private IDictionary<Guid, BaseFieldRequest> GetSourceJobRdoFieldsRequests(int objectTypeArtifactId)
+		{
+			ObjectTypeIdentifier objectType = new ObjectTypeIdentifier()
+			{
+				ArtifactID = objectTypeArtifactId
+			};
+			const int width = 100;
+			const int length = 255;
+			return new Dictionary<Guid, BaseFieldRequest>()
+			{
+				{
+					JobHistoryIdFieldGuid,
+					new WholeNumberFieldRequest()
+					{
+						Name = _SOURCEJOB_JOBHISTORYID_FIELD_NAME,
+						ObjectType = objectType,
+						IsRequired = true,
+						IsLinked = false,
+						OpenToAssociations = false,
+						AllowSortTally = false,
+						AllowGroupBy = false,
+						AllowPivot = false,
+						Width = width,
+						Wrapping = false
+					}
+				},
+				{
+					JobHistoryNameFieldGuid,
+					new FixedLengthFieldRequest()
+					{
+						Name = _SOURCEJOB_JOBHISTORYNAME_FIELD_NAME,
 						ObjectType = objectType,
 						IsRequired = true,
 						IncludeInTextIndex = false,
