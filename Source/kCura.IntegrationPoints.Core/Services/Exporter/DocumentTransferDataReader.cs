6c92a32aa9974d33c2f50cdf490109cf569568fc
Damian Janas <damian.janas@relativity.com>
2019-07-08 06:14:56 -0500

    Merge pull request #1514 in IN/integrationpoints from REL-335409-fail-job-foxglove1 to release-10.2-foxglove1
    Squashed commit of the following:
    
    commit 10276bc231d64c176f9bc32aaf5c955cf280de32
    Author: Damian Janas <damian.janas@relativity.com>
    Date:   Mon Jul 8 09:51:56 2019 +0200
    
        REL-335409 review changes + unit tests
    
    commit 14126b0423a175f43efeacdcdd6f74da7b2f9279
    Author: Damian Janas <damian.janas@relativity.com>
    Date:   Fri Jul 5 16:32:55 2019 +0200
    
        REL-335409 csproj updated
    
    commit fa88086f3d75d1fb909b822a69ec9d3fb4a791a7
    Author: Damian Janas <damian.janas@relativity.com>
    Date:   Fri Jul 5 16:21:17 2019 +0200
    
        REL-335409 refactored to extension method
    
    commit 6467c1a297ef9c000eba17e9bd9ba3aec774d367
    Author: Damian Janas <damian.janas@relativity.com>
    Date:   Fri Jul 5 16:01:00 2019 +0200
    
        REL-335409 throwing exception and logging added

--- Source/kCura.IntegrationPoints.Core/Services/Exporter/DocumentTransferDataReader.cs
+++ Source/kCura.IntegrationPoints.Core/Services/Exporter/DocumentTransferDataReader.cs
@@ -2,8 +2,8 @@
 using Stream = System.IO.Stream;
 using System.Collections.Generic;
 using System.Data;
+using kCura.IntegrationPoints.Core.Extensions;
 using kCura.IntegrationPoints.Core.Services.Exporter.Base;
-using kCura.IntegrationPoints.Core.Utils;
 using kCura.IntegrationPoints.Data.Repositories;
 using kCura.IntegrationPoints.Domain.Exceptions;
 using kCura.IntegrationPoints.Domain.Models;
@@ -31,12 +31,14 @@ namespace kCura.IntegrationPoints.Core.Services.Exporter
 		private readonly IQueryFieldLookupRepository _fieldLookupRepository;
 		private readonly IAPILog _logger;
 
+		private const string _DUPLICATED_NATIVE_KEY_ERROR_MESSAGE = "Duplicated key found. Check if there is no natives duplicates for a given document";
+
 		private static readonly string _nativeDocumentArtifactIdColumn = "DocumentArtifactID";
 		private static readonly string _nativeFileNameColumn = "Filename";
 		private static readonly string _nativeFileSizeColumn = "Size";
 		private static readonly string _nativeLocationColumn = "Location";
 		private static readonly string _separator = ",";
-		
+
 		public DocumentTransferDataReader(IExporterService relativityExportService,
 			FieldMap[] fieldMappings,
 			BaseServiceContext context,
@@ -165,8 +167,8 @@ namespace kCura.IntegrationPoints.Core.Services.Exporter
 		}
 
 		private IntegrationPointsException LogGetValueError(
-			Exception e, 
-			int index, 
+			Exception e,
+			int index,
 			bool isFieldIdentifierNumeric,
 			ArtifactFieldDTO retrievedField,
 			string fieldIdentifier,
@@ -175,17 +177,17 @@ namespace kCura.IntegrationPoints.Core.Services.Exporter
 			)
 		{
 			string message = $"Error occurred when getting value for index {index}, " +
-			              $"isFieldIdentifierNumeric: {isFieldIdentifierNumeric}, " +
-			              $"retrievedField: {retrievedField}, " +
-			              $"fieldIdentifier: {fieldIdentifier}, " +
-			              $"fieldArtifactId: {fieldArtifactId}, " +
-			              $"result: {result}";
+						  $"isFieldIdentifierNumeric: {isFieldIdentifierNumeric}, " +
+						  $"retrievedField: {retrievedField}, " +
+						  $"fieldIdentifier: {fieldIdentifier}, " +
+						  $"fieldArtifactId: {fieldArtifactId}, " +
+						  $"result: {result}";
 			string template = "Error occurred when getting value for index {index}, " +
-			              "isFieldIdentifierNumeric: {isFieldIdentifierNumeric}, " +
-			              "retrievedField: {@retrievedField}, " +
-			              "fieldIdentifier: {fieldIdentifier}, " +
-			              "fieldArtifactId: {fieldArtifactId}, " +
-			              "result: {@result}";
+						  "isFieldIdentifierNumeric: {isFieldIdentifierNumeric}, " +
+						  "retrievedField: {@retrievedField}, " +
+						  "fieldIdentifier: {fieldIdentifier}, " +
+						  "fieldArtifactId: {fieldArtifactId}, " +
+						  "result: {@result}";
 			var exc = new IntegrationPointsException(message, e);
 			_logger.LogError(exc, template, index, isFieldIdentifierNumeric, retrievedField, fieldIdentifier, fieldArtifactId, result);
 			return exc;
@@ -201,13 +203,25 @@ namespace kCura.IntegrationPoints.Core.Services.Exporter
 			for (int index = 0; index < dataView.Table.Rows.Count; index++)
 			{
 				DataRow row = dataView.Table.Rows[index];
-				int nativeDocumentArtifactId = (int)row[_nativeDocumentArtifactIdColumn];
-				string nativeFileLocation = (string)row[_nativeLocationColumn];
-				_nativeFileLocations.Add(nativeDocumentArtifactId, nativeFileLocation);
+				int nativeDocumentArtifactId = (int) row[_nativeDocumentArtifactIdColumn];
+				string nativeFileLocation = (string) row[_nativeLocationColumn];
+				_nativeFileLocations.AddOrThrowIfKeyExists(
+					nativeDocumentArtifactId, 
+					nativeFileLocation,
+					_DUPLICATED_NATIVE_KEY_ERROR_MESSAGE
+				);
 				string nativeFileName = (string)row[_nativeFileNameColumn];
-				_nativeFileNames.Add(nativeDocumentArtifactId, nativeFileName);
+				_nativeFileNames.AddOrThrowIfKeyExists(
+					nativeDocumentArtifactId, 
+					nativeFileName,
+					_DUPLICATED_NATIVE_KEY_ERROR_MESSAGE
+				);
 				long nativeFileSize = (long)row[_nativeFileSizeColumn];
-				_nativeFileSizes.Add(nativeDocumentArtifactId, nativeFileSize);
+				_nativeFileSizes.AddOrThrowIfKeyExists(
+					nativeDocumentArtifactId, 
+					nativeFileSize,
+					_DUPLICATED_NATIVE_KEY_ERROR_MESSAGE
+				);
 			}
 		}
 
@@ -217,7 +231,7 @@ namespace kCura.IntegrationPoints.Core.Services.Exporter
 			string supportedByViewerColumn = "SupportedByViewer";
 			string relativityNativeTypeColumn = "RelativityNativeType";
 
-			string [] documentColumnsToRetrieve = { supportedByViewerColumn, relativityNativeTypeColumn };
+			string[] documentColumnsToRetrieve = { supportedByViewerColumn, relativityNativeTypeColumn };
 
 			kCura.Data.DataView nativeTypeForGivenDocument = DocumentQuery.RetrieveValuesByColumnNamesAndArtifactIDs(Context, ReadingArtifactIDs, documentColumnsToRetrieve);
 
