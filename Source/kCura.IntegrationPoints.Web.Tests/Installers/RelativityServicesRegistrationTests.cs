ccf0a9cef63a8a8efc5935af53d13f6131a2ce5f
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2019-07-23 12:01:44 +0200

    REL-338924 unit tests for registrations in web

---
+++ Source/kCura.IntegrationPoints.Web.Tests/Installers/RelativityServicesRegistrationTests.cs
@@ -0,0 +1,49 @@
+﻿using Castle.Core;
+using Castle.Windsor;
+using kCura.IntegrationPoint.Tests.Core.FluentAssertions;
+using kCura.IntegrationPoints.Web.Installers;
+using NUnit.Framework;
+using Relativity.API;
+
+namespace kCura.IntegrationPoints.Web.Tests.Installers
+{
+	[TestFixture]
+	public class RelativityServicesRegistrationTests
+	{
+		private IWindsorContainer _sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_sut = new WindsorContainer();
+			_sut.AddRelativityServices();
+		}
+
+		[Test]
+		public void IStringSanitizer_ShouldBeRegisteredWithProperLifestyle()
+		{
+			// assert
+			_sut.Should()
+				.HaveRegisteredSingleComponent<IStringSanitizer>()
+				.Which.Should().BeRegisteredWithLifestyle(LifestyleType.PerWebRequest);
+		}
+
+		[Test]
+		public void IAPILog_ShouldBeRegisteredWithProperLifestyle()
+		{
+			// assert
+			_sut.Should()
+				.HaveRegisteredSingleComponent<IAPILog>()
+				.Which.Should().BeRegisteredWithLifestyle(LifestyleType.PerWebRequest);
+		}
+
+		[Test]
+		public void IAPILog_ShouldBeRegisteredWithProperName()
+		{
+			// assert
+			_sut.Should()
+				.HaveRegisteredSingleComponent<IAPILog>()
+				.Which.Should().BeRegisteredWithName("ApiLogFromWeb");
+		}
+	}
+}
