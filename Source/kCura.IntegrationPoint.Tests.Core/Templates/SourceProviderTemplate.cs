fa897121ca734e2930471dec83f7291e11c11e82
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2019-07-24 12:54:08 +0200

    REL-327611 fixed integration tests

--- Source/kCura.IntegrationPoint.Tests.Core/Templates/SourceProviderTemplate.cs
+++ Source/kCura.IntegrationPoint.Tests.Core/Templates/SourceProviderTemplate.cs
@@ -27,10 +27,10 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Castle.MicroKernel.Resolvers;
+using kCura.IntegrationPoints.FilesDestinationProvider.Core.SharedLibrary;
 using kCura.IntegrationPoint.Tests.Core.TestHelpers;
-using kCura.IntegrationPoints.Common;
-using kCura.IntegrationPoints.Common.Handlers;
 using kCura.IntegrationPoints.Common.Monitoring.Instrumentation;
+using kCura.IntegrationPoints.Core;
 using kCura.IntegrationPoints.Data.Repositories.Implementations;
 using kCura.WinEDDS.Service.Export;
 using Relativity.Services.Folder;
@@ -147,7 +147,7 @@ namespace kCura.IntegrationPoint.Tests.Core.Templates
 				.LifeStyle.Transient);
 
 			Container.Register(Component.For<IRSAPIService>().Instance(new RSAPIService(Container.Resolve<IHelper>(), WorkspaceArtifactId)).LifestyleTransient());
-			Container.Register(Component.For<IExporterFactory>().ImplementedBy<ExporterFactory>());
+			Container.Register(Component.For<IntegrationPoints.Core.Factories.IExporterFactory>().ImplementedBy<ExporterFactory>());
 			Container.Register(Component.For<IExportServiceObserversFactory>().ImplementedBy<ExportServiceObserversFactory>());
 			Container.Register(Component.For<IAuthTokenGenerator>().ImplementedBy<ClaimsTokenGenerator>().LifestyleTransient());
 
@@ -163,10 +163,8 @@ namespace kCura.IntegrationPoint.Tests.Core.Templates
 					.ImplementedBy<ExternalServiceInstrumentationProviderWithoutJobContext>()
 					.LifestyleSingleton()
 			);
-			Container.Register(Component.For<IRetryHandlerFactory>().ImplementedBy<RetryHandlerFactory>().LifestyleTransient());
-			Container.Register(Component.For<IRetryHandler>().ImplementedBy<RetryHandler>().LifestyleTransient());
 			Container.Register(Component.For<Func<ISearchManager>>()
-				.UsingFactoryMethod(k => (Func<ISearchManager>)(() => k.Resolve<kCura.WinEDDS.Service.Export.IServiceFactory>().CreateSearchManager()))
+				.UsingFactoryMethod(k => (Func<ISearchManager>)(() => k.Resolve<IServiceManagerProvider>().Create<ISearchManager, SearchManagerFactory>()))
 				.LifestyleTransient()
 			);
 			Container.Register(Component.For<IFileRepository>().ImplementedBy<FileRepository>().LifestyleTransient());
