7a9f4ce2936b8e9574df8bdea2390a9f446e7719
Patryk Spytkowski <patryk.spytkowski@relativity.com>
2019-08-05 17:21:58 +0200

    Added Sync Job parameters to every log message in sync.

--- Source/Relativity.Sync.Tests.Unit/ContextLoggerTests.cs
+++ Source/Relativity.Sync.Tests.Unit/ContextLoggerTests.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using Moq;
 using NUnit.Framework;
+using Relativity.Sync.Configuration;
 using Relativity.Sync.Logging;
 
 namespace Relativity.Sync.Tests.Unit
@@ -10,146 +11,153 @@ namespace Relativity.Sync.Tests.Unit
 	[TestFixture]
 	public sealed class ContextLoggerTests
 	{
-		private ContextLogger _instance;
-
 		private Mock<ISyncLog> _logger;
+		
+		private ContextLogger _instance;
+		private ImportSettingsDto _importSettingsDto;
+		private object[] _expectedParams;
 
-		private const string _KEY = "some key";
+		private SyncJobParameters _syncJobParameters;
+		private const int _JOB_ID = 234;
+		private const int _WORKSPACE_ID = 123;
+		private const int _PARAM1 = 1;
 
-		private const string _MESSAGE_TEMPLATE = "message template {param1}";
-		private const string _EXPECTED_MESSAGE = "{key} message template {param1}";
-
-		private readonly object[] _params = {"param1", 1};
-		private readonly object[] _expectedParams = {_KEY, "param1", 1};
-
+		private const string _MESSAGE = "message template {param1}";
 		private readonly Exception _exception = new IOException();
+
+		private readonly object[] _params = { _PARAM1 };
+
 
 		[SetUp]
 		public void SetUp()
 		{
 			_logger = new Mock<ISyncLog>();
 
-			_instance = new ContextLogger(new CorrelationId(_KEY), _logger.Object);
+			_importSettingsDto = new ImportSettingsDto();
+			_syncJobParameters = new SyncJobParameters(_JOB_ID, _WORKSPACE_ID, _importSettingsDto);
+
+			_expectedParams = new object[] {_PARAM1, _syncJobParameters};
+			_instance = new ContextLogger(_syncJobParameters, _logger.Object);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForVerboseLevel()
 		{
 			// ACT
-			_instance.LogVerbose(_MESSAGE_TEMPLATE, _params);
+			_instance.LogVerbose(_MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogVerbose(_EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogVerbose(_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForVerboseLevelWithException()
 		{
 			// ACT
-			_instance.LogVerbose(_exception, _MESSAGE_TEMPLATE, _params);
+			_instance.LogVerbose(_exception, _MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogVerbose(_exception, _EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogVerbose(_exception, _MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForDebugLevel()
 		{
 			// ACT
-			_instance.LogDebug(_MESSAGE_TEMPLATE, _params);
+			_instance.LogDebug(_MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogDebug(_EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogDebug(_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForDebugLevelWithException()
 		{
 			// ACT
-			_instance.LogDebug(_exception, _MESSAGE_TEMPLATE, _params);
+			_instance.LogDebug(_exception, _MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogDebug(_exception, _EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogDebug(_exception, _MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForInformationLevel()
 		{
 			// ACT
-			_instance.LogInformation(_MESSAGE_TEMPLATE, _params);
+			_instance.LogInformation(_MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogInformation(_EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogInformation(_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForInformationLevelWithException()
 		{
 			// ACT
-			_instance.LogInformation(_exception, _MESSAGE_TEMPLATE, _params);
+			_instance.LogInformation(_exception, _MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogInformation(_exception, _EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogInformation(_exception, _MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForWarningLevel()
 		{
 			// ACT
-			_instance.LogWarning(_MESSAGE_TEMPLATE, _params);
+			_instance.LogWarning(_MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogWarning(_EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogWarning(_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForWarningLevelWithException()
 		{
 			// ACT
-			_instance.LogWarning(_exception, _MESSAGE_TEMPLATE, _params);
+			_instance.LogWarning(_exception, _MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogWarning(_exception, _EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogWarning(_exception, _MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForErrorLevel()
 		{
 			// ACT
-			_instance.LogError(_MESSAGE_TEMPLATE, _params);
+			_instance.LogError(_MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogError(_EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogError(_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForErrorLevelWithException()
 		{
 			// ACT
-			_instance.LogError(_exception, _MESSAGE_TEMPLATE, _params);
+			_instance.LogError(_exception, _MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogError(_exception, _EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogError(_exception, _MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForFatalLevel()
 		{
 			// ACT
-			_instance.LogFatal(_MESSAGE_TEMPLATE, _params);
+			_instance.LogFatal(_MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogFatal(_EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogFatal(_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 
 		[Test]
 		public void ItShouldPassModifiedMessageAndParametersForFatalLevelWithException()
 		{
 			// ACT
-			_instance.LogFatal(_exception, _MESSAGE_TEMPLATE, _params);
+			_instance.LogFatal(_exception, _MESSAGE, _params);
 
 			// ASSERT
-			_logger.Verify(x => x.LogFatal(_exception, _EXPECTED_MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
+			_logger.Verify(x => x.LogFatal(_exception, _MESSAGE, It.Is<object[]>(y => y.SequenceEqual(_expectedParams))), Times.Once);
 		}
 	}
 }
--- Source/Relativity.Sync/Logging/ContextLogger.cs
+++ Source/Relativity.Sync/Logging/ContextLogger.cs
@@ -1,89 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Relativity.Sync.Logging
 {
 	internal sealed class ContextLogger : ISyncLog
 	{
-		private readonly CorrelationId _id;
+		private readonly SyncJobParameters _jobParameters;
 		private readonly ISyncLog _logger;
 
-		public ContextLogger(CorrelationId id, ISyncLog logger)
+		public ContextLogger(SyncJobParameters jobParameters, ISyncLog logger)
 		{
-			_id = id;
+			_jobParameters = jobParameters;
 			_logger = logger;
 		}
 
 		public void LogVerbose(string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogVerbose(FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogVerbose(messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
 		public void LogVerbose(Exception exception, string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogVerbose(exception, FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogVerbose(exception, messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
 		public void LogDebug(string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogDebug(FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogDebug(messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
 		public void LogDebug(Exception exception, string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogDebug(exception, FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogDebug(exception, messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
 		public void LogInformation(string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogInformation(FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogInformation(messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
 		public void LogInformation(Exception exception, string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogInformation(exception, FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogInformation(exception, messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
 		public void LogWarning(string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogWarning(FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogWarning(messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
 		public void LogWarning(Exception exception, string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogWarning(exception, FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogWarning(exception, messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
 		public void LogError(string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogError(FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogError(messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
 		public void LogError(Exception exception, string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogError(exception, FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogError(exception, messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
 		public void LogFatal(string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogFatal(FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogFatal(messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
 		public void LogFatal(Exception exception, string messageTemplate, params object[] propertyValues)
 		{
-			_logger.LogFatal(exception, FormatMessage(messageTemplate), ModifyPropertyValues(propertyValues));
+			_logger.LogFatal(exception, messageTemplate, ModifyPropertyValues(propertyValues));
 		}
 
-		private static string FormatMessage(string messageTemplate)
+		private object[] ModifyPropertyValues(object[] propertyValues)
 		{
-			return $"{{key}} {messageTemplate}";
-		}
-
-		private object[] ModifyPropertyValues(params object[] propertyValues)
-		{
-			object[] newValues = new object[propertyValues.Length + 1];
-			newValues[0] = _id.Value;
-			propertyValues.CopyTo(newValues, 1);
-			return newValues;
+			var result = new List<object>(propertyValues) {_jobParameters};
+			return result.ToArray();
 		}
 	}
 }
