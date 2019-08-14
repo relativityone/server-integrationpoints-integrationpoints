078a8ae354459058d922b1b29157ad1552bc6691
Billy Bafia <billy.bafia@relativity.com>
2019-08-14 09:36:44 +0200

    [REL-339707] Adjusting system test to account for new notification node automatically.

--- Source/Relativity.Sync.Tests.System/ProgressTests.cs
+++ Source/Relativity.Sync.Tests.System/ProgressTests.cs
@@ -57,7 +57,7 @@ namespace Relativity.Sync.Tests.System
 			// ASSERT
 			List<RelativityObject> progressRdos = await QueryForProgressRdosAsync(workspaceArtifactId, syncConfigurationArtifactId).ConfigureAwait(false);
 
-			const int nonNodeProgressSteps = 3; // MultiNode + Notification + JobEndMetrics
+			const int nonNodeProgressSteps = 2; // MultiNode + JobEndMetrics
 			int minimumExpectedProgressRdos = GetSyncNodes().Count + nonNodeProgressSteps;
 			progressRdos.Count.Should().Be(minimumExpectedProgressRdos);
 		}
