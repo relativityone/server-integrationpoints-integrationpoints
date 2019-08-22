091ee510a36e9a100b7db232be125dcefb3832b5
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2019-08-22 14:29:19 +0200

    REL-349583 remove not used code from Rip.SystemTests

--- Source/Rip.SystemTests/RelativityServices/TestCases/ImageTestCase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Rip.SystemTests.RelativityServices.TestCases
-{
-	public class ImageTestCase
-	{
-		public int DocumentArtifactID { get; set; }
-		public string FileName { get; set; }
-	}
-}
