bd59fa3edad7572af02db031aa70cc8766dd3835
Kasper Kądzielawa <kasper.kadzielawa@relativity.com>
2019-08-22 11:13:01 +0200

    Add NotificationExecutorTests

---
+++ Source/Relativity.Sync.Tests.Integration/NotificationExecutorTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Relativity.Sync.Configuration;
+using Relativity.Sync.Tests.Common;
+using Relativity.Sync.Tests.Integration.Helpers;
+
+namespace Relativity.Sync.Tests.Integration
+{
+	[TestFixture]
+	public class NotificationExecutorTests
+	{
+		[SetUp]
+		public void SetUp()
+		{
+			ContainerHelper.CreateContainer(cb =>
+			{
+				IntegrationTestsContainerBuilder.MockStepsExcept<INotificationConfiguration>(cb);
+			});
+		}
+	}
+}
