38184d513212e16809cb28bc987d09033415ecda
Damian Janas <damian.janas@relativity.com>
2019-08-26 16:35:52 +0200

    REL-346808 references updated

--- Source/kCura.IntegrationPoint.Tests.Core/Constants/WorkspaceTemplateNames.cs
+++ Source/kCura.IntegrationPoint.Tests.Core/Constants/WorkspaceTemplateNames.cs
@@ -3,7 +3,6 @@
 	public static class WorkspaceTemplateNames
 	{
 		public const string NEW_CASE_TEMPLATE = "New Case Template";
-		public const string RELATIVITY_STARTER_TEMPLATE_NAME = "Relativity Starter Template";
 		public const string FUNCTIONAL_TEMPLATE_NAME = "Functional Tests Template";
 		public const string LEGACY_TEMPLATE_WORKSPACE_NAME = "kCura Starter Template";
 	}
