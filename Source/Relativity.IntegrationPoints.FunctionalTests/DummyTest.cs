5dee4b79ff1a24c1b5fb405bd0346e2a63b8232b
Damian Janas <damian.janas@relativity.com>
2019-08-27 15:54:55 +0200

    REL-346808 dummy test added

---
+++ Source/Relativity.IntegrationPoints.FunctionalTests/DummyTest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Relativity.IntegrationPoints.FunctionalTests
+{
+	[TestFixture]
+	public class DummyTest
+	{
+		[Test]
+		public DummyTest()
+		{
+			var flag = true;
+			flag.Should().BeTrue();
+		}
+	}
+}
