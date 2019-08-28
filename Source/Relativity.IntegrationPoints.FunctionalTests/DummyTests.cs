9bfa364153ae99b436455cc050361b08aecc3c78
Damian Janas <damian.janas@relativity.com>
2019-08-28 11:36:03 +0200

    REL-346808 dummy test fixed

--- Source/Relativity.IntegrationPoints.FunctionalTests/DummyTests.cs
+++ Source/Relativity.IntegrationPoints.FunctionalTests/DummyTests.cs
@@ -1,13 +1,14 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using Relativity.Testing.Identification;
 
 namespace Relativity.IntegrationPoints.FunctionalTests
 {
 	[TestFixture]
-	public class DummyTest
+	public class DummyTests
 	{
 		[IdentifiedTest("08efc703-ef29-44d4-9b85-5f5893bf84ee")]
-		public DummyTest()
+		public void DummyTest()
 		{
 			var flag = true;
 			flag.Should().BeTrue();
