81a1cafc097aaebd46cd6558284b879be94b5a41
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-08-30 10:37:09 +0200

    REL-325649 Remove ImportSettingsDto

--- Source/kCura.IntegrationPoints.RelativitySync/SyncConfiguration.cs
+++ Source/kCura.IntegrationPoints.RelativitySync/SyncConfiguration.cs
@@ -7,18 +7,15 @@ namespace kCura.IntegrationPoints.RelativitySync
 	internal sealed class SyncConfiguration : IDataDestinationFinalizationConfiguration, IDataDestinationInitializationConfiguration, IJobCleanupConfiguration,
 		IJobStatusConsolidationConfiguration, IUserContextConfiguration
 	{
-		public SyncConfiguration(int submittedBy, ImportSettings destinationConfiguration, ImportSettingsDto importSettings)
+		public SyncConfiguration(int submittedBy, ImportSettings destinationConfiguration)
 		{
 			DataDestinationArtifactId = destinationConfiguration.DestinationFolderArtifactId;
-			ImportSettings = importSettings;
 			ExecutingUserId = submittedBy;
 		}
 
 		public int DataDestinationArtifactId { get; set; }
 
 		public int ExecutingUserId { get; }
-
-		public ImportSettingsDto ImportSettings { get; }
 
 		// Currently unused properties
 		public string DataDestinationName => string.Empty;
