e6dd2e877b9ede1b77e30e17c60e10d57c345a03
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2019-09-11 10:29:10 +0200

    REL-356477 Initialized integrationpoints-keplerservicesinterfaces repository with the LibraryCD template files

---
+++ source/LibraryTemplate/Class2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTemplate
+{
+	public class Class2
+	{
+		public Class1 Namer { get; set; }
+
+		public Class2()
+		{
+		}
+
+		public Class2(Class1 namer)
+		{
+			this.Namer = namer;
+		}
+
+		public string GetName()
+		{
+			return Namer != null ? Namer.Name : "I have no name.";
+		}
+	}
+}
