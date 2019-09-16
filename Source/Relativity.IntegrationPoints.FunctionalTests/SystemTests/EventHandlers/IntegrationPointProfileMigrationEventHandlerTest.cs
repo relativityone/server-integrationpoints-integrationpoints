3106803484f0a5ad49d8c856b09daff965fa4a22
Kasper Kądzielawa <kasper.kadzielawa@relativity.com>
2019-09-16 14:52:11 +0200

    Remove redundant using

--- Source/Relativity.IntegrationPoints.FunctionalTests/SystemTests/EventHandlers/IntegrationPointProfileMigrationEventHandlerTest.cs
+++ Source/Relativity.IntegrationPoints.FunctionalTests/SystemTests/EventHandlers/IntegrationPointProfileMigrationEventHandlerTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
-using kCura.IntegrationPoint.Tests.Core;
 using kCura.IntegrationPoint.Tests.Core.Exceptions;
 using kCura.IntegrationPoints.Data;
 using kCura.IntegrationPoints.Data.Factories;
