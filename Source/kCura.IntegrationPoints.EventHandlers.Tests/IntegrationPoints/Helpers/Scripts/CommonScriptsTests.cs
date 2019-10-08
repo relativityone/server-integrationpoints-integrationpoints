7ae931c34e0c7f17fbad2cfec4d747439655849e
Konrad Wędzicha <konrad.wedzicha@relativity.com>
2019-10-08 10:57:45 +0200

    [REL-227261] Refactor ComonScriptsTests.

--- Source/kCura.IntegrationPoints.EventHandlers.Tests/IntegrationPoints/Helpers/Scripts/CommonScriptsTests.cs
+++ Source/kCura.IntegrationPoints.EventHandlers.Tests/IntegrationPoints/Helpers/Scripts/CommonScriptsTests.cs
@@ -14,36 +14,37 @@ namespace kCura.IntegrationPoints.EventHandlers.Tests.IntegrationPoints.Helpers.
 	public class CommonScriptsTests
 	{
 		private CommonScripts _sut;
-		private Mock<IScriptsHelper> _scriptsHelper;
 		private Mock<IIntegrationPointBaseFieldGuidsConstants> _integrationPointBaseFieldGuidsConstants;
+		private Mock<IScriptsHelper> _scriptsHelper;
 
 		private const string _INTEGRATION_POINTS_CONTROLLER_NAME = "IntegrationPointsAPI";
 		private const string _INTEGRATION_POINTS_PROFILE_CONTROLLER_NAME = "IntegrationPointProfilesAPI";
 
-		private readonly List<string> _allScripts = new List<string>
+		private readonly List<string> _allScripts = new List<string>(new[]
 		{
-			"/Scripts/knockout-3.4.0.js",
-			"/Scripts/knockout.validation.js",
-			"/Scripts/route.js",
-			"/Scripts/date.js",
-			"/Scripts/q.js",
-			"/Scripts/core/messaging.js",
-			"/Scripts/loading-modal.js",
-			"/Scripts/dragon/dragon-dialogs.js",
-			"/Scripts/core/data.js",
-			"/Scripts/core/utils.js",
-			"/Scripts/integration-point/time-utils.js",
-			"/Scripts/integration-point/picker.js",
-			"/Scripts/Export/export-validation.js",
-			"/Scripts/integration-point/save-as-profile-modal-vm.js",
-			"/Scripts/jquery.signalR-2.3.0.js",
-			"/signalr/hubs",
-			"/Scripts/hubs/integrationPointHub.js",
+				"/Scripts/knockout-3.4.0.js",
+				"/Scripts/knockout.validation.js",
+				"/Scripts/route.js",
+				"/Scripts/date.js",
+				"/Scripts/q.js",
+				"/Scripts/core/messaging.js",
+				"/Scripts/loading-modal.js",
+				"/Scripts/dragon/dragon-dialogs.js",
+				"/Scripts/core/data.js",
+				"/Scripts/core/utils.js",
+				"/Scripts/integration-point/time-utils.js",
+				"/Scripts/integration-point/picker.js",
+				"/Scripts/Export/export-validation.js",
+				"/Scripts/integration-point/save-as-profile-modal-vm.js"
+		}).Concat(_signalRScripts).Concat(_eventHandlersScripts).ToList();
+
+		private static readonly List<string> _eventHandlersScripts = new List<string>
+		{
 			"/Scripts/EventHandlers/integration-points-view-destination.js",
 			"/Scripts/EventHandlers/integration-points-summary-page-view.js"
 		};
 
-		private readonly List<string> _signalRScripts = new List<string>
+		private static readonly List<string> _signalRScripts = new List<string>
 		{
 			"/Scripts/jquery.signalR-2.3.0.js",
 			"/signalr/hubs",
@@ -53,6 +54,7 @@ namespace kCura.IntegrationPoints.EventHandlers.Tests.IntegrationPoints.Helpers.
 		[SetUp]
 		public void SetUp()
 		{
+			_allScripts.Concat(_signalRScripts).Concat(_eventHandlersScripts);
 			_scriptsHelper = new Mock<IScriptsHelper>();
 			_integrationPointBaseFieldGuidsConstants = new Mock<IIntegrationPointBaseFieldGuidsConstants>();
 
