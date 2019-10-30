4ec0f0eaef7ec2ce29dadcc3948c329f0a79bfff
Kasper Kądzielawa <kasper.kadzielawa@relativity.com>
2019-10-30 15:13:31 +0100

    [REL-291476] Add unit tests for `OldBatchesCleanupService` and `OldBatchesCleanupServiceFactory`

---
+++ Source/kCura.IntegrationPoints.RelativitySync.Tests/OldBatchesCleanup/OldBatchesCleanupServiceFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using kCura.IntegrationPoints.RelativitySync.OldBatchesCleanup;
+using Moq;
+using NUnit.Framework;
+using Relativity.API;
+
+namespace kCura.IntegrationPoints.RelativitySync.Tests.OldBatchesCleanup
+{
+	[TestFixture]
+	public class OldBatchesCleanupServiceFactoryTests
+	{
+		private IOldBatchesCleanupServiceFactory _sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			var servicesMgrStub = new Mock<IServicesMgr>();
+			var helperStub = new Mock<IHelper>();
+
+			helperStub
+				.Setup(x => x.GetServicesManager())
+				.Returns(servicesMgrStub.Object);
+
+			_sut = new OldBatchesCleanupServiceFactory(helperStub.Object);
+		}
+
+		[Test]
+		public void ItShouldDeleteBatchesOlderThan7Days()
+		{
+			// Act
+			IOldBatchesCleanupService oldBatchesCleanupService = _sut.Create();
+
+			// Assert
+			Assert.IsNotNull(oldBatchesCleanupService, "Factory returned null instance.");
+		}
+	}
+}
---
+++ Source/kCura.IntegrationPoints.RelativitySync.Tests/OldBatchesCleanup/OldBatchesCleanupServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using kCura.IntegrationPoints.RelativitySync.OldBatchesCleanup;
+using Moq;
+using NUnit.Framework;
+using Relativity.Sync.Storage;
+
+namespace kCura.IntegrationPoints.RelativitySync.Tests.OldBatchesCleanup
+{
+	[TestFixture]
+	public class OldBatchesCleanupServiceTests
+	{
+		private const int WORKSPACE_ID = 123987;
+		private const int OLD_BATCH_DAYS_AMOUNT = 7;
+
+		private Mock<IBatchRepository> _batchRepositoryMock;
+
+		private IOldBatchesCleanupService _sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_batchRepositoryMock = new Mock<IBatchRepository>();
+			_sut = new OldBatchesCleanupService(_batchRepositoryMock.Object);
+		}
+
+		[Test]
+		public void ItShouldDeleteBatchesOlderThan7Days()
+		{
+			// Act
+			_sut.DeleteOldBatchesInWorkspaceAsync(WORKSPACE_ID);
+
+			// Assert
+			_batchRepositoryMock.Verify(x => x.DeleteAllOlderThanAsync(WORKSPACE_ID, TimeSpan.FromDays(OLD_BATCH_DAYS_AMOUNT)));
+		}
+	}
+}
