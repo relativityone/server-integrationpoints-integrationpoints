70d3260d438afa8eae2019f393428260df757cbf
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2019-10-30 14:26:37 +0100

    REL-371826 Added InternalsVisibleTo for kCura.IntegrationPoints.Core to SourceProviderInstaller project to allow RIP to build properly.

--- source/Relativity.IntegrationPoints.SourceProviderInstaller/Properties/LocalAssemblyInfo.cs
+++ source/Relativity.IntegrationPoints.SourceProviderInstaller/Properties/LocalAssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("kCura.IntegrationPoints.Core")]
 [assembly: InternalsVisibleTo("kCura.IntegrationPoints.Domain")]
 [assembly: InternalsVisibleTo("kCura.IntegrationPoints.EventHandlers")]
 [assembly: InternalsVisibleTo("Relativity.IntegrationPoints.SourceProviderInstaller.Tests")]
