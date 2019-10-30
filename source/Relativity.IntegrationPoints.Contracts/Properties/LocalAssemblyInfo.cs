c7a5d51252ea53d523591800d5644ac3bf044883
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2019-10-30 15:28:32 +0100

    REL-371826 Added InternalsVisibleTo for kCura.IntegrationPoints.EventHandlers to Contracts project to allow RIP to build properly.

--- source/Relativity.IntegrationPoints.Contracts/Properties/LocalAssemblyInfo.cs
+++ source/Relativity.IntegrationPoints.Contracts/Properties/LocalAssemblyInfo.cs
@@ -2,6 +2,7 @@
 
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo("kCura.IntegrationPoints.Core")]
+[assembly: InternalsVisibleTo("kCura.IntegrationPoints.EventHandlers")]
 [assembly: InternalsVisibleTo("kCura.IntegrationPoints.Domain")]
 [assembly: InternalsVisibleTo("Relativity.IntegrationPoints.Services")]
 [assembly: InternalsVisibleTo("Relativity.IntegrationPoints.SourceProviderInstaller")]
