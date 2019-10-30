998368b24249472953f720d9ceb70fdfa23dc3ba
Kasper Kądzielawa <kasper.kadzielawa@relativity.com>
2019-10-30 12:53:36 +0100

    [REL-291476] Add remove old jobs batches event handler

---
+++ Source/kCura.IntegrationPoints.EventHandlers/Commands/RemoveBatchesFromOldJobsCommand.cs
@@ -0,0 +1,19 @@
+﻿using kCura.IntegrationPoints.EventHandlers.Commands.Context;
+
+namespace kCura.IntegrationPoints.EventHandlers.Commands
+{
+	public class RemoveBatchesFromOldJobsCommand : IEHCommand
+	{
+		private readonly IEHContext _context;
+
+		public RemoveBatchesFromOldJobsCommand(IEHContext context)
+		{
+			_context = context;
+		}
+
+		public void Execute()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
