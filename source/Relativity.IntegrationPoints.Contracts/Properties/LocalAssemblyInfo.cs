25173fea4ea31c86ad08e0cf2cf4f180158a582e
Piotr Szmigielski <piotr.szmigielski@relativity.com>
2019-10-30 17:08:37 +0100

    REL-371826 Added missing LocalAssemblyInfo entries to fix RIP build failures

--- source/Relativity.IntegrationPoints.Contracts/Properties/LocalAssemblyInfo.cs
+++ source/Relativity.IntegrationPoints.Contracts/Properties/LocalAssemblyInfo.cs
@@ -7,6 +7,7 @@
 [assembly: InternalsVisibleTo("kCura.IntegrationPoints.EventHandlers")]
 [assembly: InternalsVisibleTo("kCura.IntegrationPoints.EventHandlers.Tests")]
 [assembly: InternalsVisibleTo("kCura.IntegrationPoints.Web")]
+[assembly: InternalsVisibleTo("kCura.IntegrationPoints.Web.Tests")]
 [assembly: InternalsVisibleTo("Relativity.IntegrationPoints.Services")]
 [assembly: InternalsVisibleTo("Relativity.IntegrationPoints.SourceProviderInstaller")]
 [assembly: InternalsVisibleTo("Relativity.IntegrationPoints.SourceProviderInstaller.Tests")]
