c41482ff77f6fb0e832e99b6af8ea521c55b16d5
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-11-21 12:57:56 +0100

    REL-364207 Fix build

--- Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/SharedLibrary/ExtendedExporterFactory.cs
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/SharedLibrary/ExtendedExporterFactory.cs
@@ -1,45 +1,39 @@
-﻿using kCura.IntegrationPoints.FilesDestinationProvider.Core.Helpers;
+﻿using System.Threading;
+using kCura.IntegrationPoints.FilesDestinationProvider.Core.Helpers;
 using kCura.WinEDDS;
-using kCura.WinEDDS.Exporters;
 using kCura.WinEDDS.Service.Export;
+using Relativity.Logging;
 
 namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.SharedLibrary
 {
 	public class ExtendedExporterFactory : IExtendedExporterFactory
 	{
 		private readonly IFactoryConfigBuilder _configFactory;
-
-
-		public ExtendedExporterFactory(IFactoryConfigBuilder factoryConfigBuilder)
+		private readonly ILog _logger;
+		
+		public ExtendedExporterFactory(IFactoryConfigBuilder factoryConfigBuilder, ILog logger)
 		{
 			_configFactory = factoryConfigBuilder;
+			_logger = logger;
 		}
 
-		public ExtendedExporter Create(ExtendedExportFile exportFile, global::Relativity.DataExchange.Process.ProcessContext context, ILoadFileHeaderFormatterFactory loadFileFormatterFactory)
+		public IExporter Create(ExportDataContext context, IServiceFactory serviceFactory)
 		{
-			return new ExtendedExporter(exportFile, context, loadFileFormatterFactory);
+			ExporterFactoryConfig config = _configFactory.BuildFactoryConfig(context, serviceFactory);
+			ExtendedExporter exporter = Create(context.ExportFile, config);
+			kCura.WinEDDS.Container.ContainerFactoryProvider.ContainerFactory = new global::Relativity.DataExchange.Export.VolumeManagerV2.Container.ContainerFactory(_logger);
+            return new StoppableExporter(exporter, config.Controller, config.JobStopManager);
 		}
 
 		private ExtendedExporter Create(ExtendedExportFile exportFile, ExporterFactoryConfig config)
 		{
-			return new ExtendedExporter(exportFile, config.Controller, config.ServiceFactory, config.LoadFileFormatterFactory, config.ExportConfig)
+			return new ExtendedExporter(exportFile, config.Controller, config.ServiceFactory, config.LoadFileFormatterFactory, config.ExportConfig, _logger, CancellationToken.None)
 			{
 				NameTextAndNativesAfterBegBates = config.NameTextAndNativesAfterBegBates,
 				FileHelper = new LongPathFileHelper(),
 				DirectoryHelper = new LongPathDirectoryHelper(),
 				FileNameProvider = config.FileNameProvider
 			};
-		}
-
-		public IExporter Create(ExportDataContext context, IServiceFactory serviceFactory)
-		{
-			ExporterFactoryConfig config = _configFactory.BuildFactoryConfig(context, serviceFactory);
-
-			ExtendedExporter exporter = Create(context.ExportFile, config);
-
-			kCura.WinEDDS.Container.ContainerFactoryProvider.ContainerFactory = new global::Relativity.DataExchange.Export.VolumeManagerV2.Container.ContainerFactory();
-
-            return new StoppableExporter(exporter, config.Controller, config.JobStopManager);
 		}
 	}
 }
