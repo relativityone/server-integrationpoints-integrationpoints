3bcc7598d27e1c5e5d2b1a38df13408dc22a8647
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-11-21 16:09:33 +0100

    REL-364207 Use static ILog instance

--- Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/SharedLibrary/ExtendedExporterFactory.cs
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/SharedLibrary/ExtendedExporterFactory.cs
@@ -9,25 +9,23 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.SharedLibrary
 	public class ExtendedExporterFactory : IExtendedExporterFactory
 	{
 		private readonly IFactoryConfigBuilder _configFactory;
-		private readonly ILog _logger;
 		
-		public ExtendedExporterFactory(IFactoryConfigBuilder factoryConfigBuilder, ILog logger)
+		public ExtendedExporterFactory(IFactoryConfigBuilder factoryConfigBuilder)
 		{
 			_configFactory = factoryConfigBuilder;
-			_logger = logger;
 		}
 
 		public IExporter Create(ExportDataContext context, IServiceFactory serviceFactory)
 		{
 			ExporterFactoryConfig config = _configFactory.BuildFactoryConfig(context, serviceFactory);
 			ExtendedExporter exporter = Create(context.ExportFile, config);
-			kCura.WinEDDS.Container.ContainerFactoryProvider.ContainerFactory = new global::Relativity.DataExchange.Export.VolumeManagerV2.Container.ContainerFactory(_logger);
+			kCura.WinEDDS.Container.ContainerFactoryProvider.ContainerFactory = new global::Relativity.DataExchange.Export.VolumeManagerV2.Container.ContainerFactory(Log.Logger);
             return new StoppableExporter(exporter, config.Controller, config.JobStopManager);
 		}
 
 		private ExtendedExporter Create(ExtendedExportFile exportFile, ExporterFactoryConfig config)
 		{
-			return new ExtendedExporter(exportFile, config.Controller, config.ServiceFactory, config.LoadFileFormatterFactory, config.ExportConfig, _logger, CancellationToken.None)
+			return new ExtendedExporter(exportFile, config.Controller, config.ServiceFactory, config.LoadFileFormatterFactory, config.ExportConfig, Log.Logger, CancellationToken.None)
 			{
 				NameTextAndNativesAfterBegBates = config.NameTextAndNativesAfterBegBates,
 				FileHelper = new LongPathFileHelper(),
