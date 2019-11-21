014a43d8408db2ddb12f856e0fab601d5ac0ffd6
Kamil Makarowski <kamil.makarowski@relativity.com>
2019-11-21 10:16:09 +0100

    DEVOPS-64580 Create empty DSL Trident Job for RelativitySync Nightly

---
+++ Trident/DSLFiles/Nightly.groovy
@@ -0,0 +1,40 @@
+folder('DataTransfer-Jobs') {
+}
+
+folder('DataTransfer-Jobs/RelativitySync') {
+}
+
+multibranchPipelineJob('DataTransfer-Jobs/RelativitySync/Nightly') {
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Trident/Jobs/Nightly.groovy')
+        }
+    }
+    branchSources {
+        branchSource {
+            source {
+                git {
+                    remote('ssh://git@git.kcura.com:7999/dtx/relativitysync.git')
+                    credentialsId('bitbucket-repo-key')
+                    id('RelativitySync-Nightly')
+                }
+            }
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+    configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait'()
+        traits << 'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
+            includes('Trident develop release-*')
+            excludes('')
+        }
+        traits << 'jenkins.plugins.git.traits.CleanAfterCheckoutTrait' {
+            extension(class:'hudson.plugins.git.extensions.impl.CleanCheckout')
+        }
+    }
+}
