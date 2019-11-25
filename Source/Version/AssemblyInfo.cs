5df89c824e2e8a607dc39302e5134e3696de84a1
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2019-11-25 15:40:40 +0100

    Change version to 1.0.0

--- Source/Version/AssemblyInfo.cs
+++ Source/Version/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("0.1.0.0")]
-[assembly: AssemblyFileVersionAttribute("0.1.0.0")]
+[assembly: AssemblyVersionAttribute("1.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: AssemblyCopyrightAttribute("Copyright (c) 2017, Relativity ODA LLC")]
 [assembly: AssemblyTitleAttribute("Relativity.IntegrationPoints.MyFirstProvider")]
 [assembly: AssemblyDescriptionAttribute("Relativity.IntegrationPoints.MyFirstProvider")]
