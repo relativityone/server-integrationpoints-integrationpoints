b71a3109f682b3e4c075cb7d296b0543d5e55d7d
Kamil Makarowski <kamil.makarowski@relativity.com>
2019-12-04 10:10:29 +0100

    REL-383300 Restore previous behavior on old jenkins

--- DevelopmentScripts/New-TestSettings.ps1
+++ DevelopmentScripts/New-TestSettings.ps1
@@ -107,6 +107,8 @@ if(-not $RAPDirectory)
     $PSBoundParameters['RAPDirectory'] = Join-Path $PSScriptRoot ..\Artifacts
 }
 
+$PSBoundParameters['IsTrident'] = "true"
+
 Remove-Item (Join-Path $PSScriptRoot ..\FunctionalTestSettings) -Force -ErrorAction SilentlyContinue
 Remove-Item (Join-Path $PSScriptRoot ..\FunctionalTest.runsettings) -Force -ErrorAction SilentlyContinue
 
--- Source/Relativity.Sync.Tests.System/SynchronizationExecutorTests.cs
+++ Source/Relativity.Sync.Tests.System/SynchronizationExecutorTests.cs
@@ -110,13 +110,16 @@ namespace Relativity.Sync.Tests.System
 			ImportJobErrors importJobErrors = await importHelper.ImportDataAsync(sourceWorkspaceArtifactId, dataTableWrapper).ConfigureAwait(false);
 			Assert.IsTrue(importJobErrors.Success, $"IAPI errors: {string.Join(global::System.Environment.NewLine, importJobErrors.Errors)}");
 
-			#region Hopper Instance workaround explanation
-			//This workaround was provided to omit Hopper Instance restrictions. IAPI which is executing on agent can't access file based on file location in database like '\\emttest\DefaultFileRepository\...'.
-			//Hopper is closed for outside traffic so there is no possibility to access fileshare from Trident Agent. Jira related to this https://jira.kcura.com/browse/DEVOPS-70257.
-			//If we decouple Sync from RIP and move it to RAP problem probably disappears. Right now as workaround we change on database this relative Fileshare path to local,
-			//where out test data are stored. So we assume in testing that push is working correctly, but whole flow (metadata, etc.) is under tests.
-			#endregion
-			UpdateNativeFilePathToLocal(sourceWorkspaceArtifactId);
+			if(AppSettings.IsTrident)
+			{
+				#region Hopper Instance workaround explanation
+				//This workaround was provided to omit Hopper Instance restrictions. IAPI which is executing on agent can't access file based on file location in database like '\\emttest\DefaultFileRepository\...'.
+				//Hopper is closed for outside traffic so there is no possibility to access fileshare from Trident Agent. Jira related to this https://jira.kcura.com/browse/DEVOPS-70257.
+				//If we decouple Sync from RIP and move it to RAP problem probably disappears. Right now as workaround we change on database this relative Fileshare path to local,
+				//where out test data are stored. So we assume in testing that push is working correctly, but whole flow (metadata, etc.) is under tests.
+				#endregion
+				UpdateNativeFilePathToLocal(sourceWorkspaceArtifactId);
+			}
 
 			// Source tags creation in destination workspace
 			IExecutor<IDestinationWorkspaceTagsCreationConfiguration> destinationWorkspaceTagsCreationExecutor = container.Resolve<IExecutor<IDestinationWorkspaceTagsCreationConfiguration>>();
