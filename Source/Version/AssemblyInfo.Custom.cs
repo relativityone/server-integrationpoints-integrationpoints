0c76d1af4405c7bce688fa58bc3b86ddf9a94743
Kamil Makarowski <kamil.makarowski@relativity.com>
2019-12-05 14:34:20 +0100

    REL-346861 Change bin\{Debug|Release} to bin\

--- Source/Version/AssemblyInfo.Custom.cs
+++ Source/Version/AssemblyInfo.Custom.cs
@@ -38,6 +38,3 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("kCura.IntegrationPoints.RelativitySync.Tests")]
 [assembly: InternalsVisibleTo("Relativity.IntegrationPoints.FunctionalTests")]
 [assembly: InternalsVisibleTo("Relativity.IntegrationPoints.Services.Tests")]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-//[assembly: Guid("9808fab4-ae17-49c5-a74a-e34543378422")]
