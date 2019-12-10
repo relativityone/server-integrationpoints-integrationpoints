0770933c141455be48a51d6f5f1be6e1b871cd51
Kasper Kądzielawa <kasper.kadzielawa@relativity.com>
2019-12-10 18:49:16 +0100

    [REL-380916] Remove redundant using

--- Source/Relativity.Sync/Executors/SynchronizationExecutor.cs
+++ Source/Relativity.Sync/Executors/SynchronizationExecutor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Concurrency;
 using System.Threading;
 using System.Threading.Tasks;
 using Relativity.Sync.Configuration;
