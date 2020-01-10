9c4f973806580d007601eeb5ca7e003eb81b907f
Krzysztof Skowronek <krzysztof.skowronek@relativity.com>
2020-01-10 16:13:27 +0100

    [REL-38085] applied changes from PR

--- Source/kCura.IntegrationPoints.Core/Services/JobHistory/JobHistoryService.cs
+++ Source/kCura.IntegrationPoints.Core/Services/JobHistory/JobHistoryService.cs
@@ -27,25 +27,19 @@ namespace kCura.IntegrationPoints.Core.Services.JobHistory
 		private readonly IWorkspaceManager _workspaceManager;
 		private readonly IAPILog _logger;
 		private readonly IIntegrationPointSerializer _serializer;
-		private readonly IProviderTypeService _providerTypeService;
-		private readonly IMessageService _messageService;
 
 		public JobHistoryService(
 			IRelativityObjectManager relativityObjectManager,
 			IFederatedInstanceManager federatedInstanceManager,
 			IWorkspaceManager workspaceManager,
 			IAPILog logger,
-			IIntegrationPointSerializer serializer,
-			IProviderTypeService providerTypeService,
-			IMessageService messageService)
+			IIntegrationPointSerializer serializer)
 		{
 			_relativityObjectManager = relativityObjectManager;
 			_federatedInstanceManager = federatedInstanceManager;
 			_workspaceManager = workspaceManager;
 			_logger = logger.ForContext<JobHistoryService>();
 			_serializer = serializer;
-			_providerTypeService = providerTypeService;
-			_messageService = messageService;
 		}
 
 		public Data.JobHistory GetRdo(Guid batchInstance)
