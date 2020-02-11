4fd164fb81ac4902e8dd92ef3f99f29946bb3527
Tomasz Pęczek <tomasz.peczek@relativity.com>
2020-02-11 15:07:14 +0000

    Merge pull request #1927 in IN/integrationpoints from REL-381531-backport-to-juniper to release-11.1-juniper
    Squashed commit of the following:
    
    commit d8218cda8b4de1beb44c5f96ff2177a4f59cacd7
    Author: Tomasz Pęczek <tomasz.peczek@relativity.com>
    Date:   Tue Feb 11 11:31:56 2020 +0100
    
        [REL-381531] Updating SYNC

--- Source/kCura.IntegrationPoints.RelativitySync/RelativitySyncAdapter.cs
+++ Source/kCura.IntegrationPoints.RelativitySync/RelativitySyncAdapter.cs
@@ -179,7 +179,7 @@ namespace kCura.IntegrationPoints.RelativitySync
 
 			SyncJobFactory jobFactory = new SyncJobFactory();
 			SyncJobParameters parameters = new SyncJobParameters(syncConfigurationArtifactId, _job.WorkspaceId, _job.IntegrationPointId, _job.JobHistoryId);
-			RelativityServices relativityServices = new RelativityServices(_apmMetrics, _ripContainer.Resolve<IHelper>().GetServicesManager(), _ripContainer.Resolve<Func<ISearchManager>>(), ExtensionPointServiceFinder.ServiceUriProvider.AuthenticationUri());
+			RelativityServices relativityServices = new RelativityServices(_apmMetrics, _ripContainer.Resolve<IHelper>().GetServicesManager(), ExtensionPointServiceFinder.ServiceUriProvider.AuthenticationUri());
 			ISyncLog syncLog = new SyncLog(_logger);
 			ISyncJob syncJob = jobFactory.Create(container, parameters, relativityServices, syncLog);
 			return syncJob;
