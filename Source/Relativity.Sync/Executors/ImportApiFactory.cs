de6916b6c4c8430ac27c9c24825bd2d7faa481fe
Tomasz Pęczek <tomasz.peczek@relativity.com>
2020-02-12 14:16:12 +0100

    [REL-402163] New way of providing tokens for Import API

--- Source/Relativity.Sync/Executors/ImportApiFactory.cs
+++ Source/Relativity.Sync/Executors/ImportApiFactory.cs
@@ -1,17 +1,52 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using Relativity.DataExchange;
+using Relativity.Sync.Configuration;
+using Relativity.Sync.Authentication;
 using kCura.Relativity.ImportAPI;
 
 namespace Relativity.Sync.Executors
 {
+#pragma warning disable RG2002
 	// We need to exclude this from code coverage, because we are using here concrete class from IAPI.
 	[ExcludeFromCodeCoverage]
 	internal sealed class ImportApiFactory : IImportApiFactory
 	{
-		public async Task<IImportAPI> CreateImportApiAsync(Uri webServiceUrl)
+		private readonly IUserContextConfiguration _userContextConfiguration;
+		private readonly IAuthTokenGenerator _tokenGenerator;
+
+#pragma warning disable RG0001 
+		private class RelativityTokenProvider : IRelativityTokenProvider
 		{
-			return await Task.Run(() => ImportAPI.CreateByRsaBearerToken(webServiceUrl.AbsoluteUri)).ConfigureAwait(false);
+			private readonly int _executingUserId;
+			private readonly IAuthTokenGenerator _tokenGenerator;
+
+			public RelativityTokenProvider(int executingUserId, IAuthTokenGenerator tokenGenerator)
+			{
+				_executingUserId = executingUserId;
+				_tokenGenerator = tokenGenerator;
+			}
+
+			public string GetToken()
+			{
+				return _tokenGenerator.GetAuthTokenAsync(_executingUserId).GetAwaiter().GetResult();
+			}
+		}
+#pragma warning restore RG0001
+
+		public ImportApiFactory(IUserContextConfiguration userContextConfiguration, IAuthTokenGenerator tokenGenerator)
+		{
+			_userContextConfiguration = userContextConfiguration;
+			_tokenGenerator = tokenGenerator;
+		}
+
+		public Task<IImportAPI> CreateImportApiAsync(Uri webServiceUrl)
+		{
+			IRelativityTokenProvider relativityTokenProvider = new RelativityTokenProvider(_userContextConfiguration.ExecutingUserId, _tokenGenerator);
+
+			return Task.FromResult((IImportAPI)ExtendedImportAPI.CreateByTokenProvider(webServiceUrl.AbsoluteUri, relativityTokenProvider));
 		}
 	}
+#pragma warning restore RG2002
 }
