64847c8a54eac80f84c2f3c30029756f5be83703
Krzysztof Skowronek <krzysztof.skowronek@relativity.com>
2020-02-12 13:28:59 +0100

    [REL-402140] added ObjectFieldsClassifier with unit tests

---
+++ Source/Relativity.IntegrationPoints.FieldsMapping.Tests/FieldsClassifiers/ObjectFieldsClassifierTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using Relativity.IntegrationPoints.FieldsMapping.FieldClassifiers;
+using Relativity.Services.Objects.DataContracts;
+
+namespace Relativity.IntegrationPoints.FieldsMapping.Tests.FieldsClassifiers
+{
+	[TestFixture, Category("Unit")]
+	public class ObjectFieldsClassifierTests
+	{
+		private const string ApiDoesNotSupportAllObjectTypes = "API does not support all object types.";
+		private ObjectFieldsClassifier _sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_sut = new ObjectFieldsClassifier();
+		}
+
+		[Test]
+		public async Task ClassifyAsync_ShouldProperlyClassifyObjectFields()
+		{
+			// Arrange 
+
+			var fields = new List<RelativityObject>
+			{
+				new RelativityObject
+				{
+					FieldValues = new List<FieldValuePair>
+					{
+						new FieldValuePair
+						{
+							Field = new Field
+							{
+								Name = "Field Type"
+							},
+							Value = "Single Object"
+						}
+					}
+				},
+				new RelativityObject
+				{
+					FieldValues = new List<FieldValuePair>
+					{
+						new FieldValuePair
+						{
+							Field = new Field
+							{
+								Name = "Field Type"
+							},
+							Value = "Multiple Object"
+						}
+					}
+				},
+				new RelativityObject
+				{
+					FieldValues = new List<FieldValuePair>
+					{
+						new FieldValuePair
+						{
+							Field = new Field
+							{
+								Name = "Field Type"
+							},
+							Value = "Other type"
+						}
+					}
+				}
+			};
+
+			// Act
+			FieldClassificationResult[] classifications = (await _sut.ClassifyAsync(fields, 0)).ToArray();
+
+			// Assert
+			classifications.Length.Should().Be(2);
+
+			classifications[0].ClassificationLevel.Should().Be(ClassificationLevel.ShowToUser);
+			classifications[0].ClassificationReason.Should().Be(ApiDoesNotSupportAllObjectTypes);
+
+			classifications[1].ClassificationLevel.Should().Be(ClassificationLevel.ShowToUser);
+			classifications[1].ClassificationReason.Should().Be(ApiDoesNotSupportAllObjectTypes);
+		}
+	}
+}
---
+++ Source/Relativity.IntegrationPoints.FieldsMapping/FieldClassifiers/ObjectFieldsClassifier.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Relativity.API;
+using Relativity.Services.Interfaces.Field;
+using Relativity.Services.Interfaces.Field.Models;
+using Relativity.Services.Objects;
+using Relativity.Services.Objects.DataContracts;
+using QueryResult = Relativity.Services.Objects.DataContracts.QueryResult;
+
+namespace Relativity.IntegrationPoints.FieldsMapping.FieldClassifiers
+{
+	public class ObjectFieldsClassifier : IFieldsClassifier
+	{
+		private const string ApiDoesNotSupportAllObjectTypes = "API does not support all object types.";
+
+		public Task<IEnumerable<FieldClassificationResult>> ClassifyAsync(ICollection<RelativityObject> fields, int workspaceID)
+		{
+			IEnumerable<FieldClassificationResult> objectFields = fields
+				.Where(x => x.FieldValues.Exists(fieldValuePair =>
+								(fieldValuePair.Field.Name == "Field Type" &&
+								 (fieldValuePair.Value.ToString() == "Single Object" ||
+								  fieldValuePair.Value.ToString() == "Multiple Object"))))
+				.Select(x => new FieldClassificationResult()
+				{
+					Name = x.Name,
+					ClassificationReason = ApiDoesNotSupportAllObjectTypes,
+					ClassificationLevel = ClassificationLevel.ShowToUser
+				});
+
+			return Task.FromResult(objectFields);
+		}
+	}
+}
