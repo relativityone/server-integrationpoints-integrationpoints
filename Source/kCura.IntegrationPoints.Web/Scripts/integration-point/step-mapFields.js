a278bdd23202f3e2f193595587b5052a7b7a2080
Krzysztof Skowronek <krzysztof.skowronek@relativity.com>
2020-02-24 16:22:14 +0100

    [REL-403224] add matching fields by name

--- Source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-mapFields.js
+++ Source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-mapFields.js
@@ -594,7 +594,7 @@ ko.validation.insertValidationMessage = function (element) {
 		this.destinationCaseArtifactID = destination.CaseArtifactId;
 
 		self.findField = function(array, field) {
-			const fields = $.grep(array, function (value, _index) { return value.fieldIdentifier === field.fieldIdentifier; });
+			const fields = $.grep(array, function (value, _index) { return value.fieldIdentifier === field.fieldIdentifier || value.name == field.displayName; });
 			const fieldFound = fields.length > 0;
 			return {
 				exist: fieldFound,
