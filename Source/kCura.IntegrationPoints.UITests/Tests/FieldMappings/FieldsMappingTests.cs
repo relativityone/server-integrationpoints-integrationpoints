00a421267e202495f48a98e10ca9993e4fde4853
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-02-24 16:14:32 +0100

    REL-402528 Update

--- Source/kCura.IntegrationPoints.UITests/Tests/FieldMappings/FieldsMappingTests.cs
+++ Source/kCura.IntegrationPoints.UITests/Tests/FieldMappings/FieldsMappingTests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using FluentAssertions;
+using kCura.IntegrationPoint.Tests.Core;
 using kCura.IntegrationPoint.Tests.Core.Models;
 using kCura.IntegrationPoints.UITests.Common;
 using kCura.IntegrationPoints.UITests.Configuration.Helpers;
@@ -9,6 +11,10 @@ using kCura.IntegrationPoints.UITests.NUnitExtensions;
 using kCura.IntegrationPoints.UITests.Pages;
 using kCura.IntegrationPoints.UITests.Tests.RelativityProvider;
 using NUnit.Framework;
+using Relativity;
+using Relativity.Services.Field;
+using Relativity.Services.Interfaces.Field.Models;
+using Relativity.Services.Interfaces.Shared.Models;
 using Relativity.Testing.Identification;
 
 namespace kCura.IntegrationPoints.UITests.Tests.FieldMappings
@@ -86,8 +92,54 @@ namespace kCura.IntegrationPoints.UITests.Tests.FieldMappings
 			//Assert
 			fieldsFromDestinationWorkspaceListBox.Should().ContainInOrder(expectedDestinationMappableFields);
 		}
+		
+		[IdentifiedTest("65917e62-2387-4b1e-afee-721bac33b1c0")]
+		[RetryOnError]
+		[Category(TestCategory.SMOKE)]
+		public async Task FieldMapping_ShouldAutoMapFieldsFromSavedSearch()
+		{
+			//Arrange
+			var createFieldRequest = new FixedLengthFieldRequest()
+			{
+				ObjectType = new ObjectTypeIdentifier()
+				{
+					ArtifactTypeID = (int) ArtifactType.Document
+				},
+				Name = "Orzel 7",
+				Length = 255,
+				
+			};
 
-        private List<string> CreateFieldMapListBoxFormatFromObjectManagerFetchedList(
+			int sourceFieldID = await SourceFieldManager
+				.CreateFixedLengthFieldAsync(SourceContext.GetWorkspaceId(), createFieldRequest)
+				.ConfigureAwait(false);
+			int destinationFieldID = await DestinationFieldManager
+				.CreateFixedLengthFieldAsync(DestinationContext.GetWorkspaceId(), createFieldRequest)
+				.ConfigureAwait(false);
+
+			int savedSearchID = await SavedSearch.CreateSavedSearchAsync(SourceContext.GetWorkspaceId(),
+				"Orzel 7 search", new[]
+				{
+					new FieldRef
+					{
+						ArtifactID = sourceFieldID
+					}
+				}).ConfigureAwait(false);
+
+			RelativityProviderModel model = CreateRelativityProviderModel();
+			model.Overwrite = RelativityProviderModel.OverwriteModeEnum.AppendOnly;
+			model.UseFolderPathInformation = RelativityProviderModel.UseFolderPathInformationEnum.No;
+
+			//Act
+			PushToRelativityThirdPage fieldMappingPage =
+				PointsAction.CreateNewRelativityProviderFieldMappingPage(model);
+			List<string> fieldsFromDestinationWorkspaceListBox =
+				fieldMappingPage.GetFieldsFromDestinationWorkspaceListBox();
+			List<string> expectedDestinationMappableFields =
+				CreateFieldMapListBoxFormatFromObjectManagerFetchedList(DestinationContext.WorkspaceMappableFields);
+		}
+
+		private List<string> CreateFieldMapListBoxFormatFromObjectManagerFetchedList(
 			List<FieldObject> mappableFieldsListFromObjectManager)
 		{
 			return mappableFieldsListFromObjectManager.OrderBy(f => f.Name)
