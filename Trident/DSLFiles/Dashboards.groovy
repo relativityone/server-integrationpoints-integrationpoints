f7cb17e55105dce8b50d7243709f4b985d9cd0ae
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-03-18 14:41:15 +0100

    TRIDENT Add Dashboards.groovy

---
+++ Trident/DSLFiles/Dashboards.groovy
@@ -0,0 +1,40 @@
+folder('DataTransfer-Jobs') {
+}
+
+folder('DataTransfer-Jobs/RelativitySync') {
+}
+
+multibranchPipelineJob('DataTransfer-Jobs/RelativitySync/UpdateSplunkDashboard') {
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Trident/Jobs/Dashboards.groovy')
+        }
+    }
+    branchSources {
+        branchSource {
+            source {
+                git {
+                    remote('ssh://git@git.kcura.com:7999/dtx/relativitysync.git')
+                    credentialsId('bitbucket-repo-key')
+                    id('RelativitySync-Dashboards')
+                }
+            }
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+    configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait'()
+        traits << 'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
+            includes('*-test develop release-*')
+            excludes('')
+        }
+        traits << 'jenkins.plugins.git.traits.CleanAfterCheckoutTrait' {
+            extension(class:'hudson.plugins.git.extensions.impl.CleanCheckout')
+        }
+    }
+}
