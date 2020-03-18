670a7fd5fd6e22ae7825ed35d6d7e016b92ade25
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-03-18 15:36:58 +0100

    REL-410548 Test

--- Trident/Jobs/Dashboards.groovy
+++ Trident/Jobs/Dashboards.groovy
@@ -1,11 +1,1 @@
-echo "Creating connection to Azure Function"
-
-def post = new URL("https://relativitysyncdashboards.azurewebsites.net/api/Function").openConnection();
-def body = '{}'
-post.setDoOutput(true)
-post.setRequestMethod("POST")
-post.setRequestProperty("x-functions-key", "key")
-post.getOutputStream().write(body)
-def response = post.getResponseCode();
-
-println(response);
+PowerShell(". '..\Scripts\updateSplunkDashboard.ps1'")
---
+++ Trident/Scripts/updateSplunkDashboard.ps1
@@ -0,0 +1,6 @@
+<#
+.SYNOPSIS
+This script will be used by nightly pipeline to complie and run RelativitySync tests
+#>
+
+WriteHost Test message
