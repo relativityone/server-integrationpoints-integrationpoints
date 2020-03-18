cb8ea620de3237cf389c17676d660e92b36a018c
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-03-18 15:32:07 +0100

    REL-410548 Update

--- Trident/Jobs/Dashboards.groovy
+++ Trident/Jobs/Dashboards.groovy
@@ -2,9 +2,10 @@ echo "Creating connection to Azure Function"
 
 def post = new URL("https://relativitysyncdashboards.azurewebsites.net/api/Function").openConnection();
 def body = '{}'
+post.setDoOutput(true)
 post.setRequestMethod("POST")
 post.setRequestProperty("x-functions-key", "key")
-post.getOutputStream().write(message.getBytes("UTF-8"))
+post.getOutputStream().write(body.getBytes("UTF-8"))
 def response = post.getResponseCode();
 
 println(response);
