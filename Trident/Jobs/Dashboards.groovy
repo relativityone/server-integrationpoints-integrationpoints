d69838c5d29f4b6acdb870013093e9d997fe07f3
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-03-20 14:04:26 +0100

    REL-410548 Test

--- Trident/Jobs/Dashboards.groovy
+++ Trident/Jobs/Dashboards.groovy
@@ -1,6 +1,32 @@
-@Library('ProjectMayhem@v1') _
+#!groovy
 
-jobWithSut {
-    sutTemplate = "aio-blazingstar-eau"
-    jobScript = "Trident/Scripts/updateSplunkDashboard.ps1"
-}
+node('role-build-agent')
+    {
+        try
+        {
+            stage ('Checkout')
+            {
+                def scmVars = checkout([
+                    $class: 'GitSCM',
+                    branches: scm.branches,
+                    extensions: scm.extensions + [[$class: 'CloneOption', depth: 3, noTags: false, reference: '', shallow: false, timeout: 30], [$class: 'CleanBeforeCheckout']],
+                    userRemoteConfigs: scm.userRemoteConfigs
+                ])
+
+                dir('\\Trident\\Scripts')
+                {
+                    jenkinsHelper = load pwd() + '/JenkinsHelpers.groovy'
+                    hopperHelper = load pwd() + '/HopperHelpers.groovy'
+                    def secrets = [
+                        [ secretType: 'Secret', name: 'FunctionAuthorizationKey', version: '', envVariable: 'FunctionAuthorizationKey' ]
+                    ]
+
+                    withAzureKeyvault(azureKeyVaultSecrets: secrets,
+                        keyVaultURLOverride: 'https://relativitysynckv.vault.azure.net/')
+                    {
+                        buildOwner = hopperHelper.getBuildOwner(bbUsername, bbPassword)
+                    }
+                }
+            }
+        }
+    }
