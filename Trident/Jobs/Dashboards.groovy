fb6bc1c8586d1be3d85ddacb2f50f846a843d295
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-03-20 13:27:05 +0100

    REL-410548 Test

--- Trident/Jobs/Dashboards.groovy
+++ Trident/Jobs/Dashboards.groovy
@@ -2,11 +2,10 @@ node('role-build-agent')
 {
     try
     {
-        echo System.getProperty("user.dir")
         powershell "Write-Host (Get-Item -Path '.\').FullName"
         powershell "Get-Location"
         powershell "Get-ChildItem"
-        powershell(script: "& '.\\Trident\\Scripts\\updateSplunkDashboard.ps1'")
+        powershell returnStatus: true, script: 'Trident/Scripts/updateSplunkDashboard.ps1'
     }
     catch (err)
     {
