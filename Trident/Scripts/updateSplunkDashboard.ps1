a72f9e0ad86cbcc13222aa04b406f293e83f1573
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-03-20 16:53:56 +0100

    REL-410548 Update powershell script

--- Trident/Scripts/updateSplunkDashboard.ps1
+++ Trident/Scripts/updateSplunkDashboard.ps1
@@ -3,4 +3,30 @@
 This script will be used by Trident pipeline to update Splunk dashboard
 #>
 
+$OriginalLocation = Get-Location
+Write-Host Current location: $OriginalLocation.Path
+
+$IssuesFilePath = "../../Source/Relativity.Sync.Dashboards/Relativity.Sync.Dashboards/issues.json"
+# $Uri = "https://relativitysyncdashboards.azurewebsites.net/api/Function"
+$Uri = "http://localhost:7071/api/Function"
+
+Write-Host Reading Function Authorizarion Key from environment variables
+Set-Location Env:
+Set-Item -Path Env:FunctionAuthorizationKey -Value "zWL4b1k/EwpPnFIwshMddgYW0G/KApRg1XsAHgRpYHRgnGW4c4GTUg=="
+$FunctionAuthorizationKey = (Get-ChildItem FunctionAuthorizationKey).Value
+Write-Host Function authorization key length: $FunctionAuthorizationKey.Length
+
+Write-Host Changing current location back to: $OriginalLocation.Path
+Set-Location $OriginalLocation.Path
+
+Write-Host Issues file path: $IssuesFilePath
+Write-Host Azure Function URL: $Uri
+
+Write-Host Loading issues file
+$Body = Get-Content $IssuesFilePath
+Write-Host $Body.Length bytes has been read
+
+Write-Host Sending request
+$Response = Invoke-RestMethod $Uri -Method POST -Headers @{'x-functions-key' = $FunctionAuthorizationKey} -ContentType "application/json" -Body $Body -ErrorAction Stop
+Write-Host $Response
 
