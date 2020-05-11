ded5e55cddf053b31ead746ba2f9933b1a0403d3
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-05-11 16:53:42 +0200

    REL-426279 REL-426279 Display field-mapping warning when Object Identifier length is longer in destination than in source

---
+++ Source/Relativity.IntegrationPoints.FieldsMapping/FieldMappingValidationResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Relativity.IntegrationPoints.FieldsMapping.Models;
+
+namespace Relativity.IntegrationPoints.FieldsMapping
+{
+	public class FieldMappingValidationResult
+	{
+		public List<FieldMap> InvalidMappedFields { get; set; } = new List<FieldMap>();
+		public bool IsObjectIdentifierMapValid { get; set; }
+	}
+}
