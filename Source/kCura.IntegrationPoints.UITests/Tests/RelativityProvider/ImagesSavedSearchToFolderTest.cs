9f208de96a266ee7af500566b893fdf4e3aa4f5a
Kamil Bizoń <kamil.bizon@relativity.com>
2020-05-12 15:04:58 +0200

    REL-386517 REL-426286 Formatted comments. Removed empty line.

--- Source/kCura.IntegrationPoints.UITests/Tests/RelativityProvider/ImagesSavedSearchToFolderTest.cs
+++ Source/kCura.IntegrationPoints.UITests/Tests/RelativityProvider/ImagesSavedSearchToFolderTest.cs
@@ -43,16 +43,16 @@ namespace kCura.IntegrationPoints.UITests.Tests.RelativityProvider
 		}
 
 		//RelativityProvider_TC_RTR_IMG_01
-		//RelativityProvider_TC_RTR_IMG_02
-		//RelativityProvider_TC_RTR_IMG_03
-		//RelativityProvider_TC_RTR_IMG_04
-		//RelativityProvider_TC_RTR_IMG_05
-		//RelativityProvider_TC_RTR_IMG_06
 		[IdentifiedTestCase("3c74b1de-c6ca-41e8-81a8-944bf856c0ff", RelativityProviderModel.OverwriteModeEnum.AppendOnly, false)]
+		//RelativityProvider_TC_RTR_IMG_02
 		[IdentifiedTestCase("b2764d2b-d107-4b7a-98e4-238e8ff9f12a", RelativityProviderModel.OverwriteModeEnum.AppendOnly, true)]
+		//RelativityProvider_TC_RTR_IMG_03
 		[IdentifiedTestCase("1f72fd1f-1109-4733-8859-4d431619a6f6", RelativityProviderModel.OverwriteModeEnum.OverlayOnly, false)]
+		//RelativityProvider_TC_RTR_IMG_04
 		[IdentifiedTestCase("661e5706-e658-4483-bf1b-4924e471c260", RelativityProviderModel.OverwriteModeEnum.OverlayOnly, true)]
+		//RelativityProvider_TC_RTR_IMG_05
 		[IdentifiedTestCase("9b2f2880-d329-4b7c-a1c1-5a09cf888072", RelativityProviderModel.OverwriteModeEnum.AppendOverlay, false)]
+		//RelativityProvider_TC_RTR_IMG_06
 		[IdentifiedTestCase("8ae832f6-830f-4380-b2f9-32974003dac3", RelativityProviderModel.OverwriteModeEnum.AppendOverlay, true)]
 		[RetryOnError]
 		public void ShouldPushImages_WhenImagePrecedenceIsOriginalImages(RelativityProviderModel.OverwriteModeEnum overwrite, bool copyFilesToRepository)
@@ -79,20 +79,20 @@ namespace kCura.IntegrationPoints.UITests.Tests.RelativityProvider
 			validator.ValidateSummaryPage(generalProperties, model, SourceContext, DestinationContext, false);
 			ValidateOriginalImages(copyFilesToRepository);
 		}
-		
+
 		//RelativityProvider_TC_RTR_IMG_07
-		//RelativityProvider_TC_RTR_IMG_08
-		//RelativityProvider_TC_RTR_IMG_09
-		//RelativityProvider_TC_RTR_IMG_10
-		//RelativityProvider_TC_RTR_IMG_11
-		//RelativityProvider_TC_RTR_IMG_12
-		//RelativityProvider_TC_RTR_IMG_13
 		[IdentifiedTestCase("83c8d913-3943-4821-94f6-6d3ad07117e0", RelativityProviderModel.OverwriteModeEnum.AppendOnly, false, false)]
+		//RelativityProvider_TC_RTR_IMG_08
 		[IdentifiedTestCase("2214a1fb-9654-4982-af54-654c4d1350d4", RelativityProviderModel.OverwriteModeEnum.AppendOnly, true, false)]
+		//RelativityProvider_TC_RTR_IMG_09
 		[IdentifiedTestCase("d07d4d84-291f-4896-ae31-3cdab325a111", RelativityProviderModel.OverwriteModeEnum.OverlayOnly, false, false)]
+		//RelativityProvider_TC_RTR_IMG_10
 		[IdentifiedTestCase("839d516a-5dab-4ff0-92de-af53fc9ac414", RelativityProviderModel.OverwriteModeEnum.OverlayOnly, true, false)]
+		//RelativityProvider_TC_RTR_IMG_11
 		[IdentifiedTestCase("1a6075b6-7949-4bbc-a14c-f1acbadb3b4c", RelativityProviderModel.OverwriteModeEnum.AppendOverlay, false, false)]
+		//RelativityProvider_TC_RTR_IMG_12
 		[IdentifiedTestCase("68331cd1-a2a6-4dca-a86b-45889f3ae8e8", RelativityProviderModel.OverwriteModeEnum.AppendOverlay, true, false)]
+		//RelativityProvider_TC_RTR_IMG_13
 		[IdentifiedTestCase("89d4dcae-bec8-4fe1-85a2-04d356326b30", RelativityProviderModel.OverwriteModeEnum.AppendOverlay, true, true)]
 		[RetryOnError]
 		public void ShouldPushImages_WhenImagePrecedenceIsProducedImages(RelativityProviderModel.OverwriteModeEnum overwrite, bool copyFilesToRepository, bool includeOriginalImagesIfNotProduced )
