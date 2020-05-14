5758f57f939efeef339d8a07851336a7b0bb9e53
Kamil Makarowski <kamil.makarowski@relativity.com>
2020-05-14 12:54:43 +0200

    REL-400277 Template for regression tests

---
+++ Trident/Jobs/Reg-RIP.groovy
@@ -0,0 +1,5 @@
+@Library('ProjectMayhem@v1') _
+
+jobWithSut {
+    jobScript = "Trident/Scripts/UI-RegressionRIP.ps1"
+}
---
+++ Trident/Jobs/Reg-Sync.groovy
@@ -0,0 +1,5 @@
+@Library('ProjectMayhem@v1') _
+
+jobWithSut {
+    jobScript = "Trident/Scripts/Reg-RIP.ps1"
+}
