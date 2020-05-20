2d1cd0177ab8dd2ff47f3a83abe798e4b6aa6273
Kamil Makarowski <kamil.makarowski@relativity.com>
2020-05-20 13:53:51 +0200

    REL-400277 Turn off OneTimeTestsSetup on regression envs

--- Source/Relativity.IntegrationPoints.FunctionalTests/UiTests/DummyUiTests.cs
+++ Source/Relativity.IntegrationPoints.FunctionalTests/UiTests/DummyUiTests.cs
@@ -8,13 +8,9 @@ namespace kCura.IntegrationPoints.UITests.DummyUiTests
 	//namespace. Remove it when any of UI test will be migrated to
 	//Relativity.IntegrationPoints.FunctionalTests project
 	[TestFixture]
-	[Category(_WEB_IMPORT_EXPORT_TEST_CATEGORY)]
-	[Category(_EXPORT_TO_RELATIVITY_TEST_CATEGORY)]
 	[Category(_ONE_TIME_TESTS_SETUP)]
 	public class DummyUiTests
     {
-        private const string _EXPORT_TO_RELATIVITY_TEST_CATEGORY = "ExportToRelativity";
-		private const string _WEB_IMPORT_EXPORT_TEST_CATEGORY = "WebImportExport";
 		private const string _ONE_TIME_TESTS_SETUP = "OneTimeTestsSetup";
 
 		[Test]
