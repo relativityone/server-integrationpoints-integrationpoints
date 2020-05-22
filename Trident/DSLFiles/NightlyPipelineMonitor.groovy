3713c4d07a5d6d56adf7bb353917fcdb7aa449ce
Krzysztof Skowronek <krzysztof.skowronek@relativity.com>
2020-05-22 12:05:29 +0200

    [DEVOPS-74067] fixed folders and whitespaces

--- Trident/DSLFiles/NightlyPipelineMonitor.groovy
+++ Trident/DSLFiles/NightlyPipelineMonitor.groovy
@@ -1,7 +1,13 @@
+folder('DataTransfer-Jobs') {
+}
+
+folder('DataTransfer-Jobs/RelativitySync') {
+}
+
 buildMonitorView('DataTransfer-Jobs/RelativitySync/Nightly/Build monitor') {
     description('All nightly jobs')
     recurse(true)
     jobs {
-        regex('/release-(.*)')
-    }
+		('/release-(.*)')
+    }
 }
