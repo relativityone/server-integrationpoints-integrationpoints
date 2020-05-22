f533410fbc7a702afad24a5d314b275fbc3ef614
Krzysztof Skowronek <krzysztof.skowronek@relativity.com>
2020-05-22 14:11:28 +0200

    [DEVOPS-74067] added missing regex

--- Trident/DSLFiles/NightlyPipelineMonitor.groovy
+++ Trident/DSLFiles/NightlyPipelineMonitor.groovy
@@ -5,9 +5,9 @@ folder('DataTransfer-Jobs/RelativitySync') {
 }
 
 buildMonitorView('DataTransfer-Jobs/RelativitySync/Nightly/Build monitor') {
-    description('All nightly jobs')
+	description('All nightly jobs')
     recurse(true)
     jobs {
-		('/release-(.*)')
+		regex('/release-(.*)')
     }
 }
