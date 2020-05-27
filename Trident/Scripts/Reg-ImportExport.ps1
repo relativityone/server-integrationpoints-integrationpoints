0cf5425736bc7831d5cf6b350a732d3e18fedc5b
Kamil Makarowski <kamil.makarowski@relativity.com>
2020-05-27 11:10:54 +0200

    REL-400277 remove test filter

--- Trident/Scripts/Reg-ImportExport.ps1
+++ Trident/Scripts/Reg-ImportExport.ps1
@@ -16,6 +16,6 @@ $TaskRunner = Resolve-Path -Path build.ps1
 
 &($TaskRunner) -Configuration Release
 
-&($TaskRunner) RegTest -Configuration Release -TestFilter "cat == WebImportExport && cat != NotWorkingOnRegressionEnvironment && cat == Test"
+&($TaskRunner) RegTest -Configuration Release -TestFilter "cat == WebImportExport && cat != NotWorkingOnRegressionEnvironment"
 
 Remove-Module Build-Util
