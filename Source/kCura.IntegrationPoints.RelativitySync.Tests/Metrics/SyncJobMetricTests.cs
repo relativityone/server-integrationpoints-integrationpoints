0abfa15077f6d1c6afc4e8d43fb782301d1dde15
Kamil Makarowski <kamil.makarowski@relativity.com>
2020-06-05 13:03:26 +0200

    REL-411371 review remarks

--- Source/kCura.IntegrationPoints.RelativitySync.Tests/Metrics/SyncJobMetricTests.cs
+++ Source/kCura.IntegrationPoints.RelativitySync.Tests/Metrics/SyncJobMetricTests.cs
@@ -2,7 +2,6 @@
 using kCura.IntegrationPoints.RelativitySync.Metrics;
 using kCura.ScheduleQueue.Core;
 using Moq;
-using NSubstitute;
 using NUnit.Framework;
 using Relativity.API;
 using System;
@@ -28,13 +27,13 @@ namespace kCura.IntegrationPoints.RelativitySync.Tests.Metrics
 			_metricsFactoryFake.Setup(x => x.CreateScheduleJobFailedMetric(It.IsAny<Job>())).Returns(_metricFake.Object);
 			_metricsFactoryFake.Setup(x => x.CreateScheduleJobStartedMetric(It.IsAny<Job>())).Returns(_metricFake.Object);
 
-			IAPILog log = Substitute.For<IAPILog>();
+			Mock<IAPILog> logFake = new Mock<IAPILog>();
 
-			_sut = new SyncJobMetric(_metricsFactoryFake.Object, log);
+			_sut = new SyncJobMetric(_metricsFactoryFake.Object, logFake.Object);
 		}
 
 		[Test]
-		public void SendJobCompletedAsync_ShouldTrowSyncMetricException_WhenExceptionOccurs()
+		public void SendJobCompletedAsync_ShouldThrowSyncMetricException_WhenExceptionOccurs()
 		{
 			// Arrange
 			_metricFake.Setup(x => x.SendAsync()).Throws<Exception>();
@@ -47,7 +46,7 @@ namespace kCura.IntegrationPoints.RelativitySync.Tests.Metrics
 		}
 
 		[Test]
-		public void SendJobFailedAsync_ShouldTrowSyncMetricException_WhenExceptionOccurs()
+		public void SendJobFailedAsync_ShouldThrowSyncMetricException_WhenExceptionOccurs()
 		{
 			// Arrange
 			_metricFake.Setup(x => x.SendAsync()).Throws<Exception>();
@@ -60,7 +59,7 @@ namespace kCura.IntegrationPoints.RelativitySync.Tests.Metrics
 		}
 
 		[Test]
-		public void SendJobStartedAsync_ShouldTrowSyncMetricException_WhenExceptionOccurs()
+		public void SendJobStartedAsync_ShouldThrowSyncMetricException_WhenExceptionOccurs()
 		{
 			// Arrange
 			_metricFake.Setup(x => x.SendAsync()).Throws<Exception>();
