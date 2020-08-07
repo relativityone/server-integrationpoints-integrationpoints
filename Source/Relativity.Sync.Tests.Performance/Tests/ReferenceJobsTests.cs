b0ad786841766ae245687f1ff19da93852e559ce
Kamil Makarowski <kamil.makarowski@relativity.com>
2020-08-07 15:40:18 +0200

    REL-414479 Add Nightly-Performance pipeline

---
+++ Source/Relativity.Sync.Tests.Performance/Tests/ReferenceJobsTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Relativity.Sync.Tests.Performance.Helpers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Relativity.Sync.Tests.Performance.Tests
+{
+	[TestFixture]
+	[Category("ReferencePerformance")]
+	public class ReferenceJobsTests : PerformanceTestBase
+	{
+		public ReferenceJobsTests() : base(WorkspaceType.ARM, "1066387_Small_jobs_tests_20200603140140.zip", null)
+		{
+		}
+
+		public static IEnumerable<TestCaseData> Cases()
+		{
+#pragma warning disable RG2009 // Hardcoded Numeric Value
+
+			PerformanceTestCase[] testCases = new[]
+			{
+				new PerformanceTestCase
+				{
+					TestCaseName = "Small-1a",
+					ExpectedItemsTransferred = 10,
+					NumberOfMappedFields = 150
+				}
+			};
+
+			return testCases.Select(x => new TestCaseData(x)
+			{
+				TestName = $"{x.TestCaseName} ({x.ExpectedItemsTransferred} docs, {x.NumberOfMappedFields} fields)"
+			});
+
+#pragma warning restore RG2009 // Hardcoded Numeric Value
+		}
+
+		[TestCaseSource(nameof(Cases))]
+		public async Task RunJob(PerformanceTestCase testCase)
+		{
+			await RunTestCaseAsync(testCase).ConfigureAwait(false);
+		}
+	}
+}
