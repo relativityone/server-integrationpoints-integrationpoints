b1538b206ed6775ff4e1228d9aac57e347be8f2b
Tomasz Pęczek <tomasz.peczek@relativity.com>
2020-08-13 12:09:18 +0200

    [REL-456825] Add "AppendToWorkspace" command line parameter

--- Source/Relativity.Sync.WorkspaceGenerator/Relativity.Sync.WorkspaceGenerator/Settings/LoadFromSettingsFileArguments.cs
+++ Source/Relativity.Sync.WorkspaceGenerator/Relativity.Sync.WorkspaceGenerator/Settings/LoadFromSettingsFileArguments.cs
@@ -8,7 +8,7 @@ namespace Relativity.Sync.WorkspaceGenerator.Settings
 		[Option("file", Required = true, HelpText = "Path to configuration file to be used as configuration source instead of command line args.")]
 		public string InputSettingsFile { get; set; }
 
-		[Option("appendToWorkspace", Required = false, HelpText = "Path to configuration file to be used as configuration source instead of command line args.")]
+		[Option("appendToWorkspace", Required = false, HelpText = "Flag determining if an existing workspace should be appended with new documents.")]
 		public bool AppendToWorkspace { get; set; }
 	}
 }
