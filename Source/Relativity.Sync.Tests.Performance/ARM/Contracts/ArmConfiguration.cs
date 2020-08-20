2c51b5e4be7e4dbf2edf23cc8f0d3203185d4ab2
Kamil Makarowski <kamil.makarowski@relativity.com>
2020-08-20 10:16:21 +0200

    REL-451256 Remove WebAPIPath from newest ARM version and change version comparison

--- Source/Relativity.Sync.Tests.Performance/ARM/Contracts/ArmConfiguration.cs
+++ Source/Relativity.Sync.Tests.Performance/ARM/Contracts/ArmConfiguration.cs
@@ -5,17 +5,15 @@ namespace Relativity.Sync.Tests.Performance.ARM.Contracts
 {
 	public class ArmConfiguration
 	{
-		public string RelativityWebApiUrl { get; set; }
 		public IEnumerable<ArchiveLocation> ArmArchiveLocations { get; set; }
 		public IEnumerable<object> EmailNotificationSettings { get; set; }
 
-		public static ContractEnvelope<ArmConfiguration> GetRequest(string webAPIPath, string location)
+		public static ContractEnvelope<ArmConfiguration> GetRequest(string location)
 		{
 			return new ContractEnvelope<ArmConfiguration>
 			{
 				Contract = new ArmConfiguration
 				{
-					RelativityWebApiUrl = webAPIPath,
 					ArmArchiveLocations = new List<ArchiveLocation>
 					{
 						new ArchiveLocation()
