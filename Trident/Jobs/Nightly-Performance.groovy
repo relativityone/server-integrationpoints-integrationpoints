89648bcdfa5642ea9240e730cfae5da771f4d2e5
Kamil Makarowski <kamil.makarowski@relativity.com>
2020-08-24 14:12:50 +0200

    REL-414479 Publish Test Results to AzureStorage Table

--- Trident/Jobs/Nightly-Performance.groovy
+++ Trident/Jobs/Nightly-Performance.groovy
@@ -1,9 +1,9 @@
 @Library('ProjectMayhem@v1') _
 
 jobWithSut {
-    //slackChannel = "sync_trident"
+    slackChannel = "sync_trident"
     sutTemplate = "LanceleafAA1"
-	//relativityBranch = "develop"
+	relativityBranch = "develop"
     jobScript = "Trident/Scripts/nightly-performance.ps1"
-    //cron = "0 3 * * *"
+    cron = "0 3 * * *"
 }
