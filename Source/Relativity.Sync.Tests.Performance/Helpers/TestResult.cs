46ef4eb115702f9a8c073c63169b98e8d5c367b4
Kamil Makarowski <kamil.makarowski@relativity.com>
2020-08-24 12:57:23 +0200

    REL-414479 Publish Test Results to AzureStorage Table

---
+++ Source/Relativity.Sync.Tests.Performance/Helpers/TestResult.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+
+namespace Relativity.Sync.Tests.Performance.Helpers
+{
+	public class TestResult : TableEntity
+	{
+		public double Duration { get; set; }
+
+		public TestResult()
+		{ }
+
+		public TestResult(string testName, string buildId)
+		{
+			PartitionKey = testName;
+			RowKey = buildId;
+		}
+	}
+}
--- Source/Relativity.Sync.Tests.Performance/Tests/ReferenceJobsTests.cs
+++ Source/Relativity.Sync.Tests.Performance/Tests/ReferenceJobsTests.cs
@@ -5,14 +5,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using EnvironmentVariable = System.Environment;
+
 namespace Relativity.Sync.Tests.Performance.Tests
 {
 	[TestFixture]
 	[Category("ReferencePerformance")]
 	public class ReferenceJobsTests : PerformanceTestBase
 	{
+		private readonly AzureTableHelper _tableHelper;
+
+		public const string _PERFORMANCE_RESULTS_TABLE_NAME = "SyncReferenceJobsPerformanceTestsResults";
+
 		public ReferenceJobsTests() : base(WorkspaceType.ARM, "Performance_Reference_Workspace.zip", null)
 		{
+			_tableHelper = AzureTableHelper.CreateFromTestConfig();
 		}
 
 		public static IEnumerable<TestCaseData> Cases()
@@ -43,6 +50,22 @@ namespace Relativity.Sync.Tests.Performance.Tests
 		public async Task RunJob(PerformanceTestCase testCase)
 		{
 			await RunTestCaseAsync(testCase).ConfigureAwait(false);
+
+			await PublishTestResult(testCase).ConfigureAwait(false);
+		}
+
+		private Task PublishTestResult(PerformanceTestCase testCase)
+		{
+			TestResult testResult = new TestResult(
+				testCase.TestCaseName,
+				EnvironmentVariable.GetEnvironmentVariable("BUILD_ID"))
+			{
+				Duration = _testTimes[testCase.TestCaseName].TotalSeconds
+			};
+
+			return _tableHelper.InsertAsync(
+				_PERFORMANCE_RESULTS_TABLE_NAME,	
+				testResult);
 		}
 	}
 }
