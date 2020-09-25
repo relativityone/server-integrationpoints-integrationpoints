c92e8e1932c895d5964a25cf43948c052f97bcd0
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-09-25 15:17:08 +0200

    REL-478106 REL-478106 UT

--- Source/kCura.IntegrationPoints.FtpProvider.Tests/SftpConnectorTests.cs
+++ Source/kCura.IntegrationPoints.FtpProvider.Tests/SftpConnectorTests.cs
@@ -12,29 +12,43 @@ namespace kCura.IntegrationPoints.FtpProvider.Tests
 	public class SftpConnectorTests
 	{
 		private Mock<IHostValidator> _hostValidatorMock;
-		private Mock<IAPILog> _loggerMock;
+		private Mock<IAPILog> _loggerFake;
+
+		private const string _HOST = "1.2.3.4";
+		private const int _PORT = 22;
+		private const string _USER = "User";
+		private const string _PASSWORD = "1234";
 
 		[SetUp]
 		public void SetUp()
 		{
 			_hostValidatorMock = new Mock<IHostValidator>();
-			_loggerMock = new Mock<IAPILog>();
+			_loggerFake = new Mock<IAPILog>();
 		}
 
 		[Test]
 		public void Constructor_ShouldNotThrow()
 		{
 			// Arrange
-			const string host = "1.2.3.4";
-			const int port = 22;
-			const string user = "user";
-			const string password = "1234";
-
-			Func<SftpConnector> createObjectAction = () => new SftpConnector(host, port, user, password, _hostValidatorMock.Object, _loggerMock.Object);
+			Func<SftpConnector> createObjectAction = () => new SftpConnector(_HOST, _PORT, _USER, _PASSWORD, _hostValidatorMock.Object, _loggerFake.Object);
 
 			// Act & Assert
 			SftpConnector sftpConnector = createObjectAction();
 			sftpConnector.Should().NotBeNull();
+		}
+
+		[Test]
+		public void TestConnection_ShouldValidateHost()
+		{
+			// Arrange
+			SftpConnector sut = new SftpConnector(_HOST, _PORT, _USER, _PASSWORD, _hostValidatorMock.Object, _loggerFake.Object);
+
+			// Act
+			bool result = sut.TestConnection();
+
+			// Assert
+			result.Should().BeFalse();
+			_hostValidatorMock.Verify(x => x.CanConnectTo(_HOST));
 		}
 	}
 }
