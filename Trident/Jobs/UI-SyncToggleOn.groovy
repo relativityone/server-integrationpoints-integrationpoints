7da4cb646d22178843476948f0030ce1cb2a5dc6
Kamil Makarowski <kamil.makarowski@relativity.com>
2020-09-30 08:58:26 +0000

    Merge pull request #2155 in IN/integrationpoints from REL-439663-branches-review-lanceleaf to release-11.2-lanceleaf
    Squashed commit of the following:
    
    commit 43c405c47f75ec4c45318c598683a40bfb626929
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Tue Sep 29 13:03:32 2020 +0200
    
        REL-439663 Review RIP branches
    
    commit a57857415f5e5865ea840e981c9ebfca9a9ee650
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Tue Sep 29 12:46:22 2020 +0200
    
        REL-439663 Review RIP branches
    
    commit b3c68d25ae5ca94e205a1591640694fb9838d70e
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Tue Sep 29 12:26:15 2020 +0200
    
        REL-439663 Review RIP branches

--- Trident/Jobs/UI-SyncToggleOn.groovy
+++ Trident/Jobs/UI-SyncToggleOn.groovy
@@ -2,7 +2,7 @@
 
 jobWithSut {
     slackChannel = "cd_trident_rip"
-    sutTemplate = "aio-lanceleaf-eau"
+    sutTemplate = "aio-lanceleaf-latest"
     relativityBranch = "release-11.2-lanceleaf"
     jobScript = "Trident/Scripts/UI-Sync.ps1 -Toggle On"
     cron = "0 1 * * *"
