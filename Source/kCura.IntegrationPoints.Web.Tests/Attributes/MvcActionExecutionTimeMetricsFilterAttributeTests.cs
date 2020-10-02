458682cde14f6e75ff00f67cb5006db33d8a27a7
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-10-02 08:29:58 +0200

    REL-470580 Add unit tests

---
+++ Source/kCura.IntegrationPoints.Web.Tests/Attributes/MvcActionExecutionTimeMetricsFilterAttributeTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using Castle.Windsor;
+using FluentAssertions;
+using kCura.IntegrationPoints.Web.Attributes;
+using kCura.IntegrationPoints.Web.Metrics;
+using Moq;
+using NUnit.Framework;
+
+namespace kCura.IntegrationPoints.Web.Tests.Attributes
+{
+	[TestFixture]
+	public class MvcActionExecutionTimeMetricsFilterAttributeTests
+	{
+		private Mock<IWindsorContainer> _containerFake;
+		private Mock<IDateTimeHelper> _dateTimeHelperFake;
+		private Mock<IControllerActionExecutionTimeMetrics> _controllerActionExecutionTimeMetricsMock;
+
+		private const string _ITEM_KEY_NAME = "MvcActionExecutionStartTimestamp";
+		
+		[SetUp]
+		public void SetUp()
+		{
+			_dateTimeHelperFake = new Mock<IDateTimeHelper>();
+			_controllerActionExecutionTimeMetricsMock = new Mock<IControllerActionExecutionTimeMetrics>();
+
+			_containerFake = new Mock<IWindsorContainer>();
+			_containerFake.Setup(x => x.Resolve<IDateTimeHelper>()).Returns(_dateTimeHelperFake.Object);
+			_containerFake.Setup(x => x.Resolve<IControllerActionExecutionTimeMetrics>()).Returns(_controllerActionExecutionTimeMetricsMock.Object);
+		}
+
+		[Test]
+		public void OnActionExecuting_ShouldAddTimestampToActionContext()
+		{
+			// Arrange
+			DateTime timestamp = DateTime.Now;
+			_dateTimeHelperFake.Setup(x => x.Now()).Returns(timestamp);
+
+			ActionExecutingContext context = PrepareActionExecutingContext();
+			MvcActionExecutionTimeMetricsFilterAttribute sut = PrepareSut();
+
+			// Act
+			sut.OnActionExecuting(context);
+
+			// Assert
+			context.HttpContext.Items.Contains(_ITEM_KEY_NAME).Should().BeTrue();
+			context.HttpContext.Items[_ITEM_KEY_NAME].Should().BeAssignableTo<DateTime>().Which.Should().Be(timestamp);
+		}
+
+		[Test]
+		public void OnResultExecuted_ShouldReportExecutionTime()
+		{
+			// Arrange
+			DateTime startTime = DateTime.Now;
+			const string rawUrl = "/Relativity/App/RIP-GUID/MyAction?param=value";
+			const string expectedMethod = "GET";
+
+			ResultExecutedContext context = PrepareResultExecutedContext(startTime, rawUrl, expectedMethod);
+			MvcActionExecutionTimeMetricsFilterAttribute sut = PrepareSut();
+
+			// Act
+			sut.OnResultExecuted(context);
+
+			// Assert
+			_controllerActionExecutionTimeMetricsMock.Verify(x => x.LogExecutionTime(
+				It.Is<string>(url => url == "/Relativity/App/RIP-GUID/MyAction"),
+				It.Is<DateTime>(dateTime => dateTime == startTime),
+				It.Is<string>(method => method == expectedMethod)));
+		}
+
+		private MvcActionExecutionTimeMetricsFilterAttribute PrepareSut()
+		{
+			return new MvcActionExecutionTimeMetricsFilterAttribute()
+			{
+				Container = _containerFake.Object
+			};
+		}
+
+		private ActionExecutingContext PrepareActionExecutingContext()
+		{
+			Mock<HttpContextBase> httpContext = new Mock<HttpContextBase>();
+			Dictionary<string, object> items = new Dictionary<string, object>();
+			httpContext.SetupGet(x => x.Items).Returns(items);
+
+			ActionExecutingContext context = new ActionExecutingContext()
+			{
+				HttpContext = httpContext.Object
+			};
+
+			return context;
+		}
+
+		private ResultExecutedContext PrepareResultExecutedContext(DateTime startTime, string rawUrl, string httpMethod)
+		{
+			Dictionary<string, object> items = new Dictionary<string, object>()
+			{
+				{ _ITEM_KEY_NAME, startTime }
+			};
+
+			Mock<HttpContextBase> httpContext = new Mock<HttpContextBase>();
+			httpContext.SetupGet(x => x.Items).Returns(items);
+			Mock<HttpRequestBase> request = new Mock<HttpRequestBase>();
+			request.SetupGet(x => x.RawUrl).Returns(rawUrl);
+			request.SetupGet(x => x.HttpMethod).Returns(httpMethod);
+			httpContext.SetupGet(x => x.Request).Returns(request.Object);
+
+			ResultExecutedContext context = new ResultExecutedContext()
+			{
+				HttpContext = httpContext.Object
+			};
+
+			return context;
+		}
+	}
+}
---
+++ Source/kCura.IntegrationPoints.Web.Tests/Metrics/ControllerActionExecutionTimeMetricsTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using kCura.IntegrationPoints.Common.Metrics;
+using kCura.IntegrationPoints.Web.Metrics;
+using Moq;
+using NUnit.Framework;
+
+namespace kCura.IntegrationPoints.Web.Tests.Metrics
+{
+	[TestFixture]
+	public class ControllerActionExecutionTimeMetricsTests
+	{
+		private Mock<IDateTimeHelper> _dateTimeFake;
+		private Mock<IRipMetrics> _ripMetricsMock;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_dateTimeFake = new Mock<IDateTimeHelper>();
+			_ripMetricsMock = new Mock<IRipMetrics>();
+		}
+
+		[Test]
+		public void LogExecutionTime_ShouldReportTimedOperation()
+		{
+			// Arrange
+			const string metricName = "IntegrationPoint.CustomPage.ResponseTime";
+			DateTime startTime = new DateTime(2020, 10, 1, 10, 0, 0);
+			DateTime endTime = startTime.AddSeconds(1);
+			TimeSpan expectedDuration = endTime - startTime;
+			_dateTimeFake.Setup(x => x.Now()).Returns(endTime);
+			const string url = "/fake/url";
+			const string method = "POST";
+			ControllerActionExecutionTimeMetrics sut = new ControllerActionExecutionTimeMetrics(_dateTimeFake.Object, _ripMetricsMock.Object);
+
+			// Act
+			sut.LogExecutionTime(url, startTime, method);
+
+			// Assert
+			_ripMetricsMock.Verify(x => x.TimedOperation(
+				It.Is<string>(name => name == metricName),
+				It.Is<TimeSpan>(time => time == expectedDuration),
+				It.Is<Dictionary<string, object>>(dictionary => dictionary.ContainsKey("ActionURL") &&
+				                                                dictionary["ActionURL"].ToString() == url &&
+																dictionary.ContainsKey("ResponseTimeMs") &&
+																(long)dictionary["ResponseTimeMs"] == (long)expectedDuration.TotalMilliseconds &&
+																dictionary.ContainsKey("Method") &&
+																dictionary["Method"].ToString() == method
+				                                                )));
+		}
+	}
+}
