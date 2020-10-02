c5ffeeea396826e070bfc8cf2966fd164d4217d9
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-10-02 08:54:53 +0200

    REL-470580 Add more unit tests

---
+++ Source/kCura.IntegrationPoints.Common.Tests/Metrics/RipMetricsTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using kCura.IntegrationPoints.Common.Metrics;
+using kCura.IntegrationPoints.Common.Metrics.Sink;
+using Moq;
+using NUnit.Framework;
+
+namespace kCura.IntegrationPoints.Common.Tests.Metrics
+{
+	[TestFixture]
+	public class RipMetricsTests
+	{
+		private RipMetrics _sut;
+		private Mock<IRipMetricsSink> _sinkMock;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_sinkMock = new Mock<IRipMetricsSink>();
+			_sut = new RipMetrics(new List<IRipMetricsSink>()
+			{
+				_sinkMock.Object
+			});
+		}
+
+
+		[Test]
+		public void TimedOperation_ShouldCreateAndLogMetric()
+		{
+			// Arrange
+			const string name = "Timed Operation";
+			TimeSpan duration = TimeSpan.FromSeconds(1);
+			const string propertyKey = "prop 1";
+			const string propertyValue = "value 1";
+			Dictionary<string, object> props = new Dictionary<string, object>()
+			{
+				{ propertyKey, propertyValue }
+			};
+
+			// Act
+			_sut.TimedOperation(name, duration, props);
+
+			// Assert
+			_sinkMock.Verify(x => x.Log(It.Is<RipMetric>(metric =>
+				metric.Name == name && 
+				metric.Type == RipMetricType.TimedOperation &&
+				metric.CustomData.ContainsKey(propertyKey) &&
+				metric.CustomData[propertyKey].ToString() == propertyValue
+			)), Times.Once);
+		}
+	}
+}
---
+++ Source/kCura.IntegrationPoints.Common.Tests/Metrics/Sink/SplunkMetricsSinkTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using kCura.IntegrationPoints.Common.Metrics;
+using kCura.IntegrationPoints.Common.Metrics.Sink;
+using Moq;
+using NUnit.Framework;
+using Relativity.API;
+
+namespace kCura.IntegrationPoints.Common.Tests.Metrics.Sink
+{
+	[TestFixture]
+	public class SplunkMetricsSinkTests
+	{
+		private Mock<IAPILog> _loggerMock;
+		private SplunkRipMetricSink _sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_loggerMock = new Mock<IAPILog>();
+			_sut = new SplunkRipMetricSink(_loggerMock.Object);
+		}
+
+		[Test]
+		public void Log_ShouldWriteMetricToSplunk()
+		{
+			// Arrange
+			RipMetric metric = RipMetric.TimedOperation("Metric Name", TimeSpan.FromSeconds(1), Guid.NewGuid().ToString());
+			const string propertyKey = "key";
+			const string propertyValue = "value";
+			metric.CustomData.Add(propertyKey, propertyValue);
+
+			// Act
+			_sut.Log(metric);
+
+			// Assert
+			_loggerMock.Verify(x => x.LogInformation(
+				It.Is<string>(template => template == "Logging metric '{MetricName}' with properties: {@MetricProperties}"),
+				It.Is<string>(name => name == metric.Name),
+				It.Is<RipMetric>(actualMetric => actualMetric == metric)));
+		}
+	}
+}
