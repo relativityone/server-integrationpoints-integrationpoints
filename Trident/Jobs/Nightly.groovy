6a0e080e7df40245afa2026bd0772a2540be9bdb
Tomasz Pęczek <tomasz.peczek@relativity.com>
2020-10-09 11:27:33 +0200

    Changing SUT template in nightly builds from aio-lanceleaf-latest to aio-lanceleaf-eau as release-11.2-lanceleaf has to lowe version

--- Trident/Jobs/Nightly.groovy
+++ Trident/Jobs/Nightly.groovy
@@ -2,7 +2,7 @@
 
 jobWithSut {
     slackChannel = "cd_trident_rip"
-    sutTemplate = "aio-lanceleaf-latest"
+    sutTemplate = "aio-lanceleaf-eau"
     relativityBranch = "release-11.2-lanceleaf"
     jobScript = "Trident/Scripts/Nightly.ps1"
     cron = "0 1 * * *"
--- Trident/Jobs/UI-SyncToggleOff.groovy
+++ Trident/Jobs/UI-SyncToggleOff.groovy
@@ -2,7 +2,7 @@
 
 jobWithSut {
     slackChannel = "cd_trident_rip"
-    sutTemplate = "aio-lanceleaf-latest"
+    sutTemplate = "aio-lanceleaf-eau"
     relativityBranch = "release-11.2-lanceleaf"
     jobScript = "Trident/Scripts/UI-Sync.ps1 -Toggle Off"
     cron = "0 1 * * *"
