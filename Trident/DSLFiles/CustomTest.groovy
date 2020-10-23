676e94121c03f4b58dd4c6f61bf452fe00b93b7d
Kamil Makarowski <kamil.makarowski@relativity.com>
2020-10-23 15:38:26 +0200

    REL-485860 Truncate name due windows max length path error

--- Trident/DSLFiles/CustomTest.groovy
+++ Trident/DSLFiles/CustomTest.groovy
@@ -1,4 +1,4 @@
-pipelineJob("IntegrationPointsTest") {
+pipelineJob("Test") {
     description('Help: JobScript - Trident/Scripts/Custom-Test.ps1 -TestFilter "cat == Test"; SutTemplate - https://app.hopper.relativity.com/#/templates')
     
     parameters {
