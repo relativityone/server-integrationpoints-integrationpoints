c9ca95939cd7d4abe33172ab0e2f1693ab3d50a2
Kamil Makarowski <kamil.makarowski@relativity.com>
2020-10-23 07:54:54 +0000

    Merge pull request #2185 in IN/integrationpoints from REL-485860-test to develop
    Squashed commit of the following:
    
    commit 1b724b0d07dd641e18886dd5526cc1d19cd23ff1
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Thu Oct 22 11:21:54 2020 +0200
    
        REL-485860 Cleanup old CustomTest pipeline
    
    commit 156fe52c90487bc4f1ca75996258d1c96e3ac482
    Merge: 6603686c1 468ac1bfa
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Thu Oct 22 11:18:05 2020 +0200
    
        REL-485860 Merge branch 'develop' into REL-485860-test
    
    commit 6603686c13dee48808ba6e20175e3374090b5e7a
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Wed Oct 21 12:56:03 2020 +0200
    
        REL-485860 Rename files
    
    commit e2a010cda7b3969cc7cecf7919c19305cda25038
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Wed Oct 21 11:23:45 2020 +0200
    
        REL-485860 Create shorter job name
    
    commit ce118880257d55a6aa4c0c3d08e93a2b61b3264f
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Wed Oct 21 10:39:52 2020 +0200
    
        REL-485860 move to another folder
    
    commit 6f17160e89b95e31f49ceb72aed4f24c12d1e80d
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Tue Oct 20 15:02:32 2020 +0200
    
        REL-485860 Reduce pipeline path length
    
    commit 26a7498ce1c6f3dc4aa2784d01388f10cb4a399f
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Tue Oct 20 14:28:17 2020 +0200
    
        REL-485860 Path exceed maximum length
    
    commit 5a15b3acf52835b9a59b29153028b58ad843a2c1
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Tue Oct 20 10:32:23 2020 +0200
    
        REL-485860 Create CustomTest pipeline
    
    commit 77119a6188a2fc7b3ded18750c90ae130196d297
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Mon Oct 19 12:57:51 2020 +0200
    
        REL-485860 Test
    
    commit 76a783a5c4d23c677a99ee028ffb232e3da4d1d0
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Mon Oct 19 11:54:01 2020 +0200
    
        REL-485860 Revert changes and one-line in Custom-Test.ps1
    
    commit d622fa65571c72879763ff0db485649cf15198b0
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Mon Oct 19 10:56:11 2020 +0200
    
        REL-485860 nex changes
    
    commit 34c3d9e5aae3b64ad821f98ed06b1bad23241b05
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Mon Oct 19 10:08:12 2020 +0200
    
        REL-485860 Run specific tasks
    
    commit 9fe877736d977ab571684e7463db2688332070d1
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Fri Oct 16 17:44:17 2020 +0200
    
        REL-485860 One more try to run tests in one line
    
    commit a125bec982a5dfb065a6fd8f9800548ec9614483
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Fri Oct 16 16:51:24 2020 +0200
    
        REL-485860 Revert changes to previously run Custom-Test.ps1 script
    
    commit 827238f498aec64afc866536236bcc08fb6a187a
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Fri Oct 16 15:47:05 2020 +0200
    
        REL-485860 Tests depends on Default
    
    commit bbf6e344607747b6acf3a4c00e209c7e21161a6e
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Fri Oct 16 12:16:23 2020 +0200
    
        REL-485860 change SUTTemplate
    
    commit b44923f9e53a5e116b77b9e1c07dd3210ebca0e5
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Fri Oct 16 11:28:10 2020 +0200
    
        REL-485860 Add failed test statistics to Trident Pipeline summary

--- Trident/DSLFiles/CustomTest.groovy
+++ Trident/DSLFiles/CustomTest.groovy
@@ -1,41 +1,36 @@
-folder('IntegrationPoints-Jobs') {
-}
-
-multibranchPipelineJob('IntegrationPoints-Jobs/IntegrationPoints-Nightly/IntegrationPoints-Custom-Test') {
-    factory {
-        workflowBranchProjectFactory {
-            scriptPath('Trident/Jobs/CustomTest.groovy')
-        }
+pipelineJob("IntegrationPointsTest") {
+    description('Help: JobScript - Trident/Scripts/Custom-Test.ps1 -TestFilter "cat == Test"; SutTemplate - https://app.hopper.relativity.com/#/templates')
+    
+    parameters {
+        stringParam('SlackChannel', '', 'The slack channel to notify on failure.')
+        stringParam('SutTemplate', '', 'The SUT template to deploy, e.g. aio-lanceleaf-eau (-latest tag can be used as well).')
+        stringParam('RelativityBranch', '', 'Cannot be used with relativityVersionPath. The branch of Relativity to upgrade to. Leave blank to skip Relativity upgrade.')
+        stringParam('InvariantBranch', '', 'Cannot be used with invariantVersionPath. The branch of Invariant to upgrade to. Leave blank to skip Invariant upgrade.')
+        stringParam('RelativityVersionPath', '', 'Cannot be used with relativityBranch. The path for the version of Relativity to upgrade to on bld-pkgs. Bld-pkgs\\\\Packages or bld-pkgs\\\\Release can be used, but path must be the folder that contains the .exe, see https://einstein.kcura.com/x/8cH9D for more information.')
+        stringParam('InvariantVersionPath', '', 'Cannot be used with invariantBranch. The path for the version of Invariant to upgrade to on bld-pkgs. Bld-pkgs\\\\Packages or bld-pkgs\\\\Release can be used, but path must be the folder that contains the .exe, see https://einstein.kcura.com/x/8cH9D for more information.')
+        stringParam('CaatVersion', '', 'The version of CAAT to upgrade to. Leave blank to skip CAAT upgrade.')
+        stringParam('JobScript', '', 'The script you want to run after getting a SUT.')
+        booleanParam('EnableTestMode', false, 'Enable Test Mode if you need Audience. Testing Kepler endpoints to be hosted.')
+        booleanParam('NotifyOnSuccess', false, 'This will send a slack message on each success of the pipeline.')
+        stringParam('Branch', '', 'The branch of this repository you want to checkout.')
     }
-    branchSources {
-        branchSource {
-            source {
+    definition {
+        cpsScm {
+            scm {
                 git {
-                    remote('ssh://git@git.kcura.com:7999/in/integrationpoints.git')
-                    credentialsId('bitbucket-repo-key')
-                    id('IntegrationPoints-Custom-Test')
+                    remote {
+                        name('origin')
+                        url('ssh://git@git.kcura.com:7999/in/integrationpoints.git')
+                        credentials('bitbucket-repo-key')
+                    }
+                    branch('*/${Branch}')
+                    extensions {
+                        cleanAfterCheckout()
+                    }
                 }
             }
-        }
-    }
-    orphanedItemStrategy {
-        discardOldItems {
-            daysToKeep(1)
-            numToKeep(10)
-        }
-    }
-    configure {
-        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
-        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait'()
-        traits << 'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
-            includes('*-test')
-            excludes('')
-        }
-        traits << 'jenkins.plugins.git.traits.CleanAfterCheckoutTrait' {
-            extension(class:'hudson.plugins.git.extensions.impl.CleanCheckout')
-        }
 
-        def namedBranchStrategies = it / sources / data / 'jenkins.branch.BranchSource' / buildStrategies / 'jenkins.branch.buildstrategies.basic.AllBranchBuildStrategyImpl' / 'strategies'
-        namedBranchStrategies / 'jenkins.branch.buildstrategies.basic.SkipInitialBuildOnFirstBranchIndexing'
+            scriptPath('Trident/Jobs/CustomTest_NEW.groovy')
+        }
     }
 }
--- psakefile.ps1
+++ psakefile.ps1
@@ -45,7 +45,7 @@ Task Test -Description "Run tests that don't require a deployed environment." {
 
 Task FunctionalTest -Depends OneTimeTestsSetup -Description "Run tests that require a deployed environment." {
     $LogPath = Join-Path $LogsDir "FunctionalTestResults.xml"
-    Invoke-Tests -WhereClause "(namespace =~ FunctionalTests || namespace =~ /Tests\.Integration[\$\.]/ || namespace =~ E2ETests) && cat != NightlyOnly && cat != NotWorkingOnTrident" -OutputFile $LogPath
+    Invoke-Tests -WhereClause "(namespace =~ FunctionalTests || namespace =~ /Tests\.Integration[\$\.]/ || namespace =~ E2ETests) && cat != NotWorkingOnTrident" -OutputFile $LogPath
 }
 
 Task Sign -Description "Sign all files" {
@@ -110,7 +110,7 @@ Task Help -Alias ? -Description "Display task information" {
 }
 
 Task OneTimeTestsSetup -Description "Should be run always before running tests that require setup in deployed environment." {
-    $LogPath = Join-Path $LogsDir "OneTimeTestsSetupResults.xml"
+    $LogPath = Join-Path $LogsDir "OneTimeSetupTestResults.xml"
     Invoke-Tests -WhereClause "cat == OneTimeTestsSetup" -OutputFile $LogPath
 }
 
@@ -124,8 +124,7 @@ Task MyTest -Depends OneTimeTestsSetup -Description "Run custom tests based on s
 
 function Invoke-MyTest
 {
-    $LogTime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
-    $LogPath = Join-Path $LogsDir "MyTest_$LogTime.xml"
+    $LogPath = Join-Path $LogsDir "MyTestResults.xml"
     Invoke-Tests -WhereClause $TestFilter -OutputFile $LogPath
 }
 
