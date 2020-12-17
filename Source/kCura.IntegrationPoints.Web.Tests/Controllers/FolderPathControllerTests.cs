f4552b5701a041e4384271efffea0e3670bbd964
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2020-12-17 14:30:18 +0100

    REL-451755 Fix UT

--- Source/kCura.IntegrationPoints.Web.Tests/Controllers/FolderPathControllerTests.cs
+++ Source/kCura.IntegrationPoints.Web.Tests/Controllers/FolderPathControllerTests.cs
@@ -1,5 +1,3 @@
-#pragma warning disable CS0618 // Type or member is obsolete (IRSAPI deprecation)
-#pragma warning disable CS0612 // Type or member is obsolete (IRSAPI deprecation)
 using System;
 using System.Net;
 using System.Net.Http;
@@ -12,31 +10,26 @@ using kCura.IntegrationPoints.Core.Services;
 using kCura.IntegrationPoints.Config;
 using kCura.IntegrationPoints.Synchronizers.RDO;
 using kCura.IntegrationPoints.Web.Controllers.API;
-using kCura.Relativity.Client;
 using kCura.Relativity.ImportAPI;
 using NSubstitute;
 using NUnit.Framework;
 using System.Collections.Generic;
 using kCura.IntegrationPoints.Common.Context;
 using kCura.IntegrationPoints.Synchronizers.RDO.ImportAPI;
-using Relativity.API;
 using Relativity.IntegrationPoints.Contracts.Models;
 using Field = kCura.Relativity.ImportAPI.Data.Field;
-using Query = kCura.Relativity.Client.Query;
 
 namespace kCura.IntegrationPoints.Web.Tests.Controllers
 {
 	[TestFixture, Category("Unit")]
 	public class FolderPathControllerTests : TestBase
 	{
-		private IRSAPIClient _client;
 		private IFieldService _fieldService;
 		private IImportApiFactory _importApiFactory;
 		private IConfig _config;
 		private IChoiceService _choiceService;
 		private IWorkspaceContext _workspaceIdProvider;
 		private IImportApiFacade _importApiFacade;
-		private ICPHelper _helper;
 
 		private HttpConfiguration _configuration;
 		private FolderPathController _instance;
@@ -44,7 +37,6 @@ namespace kCura.IntegrationPoints.Web.Tests.Controllers
 		[SetUp]
 		public override void SetUp()
 		{
-			_client = Substitute.For<IRSAPIClient>();
 			_fieldService = Substitute.For<IFieldService>();
 			_importApiFactory = Substitute.For<IImportApiFactory>();
 			_config = Substitute.For<IConfig>();
@@ -52,14 +44,13 @@ namespace kCura.IntegrationPoints.Web.Tests.Controllers
 			_workspaceIdProvider = Substitute.For<IWorkspaceContext>();
 			_importApiFacade = Substitute.For<IImportApiFacade>();
 			_configuration = Substitute.For<HttpConfiguration>();
-			_helper = Substitute.For<ICPHelper>();
 
 			var config = new HttpConfiguration();
 			var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/api/Get");
 			IHttpRoute route = config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}");
 			var routeData = new HttpRouteData(route, new HttpRouteValueDictionary { { "controller", "GetFolderPathFieldsController" } });
 
-			_instance = new FolderPathController(_client, _fieldService, _choiceService, _workspaceIdProvider, _importApiFacade, _helper)
+			_instance = new FolderPathController(_fieldService, _choiceService, _workspaceIdProvider, _importApiFacade)
 			{
 				ControllerContext = new HttpControllerContext(config, routeData, request),
 				Request = request
@@ -107,9 +98,7 @@ namespace kCura.IntegrationPoints.Web.Tests.Controllers
             var workspaceId = 123;
             var documentArtifactTypeId = 10;
 
-            var result = new QueryResult { Success = true };
             var settings = new ImportSettings { WebServiceURL = webServiceUrl };
-			_client.APIOptions = new APIOptions(workspaceId);
 
 			var listOfFields = new List<FieldEntry> {new FieldEntry()};
 	        _choiceService.GetChoiceFields(Arg.Any<int>()).Returns(listOfFields);
@@ -123,8 +112,6 @@ namespace kCura.IntegrationPoints.Web.Tests.Controllers
 
 			importApi.GetWorkspaceFields(workspaceId, documentArtifactTypeId).Returns(new List<Field> { new Field() });
 
-			_client.Query(Arg.Any<APIOptions>(), Arg.Any<Query>()).Returns(result);
-
 	        _workspaceIdProvider.GetWorkspaceID().Returns(workspaceId);
 
 	        _fieldService.GetAllTextFields(Arg.Any<int>(), Arg.Any<int>()).Returns(new List<FieldEntry>());
@@ -133,5 +120,3 @@ namespace kCura.IntegrationPoints.Web.Tests.Controllers
 		
 	}
 }
-#pragma warning restore CS0612 // Type or member is obsolete (IRSAPI deprecation)
-#pragma warning restore CS0618 // Type or member is obsolete (IRSAPI deprecation)
