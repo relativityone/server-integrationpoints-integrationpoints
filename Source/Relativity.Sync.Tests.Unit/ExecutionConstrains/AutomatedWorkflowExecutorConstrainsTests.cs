3d6370aa23ecf7c466817d781c28160a8230fb2a
Tomasz Pęczek <tomasz.peczek@relativity.com>
2020-12-18 15:19:19 +0100

    [REL-503406] Dynamically retrieving Relativity Application object type

--- Source/Relativity.Sync.Tests.Unit/ExecutionConstrains/AutomatedWorkflowExecutorConstrainsTests.cs
+++ Source/Relativity.Sync.Tests.Unit/ExecutionConstrains/AutomatedWorkflowExecutorConstrainsTests.cs
@@ -1,18 +1,18 @@
-﻿using FluentAssertions;
-using Moq;
-using NUnit.Framework;
-using Relativity.Services.Objects;
-using Relativity.Services.Objects.DataContracts;
-using Relativity.Sync.Configuration;
-using Relativity.Sync.ExecutionConstrains;
-using Relativity.Sync.KeplerFactory;
-using Relativity.Sync.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Relativity.Services.Objects;
+using Relativity.Services.Objects.DataContracts;
+using Relativity.Services.Interfaces.ObjectType;
+using Relativity.Services.Interfaces.ObjectType.Models;
+using Relativity.Sync.Logging;
+using Relativity.Sync.Configuration;
+using Relativity.Sync.KeplerFactory;
+using Relativity.Sync.ExecutionConstrains;
+using Moq;
+using NUnit.Framework;
+using FluentAssertions;
 
 namespace Relativity.Sync.Tests.Unit.ExecutionConstrains
 {
@@ -20,7 +20,9 @@ namespace Relativity.Sync.Tests.Unit.ExecutionConstrains
 	public sealed class AutomatedWorkflowExecutorConstrainsTests
 	{
 		private const int _DESTINATION_WORKSPACE_ARTIFACT_ID = 101001;
-		private const int _RELATIVITY_APPLICATIONS_ARTIFACT_TYPE_ID = 1000014;
+		private const string _RELATIVITY_APPLICATION_CONDITION = "'Name' == 'Relativity Application'";
+		private const int _RELATIVITY_APPLICATIONS_ARTIFACT_ID = 103883;
+		private const int _RELATIVITY_APPLICATIONS_ARTIFACT_TYPE_ID = 1000011;
 		private const string _AUTOMATED_WORKFLOWS_APPLICATION_CONDITION = "'Name' == 'Automated Workflows'";
 
 		private static IAutomatedWorkflowTriggerConfiguration PrepareAutomatedWorkflowTriggerConfiguration(ExecutionResult executionResult)
@@ -35,6 +37,17 @@ namespace Relativity.Sync.Tests.Unit.ExecutionConstrains
 		private static AutomatedWorkflowExecutorConstrains PrepareAutomatedWorkflowExecutorConstrains(bool isAutomatedWorkflowsInstalled)
 		{
 			Mock<IObjectManager> objectManagerFake = new Mock<IObjectManager>();
+
+			objectManagerFake.Setup(m => m.QuerySlimAsync(
+				It.Is<int>(workspaceId => workspaceId == _DESTINATION_WORKSPACE_ARTIFACT_ID),
+				It.Is<QueryRequest>(qr => qr.ObjectType.ArtifactTypeID == (int)ArtifactType.ObjectType && qr.Condition == _RELATIVITY_APPLICATION_CONDITION),
+				It.Is<int>(start => start == 0),
+				It.Is<int>(length => length == 1)
+			)).Returns(Task.FromResult(new QueryResultSlim { TotalCount = 1, ResultCount = 1, Objects = new List<RelativityObjectSlim>
+			{
+				new RelativityObjectSlim { ArtifactID = _RELATIVITY_APPLICATIONS_ARTIFACT_ID }
+			}}));
+			
 			objectManagerFake.Setup(m => m.QuerySlimAsync(
 				It.Is<int>(workspaceId => workspaceId == _DESTINATION_WORKSPACE_ARTIFACT_ID),
 				It.Is<QueryRequest>(qr => qr.ObjectType.ArtifactTypeID == _RELATIVITY_APPLICATIONS_ARTIFACT_TYPE_ID && qr.Condition == _AUTOMATED_WORKFLOWS_APPLICATION_CONDITION),
@@ -42,8 +55,16 @@ namespace Relativity.Sync.Tests.Unit.ExecutionConstrains
 				It.Is<int>(length => length == 0)
 			)).Returns(Task.FromResult(new QueryResultSlim { TotalCount = isAutomatedWorkflowsInstalled ? 1 : 0 }));
 
+			Mock<IObjectTypeManager> objectTypeManagerFake = new Mock<IObjectTypeManager>();
+
+			objectTypeManagerFake.Setup(m => m.ReadAsync(
+				It.Is<int>(workspaceId => workspaceId == _DESTINATION_WORKSPACE_ARTIFACT_ID),
+				It.Is<int>(objectTypeId => objectTypeId == _RELATIVITY_APPLICATIONS_ARTIFACT_ID)
+			)).Returns(Task.FromResult(new ObjectTypeResponse { ArtifactTypeID = _RELATIVITY_APPLICATIONS_ARTIFACT_TYPE_ID }));
+
 			Mock<IDestinationServiceFactoryForAdmin> serviceFactoryFake = new Mock<IDestinationServiceFactoryForAdmin>();
 			serviceFactoryFake.Setup(m => m.CreateProxyAsync<IObjectManager>()).Returns(Task.FromResult(objectManagerFake.Object));
+			serviceFactoryFake.Setup(m => m.CreateProxyAsync<IObjectTypeManager>()).Returns(Task.FromResult(objectTypeManagerFake.Object));
 
 			return new AutomatedWorkflowExecutorConstrains(serviceFactoryFake.Object, new EmptyLogger());
 		}
