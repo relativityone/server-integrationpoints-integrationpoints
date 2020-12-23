1d2f7fdc6705cbe29aa08a68720861a166dbce46
Tomasz Pęczek <tomasz.peczek@relativity.com>
2020-12-23 11:40:14 +0100

    REL-451755 Regitering IChoiceQueryManager in TestHelper

--- Source/kCura.IntegrationPoint.Tests.Core/TestHelpers/TestHelper.cs
+++ Source/kCura.IntegrationPoint.Tests.Core/TestHelpers/TestHelper.cs
@@ -22,6 +22,7 @@ using Relativity.Services.Interfaces.ObjectType;
 using Relativity.Services.Interfaces.Tab;
 using Relativity.Services.InternalMetricsCollection;
 using Relativity.Services.View;
+using Relativity.Services.ChoiceQuery;
 
 namespace kCura.IntegrationPoint.Tests.Core.TestHelpers
 {
@@ -74,6 +75,7 @@ namespace kCura.IntegrationPoint.Tests.Core.TestHelpers
 			RegisterProxyInServiceManagerMock<IInternalMetricsCollectionManager>(ExecutionIdentity.System);
 			RegisterProxyInServiceManagerMock<ITabManager>(ExecutionIdentity.CurrentUser);
 			RegisterProxyInServiceManagerMock<IViewManager>(ExecutionIdentity.System);
+			RegisterProxyInServiceManagerMock<IChoiceQueryManager>(ExecutionIdentity.System);
 			_serviceManager.GetServicesURL().Returns(SharedVariables.RelativityRestUri);
 		}
 		
