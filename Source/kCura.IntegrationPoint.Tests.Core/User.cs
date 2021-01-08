8aff3c236565f9ec7984102e879b69240e9a8a58
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2021-01-08 11:27:28 +0100

    REL-451745 Retrieve view by name using Kepler

--- Source/kCura.IntegrationPoint.Tests.Core/User.cs
+++ Source/kCura.IntegrationPoint.Tests.Core/User.cs
@@ -1,11 +1,16 @@
 using System.Collections.Generic;
+using System.Linq;
 using kCura.IntegrationPoint.Tests.Core.Models;
 using kCura.IntegrationPoint.Tests.Core.TestHelpers;
+using Relativity;
+using Relativity.Services.Exceptions;
 using Relativity.Services.Interfaces.Group;
 using Relativity.Services.Interfaces.Shared;
 using Relativity.Services.Interfaces.Shared.Models;
 using Relativity.Services.Interfaces.UserInfo;
 using Relativity.Services.Interfaces.UserInfo.Models;
+using Relativity.Services.Objects;
+using Relativity.Services.Objects.DataContracts;
 using Relativity.Services.Security;
 using Relativity.Services.Security.Models;
 
@@ -14,6 +19,8 @@ namespace kCura.IntegrationPoint.Tests.Core
 	public static class User
 	{
 		private const int _SYSTEM_ADMINISTRATOR_GROUP_ID = 20;
+		private const int _USER_TYPE_INTERNAL_ID = 663;
+
 		private static ITestHelper Helper => new TestHelper();
 
 		public static UserModel CreateUser(string firstName, string lastName, string emailAddress, IList<int> groupIds = null)
@@ -30,9 +37,7 @@ namespace kCura.IntegrationPoint.Tests.Core
 			using (ILoginProfileManager loginProfileManager = Helper.CreateProxy<ILoginProfileManager>())
 			using (IGroupManager groupManager = Helper.CreateProxy<IGroupManager>())
 			{
-				const string password = "Test1234!";
-
-				UserResponse userResponse = userManager.CreateAsync(new UserRequest()
+				var userRequest = new UserRequest()
 				{
 					FirstName = firstName,
 					LastName = lastName,
@@ -44,17 +49,17 @@ namespace kCura.IntegrationPoint.Tests.Core
 					{
 						Value = new ObjectIdentifier()
 						{
-							ArtifactID = 1006066
+							ArtifactID = GetClientArtifactId()
 						}
 					},
 					Type = new ObjectIdentifier()
 					{
-						ArtifactID = 663
+						ArtifactID = _USER_TYPE_INTERNAL_ID
 					},
-					DocumentViewerProperties = new DocumentViewerProperties()
-					{
-					},
-				}).GetAwaiter().GetResult();
+					DocumentViewerProperties = new DocumentViewerProperties(),
+				};
+
+				UserResponse userResponse = userManager.CreateAsync(userRequest).GetAwaiter().GetResult();
 
 				loginProfileManager.SaveLoginProfileAsync(new LoginProfile()
 				{
@@ -67,6 +72,8 @@ namespace kCura.IntegrationPoint.Tests.Core
 						MustResetPasswordOnNextLogin = false
 					}
 				}).GetAwaiter().GetResult();
+
+				string password = Helper.RelativityPassword;
 				loginProfileManager.SetPasswordAsync(userResponse.ArtifactID, password).GetAwaiter().GetResult();
 
 				foreach (int groupId in groupIds)
@@ -80,7 +87,7 @@ namespace kCura.IntegrationPoint.Tests.Core
 				return new UserModel(userResponse.ArtifactID, userResponse.EmailAddress, password);
 			}
 		}
-		
+
 		public static void DeleteUser(int userArtifactId)
 		{
 			if (userArtifactId == 0)
@@ -94,5 +101,27 @@ namespace kCura.IntegrationPoint.Tests.Core
 			}
 		}
 
+		private static int GetClientArtifactId()
+		{
+			using (IObjectManager objectManager = Helper.CreateProxy<IObjectManager>())
+			{
+				var clientRequest = new QueryRequest
+				{
+					ObjectType = new ObjectTypeRef()
+					{
+						ArtifactTypeID = (int)ArtifactType.Client
+					}
+				};
+
+				QueryResultSlim result = objectManager.QuerySlimAsync(-1, clientRequest, 0, int.MaxValue).GetAwaiter().GetResult();
+
+				if (result.ResultCount == 0)
+				{
+					throw new NotFoundException("There are no clients available on this Relativity instance.");
+				}
+
+				return result.Objects.First().ArtifactID;
+			}
+		}
 	}
 }
