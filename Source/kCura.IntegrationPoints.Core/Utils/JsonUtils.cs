9417ee8e6cf6e20810170b9863a5828ddc35f20c
Kamil Makarowski <kamil.makarowski@relativity.com>
2021-01-29 15:17:57 +0100

    Add ImportFileCopyMode property to CreateIntegrationPointRequest

--- Source/kCura.IntegrationPoints.Core/Utils/JsonUtils.cs
+++ Source/kCura.IntegrationPoints.Core/Utils/JsonUtils.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace kCura.IntegrationPoints.Core.Utils
@@ -25,6 +26,25 @@ namespace kCura.IntegrationPoints.Core.Utils
 			JObject sourceObject = JObject.Parse(json);
 
 			sourceObject.Remove(propertyName);
+
+			return JsonConvert.SerializeObject(sourceObject, Formatting.None);
+		}
+
+		public static string AddOrUpdatePropertyValues(string json, IDictionary<string, object> propertyValues)
+		{
+			JObject sourceObject = JObject.Parse(json);
+			foreach (var propertyValue in propertyValues)
+			{
+				JProperty property = sourceObject.Property(propertyValue.Key);
+				if (property == null)
+				{
+					sourceObject.Add(new JProperty(propertyValue.Key, propertyValue.Value));
+				}
+				else
+				{
+					property.Replace(new JProperty(propertyValue.Key, propertyValue.Value));
+				}
+			}
 
 			return JsonConvert.SerializeObject(sourceObject, Formatting.None);
 		}
