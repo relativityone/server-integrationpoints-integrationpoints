b6222e9e66c1a457dde3d257a13e3a71af2b1c33
Kamil Makarowski <kamil.makarowski@relativity.com>
2021-02-03 09:37:36 +0000

    Merge pull request #2283 in IN/integrationpoints from REL-517714-missing-rip-agent-manager-dependency-osier-ea to release-12.1-osier-ea
    Squashed commit of the following:
    
    commit 8a65f56405b234e44c5d8d67c24b13833a99c2d3
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Tue Feb 2 11:04:38 2021 +0100
    
        REL-517714 Bump Sync to 0.6.71
    
    commit 50a7e19ecff888e3de56ed41c41d229c7c24deed
    Author: Kamil Makarowski <kamil.makarowski@relativity.com>
    Date:   Mon Feb 1 11:33:29 2021 +0100
    
        REL-517714 Add missing IRelativityObjectManager dependency in container

--- Source/kCura.IntegrationPoints.Management/Installers/IntegrationPointsManagerInstaller.cs
+++ Source/kCura.IntegrationPoints.Management/Installers/IntegrationPointsManagerInstaller.cs
@@ -1,6 +1,9 @@
 ﻿using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
+using kCura.IntegrationPoints.Core.Services.ServiceContext;
+using kCura.IntegrationPoints.Data.Factories;
+using kCura.IntegrationPoints.Data.Factories.Implementations;
 using kCura.IntegrationPoints.Data.Repositories;
 using kCura.IntegrationPoints.Management.Tasks;
 using kCura.IntegrationPoints.Management.Tasks.Helpers;
@@ -26,6 +29,15 @@ namespace kCura.IntegrationPoints.Management.Installers
 
 			container.Register(Component.For<IJobsWithInvalidStatus>().ImplementedBy<JobsWithInvalidStatus>().LifestyleTransient());
 			container.Register(Component.For<IStuckJobs>().ImplementedBy<StuckJobs>().LifestyleTransient());
+
+			container.Register(Component.For<IRelativityObjectManagerFactory>().ImplementedBy<RelativityObjectManagerFactory>().LifestyleTransient());
+			container.Register(Component.For<IRelativityObjectManager>()
+				.UsingFactoryMethod(x =>
+				{
+					int workspaceId = x.Resolve<IServiceContextHelper>().WorkspaceID;
+					IRelativityObjectManagerFactory factory = x.Resolve<IRelativityObjectManagerFactory>();
+					return factory.CreateRelativityObjectManager(workspaceId);
+				}).LifestyleTransient());
 
 			container.Register(Classes.FromAssembly(typeof(IntegrationPointsManagerInstaller).Assembly).BasedOn<IManagementTask>().WithService.FromInterface().LifestyleTransient());
 		}
