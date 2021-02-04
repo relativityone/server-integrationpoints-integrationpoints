42821f977d4986fd6e44d7b8487d179f2d2fe1d6
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2021-02-04 13:50:50 +0100

    REL-511831 Update

--- Source/kCura.IntegrationPoints.Agent/Tasks/ServiceManagerBase.cs
+++ Source/kCura.IntegrationPoints.Agent/Tasks/ServiceManagerBase.cs
@@ -327,7 +327,7 @@ namespace kCura.IntegrationPoints.Agent.Tasks
 
 		private void ConfigureJobStopManager(Job job)
 		{
-			JobStopManager = ManagerFactory.CreateJobStopManager(JobService, JobHistoryService, Identifier, job.JobId, true, supportsDrainStop: false);
+			JobStopManager = ManagerFactory.CreateJobStopManager(JobService, JobHistoryService, Identifier, job.JobId, supportsDrainStop: false);
 			JobHistoryErrorService.JobStopManager = JobStopManager;
 		}
 
