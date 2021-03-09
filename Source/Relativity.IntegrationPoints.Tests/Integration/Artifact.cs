237daad132f4ec360e61ee9ab4fe459c2f0ed27c
Kamil Makarowski <kamil.makarowski@relativity.com>
2021-03-09 10:11:24 +0100

    REL-518952 Change ArtifactId generation to increment

--- Source/Relativity.IntegrationPoints.Tests/Integration/Artifact.cs
+++ Source/Relativity.IntegrationPoints.Tests/Integration/Artifact.cs
@@ -1,22 +1,12 @@
-﻿using System;
-
-namespace Relativity.IntegrationPoints.Tests.Integration
+﻿namespace Relativity.IntegrationPoints.Tests.Integration
 {
 	public static class Artifact
 	{
-		private static readonly Random _random;
-
-		private const int _MIN_VALUE = 100000;
-		private const int _MAX_VALUE = 999999;
-
-		static Artifact()
-		{
-			_random = new Random();
-		}
-
+		private static int _currentArtifactId = 100000;
+		
 		public static int NextId()
 		{
-			return _random.Next(_MIN_VALUE, _MAX_VALUE);
-		}
+			return ++_currentArtifactId;
+	}
 	}
 }
