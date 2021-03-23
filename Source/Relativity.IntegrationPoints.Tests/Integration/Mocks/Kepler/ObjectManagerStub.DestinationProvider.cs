186ff8f2d5562c6ad008fd134d532e3e5ffceab4
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2021-03-23 09:28:56 +0100

    REL-518954 Fix typo

--- Source/Relativity.IntegrationPoints.Tests/Integration/Mocks/Kepler/ObjectManagerStub.DestinationProvider.cs
+++ Source/Relativity.IntegrationPoints.Tests/Integration/Mocks/Kepler/ObjectManagerStub.DestinationProvider.cs
@@ -13,15 +13,15 @@ namespace Relativity.IntegrationPoints.Tests.Integration.Mocks.Kepler
 	{
 		public void SetupDestinationProviders(InMemoryDatabase database, IEnumerable<DestinationProviderTest> destinationProviders)
 		{
-			foreach (DestinationProviderTest sourceProvider in destinationProviders)
+			foreach (DestinationProviderTest destinationProvider in destinationProviders)
 			{
-				Mock.Setup(x => x.ReadAsync(sourceProvider.WorkspaceId, It.Is<ReadRequest>(r =>
-						r.Object.ArtifactID == sourceProvider.ArtifactId)))
+				Mock.Setup(x => x.ReadAsync(destinationProvider.WorkspaceId, It.Is<ReadRequest>(r =>
+						r.Object.ArtifactID == destinationProvider.ArtifactId)))
 					.Returns((int workspaceId, ReadRequest request) =>
 						{
 							ReadResult result = database.DestinationProviders.FirstOrDefault(
 								x => x.ArtifactId == request.Object.ArtifactID) != null
-								? new ReadResult { Object = sourceProvider.ToRelativityObject() }
+								? new ReadResult { Object = destinationProvider.ToRelativityObject() }
 								: new ReadResult { Object = null };
 
 							return Task.FromResult(result);
