4a97a3a8d6ac0c3a39c2997e2caee0307f9ac8e7
Kamil Makarowski <kamil.makarowski@relativity.com>
2021-05-11 12:05:29 +0200

    REL-545202 Introduce L2 Tests for LDAP Provider

--- Source/Relativity.IntegrationPoints.Tests/Integration/Mocks/Services/FakeDataSourceProvider.cs
+++ Source/Relativity.IntegrationPoints.Tests/Integration/Mocks/Services/FakeDataSourceProvider.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Data;
+using Relativity.IntegrationPoints.Contracts;
 using Relativity.IntegrationPoints.Contracts.Models;
 using Relativity.IntegrationPoints.Contracts.Provider;
 
 namespace Relativity.IntegrationPoints.Tests.Integration.Mocks.Services
 {
+	[DataSourceProvider("9A33EBEA-B4F9-4427-8AD4-5D4F35F0405A")]
 	public class FakeDataSourceProvider : IDataSourceProvider
 	{
 		private readonly IDataReader _dataReader;
---
+++ Source/Relativity.IntegrationPoints.Tests/Integration/Tests/LDAP/TestData/TestDataBase.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualBasic.FileIO;
+using Relativity.IntegrationPoints.Contracts.Models;
+
+namespace Relativity.IntegrationPoints.Tests.Integration.Tests.LDAP.TestData
+{
+	public abstract class TestDataBase
+	{
+		private const string _RELATIVE_TEST_DATA_PATH = @"Integration\Tests\LDAP\TestData";
+
+		private readonly string _TEST_DATA_PATH = Path.Combine(
+			new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName,
+			_RELATIVE_TEST_DATA_PATH);
+
+		public readonly string UniqueId;
+
+		public readonly IList<IDictionary<string, object>> Data;
+
+		public FieldEntry IdentifierFieldEntry => new FieldEntry
+		{
+			DisplayName = UniqueId,
+			FieldIdentifier = UniqueId,
+			IsIdentifier = true
+		};
+
+		public string[] AllProperties { get; protected set; }
+
+		protected TestDataBase(string testDataName, string uniqueId)
+		{
+			string testDataFile = Path.Combine(_TEST_DATA_PATH, $"{testDataName}.csv");
+			
+			TextFieldParser csvParser = new TextFieldParser(testDataFile)
+			{
+				Delimiters = new[] { "," }
+			};
+
+			AllProperties = csvParser.ReadFields();
+
+			Data = new List<IDictionary<string, object>>();
+			while (!csvParser.EndOfData)
+			{
+				string[] fields = csvParser.ReadFields();
+
+				Dictionary<string, object> row = AllProperties.Zip(fields, (prop, value) => new { prop, value })
+					.ToDictionary(x => x.prop, x => string.IsNullOrEmpty(x.value) ? null : (object)x.value);
+
+				Data.Add(row);
+			}
+
+			UniqueId = uniqueId;
+		}
+
+		public IEnumerable<FieldEntry> GetFieldEntries()
+		{
+			var entries = AllProperties.Select(x => new FieldEntry() { DisplayName = x, FieldIdentifier = x }).ToList();
+
+			entries.Single(x => x.FieldIdentifier == UniqueId).IsIdentifier = true;
+
+			return entries;
+		}
+
+		public virtual IEnumerable<string> EntryIds => Data.Select(x => x[UniqueId].ToString());
+	}
+}
