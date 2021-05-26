561a163f19932a0ee5b735c1eab51c080cfc300f
Tomasz Pęczek <tomasz.peczek@relativity.com>
2021-05-26 10:37:38 +0200

    [REL-546939] Attempting to fix restore for SDK style projects

--- psakefile.ps1
+++ psakefile.ps1
@@ -18,6 +18,7 @@ Task Analyze -Description "Run build analysis" {
 
 Task NugetRestore -Description "Restore the packages needed for this build" {
     exec { & $PaketExe restore }
+    exec { dotnet restore }
 }
 
 Task Compile -Depends NugetRestore -Description "Compile code for this repo" {
