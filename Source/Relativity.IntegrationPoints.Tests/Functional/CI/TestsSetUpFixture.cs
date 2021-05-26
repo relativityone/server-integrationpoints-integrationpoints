56781d7557bd895cfb8c3a536ec2863bf2b2416b
Tomasz Pęczek <tomasz.peczek@relativity.com>
2021-05-26 16:02:04 +0200

    [REL-546939] Adding Integration Points installation to set up

--- Source/Relativity.IntegrationPoints.Tests/Functional/CI/TestsSetUpFixture.cs
+++ Source/Relativity.IntegrationPoints.Tests/Functional/CI/TestsSetUpFixture.cs
@@ -1,8 +1,10 @@
 ﻿using Atata;
+using System.IO;
 using NUnit.Framework;
 using Relativity.Testing.Framework;
 using Relativity.Testing.Framework.Api;
 using Relativity.Testing.Framework.Api.Services;
+using Relativity.Testing.Framework.Models;
 using Relativity.Testing.Framework.Web;
 
 namespace Relativity.IntegrationPoints.Tests.Functional.CI
@@ -10,6 +12,7 @@ namespace Relativity.IntegrationPoints.Tests.Functional.CI
 	[SetUpFixture]
 	public class TestsSetUpFixture
 	{
+		private const string FUNCTIONAL_TEMPLATE_NAME = "RIP Functional Tests Template";
 		private const string FUNCTIONAL_STANDARD_ACCOUNT_EMAIL_FORMAT = "rip_func_user{0}@mail.com";
 
 		[OneTimeSetUp]
@@ -20,12 +23,48 @@ namespace Relativity.IntegrationPoints.Tests.Functional.CI
 			RelativityFacade.Instance.RelyOn<WebComponent>();
 
 			RelativityFacade.Instance.Resolve<IAccountPoolService>().StandardAccountEmailFormat = FUNCTIONAL_STANDARD_ACCOUNT_EMAIL_FORMAT;
+
+			if (TemplateWorkspaceExists())
+			{
+				return;
+			}
+
+			int workspaceId = CreateTemplateWorkspace();
+
+			InstallIntegrationPointsToWorkspace(workspaceId);
 		}
 
 		[OneTimeTearDown]
 		public void TearDown()
 		{
 			AtataContext.Current?.Dispose();
+		}
+
+		private static bool TemplateWorkspaceExists()
+			=> RelativityFacade.Instance.Resolve<IWorkspaceService>().Get(FUNCTIONAL_TEMPLATE_NAME) != null;
+
+		private static int CreateTemplateWorkspace()
+		{
+			Workspace newWorkspace = new Workspace()
+			{
+				Name = FUNCTIONAL_TEMPLATE_NAME
+			};
+
+			return RelativityFacade.Instance.Resolve<IWorkspaceService>().Create(newWorkspace).ArtifactID;
+		}
+
+		private static void InstallIntegrationPointsToWorkspace(int workspaceId)
+		{
+			string rapFileLocation = Path.Combine(TestContext.Parameters["RAPDirectory"], "RelativityIntegrationPoints.Auto.rap");
+
+			var applicationService = RelativityFacade.Instance.Resolve<ILibraryApplicationService>();
+
+			int appId = applicationService.InstallToLibrary(rapFileLocation, new LibraryApplicationInstallOptions
+			{
+				IgnoreVersion = true
+			});
+
+			applicationService.InstallToWorkspace(workspaceId, appId);
 		}
 	}
 }
