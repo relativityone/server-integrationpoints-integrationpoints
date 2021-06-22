7b3f7bb8ce7062cb1224952b80ca69ef37469d75
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2021-06-22 15:34:53 +0200

    REL-563018 Implement readiness check

---
+++ Source/kCura.IntegrationPoints.Web/Controllers/ReadinessController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Web.Mvc;
+
+namespace kCura.IntegrationPoints.Web.Controllers
+{
+	public class ReadinessController : Controller
+	{
+		[HttpGet]
+		[ActionName("Check")]
+		public ActionResult ReadinessCheck()
+		{
+			return new HttpStatusCodeResult(HttpStatusCode.OK);
+		}
+	}
+}
