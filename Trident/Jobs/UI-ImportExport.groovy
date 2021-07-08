efcacb1120dc017e7d080bbbcf234b8f4fe1ab56
Kamil Bizoń <kamil.bizon@relativity.com>
2021-07-08 13:24:55 +0200

    REL-562172 Running Nighlty with CIFunctionalTest

--- Trident/Jobs/UI-ImportExport.groovy
+++
@@ -1,9 +0,0 @@
-@Library('ProjectMayhem@v1') _
-
-jobWithSut {
-    slackChannel = "cd_trident_rip"
-    sutTemplate = "aio-ninebark-latest"
-	relativityBranch = "develop"
-    jobScript = "Trident/Scripts/UI-ImportExport.ps1"
-    cron = "0 1 * * *"
-}
--- Trident/Jobs/UI-NewSync.groovy
+++
@@ -1,9 +0,0 @@
-@Library('ProjectMayhem@v1') _
-
-jobWithSut {
-    slackChannel = "cd_trident_rip"
-    sutTemplate = "aio-ninebark-latest"
-	relativityBranch = "develop"
-    jobScript = "Trident/Scripts/UI-NewSync.ps1"
-    cron = "0 1 * * *"
-}
