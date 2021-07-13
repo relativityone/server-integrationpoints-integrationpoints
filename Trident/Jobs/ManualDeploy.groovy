3c151decb9053d70340cb52b5fa37926e4a3cdce
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2021-07-13 10:48:39 +0200

    REL-562302 REL-562302 Changed slack channel

--- Trident/Jobs/ManualDeploy.groovy
+++ Trident/Jobs/ManualDeploy.groovy
@@ -1,5 +1,5 @@
 @Library('ProjectMayhem@v1') _
   
 manualDeploy {
-    slackChannel = "proj-rip-deployment" // Optional. Note: currently slack notifications are only sent on build failure
+    slackChannel = "cd-rip" // Optional. Note: currently slack notifications are only sent on build failure
 }
