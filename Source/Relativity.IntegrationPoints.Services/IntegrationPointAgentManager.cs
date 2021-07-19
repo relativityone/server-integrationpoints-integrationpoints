e0bf76bae24a5173548caa298ecdddfff9209fa1
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2021-07-19 15:28:42 +0200

    REL-563863 Implemented empty IntegrationPointAgentManager

---
+++ Source/Relativity.IntegrationPoints.Services/IntegrationPointAgentManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Castle.Windsor;
+using Relativity.IntegrationPoints.Services.Helpers;
+using Relativity.IntegrationPoints.Services.Installers;
+using Relativity.Logging;
+using WorkloadDiscovery;
+
+namespace Relativity.IntegrationPoints.Services
+{
+	public class IntegrationPointAgentManager : KeplerServiceBase, IIntegrationPointsAgentManager
+	{
+		private Installer _installer;
+
+		protected override Installer Installer => _installer ?? (_installer = new IntegrationPointManagerInstaller());
+
+		public IntegrationPointAgentManager(ILog logger, IPermissionRepositoryFactory permissionRepositoryFactory, IWindsorContainer container)
+			: base(logger, permissionRepositoryFactory, container)
+		{
+		}
+
+		public IntegrationPointAgentManager(ILog logger)
+			: base(logger)
+		{
+		}
+
+		public Task<Workload> GetWorkloadAsync()
+		{
+			return Task.FromResult(new Workload());
+		}
+
+		public void Dispose()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
