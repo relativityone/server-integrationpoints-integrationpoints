7f112350b8c6a71d75bbf67072945debfdb06f16
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2021-07-20 15:13:06 +0200

    REL-563863 Implemented GetWorkloadAsync in IntegrationPointAgentManager Kepler service

--- Source/Relativity.IntegrationPoints.Services/Installers/IntegrationPointAgentManagerInstaller.cs
+++ Source/Relativity.IntegrationPoints.Services/Installers/IntegrationPointAgentManagerInstaller.cs
@@ -1,7 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
+using kCura.IntegrationPoints.Core.Managers.Implementations;
+using kCura.IntegrationPoints.Data;
+using kCura.IntegrationPoints.Data.Factories;
+using kCura.IntegrationPoints.Data.Factories.Implementations;
+using kCura.IntegrationPoints.Domain.Managers;
+using kCura.ScheduleQueue.Core.Data;
+using Relativity.API;
 
 namespace Relativity.IntegrationPoints.Services.Installers
 {
@@ -11,7 +19,10 @@ namespace Relativity.IntegrationPoints.Services.Installers
 
 		protected override void RegisterComponents(IWindsorContainer container, IConfigurationStore store, int workspaceID)
 		{
-			
+			Guid agentGuid = Guid.Parse(GlobalConst.RELATIVITY_INTEGRATION_POINTS_AGENT_GUID);
+			container.Register(Component.For<IQueueQueryManager>().ImplementedBy<QueueQueryManager>().DependsOn(Dependency.OnValue<Guid>(agentGuid)).LifestyleTransient());
+			container.Register(Component.For<IInstanceSettingsManager>().ImplementedBy<InstanceSettingsManager>().LifestyleTransient());
+			container.Register(Component.For<IRepositoryFactory>().UsingFactoryMethod(c => new RepositoryFactory(c.Resolve<IHelper>(), c.Resolve<IHelper>().GetServicesManager())));
 		}
 	}
 }
