99e242bb067e09bfe5fee87451f6267a1fff54a2
Kamil Makarowski <kamil.makarowski@relativity.com>
2021-07-29 07:32:40 +0200

    REL-562299 Fix Workspace name was too long error

--- Source/kCura.IntegrationPoints.FunctionalTests/Functional/TestsImplementations/SyncTestsImplementation.cs
+++ Source/kCura.IntegrationPoints.FunctionalTests/Functional/TestsImplementations/SyncTestsImplementation.cs
@@ -42,13 +42,11 @@ namespace Relativity.IntegrationPoints.Tests.Functional.TestsImplementations
 		public void SavedSearchNativesAndMetadataGoldFlow()
 		{
 			// Arrange
-			CreateDestinationWorkspace(nameof(SavedSearchNativesAndMetadataGoldFlow));
-
 			_testsImplementationTestFixture.LoginAsStandardUser();
 
 			string integrationPointName = nameof(SavedSearchNativesAndMetadataGoldFlow);
 
-			Workspace destinationWorkspace = _destinationWorkspaces[nameof(SavedSearchNativesAndMetadataGoldFlow)];
+			Workspace destinationWorkspace = CreateDestinationWorkspace();
 
 			const int keywordSearchDocumentsCount = 5;
 			KeywordSearch keywordSearch = new KeywordSearch
@@ -99,13 +97,11 @@ namespace Relativity.IntegrationPoints.Tests.Functional.TestsImplementations
 		public void ProductionImagesGoldFlow()
 		{
 			// Arrange
-			CreateDestinationWorkspace(nameof(ProductionImagesGoldFlow));
-
 			_testsImplementationTestFixture.LoginAsStandardUser();
 
 			string integrationPointName = nameof(ProductionImagesGoldFlow);
 
-			Workspace destinationWorkspace = _destinationWorkspaces[nameof(ProductionImagesGoldFlow)];
+			Workspace destinationWorkspace = CreateDestinationWorkspace();
 
 			KeywordSearch keywordSearch = RelativityFacade.Instance.Resolve<IKeywordSearchService>().Require(_testsImplementationTestFixture.Workspace.ArtifactID, new KeywordSearch
 			{
@@ -195,11 +191,15 @@ namespace Relativity.IntegrationPoints.Tests.Functional.TestsImplementations
 			transferredItemsCount.Should().Be(workspaceDocumentCount).And.Be(productionDocumentsCount);
 		}
 
-		private void CreateDestinationWorkspace(string name)
+		private Workspace CreateDestinationWorkspace()
 		{
-			string workspaceName = $"{name} - {Guid.NewGuid()}";
+			string workspaceName = $"SYNC - {Guid.NewGuid()}";
 
-			_destinationWorkspaces.Add(workspaceName, RelativityFacade.Instance.CreateWorkspace(workspaceName, _testsImplementationTestFixture.Workspace.Name));
+			Workspace workspace = RelativityFacade.Instance.CreateWorkspace(workspaceName, _testsImplementationTestFixture.Workspace.Name);
+
+			_destinationWorkspaces.Add(workspaceName, workspace);
+
+			return workspace;
 		}
 
 		private static RelativityProviderConnectToSourcePage FillOutIntegrationPointEditPageForRelativityProvider(IntegrationPointEditPage integrationPointEditPage, string integrationPointName)
