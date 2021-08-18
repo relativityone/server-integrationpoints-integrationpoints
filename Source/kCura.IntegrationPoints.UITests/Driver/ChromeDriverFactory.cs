e6f5c11082d8bd5e2fa90a1ad621605051819eab
Kamil Makarowski <kamil.makarowski@relativity.com>
2021-08-18 12:40:42 +0200

    REL-562227 restore deleted files

---
+++ Source/kCura.IntegrationPoints.UITests/Driver/ChromeDriverFactory.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium.Remote;
+
+namespace kCura.IntegrationPoints.UITests.Driver
+{
+	public static class ChromeDriverFactory
+	{
+		public static RemoteWebDriver Create()
+		{
+			return ChromiumBasedDriverFactory.Create();
+		}
+	}
+}
---
+++ Source/kCura.IntegrationPoints.UITests/Driver/ChromeOptionsExtensions.cs
@@ -0,0 +1,41 @@
+﻿using kCura.IntegrationPoint.Tests.Core;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace kCura.IntegrationPoints.UITests.Driver
+{
+	public static class ChromeOptionsExtensions
+	{
+		public static ChromeOptions AddAdditionalCapabilities(this ChromeOptions value)
+		{
+			if (SharedVariables.UiOptionsAdditionalCapabilitiesAcceptSslCertificates)
+			{
+				value.AddAdditionalCapability(CapabilityType.AcceptSslCertificates, true, true);
+			}
+			if (SharedVariables.UiOptionsAdditionalCapabilitiesAcceptInsecureCertificates)
+			{
+				value.AddAdditionalCapability(CapabilityType.AcceptInsecureCertificates, true, true);
+			}
+			return value;
+		}
+
+		public static void AddBrowserOptions(this ChromeOptions value)
+		{
+			var optionsFromAppConfig = new Dictionary<string, bool>
+			{
+				// Disables "Chrome is being controlled by automated test software." bar
+				["disable-infobars"] = SharedVariables.UiOptionsArgumentsDisableInfobars,
+				["headless"] = SharedVariables.UiOptionsArgumentsHeadless,
+				["ignore-certificate-errors"] = SharedVariables.UiOptionsArgumentsIgnoreCertificateErrors,
+				["no-sandbox"] = SharedVariables.UiOptionsArgumentsNoSandbox
+			};
+
+			foreach (KeyValuePair<string, bool> option in optionsFromAppConfig.Where(x => x.Value))
+			{
+				value.AddArgument(option.Key);
+			}
+		}
+	}
+}
