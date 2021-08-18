c4a4ca12c0e15b6ce83a91284525605859a2b2f2
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2021-08-18 12:12:11 +0200

    REL-576976 Fixed Image Statistics tests

--- Source/kCura.IntegrationPoints.Data.Tests/Statistics/ImageTotalStatisticsTests.cs
+++ Source/kCura.IntegrationPoints.Data.Tests/Statistics/ImageTotalStatisticsTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using kCura.IntegrationPoint.Tests.Core;
-using kCura.IntegrationPoints.Data.Factories;
 using kCura.IntegrationPoints.Data.Repositories;
 using kCura.IntegrationPoints.Data.Statistics.Implementations;
 using NSubstitute;
@@ -16,33 +13,19 @@ using Relativity.Services.Objects.DataContracts;
 namespace kCura.IntegrationPoints.Data.Tests.Statistics
 {
 	[TestFixture, Category("Unit")]
-	public class ImageTotalStatisticsTests : TestBase
+	public class ImageTotalStatisticsTests : ImageStatisticsTestBase
 	{
-		private const int _WORKSPACE_ID = 237273;
-
-		private IAPILog _logger;
-		private IHelper _helper;
-		private IRelativityObjectManager _rdoRepository;
-
 		private ImageTotalStatistics _instance;
 		private IExportQueryResult _exportResult;
 
 		public override void SetUp()
 		{
-			_logger = Substitute.For<IAPILog>();
-			_helper = Substitute.For<IHelper>();
-			_rdoRepository = Substitute.For<IRelativityObjectManager>();
-
+			base.SetUp();
 			_exportResult = Substitute.For<IExportQueryResult>();
-
-			_rdoRepository.QueryWithExportAsync(Arg.Any<QueryRequest>(), Arg.Any<int>(), Arg.Any<ExecutionIdentity>())
+			_relativityObjectManager.QueryWithExportAsync(Arg.Any<QueryRequest>(), Arg.Any<int>(), Arg.Any<ExecutionIdentity>())
 				.Returns(_exportResult);
-
-			var repositoryFactory = Substitute.For<IRelativityObjectManagerFactory>();
-			repositoryFactory.CreateRelativityObjectManager(_WORKSPACE_ID).Returns(_rdoRepository);
 			_helper.GetLoggerFactory().GetLogger().ForContext<ImageTotalStatistics>().Returns(_logger);
-
-			_instance = new ImageTotalStatistics(_helper, repositoryFactory);
+			_instance = new ImageTotalStatistics(_helper, _repositoryFactory);
 		}
 
 		[Test]
@@ -116,15 +99,15 @@ namespace kCura.IntegrationPoints.Data.Tests.Statistics
 
 		[Test]
 		public void ItShouldLogError()
-			{
-				_rdoRepository.QueryWithExportAsync(Arg.Any<QueryRequest>(), Arg.Any<int>(), Arg.Any<ExecutionIdentity>())
-					.Throws(new Exception());
+		{
+			_relativityObjectManager.QueryWithExportAsync(Arg.Any<QueryRequest>(), Arg.Any<int>(), Arg.Any<ExecutionIdentity>())
+				.Throws(new Exception());
 
-				Assert.That(() => _instance.ForFolder(_WORKSPACE_ID, 157, 237, true), Throws.Exception);
-				Assert.That(() => _instance.ForProduction(_WORKSPACE_ID, 465), Throws.Exception);
-				Assert.That(() => _instance.ForSavedSearch(_WORKSPACE_ID, 740), Throws.Exception);
+			Assert.That(() => _instance.ForFolder(_WORKSPACE_ID, 157, 237, true), Throws.Exception);
+			Assert.That(() => _instance.ForProduction(_WORKSPACE_ID, 465), Throws.Exception);
+			Assert.That(() => _instance.ForSavedSearch(_WORKSPACE_ID, 740), Throws.Exception);
 
-				_logger.Received(3).LogError(Arg.Any<Exception>(), Arg.Any<string>(), Arg.Any<object[]>());
-			}
+			_logger.Received(3).LogError(Arg.Any<Exception>(), Arg.Any<string>(), Arg.Any<object[]>());
 		}
 	}
+}
