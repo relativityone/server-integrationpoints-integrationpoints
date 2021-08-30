ba5325d0d8c00fadaa567db4e57119f80b90258d
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2021-08-30 14:53:22 +0200

    REL-581824 Deprecated ISearchManager in ExportFieldsService

--- Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/Services/ExportFieldsService.cs
+++ Source/kCura.IntegrationPoints.FilesDestinationProvider.Core/Services/ExportFieldsService.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using kCura.IntegrationPoints.Core;
 using kCura.IntegrationPoints.Core.Services;
-using kCura.IntegrationPoints.FilesDestinationProvider.Core.SharedLibrary;
-using kCura.WinEDDS.Service.Export;
 using Relativity.API;
 using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1;
 using Relativity.IntegrationPoints.Contracts.Models;
@@ -16,12 +12,10 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.Services
 	public class ExportFieldsService : IExportFieldsService
 	{
 		private readonly IServicesMgr _servicesMgr;
-		private readonly IServiceManagerProvider _serviceManagerProvider;
 
-		public ExportFieldsService(IServicesMgr servicesMgr, IServiceManagerProvider serviceManagerProvider)
+		public ExportFieldsService(IServicesMgr servicesMgr)
 		{
 			_servicesMgr = servicesMgr;
-			_serviceManagerProvider = serviceManagerProvider;
 		}
 
 		public FieldEntry[] GetAllExportableFields(int workspaceArtifactID, int artifactTypeID)
@@ -31,7 +25,7 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.Services
 			using (ISearchService searchService = _servicesMgr.CreateProxy<ISearchService>(ExecutionIdentity.CurrentUser))
 			{
 				DataSet dataSet = searchService.RetrieveAllExportableViewFieldsAsync(workspaceArtifactID, artifactTypeID, string.Empty).GetAwaiter().GetResult()?.Unwrap();
-				if (dataSet != null && dataSet.Tables.Count > 0)
+				if (ContainsTable(dataSet))
 				{
 					EnumerableRowCollection<DataRow> rows = dataSet.Tables[0].AsEnumerable();
 					fields = rows.Select(CreateFieldEntry).ToArray();
@@ -44,35 +38,29 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.Services
 		public FieldEntry[] GetDefaultViewFields(int workspaceArtifactID, int viewArtifactID, int artifactTypeID, bool isProduction)
 		{
 			FieldEntry[] fields = Array.Empty<FieldEntry>();
-
 			using (ISearchService searchService = _servicesMgr.CreateProxy<ISearchService>(ExecutionIdentity.CurrentUser))
 			{
 				DataSet allExportableViewFieldsDataSet = searchService.RetrieveAllExportableViewFieldsAsync(workspaceArtifactID, artifactTypeID, string.Empty).GetAwaiter().GetResult()?.Unwrap();
+				DataSet defaultViewFieldsDataSet = searchService.RetrieveDefaultViewFieldsForIdListAsync(workspaceArtifactID, artifactTypeID, new[] { viewArtifactID }, isProduction, string.Empty).GetAwaiter().GetResult()?.Unwrap();
 
-				if (allExportableViewFieldsDataSet != null && allExportableViewFieldsDataSet.Tables.Count > 0)
+				if (ContainsTable(allExportableViewFieldsDataSet) && ContainsTable(defaultViewFieldsDataSet))
 				{
-					EnumerableRowCollection<DataRow> allExportableViewFieldsRows = allExportableViewFieldsDataSet.Tables[0].AsEnumerable();
-					int[] ids = allExportableViewFieldsRows.Select(x => int.Parse(x["FieldArtifactId"].ToString())).ToArray();
-					// TODO
-					DataSet defaultViewFieldsDataSet = searchService.RetrieveDefaultViewFieldsForIdListAsync(workspaceArtifactID, artifactTypeID, ids, isProduction, string.Empty).GetAwaiter().GetResult()?.Unwrap();
+					EnumerableRowCollection<DataRow> defaultViewFieldsDataSetRows = defaultViewFieldsDataSet.Tables[0].AsEnumerable();
+					int[] viewFieldIdsNew = defaultViewFieldsDataSetRows.Select(x => int.Parse(x["ArtifactViewFieldID"].ToString())).ToArray();
+
+					EnumerableRowCollection<DataRow> allExportableFieldsRows = allExportableViewFieldsDataSet.Tables[0].AsEnumerable();
+					fields = allExportableFieldsRows
+						.Where(x =>
+						{
+							int avfId = int.Parse(x["AvfId"].ToString());
+							return viewFieldIdsNew.Contains(avfId);
+						})
+						.Select(CreateFieldEntry)
+						.ToArray();
 				}
 			}
 
-			ISearchManager searchManager = _serviceManagerProvider.Create<ISearchManager, SearchManagerFactory>();
-
-			IEnumerable<int> viewFieldIds = searchManager.RetrieveDefaultViewFieldIds(workspaceArtifactID, viewArtifactID, artifactTypeID, isProduction);
-
-			return searchManager.RetrieveAllExportableViewFields(workspaceArtifactID, artifactTypeID)
-				.Where(x => viewFieldIds.Contains(x.AvfId))
-				.Select(x => new FieldEntry
-				{
-					DisplayName = x.DisplayName,
-					FieldIdentifier = x.AvfId.ToString(),
-					FieldType = ConvertFromExFieldType(x.FieldType),
-					IsIdentifier = x.Category == global::Relativity.DataExchange.Service.FieldCategory.Identifier,
-					IsRequired = false
-				}).ToArray();
-
+			return fields;
 		}
 
 		public FieldEntry[] GetAllExportableLongTextFields(int workspaceArtifactID, int artifactTypeID)
@@ -83,7 +71,7 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.Services
 			{
 				DataSet allExportableViewFieldsDataSet = searchService.RetrieveAllExportableViewFieldsAsync(workspaceArtifactID, artifactTypeID, string.Empty).GetAwaiter().GetResult()?.Unwrap();
 
-				if (allExportableViewFieldsDataSet != null && allExportableViewFieldsDataSet.Tables.Count > 0)
+				if (ContainsTable(allExportableViewFieldsDataSet))
 				{
 					EnumerableRowCollection<DataRow> rows = allExportableViewFieldsDataSet.Tables[0].AsEnumerable();
 					fields = rows
@@ -98,6 +86,11 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.Services
 			}
 
 			return fields;
+		}
+
+		private bool ContainsTable(DataSet dataSet)
+		{
+			return dataSet != null && dataSet.Tables.Count > 0;
 		}
 
 		private FieldEntry CreateFieldEntry(DataRow dataRow)
@@ -130,11 +123,6 @@ namespace kCura.IntegrationPoints.FilesDestinationProvider.Core.Services
 			int fieldCategoryId = int.Parse(value.ToString());
 			global::Relativity.DataExchange.Service.FieldCategory fieldCategory = (global::Relativity.DataExchange.Service.FieldCategory)fieldCategoryId;
 			return fieldCategory == global::Relativity.DataExchange.Service.FieldCategory.Identifier;
-		}
-
-		private FieldType ConvertFromExFieldType(global::Relativity.DataExchange.Service.FieldType fieldType)
-		{
-			return fieldType == global::Relativity.DataExchange.Service.FieldType.File ? FieldType.File : FieldType.String;
 		}
 	}
 }
