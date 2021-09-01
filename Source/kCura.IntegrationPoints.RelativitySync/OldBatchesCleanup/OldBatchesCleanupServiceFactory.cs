5bd60d49b6a77a9f869be882a10905ccb3259056
Krzysztof Skowronek <krzysztof.skowronek@relativity.com>
2021-09-01 12:33:46 +0200

    REL-589252 update Relativity.Sync to 0.8.39

--- Source/kCura.IntegrationPoints.RelativitySync/OldBatchesCleanup/OldBatchesCleanupServiceFactory.cs
+++ Source/kCura.IntegrationPoints.RelativitySync/OldBatchesCleanup/OldBatchesCleanupServiceFactory.cs
@@ -1,31 +1,35 @@
 ﻿using System;
 using kCura.IntegrationPoints.Core.Services;
+using kCura.IntegrationPoints.RelativitySync.RipOverride;
 using Relativity.API;
 using Relativity.Sync.KeplerFactory;
+using Relativity.Sync.RDOs.Framework;
 using Relativity.Sync.Storage;
 using Relativity.Sync.Utils;
 
 namespace kCura.IntegrationPoints.RelativitySync.OldBatchesCleanup
 {
-	public class OldBatchesCleanupServiceFactory : IOldBatchesCleanupServiceFactory
-	{
-		private readonly IServicesMgr _servicesMgr;
-		private readonly Lazy<IErrorService> _errorService;
-		private readonly IAPILog _apiLog;
+    public class OldBatchesCleanupServiceFactory : IOldBatchesCleanupServiceFactory
+    {
+        private readonly IServicesMgr _servicesMgr;
+        private readonly Lazy<IErrorService> _errorService;
+        private readonly IAPILog _apiLog;
 
-		public OldBatchesCleanupServiceFactory(IHelper helper, Lazy<IErrorService> errorService, IAPILog apiLog)
-		{
-			_servicesMgr = helper.GetServicesManager();
-			_errorService = errorService;
-			_apiLog = apiLog;
-		}
+        public OldBatchesCleanupServiceFactory(IHelper helper, Lazy<IErrorService> errorService, IAPILog apiLog)
+        {
+            _servicesMgr = helper.GetServicesManager();
+            _errorService = errorService;
+            _apiLog = apiLog;
+        }
 
-		public IOldBatchesCleanupService Create()
-		{
-			ISourceServiceFactoryForAdmin serviceFactory = new ServiceFactoryForAdmin(_servicesMgr);
-			IBatchRepository batchRepository = new BatchRepository(serviceFactory, new DateTimeWrapper());
+        public IOldBatchesCleanupService Create()
+        {
+            ISourceServiceFactoryForAdmin serviceFactory = new ServiceFactoryForAdmin(_servicesMgr);
+            IBatchRepository batchRepository = new BatchRepository(
+                new RdoManager(new SyncLog(_apiLog), new SyncServiceManagerForRip(_servicesMgr), new RdoGuidProvider()),
+                serviceFactory, new DateTimeWrapper());
 
-			return new OldBatchesCleanupService(batchRepository, _errorService, _apiLog);
-		}
-	}
+            return new OldBatchesCleanupService(batchRepository, _errorService, _apiLog);
+        }
+    }
 }
