3421abbd8f06266c54451e7520369bed893b95b3
Kamil Makarowski <kamil.makarowski@relativity.com>
2021-09-28 12:11:48 +0200

    REL-595840 revert test changes

--- Trident/Scripts/Custom-Test.ps1
+++ Trident/Scripts/Custom-Test.ps1
@@ -13,4 +13,4 @@ if($EmptySUT)
 
 $TaskRunner = Resolve-Path -Path build.ps1
 
-&($TaskRunner) Compile, Package, FunctionalTest -Configuration Release -TestFilter $TestFilter
+&($TaskRunner) Compile, Package, MyTest -Configuration Release -TestFilter $TestFilter
