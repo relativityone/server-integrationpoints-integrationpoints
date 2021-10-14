5473e7be6965f9aa0efe86efbf517b06e7c13041
Kamil Makarowski <kamil.makarowski@relativity.com>
2021-10-14 08:22:29 +0200

    REL-603459 revert EntityManagerLinksSanitizer for testing purposes

--- Source/kCura.IntegrationPoints.Synchronizers.RDO/Entity/EntityManagerLinksSanitizer.cs
+++ Source/kCura.IntegrationPoints.Synchronizers.RDO/Entity/EntityManagerLinksSanitizer.cs
@@ -1,18 +1,12 @@
-﻿using System.Linq;
-
-namespace kCura.IntegrationPoints.Synchronizers.RDO.Entity
+﻿namespace kCura.IntegrationPoints.Synchronizers.RDO.Entity
 {
 	public class EntityManagerLinksSanitizer : IEntityManagerLinksSanitizer
 	{
-		public string ManagerLinksFieldIdentifier => "cn";
+		public string ManagerLinksFieldIdentifier => "distinguishedname";
 
 		public string SanitizeManagerReferenceLink(string managerLink)
 		{
-			string sanitizedManagerLink = managerLink.Split(',')
-				.Single(x => x.StartsWith(ManagerLinksFieldIdentifier))
-				.Split('=').Last();
-
-			return sanitizedManagerLink;
+			return managerLink;
 		}
 	}
 }
