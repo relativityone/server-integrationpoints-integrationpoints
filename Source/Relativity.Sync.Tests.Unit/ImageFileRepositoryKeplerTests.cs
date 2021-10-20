771d3da2228c13393c731b1931a689cb0cd626c4
Marcin Sojka <marcin.sojka@relativity.com>
2021-10-20 17:06:44 +0200

    [REL-604675] Kepler suffix added to names of Image and Native File Repository tests

---
+++ Source/Relativity.Sync.Tests.Unit/ImageFileRepositoryKeplerTests.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1;
+using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1.Models;
+using Relativity.Sync.KeplerFactory;
+using Relativity.Sync.Transfer;
+
+namespace Relativity.Sync.Tests.Unit
+{
+	[TestFixture]
+	public class ImageFileRepositoryKeplerTests
+	{
+		private const int WORKSPACE_ID = 5;
+
+		private Mock<ISyncLog> _loggerMock;
+		private ImageFileRepositoryKepler _sut;
+		private Mock<ISearchService> _searchServiceMock;
+		private Mock<ISourceServiceFactoryForUser> _serviceFactoryMock;
+
+		[SetUp]
+		public void Setup()
+		{
+			_loggerMock = new Mock<ISyncLog>();
+
+			_searchServiceMock = new Mock<ISearchService>();
+			_serviceFactoryMock = new Mock<ISourceServiceFactoryForUser>();
+
+			_serviceFactoryMock.Setup(x => x.CreateProxyAsync<ISearchService>())
+				.ReturnsAsync(_searchServiceMock.Object);
+
+			SyncJobParameters parameters = new SyncJobParameters(It.IsAny<int>(), It.IsAny<int>(), Guid.NewGuid());
+
+			_sut = new ImageFileRepositoryKepler(_serviceFactoryMock.Object, _loggerMock.Object, parameters);
+		}
+
+		[Test]
+		public async Task QueryImagesForDocumentsAsync_ShouldReturnDocumentImages()
+		{
+			// Arrange
+			var data = Enumerable.Range(1, 10)
+				.Select(x => new DocumentImageData { DocumentArtifactId = x }).ToList();
+
+			_searchServiceMock.Setup(x => x.RetrieveImagesForSearchAsync(WORKSPACE_ID, It.IsAny<int[]>(), It.IsAny<string>()))
+				.ReturnsAsync(CreateDataSet(data));
+
+			// Act
+			IEnumerable<ImageFile> result = await _sut.QueryImagesForDocumentsAsync(WORKSPACE_ID, data.Select(x => x.DocumentArtifactId).ToArray(),
+				new QueryImagesOptions()).ConfigureAwait(false);
+
+			// Assert
+			result.Select(x => x.DocumentArtifactId).Should().BeEquivalentTo(data.Select(x => x.DocumentArtifactId));
+		}
+
+		[Test]
+		public async Task QueryImagesForDocumentsAsync_ShouldReturnDocumentImages_And_SkipDocumentsWithoutImages()
+		{
+			// Arrange
+			var data = Enumerable.Range(1, 10)
+				.Select(x => new DocumentImageData { DocumentArtifactId = x }).ToList();
+
+			_searchServiceMock.Setup(x => x.RetrieveImagesForSearchAsync(WORKSPACE_ID, It.IsAny<int[]>(), It.IsAny<string>()))
+				.ReturnsAsync(CreateDataSet(data));
+
+			// Act
+			IEnumerable<ImageFile> result = await _sut.QueryImagesForDocumentsAsync(WORKSPACE_ID, data.Select(x => x.DocumentArtifactId).Concat(Enumerable.Range(20, 5)).ToArray(),
+				new QueryImagesOptions()).ConfigureAwait(false);
+
+			// Assert
+			result.Select(x => x.DocumentArtifactId).Should().BeEquivalentTo(data.Select(x => x.DocumentArtifactId));
+		}
+
+		[Test]
+		public async Task QueryImagesForDocumentsAsync_ShouldReturnMultipleImagesPerDocument()
+		{
+			// Arrange
+			var data = Enumerable.Range(1, 10)
+				.Select(x => new DocumentImageData { DocumentArtifactId = x % 2 }).ToList();
+
+			_searchServiceMock.Setup(x => x.RetrieveImagesForSearchAsync(WORKSPACE_ID, It.IsAny<int[]>(), It.IsAny<string>()))
+				.ReturnsAsync(CreateDataSet(data));
+
+			// Act
+			IEnumerable<ImageFile> result = await _sut.QueryImagesForDocumentsAsync(WORKSPACE_ID, data.Select(x => x.DocumentArtifactId).ToArray(),
+				new QueryImagesOptions()).ConfigureAwait(false);
+
+			// Assert
+			result.Select(x => x.DocumentArtifactId).Should().BeEquivalentTo(data.Select(x => x.DocumentArtifactId));
+		}
+
+		[Test]
+		public async Task QueryImagesForDocumentsAsync_ShouldReturnProducedDocumentImages()
+		{
+			// Arrange
+			var data = Enumerable.Range(1, 10)
+				.Select(x => new DocumentImageData { DocumentArtifactId = x, ProductionId = 1 }).ToList();
+
+
+			MockProductions(data);
+
+			// Act
+			IEnumerable<ImageFile> result = await _sut.QueryImagesForDocumentsAsync(WORKSPACE_ID, data.Select(x => x.DocumentArtifactId).ToArray(),
+				new QueryImagesOptions { ProductionIds = new[] { 1 } }).ConfigureAwait(false);
+
+			// Assert
+			result.Select(x => x.DocumentArtifactId).Should().BeEquivalentTo(data.Select(x => x.DocumentArtifactId));
+			result.All(x => x.ProductionId == 1).Should().BeTrue();
+		}
+
+		[Test]
+		public async Task QueryImagesForDocumentsAsync_ShouldReturnProducedDocumentImages_And_IncludeOriginalImages()
+		{
+			// Arrange
+			var data = Enumerable.Range(1, 10)
+				.Select(x => new DocumentImageData { DocumentArtifactId = x, ProductionId = (x % 2) + 1 }).ToList();
+
+			MockProductions(data);
+
+			_searchServiceMock.Setup(x => x.RetrieveImagesForSearchAsync(WORKSPACE_ID, It.IsAny<int[]>(), It.IsAny<string>()))
+				.ReturnsAsync(CreateDataSet(data.Where(x => x.ProductionId == 2)));
+			
+			// Act
+			IEnumerable<ImageFile> result = await _sut.QueryImagesForDocumentsAsync(WORKSPACE_ID, data.Select(x => x.DocumentArtifactId).ToArray(),
+				new QueryImagesOptions { ProductionIds = new[] { 1 }, IncludeOriginalImageIfNotFoundInProductions = true }).ConfigureAwait(false);
+
+			// Assert
+			data.All(x => result.Any(r => r.DocumentArtifactId == x.DocumentArtifactId)).Should().BeTrue();
+
+			result.Count(x => x.ProductionId == 1).Should().Be(5);
+			result.Count(x => x.ProductionId == null).Should().Be(5);
+		}
+
+		[Test]
+		public async Task QueryImagesForDocumentsAsync_ShouldNotCallOriginalImages_WhenIncludeOriginalImagesIsSetAndAllImagesMeetProductionPrecedence()
+		{
+			// Arrange
+			var data = Enumerable.Range(1, 10)
+				.Select(x => new DocumentImageData { DocumentArtifactId = x, ProductionId = 1 }).ToList();
+
+			MockProductions(data);
+
+			QueryImagesOptions options = new QueryImagesOptions
+				{ ProductionIds = new[] {1}, IncludeOriginalImageIfNotFoundInProductions = true };
+
+			int[] documentIds = data.Select(x => x.DocumentArtifactId).ToArray();
+
+			// Act
+			await _sut.QueryImagesForDocumentsAsync(WORKSPACE_ID, documentIds, options).ConfigureAwait(false);
+
+			// Assert
+			_searchServiceMock.Verify(x => x.RetrieveImagesForSearchAsync(WORKSPACE_ID, It.IsAny<int[]>(), It.IsAny<string>()), Times.Never);
+		}
+
+		[Test]
+		public async Task QueryImagesForDocumentsAsync_ShouldNotGetOriginalImages_WhenIncludeOriginalImagesIsSetAndImageForProductionWasFound()
+		{
+			// Arrange
+			const int productionId = 1;
+
+			var productionData = new DocumentImageData[]
+			{
+				new DocumentImageData { DocumentArtifactId = 1000, ProductionId = productionId },
+				new DocumentImageData { DocumentArtifactId = 1001, ProductionId = 2 }
+			};
+
+			MockProductions(productionData);
+
+			
+			_searchServiceMock.Setup(x => x.RetrieveImagesForSearchAsync(WORKSPACE_ID, It.IsAny<int[]>(), It.IsAny<string>()))
+				.ReturnsAsync(CreateDataSet(productionData.Where(x => x.ProductionId == 1)));
+		
+			var documentIdsWithProductionId = productionData
+				.Where(x => x.ProductionId == productionId)
+				.Select(x => x.DocumentArtifactId);
+
+			QueryImagesOptions options = new QueryImagesOptions
+				{ ProductionIds = new[] { productionId }, IncludeOriginalImageIfNotFoundInProductions = true };
+
+			int[] documentIds = productionData.Select(x => x.DocumentArtifactId).ToArray();
+
+			// Act
+			await _sut.QueryImagesForDocumentsAsync(WORKSPACE_ID, documentIds, options).ConfigureAwait(false);
+
+			// Assert
+			_searchServiceMock.Verify(x => x.RetrieveImagesForSearchAsync(WORKSPACE_ID, 
+				It.Is<int[]>(expectedIds => expectedIds.Intersect(documentIdsWithProductionId).Any()), It.IsAny<string>()), Times.Never);
+		}
+
+
+		[Test]
+		public async Task QueryImagesForDocumentsAsync_ShouldReturnProducedDocumentImages_WithRespectToProductionPrecedence()
+		{
+			// Arrange
+			var data = Enumerable.Range(1, 10)
+				.Select(x => new DocumentImageData { DocumentArtifactId = x, ProductionId = 1 })
+				.Concat(Enumerable.Range(1, 5)
+					.Select(x => new DocumentImageData { DocumentArtifactId = x, ProductionId = 2 }))
+				.ToList();
+
+			MockProductions(data);
+
+			// Act
+			IEnumerable<ImageFile> result = await _sut.QueryImagesForDocumentsAsync(WORKSPACE_ID, data.Select(x => x.DocumentArtifactId).ToArray(),
+				new QueryImagesOptions { ProductionIds = new[] { 2, 1 } }).ConfigureAwait(false);
+
+			// Assert
+			result.Select(x => x.DocumentArtifactId).Should().BeEquivalentTo(Enumerable.Range(1, 10));
+
+
+			result.Where(x => x.ProductionId == 1).Select(x => x.DocumentArtifactId).Should()
+				.BeEquivalentTo(Enumerable.Range(6, 5));
+
+			result.Where(x => x.ProductionId == 2).Select(x => x.DocumentArtifactId).Should()
+				.BeEquivalentTo(Enumerable.Range(1, 5));
+		}
+
+		[Test]
+		public async Task QueryImagesForDocumentsAsync_ShouldStopSearchForProducedDocumentImages_WhenAllImagesFound()
+		{
+			// Arrange
+			var data = Enumerable.Range(1, 10)
+				.Select(x => new DocumentImageData { DocumentArtifactId = x, ProductionId = 1 })
+				.Concat(Enumerable.Range(1, 5)
+					.Select(x => new DocumentImageData { DocumentArtifactId = x, ProductionId = 2 }))
+				.ToList();
+
+			MockProductions(data);
+			
+
+			// Act
+			IEnumerable<ImageFile> result = await _sut.QueryImagesForDocumentsAsync(WORKSPACE_ID, data.Select(x => x.DocumentArtifactId).ToArray(),
+				new QueryImagesOptions { ProductionIds = new[] { 1, 2 } }).ConfigureAwait(false);
+
+			// Assert
+			_searchServiceMock.Verify(x => x.RetrieveImagesByProductionArtifactIDForProductionExportByDocumentSetAsync(WORKSPACE_ID,1, It.IsAny<int[]>(), It.IsAny<string>()), Times.Once);
+			_searchServiceMock.Verify(x => x.RetrieveImagesByProductionArtifactIDForProductionExportByDocumentSetAsync(WORKSPACE_ID,2, It.IsAny<int[]>(), It.IsAny<string>()), Times.Never);
+		}
+
+
+		private void MockProductions(IEnumerable<DocumentImageData> data)
+		{
+			foreach (var productionSet in data.GroupBy(x => x.ProductionId).Where(x => x.Key != null))
+			{
+				_searchServiceMock
+					.Setup(x => x.RetrieveImagesByProductionArtifactIDForProductionExportByDocumentSetAsync(WORKSPACE_ID, productionSet.Key.Value, 
+						It.IsAny<int[]>(),It.IsAny<string>()))
+					.ReturnsAsync(CreateDataSet(productionSet));
+			}
+		}
+
+		private DataSetWrapper CreateDataSet(IEnumerable<DocumentImageData> data)
+		{
+			var dataTable = new DataTable();
+
+			dataTable.Columns.Add("DocumentArtifactID", typeof(int));
+			dataTable.Columns.Add("Location");
+			dataTable.Columns.Add("ImageFileName");
+			dataTable.Columns.Add("ImageSize", typeof(long));
+			dataTable.Columns.Add("Filename");
+			dataTable.Columns.Add("NativeIdentifier");
+			dataTable.Columns.Add("Identifier");
+			dataTable.Columns.Add("Size", typeof(long));
+
+			foreach (var imageData in data)
+			{
+				DataRow dataRow = dataTable.NewRow();
+
+				dataRow["DocumentArtifactID"] = imageData.DocumentArtifactId;
+				dataRow["NativeIdentifier"] = imageData.DocumentArtifactId.ToString();
+				dataRow["Identifier"] = imageData.Identifier ?? (imageData.DocumentArtifactId.ToString() + (imageData.ProductionId != null ? "_" + imageData.ProductionId : ""));
+				dataRow["Location"] = "location";
+				dataRow["ImageFileName"] = imageData.DocumentArtifactId.ToString();
+				dataRow["Filename"] = imageData.DocumentArtifactId.ToString();
+				dataRow["Size"] = imageData.ImageSize;
+				dataRow["ImageSize"] = imageData.ImageSize;
+
+				dataTable.Rows.Add(dataRow);
+			}
+
+			return new DataSetWrapper(new DataSet { Tables = { dataTable } });
+		}
+
+		private struct DocumentImageData
+		{
+			public int DocumentArtifactId { get; set; }
+			public string Identifier { get; set; }
+			public int ImageSize { get; set; }
+			public int? ProductionId { get; set; }
+		}
+	}
+}
---
+++ Source/Relativity.Sync.Tests.Unit/Transfer/NativeFileRepositoryKeplerTests.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1;
+using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1.Models;
+using Relativity.Services.Objects;
+using Relativity.Sync.KeplerFactory;
+using Relativity.Sync.Logging;
+using Relativity.Sync.Transfer;
+
+namespace Relativity.Sync.Tests.Unit.Transfer
+{
+	[TestFixture]
+	internal sealed class NativeFileRepositoryKeplerTests
+	{
+		private Mock<ISearchService> _searchService;
+		private Mock<IObjectManager> _objectManager;
+
+		private NativeFileRepositoryKepler _instance;
+		private Mock<ISourceServiceFactoryForUser> _serviceFactory;
+
+		private const string _DOCUMENT_ARTIFACT_ID_COLUMN_NAME = "DocumentArtifactID";
+		private const string _LOCATION_COLUMN_NAME = "Location";
+		private const string _FILENAME_COLUMN_NAME = "Filename";
+		private const string _SIZE_COLUMN_NAME = "Size";
+
+		[SetUp]
+		public void SetUp()
+		{
+			_searchService = new Mock<ISearchService>();
+			_serviceFactory = new Mock<ISourceServiceFactoryForUser>();
+			_objectManager = new Mock<IObjectManager>();
+
+			_serviceFactory.Setup(x => x.CreateProxyAsync<IObjectManager>()).ReturnsAsync(_objectManager.Object);
+			_serviceFactory.Setup(x => x.CreateProxyAsync<ISearchService>()).ReturnsAsync(_searchService.Object);
+
+			SyncJobParameters parameters = new SyncJobParameters(It.IsAny<int>(), It.IsAny<int>(), Guid.NewGuid());
+
+			_instance = new NativeFileRepositoryKepler(_serviceFactory.Object, new EmptyLogger(), parameters);
+		}
+
+		[Test]
+		public void ItShouldThrowProperExceptionWhenGetNativesFails()
+		{
+			// Arrange
+			const int workspaceArtifactId = 123;
+			ICollection<int> documentIds = new[] { 0, 1 };
+
+			_searchService.Setup(x => x.RetrieveNativesForSearchAsync(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>()))
+				.Throws(new InvalidOperationException());
+
+			// Act
+			Action action = () => _instance.QueryAsync(workspaceArtifactId, documentIds).ConfigureAwait(false).GetAwaiter().GetResult();
+
+			// Assert
+			action.Should().Throw<InvalidOperationException>();
+		}
+
+		[Test]
+		public async Task ItShouldCallGetNativesWithCorrectArguments()
+		{
+			// Arrange
+			const int workspaceArtifactId = 123;
+			ICollection<int> documentIds = new[] { 0, 1 };
+
+			_searchService.Setup(x => x.RetrieveNativesForSearchAsync(workspaceArtifactId, It.Is<string>(ids => ids == string.Join(",", documentIds)), It.IsAny<string>()))
+				.ReturnsAsync(CreateSampleDataSet)
+				.Verifiable();
+
+			// Act
+			await _instance.QueryAsync(workspaceArtifactId, documentIds).ConfigureAwait(false);
+
+			// Assert
+			_searchService.Verify();
+		}
+
+		[Test]
+		public async Task ItShouldShortCircuitOnEmptyDocumentIds()
+		{
+			// Arrange
+			const int workspaceArtifactId = 123;
+			ICollection<int> documentIds = Array.Empty<int>();
+
+			// Act
+			IEnumerable<INativeFile> results = await _instance.QueryAsync(workspaceArtifactId, documentIds).ConfigureAwait(false);
+
+			// Assert
+			results.Should().BeEmpty();
+			_searchService.Verify(x => x.RetrieveNativesForSearchAsync(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+		}
+
+		[Test]
+		public async Task ItShouldShortCircuitOnNullDocumentIds()
+		{
+			// Arrange
+			const int workspaceArtifactId = 123;
+			ICollection<int> documentIds = null;
+
+			// Act
+			IEnumerable<INativeFile> results = await _instance.QueryAsync(workspaceArtifactId, documentIds).ConfigureAwait(false);
+
+			// Assert
+			results.Should().BeEmpty();
+			_searchService.Verify(x => x.RetrieveNativesForSearchAsync(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+		}
+
+#pragma warning restore RG2009 // Hardcoded Numeric Value
+
+
+		private static IEnumerable<TestCaseData> TransformResponsesCorrectlyCases()
+		{
+			yield return new TestCaseData(new DataSetWrapper(new DataSet()), Enumerable.Empty<INativeFile>())
+			{
+				TestName = "Empty file response"
+			};
+
+			yield return new TestCaseData(null, Enumerable.Empty<INativeFile>())
+			{
+				TestName = "Null file response"
+			};
+
+			DataSetWrapper response = CreateSampleDataSet();
+
+			NativeFile[] expectedNativeFiles =
+			{
+				new NativeFile(123, @"\\test1\test2", "test3.txt", 11),
+				new NativeFile(456, @"\\test2\test3", "test5.txt", 12),
+				new NativeFile(789, @"\\test3\test4", "test6.html", 13)
+			};
+
+			yield return new TestCaseData(response, expectedNativeFiles)
+			{
+				TestName = "Proper file response transformation"
+			};
+		}
+
+		private static DataSetWrapper CreateSampleDataSet() => CreateSampleDataSet(null);
+
+		private static DataSetWrapper CreateSampleDataSet(IEnumerable<INativeFile> natives)
+		{
+			natives = natives ?? new List<INativeFile>
+			{
+				new NativeFile(123, @"\\test1\test2",  "test3.txt", 11),
+				new NativeFile(456, @"\\test2\test3",  "test5.txt", 12),
+				new NativeFile(789, @"\\test3\test4",  "test6.html", 13)
+			};
+
+			DataTable dataTable = new DataTable("Table");
+			dataTable.Columns.AddRange(new[]
+			{
+				new DataColumn(_DOCUMENT_ARTIFACT_ID_COLUMN_NAME, typeof(int)),
+				new DataColumn(_LOCATION_COLUMN_NAME, typeof(string)),
+				new DataColumn(_FILENAME_COLUMN_NAME, typeof(string)),
+				new DataColumn(_SIZE_COLUMN_NAME, typeof(long))
+			});
+
+			foreach (var nativeFile in natives)
+			{
+				dataTable.Rows.Add(CreateRow(dataTable, nativeFile.DocumentArtifactId, nativeFile.Location, nativeFile.Filename, nativeFile.Size));
+			}
+
+			DataSet dataSet = new DataSet();
+			dataSet.Tables.Add(dataTable);
+			return new DataSetWrapper(dataSet);
+		}
+
+		private static DataRow CreateRow(DataTable table, int id, string location, string fileName, long size)
+		{
+			DataRow row = table.NewRow();
+			row[_DOCUMENT_ARTIFACT_ID_COLUMN_NAME] = id;
+			row[_LOCATION_COLUMN_NAME] = location;
+			row[_FILENAME_COLUMN_NAME] = fileName;
+			row[_SIZE_COLUMN_NAME] = size;
+			return row;
+		}
+
+#pragma warning restore RG2009 // With the exception of zero and one, never hard-code a numeric value; always declare a constant instead
+
+		[TestCaseSource(nameof(TransformResponsesCorrectlyCases))]
+		public async Task ItShouldTransformResponsesCorrectly(DataSetWrapper response, IEnumerable<INativeFile> expected)
+		{
+			// Arrange
+			const int workspaceArtifactId = 123;
+			ICollection<int> documentIds = new[] { 0, 1 };
+
+			_searchService.Setup(x => x.RetrieveNativesForSearchAsync(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>()))
+				.ReturnsAsync(response);
+
+			// Act
+			IEnumerable<INativeFile> actual = await _instance.QueryAsync(workspaceArtifactId, documentIds).ConfigureAwait(false);
+
+			// Assert
+			List<INativeFile> expectedList = expected.ToList();
+			List<INativeFile> actualList = actual.ToList();
+
+			expectedList.Count.Should().Be(actualList.Count);
+			expectedList.Zip(actualList, AreEqual).All(x => x).Should().Be(true);
+		}
+
+		[Test]
+		public async Task ItShouldIgnoreInputLengthWhenReturningResults()
+		{
+			// Arrange
+			const int workspaceArtifactId = 123;
+			const int numDocumentIds = 10;
+			ICollection<int> documentIds = Enumerable.Range(0, numDocumentIds).ToList();
+
+			const int numFileResponses = 3;
+			_searchService.Setup(x => x.RetrieveNativesForSearchAsync(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>()))
+				.ReturnsAsync(CreateSampleDataSet);
+
+			// Act
+			IEnumerable<INativeFile> actual = await _instance.QueryAsync(workspaceArtifactId, documentIds).ConfigureAwait(false);
+
+			// Assert
+			actual.Count().Should().Be(numFileResponses);
+		}
+
+		private static bool AreEqual(INativeFile me, INativeFile you)
+		{
+			return
+				me != null && you != null &&
+				me.DocumentArtifactId == you.DocumentArtifactId &&
+				me.Location == you.Location &&
+				me.Filename == you.Filename &&
+				me.Size == you.Size;
+		}
+	}
+}
