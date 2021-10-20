a15c7b814bfff7ad7e1b65ff6baf6924a9354a7e
Marcin Sojka <marcin.sojka@relativity.com>
2021-10-20 13:11:16 +0200

    [REL-604675] EnableLegacyWebAPIToggle added

---
+++ Source/Relativity.Sync/Toggles/EnableLegacyWebAPIToggle.cs
@@ -0,0 +1,10 @@
+﻿using Relativity.Toggles;
+
+namespace Relativity.Sync.Toggles
+{
+    [DefaultValue(false)]
+    [Description("Enable toggle to use legacy WebAPI", "Adler Sieben")]
+    public class EnableLegacyWebAPIToggle : IToggle
+    {
+    }
+}
