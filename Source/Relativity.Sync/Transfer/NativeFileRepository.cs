487dc3ee8f68d8e143b7bd21344e55e18d6b0751
Marcin Sojka <marcin.sojka@relativity.com>
2021-10-20 17:05:18 +0200

    [REL-604675] EnableKeplerizedImportAPIToggle implemented for Native and Image File Repository classes

--- Source/Relativity.Sync/Transfer/NativeFileRepository.cs
+++ Source/Relativity.Sync/Transfer/NativeFileRepository.cs
@@ -1,102 +1,30 @@
-﻿using System;
+﻿using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1;
-using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1.Models;
 using Relativity.Sync.KeplerFactory;
+using Relativity.Sync.Toggles;
+using Relativity.Toggles;
 
 namespace Relativity.Sync.Transfer
 {
-	internal sealed class NativeFileRepository : INativeFileRepository
-	{
-		private readonly ISourceServiceFactoryForUser _serviceFactory;
-		private readonly ISyncLog _logger;
-		private readonly SyncJobParameters _parameters;
+    internal class NativeFileRepository : INativeFileRepository
+    {
+        private readonly INativeFileRepository _nativeFileRepository;
 
-		public NativeFileRepository(ISourceServiceFactoryForUser serviceFactory, ISyncLog logger, SyncJobParameters parameters)
-		{
-			_serviceFactory = serviceFactory;
-			_logger = logger;
-			_parameters = parameters;
-		}
+        public NativeFileRepository(IToggleProvider toggleProvider, ISearchManagerFactory searchManagerFactory, ISourceServiceFactoryForUser serviceFactory, ISyncLog logger, SyncJobParameters parameters)
+        {
+            if (toggleProvider.IsEnabled<EnableKeplerizedImportAPIToggle>())
+            {
+                _nativeFileRepository = new NativeFileRepositoryKepler(serviceFactory, logger, parameters);
+            }
+            else
+            {
+                _nativeFileRepository = new NativeFileRepositoryWebAPI(searchManagerFactory, logger);
+            }
+        }
 
-		public async Task<IEnumerable<INativeFile>> QueryAsync(int workspaceId, ICollection<int> documentIds)
-		{
-			IEnumerable<INativeFile> empty = Enumerable.Empty<INativeFile>();
-
-			if (documentIds == null || !documentIds.Any())
-			{
-				return empty;
-			}
-
-			_logger.LogInformation("Searching for native files. Documents count: {numberOfDocuments}", documentIds.Count);
-
-			using (ISearchService searchService = await _serviceFactory.CreateProxyAsync<ISearchService>().ConfigureAwait(false))
-			{
-				string concatenatedArtifactIds = string.Join(",", documentIds);
-				DataSetWrapper dataSetWrapper = await searchService.RetrieveNativesForSearchAsync(
-					workspaceId, concatenatedArtifactIds, _parameters.WorkflowId).ConfigureAwait(false);
-
-				if (dataSetWrapper == null)
-				{
-					_logger.LogWarning("SearchService returned null data set.");
-					return empty;
-				}
-
-				DataSet dataSet = dataSetWrapper.Unwrap();
-				if (dataSet.Tables.Count == 0)
-				{
-					_logger.LogWarning("SearchService returned empty data set.");
-					return empty;
-				}
-
-				DataTable dataTable = dataSet.Tables[0];
-				IList<INativeFile> nativeFiles = GetNativeFiles(dataTable);
-
-				_logger.LogInformation("Found {numberOfNatives} native files.", nativeFiles.Count);
-
-				return nativeFiles;
-			}
-		}
-
-		private IList<INativeFile> GetNativeFiles(DataTable dataTable)
-		{
-			List<INativeFile> natives = new List<INativeFile>(dataTable.Rows.Count);
-
-			foreach (DataRow dataRow in dataTable.Rows)
-			{
-				INativeFile nativeFile = GetNativeFile(dataRow);
-				natives.Add(nativeFile);
-			}
-
-			return natives;
-		}
-
-		private INativeFile GetNativeFile(DataRow dataRow)
-		{
-			int documentArtifactId = GetValue<int>(dataRow, "DocumentArtifactID");
-			string location = GetValue<string>(dataRow, "Location");
-			string fileName = GetValue<string>(dataRow, "Filename");
-			long size = GetValue<long>(dataRow, "Size");
-
-			return new NativeFile(documentArtifactId, location, fileName, size);
-		}
-
-		private T GetValue<T>(DataRow row, string columnName)
-		{
-			object value = null;
-			try
-			{
-				value = row[columnName];
-				return (T) value;
-			}
-			catch (Exception ex)
-			{
-				_logger.LogError(ex, "Error while retrieving native file info from column \"{columnName}\". Value: \"{value}\" Requested type: \"{type}\"", columnName, value, typeof(T));
-				throw;
-			}
-		}
-	}
+        public async Task<IEnumerable<INativeFile>> QueryAsync(int workspaceId, ICollection<int> documentIds)
+        {
+            return await _nativeFileRepository.QueryAsync(workspaceId, documentIds).ConfigureAwait(false);
+        }
+    }
 }
---
+++ Source/Relativity.Sync/Transfer/SearchManagerInterceptor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web.Services.Protocols;
+using Polly;
+using Polly.Retry;
+using Castle.DynamicProxy;
+using kCura.WinEDDS.Service.Export;
+
+namespace Relativity.Sync.Transfer
+{
+	internal sealed class SearchManagerInterceptor : IInterceptor
+	{
+		private const int _MAX_NUMBER_OF_RELOGINS = 3;
+
+		private readonly Func<Task<ISearchManager>> _searchManagerFactoryAsync;
+		private readonly System.Reflection.FieldInfo _currentInterceptorIndexField;
+
+		public SearchManagerInterceptor(Func<Task<ISearchManager>> searchManagerFactoryAsync)
+		{
+			_searchManagerFactoryAsync = searchManagerFactoryAsync;
+
+			_currentInterceptorIndexField = typeof(AbstractInvocation).GetField("currentInterceptorIndex", BindingFlags.NonPublic | BindingFlags.Instance);
+		}
+
+		public void Intercept(IInvocation invocation)
+		{
+			if (invocation.Method.Name == nameof(IDisposable.Dispose))
+			{
+				invocation.Proceed();
+				return;
+			}
+
+			invocation.ReturnValue = HandleWithReLogin(invocation);
+		}
+
+		private object HandleWithReLogin(IInvocation invocation)
+		{
+			RetryPolicy reLoginPolicy = Policy
+				.Handle<SoapException>(ex => ex.ToString().Contains("NeedToReLoginException"))
+				.Retry(_MAX_NUMBER_OF_RELOGINS,
+					(ex, retryCount, context) =>
+					{
+						IChangeProxyTarget changeProxyTarget = invocation as IChangeProxyTarget;
+						if (changeProxyTarget != null)
+						{
+							ISearchManager newSearchManagerInstance = _searchManagerFactoryAsync().GetAwaiter().GetResult();
+							changeProxyTarget.ChangeInvocationTarget(newSearchManagerInstance);
+						}
+						else
+						{
+							throw new SyncException($"Cannot change proxy target. Make sure ProxyGenerator is created using CreateInterfaceProxyWithTargetInterface method.");
+						}
+					});
+
+			return reLoginPolicy.Execute(() =>
+			{
+				_currentInterceptorIndexField.SetValue(invocation, 0);
+
+				invocation.Proceed();
+
+				return invocation.ReturnValue;
+			});
+		}
+	}
+}
