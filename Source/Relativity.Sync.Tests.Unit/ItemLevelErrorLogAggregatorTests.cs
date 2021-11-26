406894dd04f5f81a3e1f28059cc574994c5ca903
Krzysztof Skowronek <krzysztof.skowronek@relativity.com>
2021-11-26 16:38:04 +0100

    REL-612495 added log aggregation for each batch

--- Source/Relativity.Sync.Tests.Unit/ItemLevelErrorLogAggregatorTests.cs
+++ Source/Relativity.Sync.Tests.Unit/ItemLevelErrorLogAggregatorTests.cs
@@ -14,7 +14,6 @@ namespace Relativity.Sync.Tests.Unit
         private Mock<ISyncLog> _loggerMock;
         private ItemLevelErrorLogAggregator _sut;
 
-        private const string IdentifierReplacement = "[identifier]";
         private const string SampleErrorMessage1 = "Sample error message 1";
         private const string SampleErrorMessage2 = "Sample error message 2";
 
@@ -26,7 +25,7 @@ namespace Relativity.Sync.Tests.Unit
         }
 
         [Test]
-        public async Task LogAllItemLevelErrors_ShouldLogAllAddedErrors()
+        public async Task LogAllItemLevelErrorsAsync_ShouldLogAllAddedErrors()
         {
             // Arrange
             foreach ((ItemLevelError error, int i) in GetErrors().Select((x, i) => (x, i)))
@@ -47,15 +46,31 @@ namespace Relativity.Sync.Tests.Unit
                 , It.Is((string ids) => ids == "1, 4")));
         }
 
+        [Test]
+        public async Task LogAllItemLevelErrorsAsync_ShouldLogTotalCountOfErrors()
+        {
+            // Arrange
+            foreach ((ItemLevelError error, int i) in GetErrors().Select((x, i) => (x, i)))
+            {
+                _sut.AddItemLevelError(error, i);
+            }
+
+            // Act
+            await _sut.LogAllItemLevelErrorsAsync().ConfigureAwait(false);
+
+            // Assert
+            _loggerMock.Verify(x => x.LogWarning("Total count of item level errors in batch: {count}", 5), Times.Once);
+        }
+
         [TestCaseSource(nameof(KnownItemLevelErrors))]
         public async Task ShouldGroupAllKnowItemLevelErrors((string identifier, string error)[] errors,
             string expectedCleanedUpMessage)
         {
             // Arrange
             foreach ((ItemLevelError error, int i) in errors
-                                .Select((x, i) =>
-                                    (new ItemLevelError(x.identifier, x.error), i))
-                )
+                .Select((x, i) =>
+                    (new ItemLevelError(x.identifier, x.error), i))
+            )
             {
                 _sut.AddItemLevelError(error, i);
             }
@@ -83,27 +98,78 @@ namespace Relativity.Sync.Tests.Unit
             yield return new TestCaseData(
                 new[]
                 {
-                    ("ABC", "IAPI  - An item with identifier ABC already exists in the workspace"),
-                    ("QWERTY", "IAPI  - An item with identifier QWERTY already exists in the workspace"),
-                    ("Kapitan Bomba", "IAPI  - An item with identifier Kapitan Bomba already exists in the workspace")
-                },
-                "IAPI  - An item with identifier [identifier] already exists in the workspace"
-            )
-            {
-                TestName = "IAPI  - An item with identifier [identifier] already exists in the workspace"
-            };
-            
-            yield return new TestCaseData(
-                new[]
-                {
-                    ("ABC", "IAPI  - 20.006. Failed to copy source field into destination field due to missing child object. Review the following destination field(s): Control Number, Extracted Text"),
-                    ("QWERTY", "IAPI  - 20.006. Failed to copy source field into destination field due to missing child object. Review the following destination field(s): Some Field"),
+                    ("ABC",
+                        "IAPI  - 20.006. Failed to copy source field into destination field due to missing child object. Review the following destination field(s): Control Number, Extracted Text"),
+                    ("QWERTY",
+                        "IAPI  - 20.006. Failed to copy source field into destination field due to missing child object. Review the following destination field(s): Some Field"),
                 },
                 "IAPI  - 20.006. Failed to copy source field into destination field due to missing child object"
             )
             {
-                TestName = "IAPI  - 20.006. Failed to copy source field into destination field due to missing child object. Review the following destination field(s):"
+                TestName =
+                    "IAPI  - 20.006. Failed to copy source field into destination field due to missing child object. Review the following destination field(s):"
             };
+
+            yield return new TestCaseData(
+                new[]
+                {
+                    ("ABC", "IAPI  - An item with identifier ABC already exists in the workspace"),
+                    ("ABC",
+                        "IAPI  - An item with identifier ABC already exists in the workspace* - Field * Error: Cannot create * because a file or directory with the same name already exists."),
+                    ("ABC",
+                        "IAPI  - An item with identifier ABC already exists in the workspace* - Field * Error: Incorrect function.*"),
+                    ("ABC",
+                        "IAPI  - An item with identifier ABC already exists in the workspace* - A non unique associated object is specified for this new object"),
+                },
+                "IAPI  - An item with identifier {0} already exists in the workspace"
+            )
+            {
+                TestName = "IAPI  - An item with identifier {0} already exists in the workspace"
+            };
+
+            yield return new TestCaseData(
+                new[]
+                {
+                    ("ABC", "IAPI  - A non unique associated object is specified for this new object"),
+                    ("DEF",
+                        "IAPI  - A non unique associated object is specified for this new object* - Field * Error: Incorrect function.*")
+                },
+                "IAPI  - A non unique associated object is specified for this new object"
+            )
+            {
+                TestName = "IAPI  - A non unique associated object is specified for this new object"
+            };
+
+            yield return new TestCaseData(
+                new[]
+                {
+                    ("ABC",
+                        "IAPI Error in line 10, column 6. The input value from the * source field has a length of * character(s). This exceeds the limit for the * destination field, which is currently set to * character(s)."),
+                    ("ABC",
+                        "IAPI Error in line *, column *. Object identifier for field * references an identifier that is not unique.")
+                },
+                "IAPI Error in line *, column *."
+            ) { TestName = "IAPI Error in line *, column *." };
+
+            yield return new TestCaseData(
+                new[]
+                {
+                    ("ABC", "IAPI  - Field Control Number Error: Incorrect function."),
+                    ("ABC",
+                        "IAPI  - Field Control Number Error: Cannot create * because a file or directory with the same name already exists."),
+                    ("ABC", @"IAPI  - Field Control Number Error: Could not find file \\path\to\file."),
+                    ("ABC", "IAPI  - Field Control Number Error: Insufficient system resources exist to complete the requested service."),
+                },
+                "IAPI Error in line *, column *."
+            ) { TestName = "IAPI  - Field * Error" };
+
+            yield return new TestCaseData(
+                new[]
+                {
+                    ("ABC", "IAPI  - One of the files specified for this document does not exist" ),
+                },
+                "IAPI  - One of the files specified for this document does not exist"
+            ) { TestName = "IAPI  - One of the files specified for this document does not exist" };
         }
     }
 }
--- Source/Relativity.Sync/Logging/ItemLevelErrorLogAggregator.cs
+++ Source/Relativity.Sync/Logging/ItemLevelErrorLogAggregator.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Relativity.Sync.Transfer;
 
@@ -14,10 +16,14 @@ namespace Relativity.Sync.Logging
         private readonly Func<string, ItemLevelError, (bool matched, string newMessage)>[] _standarizationFunctions = {
             ReplaceIdentifier,
             FailedToCopySourceField,
-            TrimWhitespace
+            NonUniqueAssociatedObject,
+            ItemWithIdentifierAlreadyExists,
+            ErrorInLine,
+            FieldAndError,
         };
         
         private readonly Task _processingTask;
+        private int _itemLevelErrorCount = 0;
 
         public ItemLevelErrorLogAggregator(ISyncLog logger)
         {
@@ -33,9 +39,13 @@ namespace Relativity.Sync.Logging
             {
                 try
                 {
+                    Stopwatch stopwatch = new Stopwatch();
                     foreach ((ItemLevelError error, int artifactId) in _queue.GetConsumingEnumerable())
                     {
+                        stopwatch.Restart();
+                        _itemLevelErrorCount++;
                         string message = error.Message;
+                        
                         foreach (var f in _standarizationFunctions)
                         {
                             (bool matched, string newMessage) = f(message, error);
@@ -47,9 +57,11 @@ namespace Relativity.Sync.Logging
                         }
 
                         // no lock needed since there is only one thread doing this
+                        message = message.Trim();
                         EnsureKeyExists(message);
                         _errorsAggregate[message].Add(artifactId);
 
+                        _logger.LogInformation("Processed log for {artifactId} in {time} ms", artifactId, stopwatch.ElapsedMilliseconds);
                     }
                 }
                 catch (Exception e)
@@ -77,8 +89,13 @@ namespace Relativity.Sync.Logging
 
         public async Task LogAllItemLevelErrorsAsync()
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             _queue.CompleteAdding();
             await _processingTask.ConfigureAwait(false);
+            stopwatch.Stop();
+            
+            _logger.LogWarning("Waited {time} ms for log queue to be processed", stopwatch.ElapsedMilliseconds);
+            _logger.LogWarning("Total count of item level errors in batch: {count}", _itemLevelErrorCount);
             
             foreach (var keyValuePair in _errorsAggregate)
             {
@@ -89,12 +106,7 @@ namespace Relativity.Sync.Logging
         }
         
         #region Standarization functions
-
-        private static (bool matched, string newMessage) TrimWhitespace(string message, ItemLevelError error)
-        {
-            return (true, message.Trim());
-        }
-
+        
         private static (bool matched, string newMessage) ReplaceIdentifier(string message, ItemLevelError error)
         {
             return (false, message.Replace(error.Identifier, "[identifier]"));
@@ -104,18 +116,62 @@ namespace Relativity.Sync.Logging
         {
             const string resultMessage =
                 "IAPI  - 20.006. Failed to copy source field into destination field due to missing child object";
-            
+
             const string messageTemplate =
                 resultMessage + ". Review the following destination field(s):";
-            
+
             if (message.StartsWith(messageTemplate))
             {
                 return (true, resultMessage);
             }
 
-            return (false,null);
+            return (false, null);
+        }
+
+        private static (bool, string) NonUniqueAssociatedObject(string message, ItemLevelError error)
+        {
+            const string messageTemplate = "IAPI  - A non unique associated object is specified for this new object";
+            if (message.StartsWith(messageTemplate))
+            {
+                return (true, messageTemplate);
+            }
+
+            return (false, null);
+        }
+
+        private static (bool, string) ItemWithIdentifierAlreadyExists(string message, ItemLevelError error)
+        {
+            string messageTemplate = "IAPI  - An item with identifier {0} already exists in the workspace";
+            if (message.StartsWith(string.Format(messageTemplate, error.Identifier)))
+            {
+                return (true, messageTemplate);
+            }
+
+            return (false, null);
+        }
+
+        private static Regex ErrorInLineRegex = new Regex("IAPI Error in line (.*), column"); 
+        private static (bool, string) ErrorInLine(string message, ItemLevelError error)
+        {
+            if (ErrorInLineRegex.IsMatch(message))
+            {
+                return (true, "IAPI Error in line *, column *.");
+            }
+
+            return (false, null);
         }
         
+        private static Regex FieldAndErrorRegex = new Regex("IAPI  - Field (.*) Error"); 
+        private static (bool, string) FieldAndError(string message, ItemLevelError error)
+        {
+            if (FieldAndErrorRegex.IsMatch(message))
+            {
+                return (true, "IAPI Error in line *, column *.");
+            }
+
+            return (false, null);
+        }
+
         #endregion
     }
 }
