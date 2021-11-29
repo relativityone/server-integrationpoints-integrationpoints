59ab7302aa8ff7b93324a5a04adfd2779a8ad6e6
Marcin Sojka <marcin.sojka@relativity.com>
2021-11-29 14:14:27 +0100

    Revert "[REL-587103] IntegrationPointHealthCheckManager and it unit tests removed"
    This reverts commit 5bac1f28edf54a0b43b308e21f00901898526965.

---
+++ Source/Relativity.IntegrationPoints.Services.Tests/Managers/IntegrationPointHealthCheckManagerTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Castle.Windsor;
+using kCura.IntegrationPoint.Tests.Core;
+using kCura.IntegrationPoints.Data.Repositories;
+using NSubstitute;
+using NUnit.Framework;
+using Relativity.API;
+using Relativity.IntegrationPoints.Services.Helpers;
+using Relativity.Logging;
+using Relativity.Telemetry.APM;
+using Constants = kCura.IntegrationPoints.Core.Constants;
+
+namespace Relativity.IntegrationPoints.Services.Tests.Managers
+{
+	[TestFixture, Category("Unit")]
+	public class IntegrationPointHealthCheckManagerTests : TestBase
+	{
+		private IntegrationPointHealthCheckManager _integrationPointHealthCheckManager;
+		private ILog _logger;
+		private IWindsorContainer _container;
+		private IAPM _apmClient;
+        private IHealthMeasure _healthMeasure;
+
+		private const int _WORKSPACE_ID = 819434;
+
+		public override void SetUp()
+		{
+			_logger = Substitute.For<ILog>();
+			IPermissionRepository permissionRepository = Substitute.For<IPermissionRepository>();
+			_container = Substitute.For<IWindsorContainer>();
+
+			IPermissionRepositoryFactory permissionRepositoryFactory = Substitute.For<IPermissionRepositoryFactory>();
+			permissionRepositoryFactory.Create(Arg.Any<IHelper>(), _WORKSPACE_ID).Returns(permissionRepository);
+
+			_apmClient = Substitute.For<IAPM>();
+			Client.LazyAPMClient = new Lazy<IAPM>(() => _apmClient);
+
+            _healthMeasure = Substitute.For<IHealthMeasure>();
+
+            _apmClient.HealthCheckOperation(Constants.IntegrationPoints.Telemetry.APM_HEALTHCHECK,
+                Arg.Any<Func<HealthCheckOperationResult>>()).Returns(_healthMeasure);
+
+			_integrationPointHealthCheckManager = new IntegrationPointHealthCheckManager(_logger, permissionRepositoryFactory, _container);
+		}
+
+		[Test]
+		public void ShouldWriteHealthMeasure()
+		{
+			// Act
+			_integrationPointHealthCheckManager.RunHealthChecksAsync().Wait();
+
+			// Assert
+            _healthMeasure.Received().Write();
+		}
+
+        [Test]
+        public void ShouldBeHealthyByDefault()
+        {
+			// Act
+            HealthCheckOperationResult result = _integrationPointHealthCheckManager.RunHealthChecksAsync().Result;
+
+			// Assert
+            Assert.IsTrue(result.IsHealthy);
+        }
+	}
+}
---
+++ Source/Relativity.IntegrationPoints.Services/IntegrationPointHealthCheckManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Castle.Windsor;
+using Relativity.IntegrationPoints.Services.Helpers;
+using Relativity.IntegrationPoints.Services.Installers;
+using Relativity.Logging;
+using Relativity.Telemetry.APM;
+
+namespace Relativity.IntegrationPoints.Services
+{
+	public class IntegrationPointHealthCheckManager : KeplerServiceBase, IIntegrationPointHealthCheckManager
+	{
+		private Installer _installer;
+
+		internal IntegrationPointHealthCheckManager(ILog logger, IPermissionRepositoryFactory permissionRepositoryFactory, IWindsorContainer container)
+		: base(logger, permissionRepositoryFactory, container)
+		{ }
+
+		public IntegrationPointHealthCheckManager(ILog logger) : base(logger)
+		{ }
+
+		public Task<HealthCheckOperationResult> RunHealthChecksAsync()
+		{
+			return RunHealthCheckAsync();
+		}
+
+		public Task<HealthCheckOperationResult> RunDeploymentHealthChecksAsync()
+		{
+			return RunHealthCheckAsync();
+		}
+
+		private static Task<HealthCheckOperationResult> RunHealthCheckAsync()
+		{
+			HealthCheckOperationResult healthCheckOperationResult = new HealthCheckOperationResult(isHealthy: true, message: "Integration Points application is healthy!");
+			Client.APMClient.HealthCheckOperation(kCura.IntegrationPoints.Core.Constants.IntegrationPoints.Telemetry.APM_HEALTHCHECK, () => healthCheckOperationResult)
+				.Write();
+
+			return Task.FromResult(healthCheckOperationResult);
+		}
+
+		protected override Installer Installer => _installer ?? (_installer = new HealthCheckInstaller());
+
+		public void Dispose()
+		{
+		}
+	}
+}
