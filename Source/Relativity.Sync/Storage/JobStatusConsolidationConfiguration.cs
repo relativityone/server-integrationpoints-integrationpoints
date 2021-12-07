56c280562a5ebd55c7dbba89266351a0bdf7f8ef
Marcin Sojka <marcin.sojka@relativity.com>
2021-12-07 10:58:16 +0100

    [REL-623860] SnapshotId Null value fix implemented

--- Source/Relativity.Sync/Storage/JobStatusConsolidationConfiguration.cs
+++ Source/Relativity.Sync/Storage/JobStatusConsolidationConfiguration.cs
@@ -1,36 +1,23 @@
 ﻿using System;
-using Relativity.Services.Objects.DataContracts;
 using Relativity.Sync.Configuration;
 
 namespace Relativity.Sync.Storage
 {
-	internal class JobStatusConsolidationConfiguration : IJobStatusConsolidationConfiguration
-	{
-		private readonly IConfiguration _cache;
-		private readonly SyncJobParameters _syncJobParameters;
+    internal class JobStatusConsolidationConfiguration : IJobStatusConsolidationConfiguration
+    {
+        private readonly IConfiguration _cache;
+        private readonly SyncJobParameters _syncJobParameters;
 
-		public JobStatusConsolidationConfiguration(IConfiguration cache, SyncJobParameters syncJobParameters)
-		{
-			_cache = cache;
-			_syncJobParameters = syncJobParameters;
-		}
-
-		public int SourceWorkspaceArtifactId => _syncJobParameters.WorkspaceId;
-		public int SyncConfigurationArtifactId => _syncJobParameters.SyncConfigurationArtifactId;
-		public int JobHistoryArtifactId => _cache.GetFieldValue(x => x.JobHistoryId);
-
-        public Guid ExportRunId
+        public JobStatusConsolidationConfiguration(IConfiguration cache, SyncJobParameters syncJobParameters)
         {
-            get
-            {
-                Guid? snapshotId = _cache.GetFieldValue(x => x.SnapshotId);
-                if (snapshotId == Guid.Empty)
-                {
-                    snapshotId = null;
-                }
-
-                return snapshotId ?? throw new ArgumentException($"Run ID needs to be valid GUID, but null found.");
-            }
+            _cache = cache;
+            _syncJobParameters = syncJobParameters;
         }
-	}
+
+        public int SourceWorkspaceArtifactId => _syncJobParameters.WorkspaceId;
+        public int SyncConfigurationArtifactId => _syncJobParameters.SyncConfigurationArtifactId;
+        public int JobHistoryArtifactId => _cache.GetFieldValue(x => x.JobHistoryId);
+
+        public Guid? ExportRunId => _cache.GetFieldValue(x => x.SnapshotId);
+    }
 }
