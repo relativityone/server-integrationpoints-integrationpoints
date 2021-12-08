7025f24f7eaa6a9b2a27d9f694fe83b14e6eff26
Dawid Szafrański <dawid.szafranski@relativity.com>
2021-12-08 12:31:03 +0100

    init webpack for event handlers

---
+++ Source/kCura.IntegrationPoints.Web/Scripts/RelativityForms/IntegrationPoint/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const FormsExtensionPlugin = require("./plugins/FormsPlugin");
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'integration-point-event-handler.js',
+    path: path.resolve(__dirname),
+    library: 'extension',
+    libraryTarget: 'var',
+    libraryExport: 'default'
+  },
+  optimization: {
+    minimize: true,
+    
+  },
+  plugins: [new CleanWebpackPlugin(), new FormsExtensionPlugin()],
+};
