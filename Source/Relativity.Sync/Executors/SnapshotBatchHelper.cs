88e449f028d6576a6a78983818c7725ff581eeef
Marcin Sojka <marcin.sojka@relativity.com>
2021-12-29 15:55:04 +0100

    [REL-620923] SnapshotPartitionExecutorBase class introduced

---
+++ Source/Relativity.Sync/Executors/SnapshotBatchHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using Relativity.Sync.Configuration;
+using Relativity.Sync.Storage;
+
+namespace Relativity.Sync.Executors
+{
+    internal class SnapshotBatchHelper<T> where T : ISnapshotPartitionConfiguration
+    {
+        private readonly IBatchRepository _batchRepository;
+        private readonly ISyncLog _logger;
+
+        internal SnapshotBatchHelper(IBatchRepository batchRepository, ISyncLog logger)
+        {
+            _batchRepository = batchRepository;
+            _logger = logger;
+        }
+
+        internal async Task<IBatch> GetLastBatchAsync(T configuration)
+        {
+            IBatch batch;
+            try
+            {
+                batch = await _batchRepository.GetLastAsync(configuration.SourceWorkspaceArtifactId,
+                    configuration.SyncConfigurationArtifactId, configuration.ExportRunId).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Unable to retrieve last batch for sync configuration {artifactId}.",
+                    configuration.SyncConfigurationArtifactId);
+                throw;
+            }
+
+            return batch;
+        }
+
+        internal int GetNumberOfRecordsIncludedInBatches(IBatch batch)
+        {
+            int numberOfRecordsIncludedInBatches = 0;
+            if (batch != null)
+            {
+                numberOfRecordsIncludedInBatches = batch.StartingIndex + batch.TotalDocumentsCount;
+                _logger.LogInformation("Last batch was not null. Starting partitioning at index {index}",
+                    numberOfRecordsIncludedInBatches);
+            }
+            else
+            {
+                _logger.LogInformation("Partitioning from start");
+            }
+
+            return numberOfRecordsIncludedInBatches;
+        }
+
+        public async Task<ExecutionResult> CreateBatchesAsync(T configuration,
+            int numberOfRecordsIncludedInBatches)
+        {
+            Snapshot snapshot = new Snapshot(configuration.TotalRecordsCount, configuration.BatchSize,
+                numberOfRecordsIncludedInBatches);
+
+            try
+            {
+                foreach (SnapshotPart snapshotPart in snapshot.GetSnapshotParts())
+                {
+                    await _batchRepository.CreateAsync(configuration.SourceWorkspaceArtifactId,
+                            configuration.SyncConfigurationArtifactId, configuration.ExportRunId, snapshotPart.NumberOfRecords,
+                            snapshotPart.StartingIndex)
+                        .ConfigureAwait(false);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Unable to create batch for sync configuration {artifactId}.",
+                    configuration.SyncConfigurationArtifactId);
+                return ExecutionResult.Failure("Unable to create batches.", e);
+            }
+
+            return ExecutionResult.Success();
+        }
+    }
+}
---
+++ Source/Relativity.Sync/Executors/SnapshotPartitionExecutorBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Relativity.Sync.Configuration;
+using Relativity.Sync.Storage;
+
+namespace Relativity.Sync.Executors
+{
+    internal abstract class SnapshotPartitionExecutorBase<T>: IExecutor<T> where T : ISnapshotPartitionConfiguration
+    {
+        private readonly SnapshotBatchHelper<T> _snapshotBatchHelper;
+        private readonly ISyncLog _logger;
+
+        protected SnapshotPartitionExecutorBase(IBatchRepository batchRepository, ISyncLog logger)
+        {
+            _logger = logger;
+            _snapshotBatchHelper = new SnapshotBatchHelper<T>(batchRepository, logger);
+        }
+
+        public async Task<ExecutionResult> ExecuteAsync(T configuration, CompositeCancellationToken token)
+        {
+            LogSnapshotPartitionsInformation(configuration);
+            IBatch batch;
+            try
+            {
+                batch = await GetLastBatchAsync(configuration).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                return ExecutionResult.Failure("Cannot read last batch.", e);
+            }
+
+            int numberOfRecordsIncludedInBatches = GetNumberOfRecordsIncludedInBatches(batch);
+            ExecutionResult executionResult = await CreateBatchesAsync(configuration, numberOfRecordsIncludedInBatches).ConfigureAwait(false);
+
+            return executionResult;
+        }
+
+        protected virtual void LogSnapshotPartitionsInformation(T configuration)
+        {
+            _logger.LogInformation(
+                "Creating snapshot partitions for source workspace (workspace artifact id: {sourceWorkspaceArtifactId})",
+                configuration.SourceWorkspaceArtifactId);
+        }
+
+        private async Task<IBatch> GetLastBatchAsync(T configuration)
+        {
+            return await _snapshotBatchHelper.GetLastBatchAsync(configuration);
+        }
+
+        private int GetNumberOfRecordsIncludedInBatches(IBatch batch)
+        {
+            return _snapshotBatchHelper.GetNumberOfRecordsIncludedInBatches(batch);
+        }
+
+        private async Task<ExecutionResult> CreateBatchesAsync(T configuration,
+            int numberOfRecordsIncludedInBatches)
+        {
+            return await _snapshotBatchHelper.CreateBatchesAsync(configuration, numberOfRecordsIncludedInBatches);
+        }
+    }
+}
