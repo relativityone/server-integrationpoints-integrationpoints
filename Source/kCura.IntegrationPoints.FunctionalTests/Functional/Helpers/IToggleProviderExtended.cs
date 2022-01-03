a8f9ce414d58106de7e9e73ae63f6df46569ce71
Kamil Makarowski <kamil.makarowski@relativity.com>
2022-01-03 11:38:40 +0100

    REL-626975 Test Azure AD Provider against ADS in k8s toggle turned on

---
+++ Source/kCura.IntegrationPoints.FunctionalTests/Functional/Helpers/IToggleProviderExtended.cs
@@ -0,0 +1,10 @@
+﻿using Relativity.Toggles;
+using System.Threading.Tasks;
+
+namespace Relativity.IntegrationPoints.Tests.Functional.Helpers
+{
+    internal interface IToggleProviderExtended : IToggleProvider
+    {
+        Task SetAsync(string name, bool enabled);
+    }
+}
