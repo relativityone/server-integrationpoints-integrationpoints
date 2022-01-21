539a93ef034f2249e22783a3301714be59e2b7a4
Marcin Sojka <marcin.sojka@relativity.com>
2022-01-21 15:17:53 +0100

    [REL-637786] Small Cleanup

--- Source/kCura.IntegrationPoints.FunctionalTests/Integration/Tests/Sync/RelativitySyncTests.cs
+++ Source/kCura.IntegrationPoints.FunctionalTests/Integration/Tests/Sync/RelativitySyncTests.cs
@@ -9,7 +9,6 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using NUnit.Framework;
 
 namespace Relativity.IntegrationPoints.Tests.Integration.Tests.Sync
 {
