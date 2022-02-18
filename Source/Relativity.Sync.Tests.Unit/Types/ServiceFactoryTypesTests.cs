f382793b69d912e53da2c26cdccb17e9188a79b4
Marcin Sojka <marcin.sojka@relativity.com>
2022-02-18 16:46:36 +0100

    [REL-624513] Types checking test added

---
+++ Source/Relativity.Sync.Tests.Unit/Types/ServiceFactoryTypesTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using NUnit.Framework;
+using Relativity.Sync.Configuration;
+using Relativity.Sync.KeplerFactory;
+using Relativity.Sync.Telemetry;
+
+namespace Relativity.Sync.Tests.Unit.Types
+{
+    [TestFixture]
+    public class ServiceFactoryTypesTests
+    {
+        private List<Type> _whiteListTypes;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _whiteListTypes = new List<Type>
+            {
+                typeof(ServiceFactoryBase),
+                typeof(ServiceFactoryForAdmin),
+                typeof(ServiceFactoryForUser),
+                typeof(RelativityServices),
+            };
+        }
+
+        [Test]
+        public void RelativitySync_TypesShouldNotUseISyncServiceManagerUnlessTheyAreWhitelisted()
+        {
+            // Arrange
+            AssemblyName[] assemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
+            AssemblyName syncAssemblyName = assemblies.Single(x => x.Name == "Relativity.Sync");
+            Assembly syncAssembly = Assembly.Load(syncAssemblyName.FullName);
+            Type[] syncTypes = syncAssembly.GetTypes();
+            
+            // Act && Assert
+            foreach (Type syncType in syncTypes)
+            {
+                if (_whiteListTypes.Contains(syncType))
+                {
+                    continue;
+                }
+
+                ConstructorInfo[] constructors = syncType.GetConstructors();
+
+                foreach (ConstructorInfo constructor in constructors)
+                {
+                    var parameters = constructor.GetParameters();
+                    foreach (var parameter in parameters)
+                    {
+                        parameter.ParameterType.Should().NotBe(typeof(ISyncServiceManager), 
+                        $"Class {syncType} should not use {nameof(ISyncServiceManager)} type. Use {nameof(ISourceServiceFactoryForAdmin)} or {nameof(ISourceServiceFactoryForUser)} instead.");
+                    }
+                }
+            }
+        }
+    }
+}
