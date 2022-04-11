7fb633114c2eb23b291953de3a203f34c38da868
Kamil Bizoń <kamil.bizon@relativity.com>
2022-04-11 17:03:49 +0200

    REL-667979 Partially implemented  EnableNonAdminSyncLinksToggle usage.

---
+++ Source/Relativity.Sync/Transfer/NonAdminCanSyncUsingLinks.cs
@@ -0,0 +1,39 @@
+﻿using Relativity.API;
+using Relativity.Sync.Configuration;
+using Relativity.Sync.Toggles;
+using Relativity.Toggles;
+
+namespace Relativity.Sync.Transfer
+{
+	internal class NonAdminCanSyncUsingLinks : INonAdminCanSyncUsingLinks
+	{
+		private bool? _isEnabled;
+		
+		private readonly IToggleProvider _toggleProvider;
+		private readonly IAPILog _logger;
+		private readonly IDocumentSynchronizationConfiguration _synchronizationConfiguration;
+
+		public NonAdminCanSyncUsingLinks(IToggleProvider toggleProvider, IAPILog logger, IDocumentSynchronizationConfiguration synchronizationConfiguration)
+		{
+			_toggleProvider = toggleProvider;
+			_logger = logger;
+			_synchronizationConfiguration = synchronizationConfiguration;
+		}
+		
+		public bool IsEnabled()
+		{
+			if (_isEnabled != null)
+			{
+				return (bool)_isEnabled;
+			}
+
+			bool toggleValue = _toggleProvider.IsEnabled<EnableNonAdminSyncLinksToggle>();
+			bool isCopyModeFileLinks = _synchronizationConfiguration.ImportNativeFileCopyMode == ImportNativeFileCopyMode.SetFileLinks;
+			_logger.LogInformation("Validating if NonAdmin user can sync using Links options. Toggle EnableNonAdminSyncLinksToggle: {toggleValue}, CopyMode: {ImportNativeFileCopyMode}", toggleValue, isCopyModeFileLinks);
+			if (toggleValue && isCopyModeFileLinks)
+				_isEnabled = true;
+			
+			return (bool)_isEnabled;
+		}
+	}
+}
