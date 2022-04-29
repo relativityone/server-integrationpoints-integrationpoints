3c9bf29f29cbff0e0801191cf1200db187a2bf9d
Marcin Sojka <marcin.sojka@relativity.com>
2022-04-29 14:09:21 +0200

    [REL-599152] TestType MainFlow added SyncImageApiTests

--- Source/kCura.IntegrationPoints.FunctionalTests/Functional/CI/ApiTests/SyncImageApiTests.cs
+++ Source/kCura.IntegrationPoints.FunctionalTests/Functional/CI/ApiTests/SyncImageApiTests.cs
@@ -1,9 +1,11 @@
 ﻿using System.Threading.Tasks;
 using NUnit.Framework;
 using Relativity.IntegrationPoints.Tests.Functional.TestsImplementations.ApiTests;
+using Relativity.Testing.Identification;
 
 namespace Relativity.IntegrationPoints.Tests.Functional.CI.ApiTests
 {
+    [TestType.MainFlow]
     internal class SyncImageApiTests : TestsBase
     {
         private readonly SyncImageApiTestsImplementation _implementation;
