e8ee2a7c46c207b1e0b14a91d9068c3458bdba3b
Marcin Sojka <marcin.sojka@relativity.com>
2022-05-04 12:13:29 +0200

    [REL-599152] destination configuration class added

---
+++ Source/kCura.IntegrationPoints.FunctionalTests/Functional/DataModels/DestinationConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Relativity.IntegrationPoints.Services;
+
+namespace Relativity.IntegrationPoints.Tests.Functional.DataModels
+{
+    public class DestinationConfiguration : RelativityProviderDestinationConfiguration
+    {
+        public bool ImageImport { get; set; }
+    }
+}
