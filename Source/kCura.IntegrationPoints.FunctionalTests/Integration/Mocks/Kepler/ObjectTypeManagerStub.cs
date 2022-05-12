cbd3709ba6203921d9840ce11d9d0690b7750106
Marcin Sojka <marcin.sojka@relativity.com>
2022-05-12 12:19:05 +0200

    [REL-628796] IntegrationPointManager Tests removed

--- Source/kCura.IntegrationPoints.FunctionalTests/Integration/Mocks/Kepler/ObjectTypeManagerStub.cs
+++ Source/kCura.IntegrationPoints.FunctionalTests/Integration/Mocks/Kepler/ObjectTypeManagerStub.cs
@@ -1,7 +1,4 @@
-﻿using System.Threading.Tasks;
-using Moq;
-using Relativity.Services.Interfaces.ObjectType;
-using Relativity.Services.Interfaces.ObjectType.Models;
+﻿using Relativity.Services.Interfaces.ObjectType;
 
 namespace Relativity.IntegrationPoints.Tests.Integration.Mocks.Kepler
 {
