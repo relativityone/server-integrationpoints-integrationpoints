719ae1f989a556739bc5a94c34f0746cffd8b9eb
Kamil Makarowski <kamil.makarowski@relativity.com>
2022-05-20 11:13:05 +0200

    REL-685818 Turn off "Install Relativity" and unify Slack channels

--- Trident/Jobs/Nightly-Performance.groovy
+++ Trident/Jobs/Nightly-Performance.groovy
@@ -1,9 +1,9 @@
 @Library('ProjectMayhem@v1') _
 
 jobWithSut {
-    slackChannel = "sync_trident"
-    sutTemplate = "aio-sundrop-latest"
-	relativityBranch = "develop"
+    slackChannel = "ci-sync"
+    sutTemplate = "aio-whitesedge-latest"
+	//relativityBranch = "develop"
     jobScript = "Trident/Scripts/nightly-performance.ps1"
     cron = "0 3 * * *"
 }
