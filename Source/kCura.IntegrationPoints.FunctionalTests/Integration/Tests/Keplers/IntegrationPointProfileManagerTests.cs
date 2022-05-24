af8e265ce1d451b6983ede7dfd6820282142af8a
Aleksandra Łopińska <aleksandra.lopinska@relativity.com>
2022-05-24 17:00:43 +0200

    REL-628797 integration point profile tests class added

---
+++ Source/kCura.IntegrationPoints.FunctionalTests/Integration/Tests/Keplers/IntegrationPointProfileManagerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Relativity.IntegrationPoints.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relativity.IntegrationPoints.Tests.Integration.Tests.Keplers
+{
+    public class IntegrationPointProfileManagerTests : TestsBase
+    {
+        private IIntegrationPointProfileManager _sut;
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            _sut = Container.Resolve<IIntegrationPointProfileManager>();
+        }
+
+        public async Task CreateIntegrationPointProfileAsync_ShouldReturnCorrectIntegrationPointModel()
+        {
+            //Arrange
+            CreateIntegrationPointRequest request = new CreateIntegrationPointRequest();
+            //Act
+            IntegrationPointModel result = await _sut.CreateIntegrationPointProfileAsync(request);
+
+            //Assert
+
+
+        }
+    }
+}
