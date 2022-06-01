808608a7cec1ad73ba7ddf3a937af4380982c1ea
Marcin Sojka <marcin.sojka@relativity.com>
2022-06-01 17:17:00 +0200

    [REL-631074] User validation added before job run

--- Source/kCura.ScheduleQueue.Core/Validation/QueueJobValidator.cs
+++ Source/kCura.ScheduleQueue.Core/Validation/QueueJobValidator.cs
@@ -18,11 +18,17 @@ namespace kCura.ScheduleQueue.Core.Validation
 
 		public async Task<ValidationResult> ValidateAsync(Job job)
 		{
-			ValidationResult validationResult = await ValidateWorkspaceExistsAsync(job.WorkspaceID).ConfigureAwait(false);
+            ValidationResult validationResult = await ValidateWorkspaceExistsAsync(job.WorkspaceID).ConfigureAwait(false);
 			if (!validationResult.IsValid)
 			{
 				return validationResult;
 			}
+
+            validationResult = await ValidateUserExists(job.SubmittedBy).ConfigureAwait(false);
+            if (!validationResult.IsValid)
+            {
+                return validationResult;
+            }
 
 			validationResult = await ValidateIntegrationPointExistsAsync(job.RelatedObjectArtifactID, job.WorkspaceID).ConfigureAwait(false);
 
@@ -58,6 +64,22 @@ namespace kCura.ScheduleQueue.Core.Validation
 
 				return result.Object != null ? ValidationResult.Success : ValidationResult.Failed($"Integration Point {integrationPointId} does not exist anymore");
 			}
+		}
+
+		private async Task<ValidationResult> ValidateUserExists(int userId)
+        {
+            using (IObjectManager proxy = _helper.GetServicesManager().CreateProxy<IObjectManager>(ExecutionIdentity.System))
+            {
+                var request = new QueryRequest()
+                {
+                    ObjectType = new ObjectTypeRef() { ArtifactTypeID = (int)ArtifactType.User },
+                    Condition = $"'ArtifactID' == {userId}",
+                };
+
+                QueryResultSlim result = await proxy.QuerySlimAsync(-1, request, 0, 1).ConfigureAwait(false);
+
+                return result.TotalCount > 0 ? ValidationResult.Success : ValidationResult.Failed($"User {userId} does not exist anymore");
+            }
 		}
 	}
 }
