132a1be879e22b9c45ff987db7f1d2e1e4be82d6
Marcin Sojka <marcin.sojka@relativity.com>
2022-06-28 00:55:40 +0200

    [REL-669210] OnSetup and OnTearDown fixtures fixed for SyncSummaryPageTestsImplementation

--- Source/kCura.IntegrationPoints.FunctionalTests/Functional/TestsImplementations/SyncSummaryPageTestsImplementation.cs
+++ Source/kCura.IntegrationPoints.FunctionalTests/Functional/TestsImplementations/SyncSummaryPageTestsImplementation.cs
@@ -1,15 +1,34 @@
-﻿using Relativity.IntegrationPoints.Tests.Functional.Web.Components;
+﻿using Relativity.IntegrationPoints.Tests.Functional.Helpers;
+using Relativity.IntegrationPoints.Tests.Functional.Helpers.LoadFiles;
+using Relativity.IntegrationPoints.Tests.Functional.Web.Components;
+using Relativity.Testing.Framework;
+using Relativity.Testing.Framework.Models;
 using Relativity.Testing.Framework.Web.Models;
+using System.Collections.Generic;
 
 namespace Relativity.IntegrationPoints.Tests.Functional.TestsImplementations
 {
     internal class SyncSummaryPageTestsImplementation
     {
         private readonly ITestsImplementationTestFixture _testsImplementationTestFixture;
+        private readonly Dictionary<string, Workspace> _destinationWorkspaces = new Dictionary<string, Workspace>();
 
         public SyncSummaryPageTestsImplementation(ITestsImplementationTestFixture testsImplementationTestFixture)
         {
-            this._testsImplementationTestFixture = testsImplementationTestFixture;
+            _testsImplementationTestFixture = testsImplementationTestFixture;
+        }
+
+        public void OnSetUpFixture()
+        {
+            RelativityFacade.Instance.ImportDocumentsFromCsv(_testsImplementationTestFixture.Workspace, LoadFilesGenerator.GetOrCreateNativesLoadFile());
+        }
+
+        public void OnTearDownFixture()
+        {
+            foreach (KeyValuePair<string, Workspace> destinationWorkspace in _destinationWorkspaces)
+            {
+                RelativityFacade.Instance.DeleteWorkspace(destinationWorkspace.Value);
+            }
         }
 
         public void SavedSearchNativesAndMetadataSummaryPage()
