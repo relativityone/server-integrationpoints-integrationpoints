b7b1e8ea59683c08fc8a8b48a63dbc389744e221
Kamil Makarowski <kamil.makarowski@relativity.com>
2022-06-28 15:05:00 +0200

    REL-696947 Introduce Heartbeat toggle - in case of to often database updates

--- Source/kCura.IntegrationPoints.Agent.Tests/Monitoring/HeartbeatReporterTests.cs
+++ Source/kCura.IntegrationPoints.Agent.Tests/Monitoring/HeartbeatReporterTests.cs
@@ -1,12 +1,14 @@
 ﻿using kCura.IntegrationPoint.Tests.Core.Queries;
 using kCura.IntegrationPoints.Agent.Monitoring;
 using kCura.IntegrationPoints.Agent.Monitoring.HearbeatReporter;
+using kCura.IntegrationPoints.Agent.Toggles;
 using kCura.IntegrationPoints.Common.Helpers;
 using kCura.IntegrationPoints.Core.Managers;
 using kCura.IntegrationPoints.Data;
 using Moq;
 using NUnit.Framework;
 using Relativity.API;
+using Relativity.Toggles;
 using System;
 using System.Threading;
 
@@ -16,7 +18,8 @@ namespace kCura.IntegrationPoints.Agent.Tests.Monitoring
     {
         private Mock<IQueueQueryManager> _queueManagerMock;
         private Mock<IMonitoringConfig> _configFake;
-        private Mock<IDateTime> _dateTime;
+        private Mock<IDateTime> _dateTimeFake;
+        private Mock<IToggleProvider> _toggleProviderFake;
 
         private HeartbeatReporter _sut;
 
@@ -32,10 +35,14 @@ namespace kCura.IntegrationPoints.Agent.Tests.Monitoring
 
             _configFake = new Mock<IMonitoringConfig>();
 
-            _dateTime = new Mock<IDateTime>();
-            _dateTime.Setup(x => x.UtcNow).Returns(_EXPECTED_HEARTBEAT_TIME);
+            _dateTimeFake = new Mock<IDateTime>();
+            _dateTimeFake.Setup(x => x.UtcNow).Returns(_EXPECTED_HEARTBEAT_TIME);
 
-            _sut = new HeartbeatReporter(_queueManagerMock.Object, _configFake.Object, _dateTime.Object, log.Object);
+            _toggleProviderFake = new Mock<IToggleProvider>();
+            _toggleProviderFake.Setup(x => x.IsEnabled<EnableHeartbeatToggle>()).Returns(true);
+
+            _sut = new HeartbeatReporter(_queueManagerMock.Object, _configFake.Object,
+                _dateTimeFake.Object, log.Object, _toggleProviderFake.Object);
         }
 
         [Test]
@@ -48,7 +55,10 @@ namespace kCura.IntegrationPoints.Agent.Tests.Monitoring
             _configFake.Setup(x => x.HeartbeatInterval).Returns(TimeSpan.FromDays(1));
 
             // Act
-            using (_sut.ActivateHeartbeat(_JOB_ID)) { }
+            using (_sut.ActivateHeartbeat(_JOB_ID)) 
+            {
+                Thread.Sleep(100);
+            }
 
             // Assert
             _queueManagerMock.Verify(x => x.Heartbeat(_JOB_ID, _EXPECTED_HEARTBEAT_TIME), Times.Once);
@@ -69,7 +79,7 @@ namespace kCura.IntegrationPoints.Agent.Tests.Monitoring
             // Assert
             DateTime newTime = new DateTime(1900, 10, 10);
 
-            _dateTime.Setup(x => x.UtcNow).Returns(newTime);
+            _dateTimeFake.Setup(x => x.UtcNow).Returns(newTime);
             Thread.Sleep(100);
 
             _queueManagerMock.Verify(x => x.Heartbeat(_JOB_ID, newTime), Times.Never);
@@ -92,6 +102,24 @@ namespace kCura.IntegrationPoints.Agent.Tests.Monitoring
 
             // Assert
             _queueManagerMock.Verify(x => x.Heartbeat(_JOB_ID, _EXPECTED_HEARTBEAT_TIME), Times.AtLeast(2));
+        }
+
+        [Test]
+        public void ActivateHeartbeat_ShouldNotUpdateHearbeat_WhenToggleIsDisabled()
+        {
+            // Arrange
+            _toggleProviderFake.Setup(x => x.IsEnabled<EnableHeartbeatToggle>()).Returns(false);
+
+            _configFake.Setup(x => x.HeartbeatInterval).Returns(TimeSpan.FromDays(1));
+
+            // Act
+            using (_sut.ActivateHeartbeat(_JOB_ID))
+            {
+                Thread.Sleep(100);
+            }
+
+            // Assert
+            _queueManagerMock.Verify(x => x.Heartbeat(_JOB_ID, _EXPECTED_HEARTBEAT_TIME), Times.Never);
         }
     }
 }
