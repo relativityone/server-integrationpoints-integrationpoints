61fd769f75f890515da5ee42c7b4d29f0c0d5cd8
Marcin Sojka <marcin.sojka@relativity.com>
2022-06-29 16:19:24 +0200

    [REL-703878] Long Running jobs alert added

--- Source/kCura.IntegrationPoints.Agent/Monitoring/MemoryUsageReporter/MemoryUsageReporter.cs
+++ Source/kCura.IntegrationPoints.Agent/Monitoring/MemoryUsageReporter/MemoryUsageReporter.cs
@@ -11,6 +11,9 @@ namespace kCura.IntegrationPoints.Agent.Monitoring.MemoryUsageReporter
 {
     public class MemoryUsageReporter : IMemoryUsageReporter
     {
+        private DateTime _startDateTime;
+        private bool _runningJobTimeExceededCheck;
+
         private readonly IAPM _apmClient;
         private readonly IAPILog _logger;
         private readonly IRipMetrics _ripMetric;
@@ -19,6 +22,7 @@ namespace kCura.IntegrationPoints.Agent.Monitoring.MemoryUsageReporter
         private readonly IMonitoringConfig _config;
 
         private static string _METRIC_LOG_NAME = "Relativity.IntegrationPoints.Performance.System";
+        private static string _METRIC_RUNNING_JOB_TIME_EXCEEDED_NAME = "Relativity.IntegrationPoints.Performance.RunningJobTimeExceeded";
         private static string _METRIC_NAME = "IntegrationPoints.Performance.System";
 
 
@@ -32,10 +36,12 @@ namespace kCura.IntegrationPoints.Agent.Monitoring.MemoryUsageReporter
             _ripMetric = ripMetric;
             _appDomainMonitoringEnabler = appDomainMonitoringEnabler;
             _config = config;
+            _runningJobTimeExceededCheck = true;
         }
 
         public IDisposable ActivateTimer(long jobId, string jobDetails, string jobType)
         {
+            _startDateTime = DateTime.Now;
             return _appDomainMonitoringEnabler.EnableMonitoring()
                 ? new Timer(state => Execute(jobId, jobDetails, jobType), null, TimeSpan.Zero, _config.MemoryUsageInterval)
                 : Disposable.Empty;
@@ -45,13 +51,22 @@ namespace kCura.IntegrationPoints.Agent.Monitoring.MemoryUsageReporter
         {
             try
             {
-
                 Dictionary<string, object> customData = new Dictionary<string, object>()
                 {
+                    { "r1.team.id", "PTCI-RIP" },
                     { "JobId", jobId },
                     { "JobType", jobType },
                     { "WorkflowId", workflowId}
                 };
+
+                const int runningJobTimeThreshold = 8;
+                if (_runningJobTimeExceededCheck && (DateTime.Now - _startDateTime).Hours > runningJobTimeThreshold)
+                {
+                    _apmClient.CountOperation(_METRIC_RUNNING_JOB_TIME_EXCEEDED_NAME, correlationID: workflowId, customData: customData)
+                        .Write();
+
+                    _runningJobTimeExceededCheck = false;
+                }
 
                 _processMemoryHelper.GetApplicationSystemStats().ToList().ForEach(x => customData.Add(x.Key, x.Value));
 
