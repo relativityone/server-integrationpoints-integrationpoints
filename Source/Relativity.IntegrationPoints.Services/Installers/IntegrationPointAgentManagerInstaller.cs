980e84272f4043e671871775551aae66f49215f2
Kamil Bizoń <kamil.bizon@relativity.com>
2022-07-07 12:00:20 +0200

    REL-706011 Cleaned duplicated Registration.

--- Source/Relativity.IntegrationPoints.Services/Installers/IntegrationPointAgentManagerInstaller.cs
+++ Source/Relativity.IntegrationPoints.Services/Installers/IntegrationPointAgentManagerInstaller.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 using kCura.IntegrationPoints.Core.Installers;
 using kCura.IntegrationPoints.Core.Managers.Implementations;
-using kCura.IntegrationPoints.Data;
 using kCura.IntegrationPoints.Data.Factories;
 using kCura.IntegrationPoints.Data.Factories.Implementations;
 using kCura.IntegrationPoints.Domain.EnvironmentalVariables;
 using kCura.IntegrationPoints.Domain.Managers;
-using kCura.ScheduleQueue.Core;
-using kCura.ScheduleQueue.Core.Data;
-using kCura.ScheduleQueue.Core.Services;
 using Relativity.API;
 
 namespace Relativity.IntegrationPoints.Services.Installers
@@ -26,11 +21,8 @@ namespace Relativity.IntegrationPoints.Services.Installers
 
 		protected override void RegisterComponents(IWindsorContainer container, IConfigurationStore store, int workspaceID)
 		{
-			Guid agentGuid = Guid.Parse(GlobalConst.RELATIVITY_INTEGRATION_POINTS_AGENT_GUID);
-			container.Register(Component.For<IQueueQueryManager>().ImplementedBy<QueueQueryManager>().DependsOn(Dependency.OnValue<Guid>(agentGuid)).LifestyleTransient());
 			container.Register(Component.For<IInstanceSettingsManager>().ImplementedBy<InstanceSettingsManager>().LifestyleTransient());
 			container.Register(Component.For<IRepositoryFactory>().UsingFactoryMethod(c => new RepositoryFactory(c.Resolve<IHelper>(), c.Resolve<IHelper>().GetServicesManager())));
-			container.Register(Component.For<IJobService>().ImplementedBy<JobService>().LifestyleTransient());
 			container.Register(Component.For<IKubernetesMode>().ImplementedBy<KubernetesMode>().LifestyleSingleton());
 		}
 	}
