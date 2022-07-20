be46b36e379524eddfa5757e0944289cbb9270e6
Kamil Bizoń <kamil.bizon@relativity.com>
2022-07-20 08:58:12 +0200

    REL-691283 Added missing XML comments

--- Source/Relativity.Sync/Transfer/ADF/HelperFactoryWrapper.cs
+++ Source/Relativity.Sync/Transfer/ADF/HelperFactoryWrapper.cs
@@ -6,14 +6,26 @@ using Relativity.Storage.Extensions.Models;
 
 namespace Relativity.Sync.Transfer.ADF
 {
+	/// <summary>
+	/// FactorWrapper for Relativity.Helper, created to enable testing and mocking
+	/// </summary>
 	public class HelperFactoryWrapper : IHelperFactory
 	{
 		private readonly IHelper _helper;
 
+		/// <summary>
+		/// Ctor for HelperFactoryWrapper, that DI IHelper from container
+		/// </summary>
+		/// <param name="helper"></param>
 		public HelperFactoryWrapper(IHelper helper)
 		{
 			_helper = helper;
 		}
+		/// <summary>
+		/// This method return Bedrock storage endpoints
+		/// </summary>
+		/// <param name="applicationDetails"></param>
+		/// <returns></returns>
 		public Task<StorageEndpoint[]> GetStorageEndpointsAsync(ApplicationDetails applicationDetails)
 		{
 			return _helper.GetStorageEndpointsAsync(applicationDetails);
--- Source/Relativity.Sync/Transfer/ADF/IHelperFactory.cs
+++ Source/Relativity.Sync/Transfer/ADF/IHelperFactory.cs
@@ -4,8 +4,16 @@ using Relativity.Storage.Extensions.Models;
 
 namespace Relativity.Sync.Transfer.ADF
 {
+	/// <summary>
+	/// Use to mock IHelperFactory
+	/// </summary>
 	public interface IHelperFactory
 	{
+		/// <summary>
+		/// Method for mocking endpoints returned from Bedrock
+		/// </summary>
+		/// <param name="applicationDetails"></param>
+		/// <returns></returns>
 		Task<StorageEndpoint[]> GetStorageEndpointsAsync(ApplicationDetails applicationDetails);
 	}
 }
