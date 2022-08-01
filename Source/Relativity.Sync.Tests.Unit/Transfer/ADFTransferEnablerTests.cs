aee5a46709cd719c871e1a3e249f3ec228c0287b
Aleksandra Łopińska <aleksandra.lopinska@relativity.com>
2022-08-01 12:49:05 +0200

    tenants ADLS migration status disabled when UseFMS toggle is set to 'false'

--- Source/Relativity.Sync.Tests.Unit/Transfer/ADFTransferEnablerTests.cs
+++ Source/Relativity.Sync.Tests.Unit/Transfer/ADFTransferEnablerTests.cs
@@ -9,58 +9,73 @@ using Relativity.Sync.Transfer.ADF;
 
 namespace Relativity.Sync.Tests.Unit.Transfer
 {
-	[TestFixture]
-	internal class ADFTransferEnablerTests
-	{
-		private ADFTransferEnabler _sut;
-		private Mock<IADLSMigrationStatus> _migrationStatusMock;
-		private Mock<ISyncToggles> _syncTogglesMock;
-		private Mock<IInstanceSettings> _instanceSettingsMock;
-		private Mock<IAPILog> _loggermock;
-		
-		[SetUp]
-		public void SetUp()
-		{
-			_migrationStatusMock = new Mock<IADLSMigrationStatus>();
-			_syncTogglesMock = new Mock<ISyncToggles>();
-			_instanceSettingsMock = new Mock<IInstanceSettings>();
-			_loggermock = new Mock<IAPILog>();
-			_sut = new ADFTransferEnabler(_migrationStatusMock.Object, _syncTogglesMock.Object, _instanceSettingsMock.Object, _loggermock.Object);
-		}
+    [TestFixture]
+    internal class ADFTransferEnablerTests
+    {
+        private ADFTransferEnabler _sut;
+        private Mock<IADLSMigrationStatus> _migrationStatusMock;
+        private Mock<ISyncToggles> _syncTogglesMock;
+        private Mock<IInstanceSettings> _instanceSettingsMock;
+        private Mock<IAPILog> _loggermock;
 
-		[TestCase(false, false, true)]
-		[TestCase(true, false, true)]
-		[TestCase(false, true, true)]
-		[TestCase(true, true, false)]
-		public async Task ADFEnabler_ShouldUseADFTransferAsync_ShouldReturnTrue(bool useFMS, bool tenantIsMigrated, bool forceADF)
-		{
-			// ARRANGE
-			_syncTogglesMock.Setup(x => x.IsEnabled<UseFMS>()).Returns(useFMS);
-			_migrationStatusMock.Setup(x => x.IsTenantFullyMigratedAsync()).ReturnsAsync(tenantIsMigrated);
-			_instanceSettingsMock.Setup(x => x.GetShouldForceADFTransferAsync(It.IsAny<bool>())).ReturnsAsync(forceADF);
-			
-			// ACT
-			bool shouldUseADFToCopyFiles = await _sut.ShouldUseADFTransferAsync().ConfigureAwait(false);
-			
-			// ASSERT
-			shouldUseADFToCopyFiles.Should().BeTrue();
-		}
-		
-		[TestCase(false, false, false)]
-		[TestCase(true, false, false)]
-		[TestCase(false, true, false)]
-		public async Task ADFEnabler_ShouldUseADFTransferAsync_ShouldReturnFalse(bool useFMS, bool tenantIsMigrated, bool forceADF)
-		{
-			// ARRANGE
-			_syncTogglesMock.Setup(x => x.IsEnabled<UseFMS>()).Returns(useFMS);
-			_migrationStatusMock.Setup(x => x.IsTenantFullyMigratedAsync()).ReturnsAsync(tenantIsMigrated);
-			_instanceSettingsMock.Setup(x => x.GetShouldForceADFTransferAsync(It.IsAny<bool>())).ReturnsAsync(forceADF);
-			
-			// ACT
-			bool shouldUseADFToCopyFiles = await _sut.ShouldUseADFTransferAsync().ConfigureAwait(false);
-			
-			// ASSERT
-			shouldUseADFToCopyFiles.Should().BeFalse();
-		}
-	}
+        [SetUp]
+        public void SetUp()
+        {
+            _migrationStatusMock = new Mock<IADLSMigrationStatus>();
+            _syncTogglesMock = new Mock<ISyncToggles>();
+            _instanceSettingsMock = new Mock<IInstanceSettings>();
+            _loggermock = new Mock<IAPILog>();
+            _sut = new ADFTransferEnabler(_migrationStatusMock.Object, _syncTogglesMock.Object, _instanceSettingsMock.Object, _loggermock.Object);
+        }
+
+        [TestCase(false, false, true)]
+        [TestCase(true, false, true)]
+        [TestCase(false, true, true)]
+        [TestCase(true, true, false)]
+        public async Task ADFEnabler_ShouldUseADFTransferAsync_ShouldReturnTrue(bool useFMS, bool tenantIsMigrated, bool forceADF)
+        {
+            // ARRANGE
+            _syncTogglesMock.Setup(x => x.IsEnabled<UseFMS>()).Returns(useFMS);
+            _migrationStatusMock.Setup(x => x.IsTenantFullyMigratedAsync()).ReturnsAsync(tenantIsMigrated);
+            _instanceSettingsMock.Setup(x => x.GetShouldForceADFTransferAsync(It.IsAny<bool>())).ReturnsAsync(forceADF);
+
+            // ACT
+            bool shouldUseADFToCopyFiles = await _sut.ShouldUseADFTransferAsync().ConfigureAwait(false);
+
+            // ASSERT
+            shouldUseADFToCopyFiles.Should().BeTrue();
+        }
+
+        [TestCase(false, false, false)]
+        [TestCase(true, false, false)]
+        [TestCase(false, true, false)]
+        public async Task ADFEnabler_ShouldUseADFTransferAsync_ShouldReturnFalse(bool useFMS, bool tenantIsMigrated, bool forceADF)
+        {
+            // ARRANGE
+            _syncTogglesMock.Setup(x => x.IsEnabled<UseFMS>()).Returns(useFMS);
+            _migrationStatusMock.Setup(x => x.IsTenantFullyMigratedAsync()).ReturnsAsync(tenantIsMigrated);
+            _instanceSettingsMock.Setup(x => x.GetShouldForceADFTransferAsync(It.IsAny<bool>())).ReturnsAsync(forceADF);
+
+            // ACT
+            bool shouldUseADFToCopyFiles = await _sut.ShouldUseADFTransferAsync().ConfigureAwait(false);
+
+            // ASSERT
+            shouldUseADFToCopyFiles.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task ADFEnabler_ShouldNotVerifyTenantsMigrationToADLS_WhenFMSTransferIsDisabled()
+        {
+            // ARRANGE
+            _syncTogglesMock.Setup(x => x.IsEnabled<UseFMS>()).Returns(false);
+
+            // ACT
+            bool shouldUseADFToCopyFiles = await _sut.ShouldUseADFTransferAsync().ConfigureAwait(false);
+
+            // ASSERT
+            var test = Times.Once();
+
+            _migrationStatusMock.Verify(x => x.IsTenantFullyMigratedAsync(), Times.Never());
+        }
+    }
 }
