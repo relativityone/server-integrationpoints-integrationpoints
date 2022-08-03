e7fc238c66a2512afb30c9f179d3e3c2a6dd9ef7
Marcin Sojka <marcin.sojka@relativity.com>
2022-08-03 10:45:51 +0200

    [REL-714397] FilePathInfo removed

--- Source/Relativity.Sync/Transfer/ADF/FilePathInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Relativity.Sync.Transfer.ADF
-{
-    internal class FilePathInfo
-    {
-        public int ArtifactId { get; set; }
-        public string SourceLocationShortToLoadFile { get; set; }
-        public string DestinationLocationFullPathToLink { get; set; }
-    }
-}
