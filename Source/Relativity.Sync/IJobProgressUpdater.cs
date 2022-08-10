6c84d8dc5bbf9e3256fff33c39036ec1d4656eb2
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2022-08-10 15:23:53 +0200

    REL-714406 Review remarks

--- Source/Relativity.Sync/IJobProgressUpdater.cs
+++ Source/Relativity.Sync/IJobProgressUpdater.cs
@@ -3,26 +3,16 @@ using System.Threading.Tasks;
 
 namespace Relativity.Sync
 {
-    internal enum JobHistoryStatus
-    {
-        Validating,
-        ValidationFailed,
-        Processing,
-        Completed,
-        CompletedWithErrors,
-        Failed,
-        Stopping,
-        Stopped,
-        Suspending,
-        Suspended,
-    }
-
     internal interface IJobProgressUpdater
     {
         Task SetTotalItemsCountAsync(int totalItemsCount);
+
         Task SetJobStartedAsync();
+
         Task UpdateJobProgressAsync(int completedRecordsCount, int failedRecordsCount);
+
         Task UpdateJobStatusAsync(JobHistoryStatus status);
+
         Task AddJobErrorAsync(Exception ex);
     }
 }
