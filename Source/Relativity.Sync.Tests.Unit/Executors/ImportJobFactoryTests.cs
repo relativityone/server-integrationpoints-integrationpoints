d975d1dd5ec73350c10065d5cfd33d116a57e7fa
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2022-08-17 14:58:42 +0200

    REL-713806 Fixed build

--- Source/Relativity.Sync.Tests.Unit/Executors/ImportJobFactoryTests.cs
+++ Source/Relativity.Sync.Tests.Unit/Executors/ImportJobFactoryTests.cs
@@ -17,6 +17,8 @@ using Relativity.Sync.Executors;
 using Relativity.Sync.Logging;
 using Relativity.Sync.Storage;
 using Relativity.Sync.Tests.Common;
+using Relativity.Sync.Toggles;
+using Relativity.Sync.Toggles.Service;
 using Relativity.Sync.Transfer;
 using Relativity.Sync.Transfer.ADLS;
 
