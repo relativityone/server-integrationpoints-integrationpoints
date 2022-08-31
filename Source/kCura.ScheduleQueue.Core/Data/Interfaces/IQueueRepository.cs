a5cbe70ae6127db95702e0f4032aca30f5825298
Aleksandra Łopińska <aleksandra.lopinska@relativity.com>
2022-08-31 17:07:35 +0200

    REL-706232 Dapper libs and basic QueueRepository interface added

---
+++ Source/kCura.ScheduleQueue.Core/Data/Interfaces/IQueueRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using kCura.IntegrationPoints.Data;
+
+namespace kCura.ScheduleQueue.Core.Data.Interfaces
+{
+    public interface IQueueRepository
+    {
+        Job AddJob(Job job);
+
+        Job GetJob(int jobId);
+
+        Job GetJob(int relatedObjectId, string taskType);
+
+        IList<Job> GetAllJobs();
+
+        bool UpdateJob(Job job);
+
+        bool DeleteJob(int jobId);
+
+        void CleanupQueue();
+
+        void CleanupQueue(string agentGuid);
+
+        bool AllSyncWorkerBatchesAreFinished(int rootJobId);
+    }
+}
