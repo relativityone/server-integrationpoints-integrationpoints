61175ca2397d6137f31880302ec04cab726282ec
Marcin Sojka <marcin.sojka@relativity.com>
2022-09-20 10:41:23 +0200

    [REL-735998] ServicesAccessChecker Tests added

---
+++ Source/kCura.IntegrationPoints.Core.Tests/Checkers/ServicesAccessCheckerTests.cs
@@ -0,0 +1,91 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using kCura.IntegrationPoints.Core.Checkers;
+using kCura.IntegrationPoints.Core.Monitoring.SystemReporter;
+using Moq;
+using NUnit.Framework;
+using Relativity.API;
+
+namespace kCura.IntegrationPoints.Core.Tests.Checkers
+{
+    public class ServicesAccessCheckerTests
+    {
+        private Mock<IAPILog> _loggerMock;
+        private Mock<IIsServiceHealthy> _databasePingReporterMock;
+        private Mock<IIsServiceHealthy> _keplerPingReporterMock;
+        private Mock<IIsServiceHealthy> _fileShareDiskUsageReporterMock;
+
+        private ServicesAccessChecker _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loggerMock = new Mock<IAPILog>();
+            _databasePingReporterMock = new Mock<IIsServiceHealthy>();
+            _keplerPingReporterMock = new Mock<IIsServiceHealthy>();
+            _fileShareDiskUsageReporterMock = new Mock<IIsServiceHealthy>();
+
+            _databasePingReporterMock.Setup(x => x.IsServiceHealthyAsync()).ReturnsAsync(true);
+            _keplerPingReporterMock.Setup(x => x.IsServiceHealthyAsync()).ReturnsAsync(true);
+            _fileShareDiskUsageReporterMock.Setup(x => x.IsServiceHealthyAsync()).ReturnsAsync(true);
+
+            _sut = new ServicesAccessChecker(
+                _databasePingReporterMock.Object,
+                _keplerPingReporterMock.Object,
+                _fileShareDiskUsageReporterMock.Object,
+                _loggerMock.Object);
+        }
+
+        [Test]
+        public async Task AreServicesHealthy_ShouldReturnHealthyState_WhenAllServicesAreHealthy()
+        {
+            // Act
+            bool areServicesHealthy = await _sut.AreServicesHealthyAsync().ConfigureAwait(false);
+
+            // Assert
+            areServicesHealthy.ShouldBeEquivalentTo(true);
+        }
+
+        [Test]
+        public async Task AreServicesHealthy_ShouldReturnNotHealthyState_WhenDatabasePingReporterIsNotHealthy()
+        {
+            // Arrange
+            _databasePingReporterMock.Setup(x => x.IsServiceHealthyAsync()).ReturnsAsync(false);
+
+            // Act
+            bool areServicesHealthy = await _sut.AreServicesHealthyAsync().ConfigureAwait(false);
+
+            // Assert
+            areServicesHealthy.ShouldBeEquivalentTo(false);
+            _loggerMock.Verify(x => x.LogError("Database is not accessible."), Times.Once);
+        }
+
+        [Test]
+        public async Task AreServicesHealthy_ShouldReturnNotHealthyState_WhenKeplerPingReporterIsNotHealthy()
+        {
+            // Arrange
+            _keplerPingReporterMock.Setup(x => x.IsServiceHealthyAsync()).ReturnsAsync(false);
+
+            // Act
+            bool areServicesHealthy = await _sut.AreServicesHealthyAsync().ConfigureAwait(false);
+
+            // Assert
+            areServicesHealthy.ShouldBeEquivalentTo(false);
+            _loggerMock.Verify(x => x.LogError("Kepler service is not accessible."), Times.Once);
+        }
+
+        [Test]
+        public async Task AreServicesHealthy_ShouldReturnNotHealthyState_WhenFileShareDiskUsageReporterIsNotHealthy()
+        {
+            // Arrange
+            _fileShareDiskUsageReporterMock.Setup(x => x.IsServiceHealthyAsync()).ReturnsAsync(false);
+
+            // Act
+            bool areServicesHealthy = await _sut.AreServicesHealthyAsync().ConfigureAwait(false);
+
+            // Assert
+            areServicesHealthy.ShouldBeEquivalentTo(false);
+            _loggerMock.Verify(x => x.LogError("Not all fileShares are healthy."), Times.Once);
+        }
+    }
+}
