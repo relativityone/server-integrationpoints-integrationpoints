d371ba2dce6a8a480bffc25981b1f79609df17dd
Kamil Makarowski <kamil.makarowski@relativity.com>
2022-10-12 15:51:40 +0200

    REL-690562 Add missing TestsSetUpFixture

---
+++ Source/kCura.IntegrationPoints.FunctionalTests/Functional/CI-REG/TestsSetUpFixture.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace Relativity.IntegrationPoints.Tests.Functional.CI_REG
+{
+    [SetUpFixture]
+    public class TestsSetUpFixture : CI.TestsSetUpFixture
+    {
+    }
+}
