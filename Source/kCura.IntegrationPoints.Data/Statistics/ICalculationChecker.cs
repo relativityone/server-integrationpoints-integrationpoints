d2be114b28092f4ff107346bd218e54e609c24bf
Aleksandra Łopińska <aleksandra.lopinska@relativity.com>
2022-11-24 14:58:02 +0100

    REL-716352 calculation checker added

---
+++ Source/kCura.IntegrationPoints.Data/Statistics/ICalculationChecker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kCura.IntegrationPoints.Data.Statistics
+{
+    public interface ICalculationChecker
+    {
+        bool IsCalculating(int integrationPointId);
+
+        void MarkAsCalculating(int integrationPointId);
+
+        void MarkCalculationFinished(int integrationPointId);
+    }
+}
---
+++ Source/kCura.IntegrationPoints.Data/Statistics/Implementations/CalculationChecker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kCura.IntegrationPoints.Data.Statistics.Implementations
+{
+    public class CalculationChecker : ICalculationChecker
+    {
+        private ConcurrentDictionary<int, bool> _itemsWithCalculationInProgress = new ConcurrentDictionary<int, bool>();
+
+        public bool IsCalculating(int integrationPointId)
+        {
+            if (_itemsWithCalculationInProgress.ContainsKey(integrationPointId))
+            {
+                return _itemsWithCalculationInProgress[integrationPointId];
+            }
+
+            return false;
+        }
+
+        public void MarkAsCalculating(int integrationPointId)
+        {
+            if (_itemsWithCalculationInProgress.ContainsKey(integrationPointId))
+            {
+                _itemsWithCalculationInProgress[integrationPointId] = true;
+            }
+            else
+            {
+                _itemsWithCalculationInProgress.TryAdd(integrationPointId, true);
+            }
+        }
+
+        public void MarkCalculationFinished(int integrationPointId)
+        {
+            bool removed = _itemsWithCalculationInProgress.TryRemove(integrationPointId, out bool value);
+
+            if (!removed && _itemsWithCalculationInProgress.ContainsKey(integrationPointId))
+            {
+                _itemsWithCalculationInProgress[integrationPointId] = false;
+            }
+        }
+    }
+}
