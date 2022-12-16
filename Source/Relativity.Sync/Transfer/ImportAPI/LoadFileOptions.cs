f86e0298f91485a2d706dadbe7138d8ab4f40eb3
Marcin Sojka <marcin.sojka@relativity.com>
2022-12-16 11:38:21 +0100

    [REL-768268] Delimiters fix

--- Source/Relativity.Sync/Transfer/ImportAPI/LoadFileOptions.cs
+++ Source/Relativity.Sync/Transfer/ImportAPI/LoadFileOptions.cs
@@ -2,9 +2,9 @@
 {
     internal static class LoadFileOptions
     {
-        internal const char _DEFAULT_COLUMN_DELIMITER_ASCII = (char)029;
+        internal const char _DEFAULT_COLUMN_DELIMITER_ASCII = (char)020;
         internal const char _DEFAULT_MULTI_VALUE_ASCII = (char)030;
-        internal const char _DEFAULT_NESTED_VALUE_ASCII = (char)092;
+        internal const char _DEFAULT_NESTED_VALUE_ASCII = (char)029;
         internal const char _DEFAULT_NEWLINE_DELIMITER_ASCII = (char)174;
         internal const char _DEFAULT_QUOTE_DELIMITER_ASCII = (char)254;
     }
