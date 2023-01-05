0325684a12f9c1dd321bf3129d0c625af0f71e70
Marcin Sojka <marcin.sojka@relativity.com>
2023-01-05 16:21:56 +0100

    [REL-761371] jquery.validate replaced with latest version from node package manager

--- Source/kCura.IntegrationPoints.Web/Scripts/_references.js
+++ Source/kCura.IntegrationPoints.Web/Scripts/_references.js
@@ -1,3 +1,2 @@
-﻿/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.validate.unobtrusive.js" />
+﻿/// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="modernizr-2.6.2.js" />
