227747aa9d2cc62fbd64bf3e961be77ee7394a14
Aleksandra Łopińska <aleksandra.lopinska@relativity.com>
2023-02-08 10:53:20 +0100

    REL-785105 immediate agent start by kepler call hidded behind toggle

--- Source/kCura.IntegrationPoints.Core/Services/AgentLauncher.cs
+++ Source/kCura.IntegrationPoints.Core/Services/AgentLauncher.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
+using kCura.IntegrationPoints.Common.Toggles;
 using kCura.IntegrationPoints.Data;
 using Relativity.API;
 using Relativity.HostingBridge.V1.AgentStatusManager;
+using Relativity.Toggles;
 
 namespace kCura.IntegrationPoints.Core.Services
 {
@@ -10,20 +12,26 @@ namespace kCura.IntegrationPoints.Core.Services
     {
         private readonly IServicesMgr _serviceManager;
         private readonly IAPILog _logger;
+        private readonly IToggleProvider _toggleProvider;
 
-        public AgentLauncher(IServicesMgr serviceManager, IAPILog logger)
+        public AgentLauncher(IServicesMgr serviceManager, IAPILog logger, IToggleProvider toggleProvider)
         {
             _serviceManager = serviceManager;
             _logger = logger;
+            _toggleProvider = toggleProvider;
         }
 
         public async Task LaunchAgentAsync()
         {
             try
             {
-                using (IAgentStatusManagerService agentService = _serviceManager.CreateProxy<IAgentStatusManagerService>(ExecutionIdentity.System))
+                if (_toggleProvider.IsEnabled<EnableAgentLaunchOnJobStartToggle>())
                 {
-                    await agentService.StartAgentAsync(Guid.Parse(GlobalConst.RELATIVITY_INTEGRATION_POINTS_AGENT_GUID)).ConfigureAwait(false);
+                    using (IAgentStatusManagerService agentService = _serviceManager.CreateProxy<IAgentStatusManagerService>(ExecutionIdentity.System))
+                    {
+                        await agentService.StartAgentAsync(Guid.Parse(GlobalConst.RELATIVITY_INTEGRATION_POINTS_AGENT_GUID)).ConfigureAwait(false);
+                        _logger.LogInformation("StartAgentAsync was called for agent {guid}", Guid.Parse(GlobalConst.RELATIVITY_INTEGRATION_POINTS_AGENT_GUID));
+                    }
                 }
             }
             catch (Exception ex)
