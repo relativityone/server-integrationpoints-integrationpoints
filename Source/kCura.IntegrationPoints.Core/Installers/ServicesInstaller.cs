001fc1666fd04cec7715251f0019525e2ee40dab
Aleksandra Łopińska <aleksandra.lopinska@relativity.com>
2023-02-10 14:49:28 +0100

    REL-819368 ButtonStateBuilder registered as transient

--- Source/kCura.IntegrationPoints.Core/Installers/ServicesInstaller.cs
+++ Source/kCura.IntegrationPoints.Core/Installers/ServicesInstaller.cs
@@ -84,7 +84,7 @@ namespace kCura.IntegrationPoints.Core.Installers
             container.Register(Component.For<IDbContextFactory>().ImplementedBy<DbContextFactory>().LifestyleTransient());
             container.Register(Component.For<IRelativitySyncConstrainsChecker>().ImplementedBy<RelativitySyncConstrainsChecker>());
             container.Register(Component.For<IRelativitySyncAppIntegration>().ImplementedBy<RelativitySyncAppIntegration>());
-            container.Register(Component.For<IButtonStateBuilder>().ImplementedBy<ButtonStateBuilder>());
+            container.Register(Component.For<IButtonStateBuilder>().ImplementedBy<ButtonStateBuilder>().LifestyleTransient());
 
             container.Register(Component.For<ObjectTypeService>().ImplementedBy<ObjectTypeService>().LifestyleTransient());
             container.Register(Component.For<IPluginProvider>().ImplementedBy<DefaultSourcePluginProvider>().LifestyleTransient());
