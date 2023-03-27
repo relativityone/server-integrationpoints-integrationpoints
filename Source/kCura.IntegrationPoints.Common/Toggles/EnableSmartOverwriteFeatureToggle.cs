a7894cfebe812a9969b407d366d4994dc1c0eb2c
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2023-03-27 15:43:50 +0200

    REL-831870 Added EnableSmartOverwriteFeatureToggle

---
+++ Source/kCura.IntegrationPoints.Common/Toggles/EnableSmartOverwriteFeatureToggle.cs
@@ -0,0 +1,10 @@
+﻿using Relativity.Toggles;
+
+namespace kCura.IntegrationPoints.Common.Toggles
+{
+    [Description("Enables or disables Smart Overwrite feature in RIP", "RIP and SFU")]
+    internal class EnableSmartOverwriteFeatureToggle : IToggle
+    {
+        
+    }
+}
