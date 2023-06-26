9921f4e218f1db5e4e74d245ad2e76e622d23851
Piotr Sowała <piotr.sowala@relativity.com>
2023-06-26 18:42:13 +0200

    REL-856098 Remove async calls to IToggleProvider

--- Source/kCura.IntegrationPoints.Common/Toggles/IRipToggleProvider.cs
+++ Source/kCura.IntegrationPoints.Common/Toggles/IRipToggleProvider.cs
@@ -1,16 +1,11 @@
-﻿using System.Threading.Tasks;
-using Relativity.Toggles;
+﻿using Relativity.Toggles;
 
 namespace kCura.IntegrationPoints.Common.Toggles
 {
     public interface IRipToggleProvider
     {
-        Task<bool> IsEnabledAsync<T>() where T : IToggle;
-
         bool IsEnabled<T>() where T : IToggle;
 
         bool IsEnabledByName(string name);
-
-        Task<bool> IsEnabledByNameAsync(string name);
     }
 }
