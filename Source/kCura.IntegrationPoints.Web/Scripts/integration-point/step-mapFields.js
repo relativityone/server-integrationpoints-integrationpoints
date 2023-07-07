bb08476c78505fc8b3b7cb5d3199fcc0d4e5056e
Magdalena Górska <magdalena.gorska@relativity.com>
2023-07-07 12:08:31 +0200

    REL-824156 rename to IsNotImportFromCustomProviders()

--- Source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-mapFields.js
+++ Source/kCura.IntegrationPoints.Web/Scripts/integration-point/step-mapFields.js
@@ -258,7 +258,7 @@ ko.validation.insertValidationMessage = function (element) {
 		//use this to bind which elements show up depending on if the user is accessing Relativity Provider or not
 		this.IsRelativityProvider = ko.observable(IP.reverseMapFields);
 
-		this.IsImportFromLoadFileOrExport = ko.observable(model.source.selectedType == "548f0873-8e5e-4da6-9f27-5f9cda764636" || model.isExportType);
+		this.IsNotImportFromCustomProviders = ko.observable(model.source.selectedType == "548f0873-8e5e-4da6-9f27-5f9cda764636" || model.isExportType);
 
 		var isNonDocumentObjectFlow = IP.data.params['EnableSyncNonDocumentFlowToggleValue'] &&
 										IP.data.params['TransferredRDOArtifactTypeID'] != documentArtifactTypeId &&
