80ae75834e221d1ffaf588da78f77052bd926971
Kamil Makarowski <kamil.makarowski@relativity.com>
2023-07-31 16:37:52 +0200

    SERVER-RC remove CustomProvider feature

--- Source/kCura.IntegrationPoints.Agent/Installer/AgentInstaller.cs
+++ Source/kCura.IntegrationPoints.Agent/Installer/AgentInstaller.cs
@@ -6,10 +6,6 @@ using Castle.MicroKernel.Resolvers.SpecializedResolvers;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 using kCura.IntegrationPoints.Agent.Context;
-using kCura.IntegrationPoints.Agent.CustomProvider;
-using kCura.IntegrationPoints.Agent.CustomProvider.Services;
-using kCura.IntegrationPoints.Agent.CustomProvider.Services.FileShare;
-using kCura.IntegrationPoints.Agent.CustomProvider.Services.InstanceSettings;
 using kCura.IntegrationPoints.Agent.Installer.Components;
 using kCura.IntegrationPoints.Agent.Monitoring;
 using kCura.IntegrationPoints.Agent.Monitoring.HearbeatReporter;
@@ -197,18 +193,6 @@ namespace kCura.IntegrationPoints.Agent.Installer
             container.Register(Component.For<IHealthStatisticReporter>().ImplementedBy<SystemStatisticsReporter>().LifestyleTransient());
 
             container.Register(Component.For<IHeartbeatReporter>().ImplementedBy<HeartbeatReporter>().LifestyleTransient());
-        }
-
-        private static void ConfigureOtherProviderFlow(IWindsorContainer container)
-        {
-            container.Register(Component.For<ICustomProviderTask>().ImplementedBy<CustomProviderTask>().LifestyleTransient());
-            container.Register(Component.For<IImportApiRunnerFactory>().ImplementedBy<ImportApiRunnerFactory>().LifestyleTransient());
-            container.Register(Component.For<IImportApiService>().ImplementedBy<ImportApiService>().LifestyleTransient());
-            container.Register(Component.For<ICustomProviderFlowCheck>().ImplementedBy<CustomProviderFlowCheck>().LifestyleTransient());
-            container.Register(Component.For<IDocumentImportSettingsBuilder>().ImplementedBy<DocumentImportSettingsBuilder>().LifestyleTransient());
-            container.Register(Component.For<IRdoImportSettingsBuilder>().ImplementedBy<RdoImportSettingsBuilder>().LifestyleTransient());
-            container.Register(Component.For<DocumentImportApiRunner>().ImplementedBy<DocumentImportApiRunner>().LifestyleTransient());
-            container.Register(Component.For<RdoImportApiRunner>().ImplementedBy<RdoImportApiRunner>().LifestyleTransient());
         }
     }
 }
--- Source/kCura.IntegrationPoints.Agent/Toggles/EnableImportApiV2ForCustomProvidersToggle.cs
+++
@@ -1,9 +0,0 @@
-﻿using Relativity.Toggles;
-
-namespace kCura.IntegrationPoints.Agent.Toggles
-{
-    [Description("Enable IAPI 2.0 for Other Providers (Azure AD, LDAP, FTP...)", "RIP and SFU")]
-    internal class EnableImportApiV2ForCustomProvidersToggle : IToggle
-    {
-    }
-}
