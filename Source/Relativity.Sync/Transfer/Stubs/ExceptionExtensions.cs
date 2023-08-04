f91285da5007a17e4df8717f75d152caff419c3c
Ravi Konnur <ravi.konnur@relativity.com>
2023-08-04 19:50:50 +0530

    addressed the review comments

---
+++ Source/Relativity.Sync/Transfer/Stubs/ExceptionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relativity.Sync.AntiMalware.SDK
+{
+	public static class ExceptionExtensions
+	{
+		public static bool ContainsAntiMalwareEvent(this Exception ex)
+		{
+			return false;
+		}
+	}
+}
