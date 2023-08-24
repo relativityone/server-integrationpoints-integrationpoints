f3a9f2b6a2f44c11e2f33a3b4001e42ee40083f8
Shivaraj Nagaraj <shivaraj.nagaraj@relativity.com>
2023-08-24 08:22:33 +0530

    REL-870446 Added DefaultValue attribute to toggles EnableSyncNonDocumentFlowToggle and EnableRelativitySyncApplicationToggle.

--- Source/kCura.IntegrationPoints.Common/Toggles/EnableSyncNonDocumentFlowToggle.cs
+++ Source/kCura.IntegrationPoints.Common/Toggles/EnableSyncNonDocumentFlowToggle.cs
@@ -2,6 +2,11 @@
 
 namespace kCura.IntegrationPoints.Common.Toggles
 {
+	/// <summary>
+	/// Enable Integration Points to use Non-document objects Sync workflow
+	/// </summary>
+	[DefaultValue(true)]
+	[ExpectedRemovalDate(2023, 09, 30)]
     [Description("Enable Integration Points to use Non-document objects Sync workflow", "Adler Sieben")]
     public class EnableSyncNonDocumentFlowToggle : IToggle
     {
