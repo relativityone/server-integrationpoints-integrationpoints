5bc0bb97d95dd3afd7e1591330afe07b0b720dcc
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2023-10-13 09:39:23 +0200

    REL-877532 Review remarks

---
+++ Source/kCura.ScheduleQueue.AgentBase/ProviderHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using kCura.IntegrationPoints.Core;
+using kCura.IntegrationPoints.Core.Models;
+
+namespace kCura.ScheduleQueue.AgentBase
+{
+    internal class ProviderHelpers
+    {
+        public static ProviderType GetProviderType(string sourceProviderGuid, string destinationProviderGuid)
+        {
+            if (sourceProviderGuid.Equals(IntegrationPoints.Core.Constants.IntegrationPoints.SourceProviders.FTP, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return ProviderType.FTP;
+            }
+            if (sourceProviderGuid.Equals(IntegrationPoints.Core.Constants.IntegrationPoints.SourceProviders.LDAP, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return ProviderType.LDAP;
+            }
+            if (sourceProviderGuid.Equals(IntegrationPoints.Core.Constants.IntegrationPoints.SourceProviders.IMPORTLOADFILE, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return ProviderType.ImportLoadFile;
+            }
+            if (sourceProviderGuid.Equals(IntegrationPoints.Core.Constants.IntegrationPoints.SourceProviders.RELATIVITY, StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (destinationProviderGuid.Equals(IntegrationPoints.Core.Constants.IntegrationPoints.DestinationProviders.RELATIVITY, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return ProviderType.Relativity;
+                }
+                if (destinationProviderGuid.Equals(Constants.IntegrationPoints.DestinationProviders.LOADFILE, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return ProviderType.LoadFile;
+                }
+            }
+            return ProviderType.Other;
+        }
+    }
+}
--- Source/kCura.ScheduleQueue.AgentBase/ScheduleQueueAgentBase.cs
+++ Source/kCura.ScheduleQueue.AgentBase/ScheduleQueueAgentBase.cs
@@ -349,40 +349,12 @@ namespace kCura.ScheduleQueue.AgentBase
                 { "LastHeartbeat", job.Heartbeat.ToString() }
             };
 
-            ProviderType providerType = GetProviderType(sourceProvider.Identifier, destinationProvider.Identifier);
+            ProviderType providerType = ProviderHelpers.GetProviderType(sourceProvider.Identifier, destinationProvider.Identifier);
 
             _apm.CountOperation($"IntegrationPoints.Performance.JobFailedCount.{providerType.ToString()}", customData: jobInTransientStateCustomData)
                 .Write();
 
             Logger.LogInformation("JobInTransientState metric sent");
-        }
-
-        private ProviderType GetProviderType(string sourceProviderGuid, string destinationProviderGuid)
-        {
-            if (sourceProviderGuid.Equals(IntegrationPoints.Core.Constants.IntegrationPoints.SourceProviders.FTP, StringComparison.InvariantCultureIgnoreCase))
-            {
-                return ProviderType.FTP;
-            }
-            if (sourceProviderGuid.Equals(IntegrationPoints.Core.Constants.IntegrationPoints.SourceProviders.LDAP, StringComparison.InvariantCultureIgnoreCase))
-            {
-                return ProviderType.LDAP;
-            }
-            if (sourceProviderGuid.Equals(IntegrationPoints.Core.Constants.IntegrationPoints.SourceProviders.IMPORTLOADFILE, StringComparison.InvariantCultureIgnoreCase))
-            {
-                return ProviderType.ImportLoadFile;
-            }
-            if (sourceProviderGuid.Equals(IntegrationPoints.Core.Constants.IntegrationPoints.SourceProviders.RELATIVITY, StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (destinationProviderGuid.Equals(IntegrationPoints.Core.Constants.IntegrationPoints.DestinationProviders.RELATIVITY, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    return ProviderType.Relativity;
-                }
-                if (destinationProviderGuid.Equals(Constants.IntegrationPoints.DestinationProviders.LOADFILE, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    return ProviderType.LoadFile;
-                }
-            }
-            return ProviderType.Other;
         }
 
         private void RemoveInvalidJobFromQueue(PreValidationResult validationResult, Job job)
