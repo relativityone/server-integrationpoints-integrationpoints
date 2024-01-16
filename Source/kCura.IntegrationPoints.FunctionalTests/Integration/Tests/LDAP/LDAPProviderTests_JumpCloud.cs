13595e23b6480ca730482c7f56915971b99f9782
Ravi Konnur <ravi.konnur@relativity.com>
2024-01-16 19:43:47 +0530

    Ignored tests as its failed in azure pipeline

--- Source/kCura.IntegrationPoints.FunctionalTests/Integration/Tests/LDAP/LDAPProviderTests_JumpCloud.cs
+++ Source/kCura.IntegrationPoints.FunctionalTests/Integration/Tests/LDAP/LDAPProviderTests_JumpCloud.cs
@@ -14,7 +14,7 @@ using Relativity.Testing.Identification;
 namespace Relativity.IntegrationPoints.Tests.Integration.Tests.LDAP
 {
     [TestExecutionCategory.CI, TestLevel.L2]
-    [Ignore("")]
+    [Ignore("REL-865787")]
     public class LDAPProviderTests_JumpCloud : TestsBase
     {
         public readonly JumpCloudUsersTestData UsersTestData;
